c0edc1be5fc03d14d9d73d9108c803bcb5acd146
Montel Laurent <montel@kde.org>
2017-08-04 07:32:39 +0200

    Extract method so we will be able to create autotest

---
+++ src/rocketchatmessage.cpp
@@ -0,0 +1,54 @@
+/*
+   Copyright (c) 2017 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+#include "rocketchatmessage.h"
+
+#include <QJsonArray>
+#include <QJsonObject>
+
+RocketChatMessage::RocketChatMessage()
+{
+
+}
+
+QString RocketChatMessage::hideRoom(const QString &roomID, unsigned id)
+{
+    QJsonArray params;
+    params.append(QJsonValue(roomID));
+    return generateMethod(QStringLiteral("hideRoom"), QJsonDocument(params), id);
+}
+
+QString RocketChatMessage::generateMethod(const QString &method, const QJsonDocument &params, unsigned id)
+{
+    QJsonObject json;
+    json[QStringLiteral("msg")] = QStringLiteral("method");
+    json[QStringLiteral("method")] = method;
+    json[QStringLiteral("id")] = QString::number(id);
+
+    if (params.isArray()) {
+        json[QStringLiteral("params")] = params.array();
+    } else if (params.isObject()) {
+        QJsonArray arr;
+        arr.append(params.object());
+        json[QStringLiteral("params")] = arr;
+    }
+    return QString::fromUtf8(QJsonDocument(json).toJson(QJsonDocument::Compact));
+}
