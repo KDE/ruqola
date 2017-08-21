2bcfe4264fa19e137dd14a1807de894f2cba6e03
Montel Laurent <montel@kde.org>
2017-08-21 13:41:33 +0200

    Move in own directory

---
+++ src/restapi/restapirequest.h
@@ -0,0 +1,59 @@
+/*
+   Copyright (c) 2017 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef RESTAPIREQUEST_H
+#define RESTAPIREQUEST_H
+
+#include <QObject>
+#include <QSslError>
+class QNetworkAccessManager;
+class QNetworkReply;
+class RestApiRequest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit RestApiRequest(QObject *parent = nullptr);
+    ~RestApiRequest();
+
+
+    QString userId() const;
+    QString authToken() const;
+
+
+    //Assign/get server url
+    QString serverUrl() const;
+    void setServerUrl(const QString &serverUrl);
+
+    void setUserName(const QString &userName);
+    void setPassword(const QString &password);
+
+    void login();
+private:
+    void slotResult(QNetworkReply *reply);
+    void slotSslErrors(QNetworkReply *reply, const QList<QSslError> &error);
+    QNetworkAccessManager *mNetworkAccessManager = nullptr;
+    QString mUserId;
+    QString mAuthToken;
+    QString mServerUrl;
+    QString mUserName;
+    QString mPassword;
+};
+
+#endif // RESTAPIREQUEST_H
