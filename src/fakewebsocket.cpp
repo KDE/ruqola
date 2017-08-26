c4f41ec245ff0caaa0efb512239b3a10f5fa7024
Montel Laurent <montel@kde.org>
2017-08-26 07:46:22 +0200

    Start to create a fakewebsocket for autotest

---
+++ src/fakewebsocket.cpp
@@ -0,0 +1,78 @@
+/*
+   Copyright (c) 2017 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "fakewebsocket.h"
+
+FakeWebSocket::FakeWebSocket(QObject *parent)
+    : AbstractWebSocket(parent)
+{
+
+}
+
+void FakeWebSocket::openUrl(const QUrl &url)
+{
+}
+
+qint64 FakeWebSocket::sendTextMessage(const QString &message)
+{
+    return {};
+}
+
+bool FakeWebSocket::isValid() const
+{
+    return true;
+}
+
+void FakeWebSocket::flush()
+{
+}
+
+void FakeWebSocket::close()
+{
+}
+
+QAbstractSocket::SocketError FakeWebSocket::error() const
+{
+    return {};
+}
+
+QUrl FakeWebSocket::requestUrl() const
+{
+    return {};
+}
+
+QWebSocketProtocol::CloseCode FakeWebSocket::closeCode() const
+{
+    return {};
+}
+
+QString FakeWebSocket::closeReason() const
+{
+    return {};
+}
+
+qint64 FakeWebSocket::sendBinaryMessage(const QByteArray &data)
+{
+    return {};
+}
+
+void FakeWebSocket::ignoreSslErrors()
+{
+}
---
+++ src/fakewebsocket.h
@@ -0,0 +1,50 @@
+/*
+   Copyright (c) 2017 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef FAKEWEBSOCKET_H
+#define FAKEWEBSOCKET_H
+
+#include "abstractwebsocket.h"
+
+#include <QAbstractSocket>
+#include <QUrl>
+
+class FakeWebSocket : public AbstractWebSocket
+{
+    Q_OBJECT
+public:
+    explicit FakeWebSocket(QObject *parent = nullptr);
+
+    // AbstractWebSocket interface
+public:
+    void openUrl(const QUrl &url) override;
+    qint64 sendTextMessage(const QString &message) override;
+    bool isValid() const override;
+    void flush() override;
+    void close() override;
+    QAbstractSocket::SocketError error() const override;
+    QUrl requestUrl() const override;
+    QWebSocketProtocol::CloseCode closeCode() const override;
+    QString closeReason() const override;
+    qint64 sendBinaryMessage(const QByteArray &data) override;
+    void ignoreSslErrors() override;
+};
+
+#endif // FAKEWEBSOCKET_H
