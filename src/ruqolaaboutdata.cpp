4439127f160388a92f05e6f51230c38b7c45ab8c
Montel Laurent <montel@kde.org>
2017-10-19 13:15:45 +0200

    Add wrapper around kaboutdata

---
+++ src/ruqolaaboutdata.cpp
@@ -0,0 +1,45 @@
+/*
+   Copyright (c) 2017 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "ruqolaaboutdata.h"
+
+#include <KAboutData>
+#include <KLocalizedString>
+
+RuqolaAboutData::RuqolaAboutData(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+RuqolaAboutData::~RuqolaAboutData()
+{
+
+}
+
+QString RuqolaAboutData::version() const
+{
+    return KAboutData::applicationData().version();
+}
+
+QString RuqolaAboutData::title() const
+{
+    return i18n("<html><font size=\"5\">%1</font><br />Version %2</html>", KAboutData::applicationData().displayName(), KAboutData::applicationData().version());
+}
