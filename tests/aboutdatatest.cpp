e5e8236e0cfdf9503de8b724a9541d3f879b50bc
Montel Laurent <montel@kde.org>
2017-10-30 13:33:21 +0100

    Start to create a test apps for aboutdata qml dialog

---
+++ tests/aboutdatatest.cpp
@@ -0,0 +1,62 @@
+/*
+   Copyright (c) 2017 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "aboutdatatest.h"
+
+#include <QApplication>
+#include <QStandardPaths>
+#include <KAboutData>
+#include <QCommandLineParser>
+
+AboutDataTest::AboutDataTest(QWidget *parent)
+    : QWidget(parent)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QStandardPaths::setTestModeEnabled(true);
+
+    KAboutData aboutData(QStringLiteral("ruqola"),
+                         QStringLiteral("Ruqola"),
+                         QStringLiteral("1.0.0"),
+                         QStringLiteral("Rocket Chat Client"),
+                         KAboutLicense::GPL_V2,
+                         QStringLiteral("Copyright © 2017 Ruqola authors"));
+
+    aboutData.addAuthor(QStringLiteral("Laurent Montel"), QStringLiteral("Maintainer"), QStringLiteral("montel@kde.org"));
+    aboutData.addAuthor(QStringLiteral("Riccardo Iaconelli"), QStringLiteral("Original author"), QStringLiteral("riccardo@kde.org"));
+    aboutData.addAuthor(QStringLiteral("Vasudha Mathur"), QStringLiteral("Former core developer"), QStringLiteral("vasudhamathur96@gmail.com"));
+
+    aboutData.setOrganizationDomain(QByteArrayLiteral("kde.org"));
+    aboutData.setProductName(QByteArrayLiteral("ruqola"));
+    KAboutData::setApplicationData(aboutData);
+
+    QCommandLineParser parser;
+    aboutData.setupCommandLine(&parser);
+    parser.process(app);
+    aboutData.processCommandLine(&parser);
+
+    AboutDataTest w;
+    w.show();
+    return app.exec();
+}
