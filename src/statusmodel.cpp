13f39b184251ef4ed4ac149f83631a1d78a4abf8
Laurent Montel <montel@kde.org>
2017-12-29 17:23:31 +0100

    Start to create status model

---
+++ src/statusmodel.cpp
@@ -0,0 +1,96 @@
+/*
+   Copyright (c) 2017 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "statusmodel.h"
+#include <KLocalizedString>
+
+
+StatusModel::StatusModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+    fillModel();
+}
+
+StatusModel::~StatusModel()
+{
+
+}
+
+QHash<int, QByteArray> StatusModel::roleNames() const
+{
+    QHash<int, QByteArray> roles;
+    roles[StatusI18n] = "text";
+    roles[Status] = "status";
+    roles[Icon] = "icon";
+    return roles;
+}
+
+void StatusModel::fillModel()
+{
+    {
+        StatusInfo statusInfo;
+        statusInfo.displayText = i18n("Online");
+        statusInfo.icon = QIcon::fromTheme(QStringLiteral("im-user-online"));
+        statusInfo.status = User::PresenceStatus::PresenceOnline;
+        mStatusList.append(statusInfo);
+    }
+    {
+        StatusInfo statusInfo;
+        statusInfo.displayText = i18n("Busy");
+        statusInfo.icon = QIcon::fromTheme(QStringLiteral("im-user-busy"));
+        statusInfo.status = User::PresenceStatus::PresenceBusy;
+        mStatusList.append(statusInfo);
+    }
+    {
+        StatusInfo statusInfo;
+        statusInfo.displayText = i18n("Away");
+        statusInfo.icon = QIcon::fromTheme(QStringLiteral("im-user-away"));
+        statusInfo.status = User::PresenceStatus::PresenceAway;
+        mStatusList.append(statusInfo);
+    }
+    {
+        StatusInfo statusInfo;
+        statusInfo.displayText = i18n("Offline");
+        statusInfo.icon = QIcon::fromTheme(QStringLiteral("im-user-offline"));
+        statusInfo.status = User::PresenceStatus::PresenceOffline;
+        mStatusList.append(statusInfo);
+    }
+}
+
+int StatusModel::rowCount(const QModelIndex &parent) const
+{
+    return mStatusList.count();
+}
+
+QVariant StatusModel::data(const QModelIndex &index, int role) const
+{
+    StatusInfo statusInfo = mStatusList.at(index.row());
+
+    switch (role) {
+    case StatusI18n:
+        return statusInfo.displayText;
+    case Status:
+        return QVariant::fromValue(statusInfo.status);
+    case Icon:
+        return statusInfo.icon;
+    }
+    return {};
+
+}
---
+++ src/statusmodel.h
@@ -0,0 +1,61 @@
+/*
+   Copyright (c) 2017 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef STATUSMODEL_H
+#define STATUSMODEL_H
+
+#include "user.h"
+
+#include <QAbstractListModel>
+#include <QIcon>
+
+struct StatusInfo {
+    QString displayText;
+    QIcon icon;
+    User::PresenceStatus status;
+};
+
+class StatusModel : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    enum StatusRoles {
+        StatusI18n = Qt::UserRole + 1,
+        Status,
+        Icon,
+    };
+    Q_ENUM(StatusRoles)
+
+    explicit StatusModel(QObject *parent = nullptr);
+    ~StatusModel();
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+protected:
+    QHash<int, QByteArray> roleNames() const override;
+
+private:
+    void fillModel();
+    QVector<StatusInfo> mStatusList;
+
+};
+
+#endif // STATUSMODEL_H
