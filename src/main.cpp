35dfc941a4c0550d7da9347ac141c0ecde47b181
Laurent Montel <montel@kde.org>
2018-01-25 13:36:05 +0100

    Add Veluri Mithun as contributor

--- src/main.cpp
+++ src/main.cpp
@@ -69,6 +69,7 @@ int main(int argc, char *argv[])
     aboutData.setOrganizationDomain(QByteArrayLiteral("kde.org"));
     aboutData.setProductName(QByteArrayLiteral("ruqola"));
     aboutData.addCredit(i18n("Paul Lemire"), i18n("Help for debugging QML"), QStringLiteral("paul.lemire@kdab.com"));
+    aboutData.addCredit(i18n("Veluri Mithun"), i18n("Autotest Improvment and created some tests apps"), QStringLiteral("velurimithun38@gmail.com"));
 
     KAboutData::setApplicationData(aboutData);
 
