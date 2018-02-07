026df0e8b32ce48f2e179e8716fb1057b58ae433
Laurent Montel <montel@kde.org>
2018-02-07 20:19:43 +0100

    Start to create model for searching room

---
+++ src/model/searchchannelroommodel.cpp
@@ -0,0 +1,47 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "searchchannelroommodel.h"
+
+SearchChannelRoomModel::SearchChannelRoomModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+
+}
+
+SearchChannelRoomModel::~SearchChannelRoomModel()
+{
+
+}
+
+int SearchChannelRoomModel::rowCount(const QModelIndex &parent) const
+{
+    return {};
+}
+
+QVariant SearchChannelRoomModel::data(const QModelIndex &index, int role) const
+{
+    return {};
+}
+
+QHash<int, QByteArray> SearchChannelRoomModel::roleNames() const
+{
+    return {};
+}
