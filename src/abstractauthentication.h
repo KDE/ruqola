3a2f96eba67a81ff4aeb7cc4357b4236b4d95ab2
Laurent Montel <montel@kde.org>
2018-02-13 23:06:56 +0100

    Start to implement plugin support

---
+++ src/abstractauthentication.h
@@ -0,0 +1,38 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+#ifndef ABSTRACTAUTHENTICATION_H
+#define ABSTRACTAUTHENTICATION_H
+
+#include <QObject>
+#include "libruqolacore_export.h"
+
+class LIBRUQOLACORE_EXPORT AbstractAuthentication : public QObject
+{
+    Q_OBJECT
+public:
+    explicit AbstractAuthentication(QObject *parent = nullptr);
+    ~AbstractAuthentication();
+
+    virtual void login() = 0;
+};
+
+#endif // ABSTRACTAUTHENTICATION_H
