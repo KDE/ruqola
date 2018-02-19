59d2379af82708a126e54a945ae5a4136e565cdc
Laurent Montel <montel@kde.org>
2018-02-19 08:06:00 +0100

    Add proxymodel

---
+++ src/model/searchmessagefilterproxymodel.cpp
@@ -0,0 +1,60 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+#include "searchmessagefilterproxymodel.h"
+
+SearchMessageFilterProxyModel::SearchMessageFilterProxyModel(QObject *parent)
+    : QSortFilterProxyModel(parent)
+{
+    setDynamicSortFilter(true);
+    setFilterCaseSensitivity(Qt::CaseInsensitive);
+    //setFilterRole(UserCompleterModel::UserName);
+    sort(0);
+}
+
+SearchMessageFilterProxyModel::~SearchMessageFilterProxyModel()
+{
+
+}
+
+
+QHash<int, QByteArray> SearchMessageFilterProxyModel::roleNames() const
+{
+    if (QAbstractItemModel *source = sourceModel()) {
+        return source->roleNames();
+    }
+    return QHash<int, QByteArray>();
+}
+/*
+bool SearchMessageFilterProxyModel::lessThan(const QModelIndex &left, const QModelIndex &right) const
+{
+    if (!sourceModel()) {
+        return false;
+    }
+    if (left.isValid() && right.isValid()) {
+        const QString leftString = sourceModel()->data(left, UserCompleterModel::UserName).toString();
+        const QString rightString = sourceModel()->data(right, UserCompleterModel::UserName).toString();
+        return QString::localeAwareCompare(leftString, rightString) < 0;
+    } else {
+        return false;
+    }
+}
+*/
