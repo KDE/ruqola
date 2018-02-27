27c8932d1fe2dc3bbc64b264dfbf583c57b64464
Laurent Montel <montel@kde.org>
2018-02-27 23:19:38 +0100

    GIT_SILENT: Fix copyright

--- src/room.h
+++ src/room.h
@@ -1,6 +1,6 @@
 /*
  * Copyright 2016  Riccardo Iaconelli <riccardo@kde.org>
- * Copyright 2017  Laurent Montel <montel@kde.org>
+ * Copyright 2017-2018 Laurent Montel <montel@kde.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
