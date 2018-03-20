967c3094fea975ec7a5fab32d05c548957e59690
Laurent Montel <montel@kde.org>
2018-03-20 21:35:14 +0100

    Add changechannelname

---
+++ autotests/changechannelnamejobtest.cpp
@@ -0,0 +1,58 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "changechannelnamejobtest.h"
+#include "restapi/channels/changechannelnamejob.h"
+#include "ruqola_restapi_helper.h"
+#include <QJsonDocument>
+#include <QTest>
+QTEST_GUILESS_MAIN(ChangeChannelNameJobTest)
+
+ChangeChannelNameJobTest::ChangeChannelNameJobTest(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void ChangeChannelNameJobTest::shouldHaveDefaultValue()
+{
+    ChangeChannelNameJob job;
+    verifyDefaultValue(&job);
+    QVERIFY(job.name().isEmpty());
+    QVERIFY(job.roomId().isEmpty());
+}
+
+void ChangeChannelNameJobTest::shouldGenerateRequest()
+{
+    ChangeChannelNameJob job;
+    QNetworkRequest request = QNetworkRequest(QUrl());
+    verifyAuthentication(&job, request);
+    QCOMPARE(request.url(), QUrl(QStringLiteral("http://www.kde.org/api/v1/channels.setAnnouncement")));
+    QCOMPARE(request.header(QNetworkRequest::ContentTypeHeader).toString(), QStringLiteral("application/json"));
+}
+
+void ChangeChannelNameJobTest::shouldGenerateJson()
+{
+    ChangeChannelNameJob job;
+    const QString roomId = QStringLiteral("foo1");
+    const QString name = QStringLiteral("topic1");
+    job.setRoomId(roomId);
+    job.setName(name);
+    QCOMPARE(job.json().toJson(QJsonDocument::Compact), QStringLiteral("{\"name\":\"%1\",\"roomId\":\"%2\"}").arg(name).arg(roomId).toLatin1());
+}
---
+++ src/restapi/channels/changechannelnamejob.cpp
@@ -0,0 +1,131 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "changechannelnamejob.h"
+
+#include "ruqola_restapi_debug.h"
+#include "restapimethod.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+
+ChangeChannelNameJob::ChangeChannelNameJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+ChangeChannelNameJob::~ChangeChannelNameJob()
+{
+}
+
+bool ChangeChannelNameJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        return false;
+    }
+    const QByteArray baPostData = json().toJson(QJsonDocument::Compact);
+    addLoggerInfo("ChangeChannelNameJob::start: " + baPostData);
+    QNetworkReply *reply = mNetworkAccessManager->post(request(), baPostData);
+    connect(reply, &QNetworkReply::finished, this, &ChangeChannelNameJob::slotChangeTopicFinished);
+    return true;
+}
+
+void ChangeChannelNameJob::slotChangeTopicFinished()
+{
+    QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QByteArray data = reply->readAll();
+        const QJsonDocument replyJson = QJsonDocument::fromJson(data);
+        const QJsonObject replyObject = replyJson.object();
+
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            qCDebug(RUQOLA_RESTAPI_LOG) << "Change Topic success";
+            Q_EMIT changeAnnouncementDone();
+        } else {
+            qCWarning(RUQOLA_RESTAPI_LOG) <<" Problem when we tried to change topic" << data;
+        }
+    }
+    qDebug() << " DONE !";
+    deleteLater();
+}
+
+bool ChangeChannelNameJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool ChangeChannelNameJob::canStart() const
+{
+    if (mName.isEmpty()) {
+        qCWarning(RUQOLA_RESTAPI_LOG) << "ChangeChannelNameJob: name is empty";
+        return false;
+    }
+    if (mRoomId.isEmpty()) {
+        qCWarning(RUQOLA_RESTAPI_LOG) << "ChangeChannelNameJob: RoomId is empty";
+        return false;
+    }
+    if (!RestApiAbstractJob::canStart()) {
+        qCWarning(RUQOLA_RESTAPI_LOG) << "Impossible to start ChangeChannelNameJob job";
+        return false;
+    }
+    return true;
+}
+
+QJsonDocument ChangeChannelNameJob::json() const
+{
+    QJsonObject jsonObj;
+    jsonObj[QLatin1String("roomId")] = roomId();
+    jsonObj[QLatin1String("name")] = name();
+
+    const QJsonDocument postData = QJsonDocument(jsonObj);
+    return postData;
+}
+
+QString ChangeChannelNameJob::roomId() const
+{
+    return mRoomId;
+}
+
+void ChangeChannelNameJob::setRoomId(const QString &roomId)
+{
+    mRoomId = roomId;
+}
+
+QString ChangeChannelNameJob::name() const
+{
+    return mName;
+}
+
+void ChangeChannelNameJob::setName(const QString &t)
+{
+    mName = t;
+}
+
+QNetworkRequest ChangeChannelNameJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::ChannelsRename);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    request.setAttribute(QNetworkRequest::HttpPipeliningAllowedAttribute, true);
+    request.setAttribute(QNetworkRequest::HTTP2AllowedAttribute, true);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, QStringLiteral("application/json"));
+    return request;
+}
