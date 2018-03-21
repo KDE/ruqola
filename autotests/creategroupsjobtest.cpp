0ccc235731ba57074ad92ba91f269e43e68b33b1
Laurent Montel <montel@kde.org>
2018-03-21 13:29:33 +0100

    Add creategroups too

---
+++ autotests/creategroupsjobtest.cpp
@@ -0,0 +1,96 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "creategroupsjobtest.h"
+#include "restapi/groups/creategroupsjob.h"
+#include "ruqola_restapi_helper.h"
+#include <QTest>
+#include <QJsonDocument>
+QTEST_GUILESS_MAIN(CreateGroupsJobTest)
+
+CreateGroupsJobTest::CreateGroupsJobTest(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void CreateGroupsJobTest::shouldHaveDefaultValue()
+{
+    CreateGroupsJob job;
+    verifyDefaultValue(&job);
+    QVERIFY(job.requireHttpAuthentication());
+    QVERIFY(!job.readOnly());
+    QVERIFY(job.channelName().isEmpty());
+    QVERIFY(job.members().isEmpty());
+}
+
+void CreateGroupsJobTest::shouldGenerateRequest()
+{
+    CreateGroupsJob job;
+    QNetworkRequest request = QNetworkRequest(QUrl());
+    verifyAuthentication(&job, request);
+    QCOMPARE(request.url(), QUrl(QStringLiteral("http://www.kde.org/api/v1/groups.create")));
+    QCOMPARE(request.header(QNetworkRequest::ContentTypeHeader).toString(), QStringLiteral("application/json"));
+}
+
+void CreateGroupsJobTest::shouldGenerateJson()
+{
+    CreateGroupsJob job;
+    const QString channelname = QStringLiteral("foo1");
+    job.setChannelName(channelname);
+    QCOMPARE(job.json().toJson(QJsonDocument::Compact), QStringLiteral("{\"name\":\"%1\"}").arg(channelname).toLatin1());
+
+    bool readOnly = false;
+    job.setReadOnly(readOnly);
+    QCOMPARE(job.json().toJson(QJsonDocument::Compact), QStringLiteral("{\"name\":\"%1\"}").arg(channelname).toLatin1());
+
+    readOnly = true;
+    job.setReadOnly(readOnly);
+    QCOMPARE(job.json().toJson(QJsonDocument::Compact), QStringLiteral("{\"name\":\"%1\",\"readOnly\":true}").arg(channelname).toLatin1());
+
+    const QStringList members = {QStringLiteral("foo"), QStringLiteral("bla")};
+    job.setMembers(members);
+    QCOMPARE(job.json().toJson(QJsonDocument::Compact), QStringLiteral("{\"members\":[\"foo\",\"bla\"],\"name\":\"%1\",\"readOnly\":true}").arg(channelname).toLatin1());
+
+}
+
+void CreateGroupsJobTest::shouldNotStarting()
+{
+    CreateGroupsJob job;
+
+    RestApiMethod *method = new RestApiMethod;
+    method->setServerUrl(QStringLiteral("http://www.kde.org"));
+    job.setRestApiMethod(method);
+
+    QNetworkAccessManager *mNetworkAccessManager = new QNetworkAccessManager;
+    job.setNetworkAccessManager(mNetworkAccessManager);
+    QVERIFY(!job.canStart());
+    const QString auth = QStringLiteral("foo");
+    const QString userId = QStringLiteral("foo");
+    job.setAuthToken(auth);
+    QVERIFY(!job.canStart());
+    job.setUserId(userId);
+    QVERIFY(!job.canStart());
+    const QString roomId = QStringLiteral("foo1");
+    job.setChannelName(roomId);
+    QVERIFY(job.canStart());
+
+    delete method;
+    delete mNetworkAccessManager;
+}
---
+++ src/restapi/groups/creategroupsjob.cpp
@@ -0,0 +1,142 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "creategroupsjob.h"
+
+#include "ruqola_restapi_debug.h"
+#include "restapimethod.h"
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+
+CreateGroupsJob::CreateGroupsJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+CreateGroupsJob::~CreateGroupsJob()
+{
+}
+
+bool CreateGroupsJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        return false;
+    }
+    const QByteArray baPostData = json().toJson(QJsonDocument::Compact);
+    addLoggerInfo("CreateGroupsJob::start: " + baPostData);
+    QNetworkReply *reply = mNetworkAccessManager->post(request(), baPostData);
+    connect(reply, &QNetworkReply::finished, this, &CreateGroupsJob::slotCreateChannelFinished);
+    return true;
+}
+
+void CreateGroupsJob::slotCreateChannelFinished()
+{
+    QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QByteArray data = reply->readAll();
+        const QJsonDocument replyJson = QJsonDocument::fromJson(data);
+        const QJsonObject replyObject = replyJson.object();
+
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            qCDebug(RUQOLA_RESTAPI_LOG) << "Change Topic success";
+            Q_EMIT createChannelDone();
+        } else {
+            qCWarning(RUQOLA_RESTAPI_LOG) <<" Problem when we tried to change topic" << data;
+        }
+    }
+    deleteLater();
+}
+
+QStringList CreateGroupsJob::members() const
+{
+    return mMembers;
+}
+
+void CreateGroupsJob::setMembers(const QStringList &members)
+{
+    mMembers = members;
+}
+
+QString CreateGroupsJob::channelName() const
+{
+    return mChannelName;
+}
+
+void CreateGroupsJob::setChannelName(const QString &channelName)
+{
+    mChannelName = channelName;
+}
+
+bool CreateGroupsJob::readOnly() const
+{
+    return mReadOnly;
+}
+
+void CreateGroupsJob::setReadOnly(bool readOnly)
+{
+    mReadOnly = readOnly;
+}
+
+bool CreateGroupsJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool CreateGroupsJob::canStart() const
+{
+    if (mChannelName.isEmpty()) {
+        qCWarning(RUQOLA_RESTAPI_LOG) << "CreateGroupsJob: channelname is empty";
+        return false;
+    }
+    if (!RestApiAbstractJob::canStart()) {
+        qCWarning(RUQOLA_RESTAPI_LOG) << "Impossible to start CreateGroupsJob job";
+        return false;
+    }
+    return true;
+}
+
+QJsonDocument CreateGroupsJob::json() const
+{
+    QJsonObject jsonObj;
+    if (!mMembers.isEmpty()) {
+        jsonObj[QLatin1String("members")] = QJsonArray::fromStringList(mMembers);
+    }
+    jsonObj[QLatin1String("name")] = channelName();
+    if (mReadOnly) {
+        jsonObj[QLatin1String("readOnly")] = true;
+    } //Default is false
+
+    const QJsonDocument postData = QJsonDocument(jsonObj);
+    return postData;
+}
+
+QNetworkRequest CreateGroupsJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::GroupsCreate);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    request.setAttribute(QNetworkRequest::HttpPipeliningAllowedAttribute, true);
+    request.setAttribute(QNetworkRequest::HTTP2AllowedAttribute, true);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, QStringLiteral("application/json"));
+    return request;
+}
