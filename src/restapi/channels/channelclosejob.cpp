03a7576115e534578949d38a7eca90a72d80b451
Laurent Montel <montel@kde.org>
2018-03-23 08:18:39 +0100

    Add channels close

---
+++ src/restapi/channels/channelclosejob.cpp
@@ -0,0 +1,115 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "channelclosejob.h"
+
+#include "ruqola_restapi_debug.h"
+#include "restapimethod.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+
+ChannelCloseJob::ChannelCloseJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+ChannelCloseJob::~ChannelCloseJob()
+{
+}
+
+bool ChannelCloseJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        return false;
+    }
+    const QByteArray baPostData = json().toJson(QJsonDocument::Compact);
+    addLoggerInfo("ChannelCloseJob::start: " + baPostData);
+    QNetworkReply *reply = mNetworkAccessManager->post(request(), baPostData);
+    connect(reply, &QNetworkReply::finished, this, &ChannelCloseJob::slotLeaveChannelFinished);
+    return true;
+}
+
+void ChannelCloseJob::slotLeaveChannelFinished()
+{
+    QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QByteArray data = reply->readAll();
+        const QJsonDocument replyJson = QJsonDocument::fromJson(data);
+        const QJsonObject replyObject = replyJson.object();
+
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            qCDebug(RUQOLA_RESTAPI_LOG) << "close channel success: " << data;
+            Q_EMIT leaveChannelDone();
+        } else {
+            qCWarning(RUQOLA_RESTAPI_LOG) <<" Problem when we tried to close channel" << data;
+        }
+    }
+    deleteLater();
+}
+
+bool ChannelCloseJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool ChannelCloseJob::canStart() const
+{
+    if (mRoomId.isEmpty()) {
+        qCWarning(RUQOLA_RESTAPI_LOG) << "ChannelCloseJob: RoomId is empty";
+        return false;
+    }
+    if (!RestApiAbstractJob::canStart()) {
+        qCWarning(RUQOLA_RESTAPI_LOG) << "Impossible to start ChannelCloseJob job";
+        return false;
+    }
+    return true;
+}
+
+QJsonDocument ChannelCloseJob::json() const
+{
+    QJsonObject jsonObj;
+    jsonObj[QLatin1String("roomId")] = roomId();
+
+    const QJsonDocument postData = QJsonDocument(jsonObj);
+    return postData;
+}
+
+QString ChannelCloseJob::roomId() const
+{
+    return mRoomId;
+}
+
+void ChannelCloseJob::setRoomId(const QString &roomId)
+{
+    mRoomId = roomId;
+}
+
+QNetworkRequest ChannelCloseJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::ChannelsClose);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    request.setAttribute(QNetworkRequest::HttpPipeliningAllowedAttribute, true);
+    request.setAttribute(QNetworkRequest::HTTP2AllowedAttribute, true);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, QStringLiteral("application/json"));
+    return request;
+}
