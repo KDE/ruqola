deb3994f65c631fb46fca6fa1b472a0656beaf1c
Laurent Montel <montel@kde.org>
2018-04-16 08:54:02 +0200

    Start to implement settings oauth job

---
+++ autotests/settingsoauthjobtest.cpp
@@ -0,0 +1,46 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "settingsoauthjobtest.h"
+#include "restapi/misc/settingsoauthjob.h"
+#include "ruqola_restapi_helper.h"
+#include <QTest>
+#include <restapi/restapimethod.h>
+QTEST_GUILESS_MAIN(SettingsOauthJobTest)
+
+SettingsOauthJobTest::SettingsOauthJobTest(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void SettingsOauthJobTest::shouldHaveDefaultValue()
+{
+    SettingsOauthJob job;
+    verifyDefaultValue(&job);
+    QVERIFY(job.requireHttpAuthentication());
+}
+
+void SettingsOauthJobTest::shouldGenerateRequest()
+{
+    SettingsOauthJob job;
+    QNetworkRequest request = QNetworkRequest(QUrl());
+    verifyAuthentication(&job, request);
+    QCOMPARE(request.url(), QUrl(QStringLiteral("http://www.kde.org/api/v1/me")));
+}
---
+++ src/restapi/misc/settingsoauthjob.cpp
@@ -0,0 +1,76 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "settingsoauthjob.h"
+#include "restapimethod.h"
+#include "ruqola_restapi_debug.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+
+SettingsOauthJob::SettingsOauthJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+SettingsOauthJob::~SettingsOauthJob()
+{
+}
+
+bool SettingsOauthJob::requireHttpAuthentication() const
+{
+    return false;
+}
+
+bool SettingsOauthJob::start()
+{
+    if (!canStart()) {
+        qCWarning(RUQOLA_RESTAPI_LOG) << "Impossible to start owninfo job";
+        deleteLater();
+        return false;
+    }
+    QNetworkReply *reply = mNetworkAccessManager->get(request());
+    connect(reply, &QNetworkReply::finished, this, &SettingsOauthJob::slotServerInfoFinished);
+    addLoggerInfo(QByteArrayLiteral("SettingsOauthJob: Ask info about me"));
+    return true;
+}
+
+void SettingsOauthJob::slotServerInfoFinished()
+{
+    QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QByteArray data = reply->readAll();
+        const QJsonDocument replyJson = QJsonDocument::fromJson(data);
+        const QJsonObject replyObject = replyJson.object();
+	addLoggerInfo(QByteArrayLiteral("SettingsOauthJob: finished: ") + replyJson.toJson(QJsonDocument::Indented));
+        Q_EMIT ownInfoDone(replyObject);
+    }
+    deleteLater();
+}
+
+QNetworkRequest SettingsOauthJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::Me);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    request.setAttribute(QNetworkRequest::HttpPipeliningAllowedAttribute, true);
+    request.setAttribute(QNetworkRequest::HTTP2AllowedAttribute, true);
+    return request;
+}
