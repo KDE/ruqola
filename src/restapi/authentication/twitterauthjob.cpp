335798f08c75c26b3f9cf234c1739b81849c0886
Laurent Montel <montel@kde.org>
2018-04-19 08:09:49 +0200

    Look at to implement twitterauth job

---
+++ src/restapi/authentication/twitterauthjob.cpp
@@ -0,0 +1,149 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "twitterauthjob.h"
+#include "ruqola_restapi_debug.h"
+#include "restapimethod.h"
+
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+
+TwitterAuthJob::TwitterAuthJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+TwitterAuthJob::~TwitterAuthJob()
+{
+}
+
+bool TwitterAuthJob::canStart() const
+{
+    if (!RestApiAbstractJob::canStart()) {
+        qCWarning(RUQOLA_RESTAPI_LOG) << "Impossible to start facebook login job";
+        return false;
+    }
+    if (mAccessToken.isEmpty()) {
+        qCWarning(RUQOLA_RESTAPI_LOG) << "Access Token is empty";
+        return false;
+    }
+    if (mSecret.isEmpty()) {
+        qCWarning(RUQOLA_RESTAPI_LOG) << "Secret is empty";
+        return false;
+    }
+    if (mExpireTokenInSeconds <= 0) {
+        qCWarning(RUQOLA_RESTAPI_LOG) << "Expire token is not defined";
+        return false;
+    }
+    return true;
+}
+
+bool TwitterAuthJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        return false;
+    }
+    const QByteArray baPostData = json().toJson(QJsonDocument::Compact);
+
+    QNetworkReply *reply = mNetworkAccessManager->post(request(), baPostData);
+    addLoggerInfo("TwitterAuthJob started ");
+    connect(reply, &QNetworkReply::finished, this, &TwitterAuthJob::slotFacebookauthDone);
+
+    return false;
+}
+
+void TwitterAuthJob::slotFacebookauthDone()
+{
+    QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QByteArray data = reply->readAll();
+        const QJsonDocument replyJson = QJsonDocument::fromJson(data);
+        const QJsonObject replyObject = replyJson.object();
+
+        if (replyObject[QStringLiteral("status")].toString() == QStringLiteral("success") && replyObject.contains(QLatin1String("data"))) {
+            QJsonObject data = replyObject[QStringLiteral("data")].toObject();
+
+            if (data.contains(QLatin1String("authToken")) && data.contains(QLatin1String("userId"))) {
+                const QString authToken = data[QStringLiteral("authToken")].toString();
+                const QString userId = data[QStringLiteral("userId")].toString();
+                Q_EMIT facebookDone(authToken, userId);
+            }
+        } else {
+            qCWarning(RUQOLA_RESTAPI_LOG) << "Error during login" << data;
+        }
+    }
+    deleteLater();
+}
+
+int TwitterAuthJob::expireTokenInSeconds() const
+{
+    return mExpireTokenInSeconds;
+}
+
+void TwitterAuthJob::setExpireTokenInSeconds(int expireTokenInSeconds)
+{
+    mExpireTokenInSeconds = expireTokenInSeconds;
+}
+
+QString TwitterAuthJob::secret() const
+{
+    return mSecret;
+}
+
+void TwitterAuthJob::setSecret(const QString &secret)
+{
+    mSecret = secret;
+}
+
+QString TwitterAuthJob::accessToken() const
+{
+    return mAccessToken;
+}
+
+void TwitterAuthJob::setAccessToken(const QString &accessToken)
+{
+    mAccessToken = accessToken;
+}
+
+QJsonDocument TwitterAuthJob::json() const
+{
+    QVariantMap loginMap;
+    loginMap.insert(QStringLiteral("serviceName"), QStringLiteral("facebook"));
+    loginMap.insert(QStringLiteral("accessToken"), mAccessToken);
+    loginMap.insert(QStringLiteral("secret"), mSecret);
+    loginMap.insert(QStringLiteral("expiresIn"), mExpireTokenInSeconds);
+    const QJsonDocument postData = QJsonDocument::fromVariant(loginMap);
+    return postData;
+}
+
+QNetworkRequest TwitterAuthJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::Login);
+    QNetworkRequest request(url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, QStringLiteral("application/json"));
+    return request;
+}
+
+bool TwitterAuthJob::requireHttpAuthentication() const
+{
+    return false;
+}
---
+++ src/restapi/authentication/twitterauthjob.h
@@ -0,0 +1,64 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef TWITTERAUTHJOB_H
+#define TWITTERAUTHJOB_H
+#include "libruqola_private_export.h"
+#include "restapiabstractjob.h"
+
+#include <QNetworkRequest>
+
+class LIBRUQOLACORE_TESTS_EXPORT TwitterAuthJob : public RestApiAbstractJob
+{
+    Q_OBJECT
+public:
+    explicit TwitterAuthJob(QObject *parent = nullptr);
+    ~TwitterAuthJob() override;
+
+    bool start() override;
+    bool requireHttpAuthentication() const override;
+
+    bool canStart() const override;
+
+    QNetworkRequest request() const override;
+    QJsonDocument json() const;
+
+    QString accessToken() const;
+    void setAccessToken(const QString &accessToken);
+
+    QString secret() const;
+    void setSecret(const QString &secret);
+
+    int expireTokenInSeconds() const;
+    void setExpireTokenInSeconds(int expireTokenInSeconds);
+
+Q_SIGNALS:
+    void facebookDone(const QString &authToken, const QString &userId);
+
+private:
+    Q_DISABLE_COPY(TwitterAuthJob)
+    void slotFacebookauthDone();
+    QString mAccessToken;
+    QString mSecret;
+    int mExpireTokenInSeconds = -1;
+    //TODO identity ???? it's optional
+};
+
+#endif // TWITTERAUTHJOB_H
