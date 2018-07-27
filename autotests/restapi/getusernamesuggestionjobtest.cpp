a9e31e8339cf13f90831cdfb85f99dd49f8dc8ff
Laurent Montel <montel@kde.org>
2018-07-27 13:59:34 +0200

    Start to implement getusernamesuggestionjob

---
+++ autotests/restapi/getusernamesuggestionjobtest.cpp
@@ -0,0 +1,51 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "getusernamesuggestionjobtest.h"
+#include "restapi/users/getusernamesuggestionjob.h"
+#include "restapi/restapimethod.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(GetUsernameSuggestionJobTest)
+
+GetUsernameSuggestionJobTest::GetUsernameSuggestionJobTest(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void GetUsernameSuggestionJobTest::shouldHaveDefaultValue()
+{
+    GetUsernameSuggestionJob job;
+    QVERIFY(!job.restApiMethod());
+    QVERIFY(!job.networkAccessManager());
+    QVERIFY(!job.start());
+    QVERIFY(job.requireHttpAuthentication());
+    QVERIFY(!job.ruqolaLogger());
+}
+
+void GetUsernameSuggestionJobTest::shouldGenerateRequest()
+{
+    GetUsernameSuggestionJob job;
+    RestApiMethod *method = new RestApiMethod;
+    method->setServerUrl(QStringLiteral("http://www.kde.org"));
+    job.setRestApiMethod(method);
+    const QNetworkRequest request = job.request();
+    QCOMPARE(request.url(), QUrl(QStringLiteral("http://www.kde.org/api/v1/users.getPresence?userId=avat")));
+    delete method;
+}
---
+++ src/restapi/users/getusernamesuggestionjob.h
@@ -0,0 +1,53 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef GETUSERNAMESUGGESTIONJOB_H
+#define GETUSERNAMESUGGESTIONJOB_H
+
+#include "libruqola_private_export.h"
+#include "restapiabstractjob.h"
+
+#include <QNetworkRequest>
+
+class LIBRUQOLACORE_TESTS_EXPORT GetUsernameSuggestionJob : public RestApiAbstractJob
+{
+    Q_OBJECT
+public:
+    //Since 0.65
+    explicit GetUsernameSuggestionJob(QObject *parent = nullptr);
+    ~GetUsernameSuggestionJob() override;
+
+    Q_REQUIRED_RESULT bool start() override;
+    Q_REQUIRED_RESULT bool requireHttpAuthentication() const override;
+
+
+    Q_REQUIRED_RESULT QNetworkRequest request() const override;
+
+    Q_REQUIRED_RESULT bool canStart() const override;
+
+Q_SIGNALS:
+    void getUsernameSuggestionDone(const QString &result);
+
+private:
+    Q_DISABLE_COPY(GetUsernameSuggestionJob)
+    void slotGetUsernameSuggestion();
+};
+
+#endif // GETUSERNAMESUGGESTIONJOB_H
