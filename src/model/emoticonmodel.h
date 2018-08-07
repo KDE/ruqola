055d0c0e0f49ebdf5ece62448de1d31aa2da2ccb
Laurent Montel <montel@kde.org>
2018-08-07 06:46:05 +0200

    Start to create emoticon model

---
+++ src/model/emoticonmodel.h
@@ -0,0 +1,40 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef EMOTICONMODEL_H
+#define EMOTICONMODEL_H
+
+#include <QAbstractListModel>
+#include "libruqola_private_export.h"
+
+class LIBRUQOLACORE_TESTS_EXPORT EmoticonModel : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    explicit EmoticonModel(QObject *parent = nullptr);
+    ~EmoticonModel() override;
+
+    Q_REQUIRED_RESULT int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    Q_REQUIRED_RESULT QVariant data(const QModelIndex &index, int role) const override;
+
+    Q_REQUIRED_RESULT QHash<int, QByteArray> roleNames() const override;
+};
+
+#endif // EMOTICONMODEL_H
