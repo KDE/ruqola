8e11b62102b3c4f5a4643318951e3359f11ba627
Laurent Montel <montel@kde.org>
2018-09-28 08:53:05 +0200

    Fix close channel

--- src/ruqolacore/rocketchataccount.cpp
+++ src/ruqolacore/rocketchataccount.cpp
@@ -370,15 +370,7 @@ void RocketChatAccount::leaveRoom(const QString &roomId, const QString &channelT
 
 void RocketChatAccount::hideRoom(const QString &roomId, const QString &channelType)
 {
-#ifdef USE_REASTAPI_JOB_NOTSUPPORTED_YET
-    if (channelType == QStringLiteral("c")) {
-        restApi()->leaveChannel(roomId);
-    } else if (channelType == QStringLiteral("p")) {
-        restApi()->leaveGroups(roomId);
-    } else if (channelType == QStringLiteral("d")) {
-    } else {
-        qCWarning(RUQOLA_LOG) << " unsupport hide room for type " << channelType;
-    }
+#ifdef USE_REASTAPI_JOB
     restApi()->closeChannel(roomId, channelType);
 #else
     Q_UNUSED(channelType)
