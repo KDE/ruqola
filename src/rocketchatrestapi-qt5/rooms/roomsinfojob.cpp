7736dbaf176808ff7a3b6fe6d0f5ae662da93e23
Laurent Montel <montel@kde.org>
2018-11-20 13:58:25 +0100

    Add room infos

---
+++ src/rocketchatrestapi-qt5/rooms/roomsinfojob.cpp
@@ -0,0 +1,81 @@
+/*
+   Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "roomsinfojob.h"
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+using namespace RocketChatRestApi;
+RoomsInfoJob::RoomsInfoJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+RoomsInfoJob::~RoomsInfoJob()
+{
+}
+
+bool RoomsInfoJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool RoomsInfoJob::start()
+{
+    if (!canStart()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "Impossible to start roomsinfo job";
+        deleteLater();
+        return false;
+    }
+    QNetworkReply *reply = mNetworkAccessManager->get(request());
+    connect(reply, &QNetworkReply::finished, this, &RoomsInfoJob::slotRoomsInfoFinished);
+    addLoggerInfo(QByteArrayLiteral("RoomsInfoJob: Ask info about rooms"));
+    return true;
+}
+
+void RoomsInfoJob::slotRoomsInfoFinished()
+{
+    QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QByteArray data = reply->readAll();
+        const QJsonDocument replyJson = QJsonDocument::fromJson(data);
+        const QJsonObject replyObject = replyJson.object();
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            addLoggerInfo(QByteArrayLiteral("RoomsInfoJob: finished: ") + replyJson.toJson(QJsonDocument::Indented));
+            Q_EMIT roomsInfoDone(replyObject);
+        } else {
+            addLoggerWarning(QByteArrayLiteral("RoomsInfoJob: Problem: ") + replyJson.toJson(QJsonDocument::Indented));
+        }
+    }
+    deleteLater();
+}
+
+QNetworkRequest RoomsInfoJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::RoomsInfo);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    request.setAttribute(QNetworkRequest::HttpPipeliningAllowedAttribute, true);
+    request.setAttribute(QNetworkRequest::HTTP2AllowedAttribute, true);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, QStringLiteral("application/json"));
+    return request;
+}
