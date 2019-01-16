4fd20e30f50d89fc3c9c3c0664c2a77cd95fcaa9
Laurent Montel <montel@kde.org>
2019-01-16 13:53:09 +0100

    Start to implement channelinfo

---
+++ src/rocketchatrestapi-qt5/channels/channelinfojob.cpp
@@ -0,0 +1,110 @@
+/*
+   Copyright (c) 2019 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "channelinfojob.h"
+
+#include "rocketchatqtrestapi_debug.h"
+#include "restapimethod.h"
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+#include <QUrlQuery>
+using namespace RocketChatRestApi;
+ChannelInfoJob::ChannelInfoJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+ChannelInfoJob::~ChannelInfoJob()
+{
+}
+
+bool ChannelInfoJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        return false;
+    }
+    addLoggerInfo("ChannelInfoJob::start: ");
+    QNetworkReply *reply = mNetworkAccessManager->get(request());
+    connect(reply, &QNetworkReply::finished, this, &ChannelInfoJob::slotFilesinChannelFinished);
+    return true;
+}
+
+void ChannelInfoJob::slotFilesinChannelFinished()
+{
+    QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QByteArray data = reply->readAll();
+        const QJsonDocument replyJson = QJsonDocument::fromJson(data);
+        const QJsonObject replyObject = replyJson.object();
+
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            addLoggerInfo(QByteArrayLiteral("channelFilesDone success: ") + replyJson.toJson(QJsonDocument::Indented));
+            Q_EMIT channelInfoDone(replyObject, mRoomId);
+        } else {
+            addLoggerWarning(QByteArrayLiteral("channelFilesDone problem: ") + replyJson.toJson(QJsonDocument::Indented));
+        }
+    }
+    deleteLater();
+}
+
+bool ChannelInfoJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool ChannelInfoJob::canStart() const
+{
+    if (mRoomId.isEmpty()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "ChannelInfoJob: RoomId is empty";
+        return false;
+    }
+    if (!RestApiAbstractJob::canStart()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "Impossible to start ChannelInfoJob job";
+        return false;
+    }
+    return true;
+}
+
+QString ChannelInfoJob::roomId() const
+{
+    return mRoomId;
+}
+
+void ChannelInfoJob::setRoomId(const QString &roomId)
+{
+    mRoomId = roomId;
+}
+
+QNetworkRequest ChannelInfoJob::request() const
+{
+    QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::ChannelsInfo);
+    QUrlQuery queryUrl;
+    queryUrl.addQueryItem(QStringLiteral("roomId"), mRoomId);
+    url.setQuery(queryUrl);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+
+    request.setAttribute(QNetworkRequest::HttpPipeliningAllowedAttribute, true);
+    request.setAttribute(QNetworkRequest::HTTP2AllowedAttribute, true);
+    return request;
+}
