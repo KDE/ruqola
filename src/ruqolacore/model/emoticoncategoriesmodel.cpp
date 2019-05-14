545bb74ff7f35d04268007b4cc2c3e6acb5ae990
Laurent Montel <montel@kde.org>
2019-05-14 08:58:08 +0200

    We need a model for emoticon categories

---
+++ src/ruqolacore/model/emoticoncategoriesmodel.cpp
@@ -0,0 +1,71 @@
+/*
+   Copyright (c) 2018-2019 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "emoticoncategoriesmodel.h"
+
+EmoticonCategoriesModel::EmoticonCategoriesModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+}
+
+EmoticonCategoriesModel::~EmoticonCategoriesModel()
+{
+}
+
+int EmoticonCategoriesModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return 0;
+}
+
+QVariant EmoticonCategoriesModel::data(const QModelIndex &index, int role) const
+{
+//    const QVector<UnicodeEmoticon> emoticonsFromCategoryList = mEmoticons.value(mCurrentCategory);
+//    if (index.row() < 0 || index.row() >= emoticonsFromCategoryList.count()) {
+//        return {};
+//    }
+    return {};
+}
+
+QHash<int, QByteArray> EmoticonCategoriesModel::roleNames() const
+{
+    QHash<int, QByteArray> roles;
+//    roles[Identifier] = QByteArrayLiteral("identifier");
+    return roles;
+}
+
+//void EmoticonCategoriesModel::setEmoticons(const QMap<QString, QVector<UnicodeEmoticon> > &emoticons)
+//{
+////    if (rowCount() != 0) {
+////        beginRemoveRows(QModelIndex(), 0, mEmoticons.count() - 1);
+////        mEmoticons.clear();
+////        endRemoveRows();
+////    }
+////    if (!emoticons.isEmpty()) {
+////        beginInsertRows(QModelIndex(), 0, mEmoticons.count() - 1);
+////        mEmoticons = emoticons;
+////        endInsertRows();
+////    }
+////    if (!mEmoticons.isEmpty()) {
+////        mCurrentCategory = mEmoticons.keys().at(0);
+////        mCategories = mEmoticons.keys();
+////    }
+//}
+
