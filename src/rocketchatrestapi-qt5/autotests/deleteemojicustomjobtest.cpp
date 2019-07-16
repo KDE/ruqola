da9f1ae577b9d7eccba9df063a08b35f2c1660fc
Laurent Montel <montel@kde.org>
2019-07-16 13:29:36 +0200

    Add method for deleting custom emoji

---
+++ src/rocketchatrestapi-qt5/autotests/deleteemojicustomjobtest.cpp
@@ -0,0 +1,56 @@
+/*
+   Copyright (c) 2019 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "deleteemojicustomjobtest.h"
+#include "emoji/deleteemojicustomjob.h"
+#include "ruqola_restapi_helper.h"
+#include <QJsonDocument>
+#include <QTest>
+QTEST_GUILESS_MAIN(DeleteEmojiCustomJobTest)
+using namespace RocketChatRestApi;
+DeleteEmojiCustomJobTest::DeleteEmojiCustomJobTest(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void DeleteEmojiCustomJobTest::shouldHaveDefaultValue()
+{
+    DeleteEmojiCustomJob job;
+    verifyDefaultValue(&job);
+    QVERIFY(job.emojiId().isEmpty());
+    QVERIFY(!job.hasQueryParameterSupport());
+}
+
+void DeleteEmojiCustomJobTest::shouldGenerateRequest()
+{
+    DeleteEmojiCustomJob job;
+    QNetworkRequest request = QNetworkRequest(QUrl());
+    verifyAuthentication(&job, request);
+    QCOMPARE(request.url(), QUrl(QStringLiteral("http://www.kde.org/api/v1/emoji-custom.delete")));
+    QCOMPARE(request.header(QNetworkRequest::ContentTypeHeader).toString(), QStringLiteral("application/json"));
+}
+
+void DeleteEmojiCustomJobTest::shouldGenerateJson()
+{
+    DeleteEmojiCustomJob job;
+    const QString emojiId = QStringLiteral("foo1");
+    job.setEmojiId(emojiId);
+    QCOMPARE(job.json().toJson(QJsonDocument::Compact), QStringLiteral("{\"emojiId\":\"%1\"}").arg(emojiId).toLatin1());
+}
---
+++ src/rocketchatrestapi-qt5/emoji/deleteemojicustomjob.cpp
@@ -0,0 +1,115 @@
+/*
+   Copyright (c) 2019 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "deleteemojicustomjob.h"
+
+#include "rocketchatqtrestapi_debug.h"
+#include "restapimethod.h"
+#include <KLocalizedString>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+using namespace RocketChatRestApi;
+DeleteEmojiCustomJob::DeleteEmojiCustomJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+DeleteEmojiCustomJob::~DeleteEmojiCustomJob()
+{
+}
+
+bool DeleteEmojiCustomJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        return false;
+    }
+    const QByteArray baPostData = json().toJson(QJsonDocument::Compact);
+    addLoggerInfo("DeleteEmojiCustomJob::start: " + baPostData);
+    QNetworkReply *reply = mNetworkAccessManager->post(request(), baPostData);
+    connect(reply, &QNetworkReply::finished, this, &DeleteEmojiCustomJob::slotDeleteEmojiFinished);
+    return true;
+}
+
+void DeleteEmojiCustomJob::slotDeleteEmojiFinished()
+{
+    QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QByteArray data = reply->readAll();
+        const QJsonDocument replyJson = QJsonDocument::fromJson(data);
+        const QJsonObject replyObject = replyJson.object();
+
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            addLoggerInfo(QByteArrayLiteral("DeleteEmojiCustomJob success: ") + replyJson.toJson(QJsonDocument::Indented));
+            Q_EMIT removeOwnerDone();
+        } else {
+            emitFailedMessage(replyObject);
+            addLoggerWarning(QByteArrayLiteral("DeleteEmojiCustomJob problem: ") + replyJson.toJson(QJsonDocument::Indented));
+        }
+        reply->deleteLater();
+    }
+    deleteLater();
+}
+
+QString DeleteEmojiCustomJob::emojiId() const
+{
+    return mEmojiId;
+}
+
+void DeleteEmojiCustomJob::setEmojiId(const QString &emojiId)
+{
+    mEmojiId = emojiId;
+}
+
+bool DeleteEmojiCustomJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool DeleteEmojiCustomJob::canStart() const
+{
+    if (mEmojiId.isEmpty()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "DeleteEmojiCustomJob: remove mEmojiId is empty";
+        return false;
+    }
+    if (!RestApiAbstractJob::canStart()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "Impossible to start DeleteEmojiCustomJob job";
+        return false;
+    }
+    return true;
+}
+
+QJsonDocument DeleteEmojiCustomJob::json() const
+{
+    QJsonObject jsonObj;
+    jsonObj[QLatin1String("emojiId")] = emojiId();
+    const QJsonDocument postData = QJsonDocument(jsonObj);
+    return postData;
+}
+
+QNetworkRequest DeleteEmojiCustomJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::DeleteEmojiCustom);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    addRequestAttribute(request);
+    return request;
+}
