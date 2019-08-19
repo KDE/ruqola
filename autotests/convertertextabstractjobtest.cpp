43b8c77124259c56b2ef235bbded5126d9d1754b
Laurent Montel <montel@kde.org>
2019-08-19 13:52:44 +0200

    Start to implement for changing message text (for example for
    translating it)

---
+++ autotests/convertertextabstractjobtest.cpp
@@ -0,0 +1,48 @@
+/*
+   Copyright (c) 2019 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "convertertextabstractjobtest.h"
+#include "convertertextjob/convertertextabstractjob.h"
+#include <QTest>
+class ConverterTextAbstractJobImpl : public ConverterTextAbstractJob
+{
+public:
+    explicit ConverterTextAbstractJobImpl(QObject *parent = nullptr)
+        : ConverterTextAbstractJob(parent)
+    {
+
+    }
+    void start() override {}
+};
+
+QTEST_GUILESS_MAIN(ConverterTextAbstractJobTest)
+
+
+ConverterTextAbstractJobTest::ConverterTextAbstractJobTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+void ConverterTextAbstractJobTest::shouldHaveDefaultValue()
+{
+    ConverterTextAbstractJobImpl job;
+    QVERIFY(job.messageId().isEmpty());
+}
