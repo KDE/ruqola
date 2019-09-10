3cc61b178deda76004f5bf711384e058dc7b970e
Laurent Montel <montel@kde.org>
2019-09-10 19:39:03 +0200

    Start to implement translatesavesettingsjob

---
+++ src/rocketchatrestapi-qt5/autotests/translatesavesettingsjobtest.cpp
@@ -0,0 +1,88 @@
+/*
+   Copyright (c) 2019 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "translatesavesettingsjobtest.h"
+#include "autotranslate/translatesavesettingsjob.h"
+#include "ruqola_restapi_helper.h"
+#include <QTest>
+#include <QJsonDocument>
+QTEST_GUILESS_MAIN(TranslateSaveSettingsJobTest)
+using namespace RocketChatRestApi;
+TranslateSaveSettingsJobTest::TranslateSaveSettingsJobTest(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void TranslateSaveSettingsJobTest::shouldHaveDefaultValue()
+{
+    TranslateSaveSettingsJob job;
+    verifyDefaultValue(&job);
+    QVERIFY(job.requireHttpAuthentication());
+//    QVERIFY(job.messageId().isEmpty());
+//    QVERIFY(job.targetLanguage().isEmpty());
+    QVERIFY(!job.hasQueryParameterSupport());
+}
+
+void TranslateSaveSettingsJobTest::shouldGenerateRequest()
+{
+    TranslateSaveSettingsJob job;
+    QNetworkRequest request = QNetworkRequest(QUrl());
+    verifyAuthentication(&job, request);
+    QCOMPARE(request.url(), QUrl(QStringLiteral("http://www.kde.org/api/v1/autotranslate.translateMessage")));
+    QCOMPARE(request.header(QNetworkRequest::ContentTypeHeader).toString(), QStringLiteral("application/json"));
+}
+
+void TranslateSaveSettingsJobTest::shouldGenerateJson()
+{
+//    TranslateSaveSettingsJob job;
+//    const QString messageId = QStringLiteral("foo1");
+//    job.setMessageId(messageId);
+//    const QString targetLanguage = QStringLiteral("bla");
+//    job.setTargetLanguage(targetLanguage);
+//    QCOMPARE(job.json().toJson(QJsonDocument::Compact), QStringLiteral("{\"messageId\":\"%1\",\"targetLanguage\":\"%2\"}").arg(messageId).arg(targetLanguage).toLatin1());
+}
+
+void TranslateSaveSettingsJobTest::shouldNotStarting()
+{
+    TranslateSaveSettingsJob job;
+
+    RestApiMethod *method = new RestApiMethod;
+    method->setServerUrl(QStringLiteral("http://www.kde.org"));
+    job.setRestApiMethod(method);
+
+    QNetworkAccessManager *mNetworkAccessManager = new QNetworkAccessManager;
+    job.setNetworkAccessManager(mNetworkAccessManager);
+    QVERIFY(!job.canStart());
+    const QString auth = QStringLiteral("foo");
+    const QString userId = QStringLiteral("foo");
+    job.setAuthToken(auth);
+    QVERIFY(!job.canStart());
+    job.setUserId(userId);
+    QVERIFY(!job.canStart());
+//    const QString messageId = QStringLiteral("foo1");
+//    job.setMessageId(messageId);
+//    QVERIFY(!job.canStart());
+//    const QString targetLanguage = QStringLiteral("bla");
+//    job.setTargetLanguage(targetLanguage);
+//    QVERIFY(job.canStart());
+
+    delete method;
+    delete mNetworkAccessManager;
+}
---
+++ src/rocketchatrestapi-qt5/autotranslate/translatesavesettingsjob.cpp
@@ -0,0 +1,116 @@
+/*
+   Copyright (c) 2019 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "translatesavesettingsjob.h"
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+using namespace RocketChatRestApi;
+TranslateSaveSettingsJob::TranslateSaveSettingsJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+TranslateSaveSettingsJob::~TranslateSaveSettingsJob()
+{
+}
+
+bool TranslateSaveSettingsJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        return false;
+    }
+    const QByteArray baPostData = json().toJson(QJsonDocument::Compact);
+    addLoggerInfo("TranslateSaveSettingsJob::start: " + baPostData);
+    QNetworkReply *reply = mNetworkAccessManager->post(request(), baPostData);
+    connect(reply, &QNetworkReply::finished, this, &TranslateSaveSettingsJob::slotTranslateSaveSettingsFinished);
+    return true;
+}
+
+void TranslateSaveSettingsJob::slotTranslateSaveSettingsFinished()
+{
+    QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QByteArray data = reply->readAll();
+        const QJsonDocument replyJson = QJsonDocument::fromJson(data);
+        const QJsonObject replyObject = replyJson.object();
+
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            addLoggerInfo(QByteArrayLiteral("TranslateSaveSettingsJob success: ") + replyJson.toJson(QJsonDocument::Indented));
+            Q_EMIT translateSavesettingsDone();
+        } else {
+            emitFailedMessage(replyObject);
+            addLoggerWarning(QByteArrayLiteral("TranslateSaveSettingsJob: Problem: ") + replyJson.toJson(QJsonDocument::Indented));
+        }
+        reply->deleteLater();
+    }
+    deleteLater();
+}
+
+QString TranslateSaveSettingsJob::roomId() const
+{
+    return mRoomId;
+}
+
+void TranslateSaveSettingsJob::setRoomId(const QString &roomId)
+{
+    mRoomId = roomId;
+}
+
+bool TranslateSaveSettingsJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool TranslateSaveSettingsJob::canStart() const
+{
+    if (!RestApiAbstractJob::canStart()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "Impossible to start TranslateSaveSettingsJob";
+        return false;
+    }
+    if (mRoomId.isEmpty()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "TranslateSaveSettingsJob: mRoomId is empty";
+        return false;
+    }
+    return true;
+}
+
+QNetworkRequest TranslateSaveSettingsJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::AutoTranslateSaveSettings);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    addRequestAttribute(request);
+    return request;
+}
+
+QJsonDocument TranslateSaveSettingsJob::json() const
+{
+    QJsonObject jsonObj;
+//    jsonObj[QLatin1String("messageId")] = mMessageId;
+//    jsonObj[QLatin1String("targetLanguage")] = mTargetLanguage;
+    const QJsonDocument postData = QJsonDocument(jsonObj);
+    return postData;
+}
