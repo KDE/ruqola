e6f1665d400412dc675a0e8579dfc578b455cdb8
Laurent Montel <montel@kde.org>
2019-12-23 10:41:40 +0100

    Add customsoundsjob

---
+++ src/rocketchatrestapi-qt5/custom/customsoundsjob.h
@@ -0,0 +1,47 @@
+/*
+   Copyright (c) 2019 Montel Laurent <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef CUSTOMSOUNDSJOB_H
+#define CUSTOMSOUNDSJOB_H
+
+#include "restapiabstractjob.h"
+
+#include "librestapi_private_export.h"
+
+#include <QNetworkRequest>
+namespace RocketChatRestApi {
+class LIBROCKETCHATRESTAPI_QT5_TESTS_EXPORT CustomSoundsJob : public RestApiAbstractJob
+{
+    Q_OBJECT
+public:
+    explicit CustomSoundsJob(QObject *parent = nullptr);
+    ~CustomSoundsJob() override;
+
+    Q_REQUIRED_RESULT bool start() override;
+    Q_REQUIRED_RESULT bool requireHttpAuthentication() const override;
+    Q_REQUIRED_RESULT QNetworkRequest request() const override;
+Q_SIGNALS:
+    void customSoundsDone(const QByteArray &data);
+private:
+    Q_DISABLE_COPY(CustomSoundsJob)
+    void slotCustomSoundsDone();
+};
+}
+#endif // CUSTOMUSERSTATUSJOB_H
