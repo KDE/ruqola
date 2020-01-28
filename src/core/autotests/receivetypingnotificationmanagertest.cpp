a2595f525566e51a0f6a3a71a8715f2edb81815c
Kevin Funk <kfunk@kde.org>
2020-01-28 12:00:14 +0100

    Fix unit tests

--- src/core/autotests/receivetypingnotificationmanagertest.cpp
+++ src/core/autotests/receivetypingnotificationmanagertest.cpp
@@ -46,37 +46,37 @@ void ReceiveTypingNotificationManagerTest::shouldAddNotifications()
     spy.clear();
 
     manager.insertTypingNotification(QStringLiteral("room1"), QStringLiteral("user1"), true);
-    QCOMPARE(manager.typingNotification(QStringLiteral("room1")), QStringLiteral("user1 is typing..."));
+    QCOMPARE(manager.typingNotification(QStringLiteral("room1")), QStringLiteral("<strong>user1</strong> is typing..."));
     QCOMPARE(spy.count(), 1);
     manager.insertTypingNotification(QStringLiteral("room1"), QStringLiteral("user1"), true);
-    QCOMPARE(manager.typingNotification(QStringLiteral("room1")), QStringLiteral("user1 is typing..."));
+    QCOMPARE(manager.typingNotification(QStringLiteral("room1")), QStringLiteral("<strong>user1</strong> is typing..."));
     QCOMPARE(spy.count(), 1);
 
     spy.clear();
     //Add different user.
     manager.insertTypingNotification(QStringLiteral("room2"), QStringLiteral("user2"), true);
-    QCOMPARE(manager.typingNotification(QStringLiteral("room2")), QStringLiteral("user2 is typing..."));
+    QCOMPARE(manager.typingNotification(QStringLiteral("room2")), QStringLiteral("<strong>user2</strong> is typing..."));
     QCOMPARE(spy.count(), 1);
 
     manager.insertTypingNotification(QStringLiteral("room2"), QStringLiteral("user3"), true);
-    QCOMPARE(manager.typingNotification(QStringLiteral("room2")), QStringLiteral("user2 and user3 are typing..."));
+    QCOMPARE(manager.typingNotification(QStringLiteral("room2")), QStringLiteral("<strong>user2 and user3</strong> are typing..."));
     QCOMPARE(spy.count(), 2);
 
     manager.insertTypingNotification(QStringLiteral("room2"), QStringLiteral("user1"), true);
-    QCOMPARE(manager.typingNotification(QStringLiteral("room2")), QStringLiteral("user2, user3 and user1 are typing..."));
+    QCOMPARE(manager.typingNotification(QStringLiteral("room2")), QStringLiteral("<strong>user2, user3 and user1</strong> are typing..."));
     QCOMPARE(spy.count(), 3);
 
     //Remove typing
     manager.insertTypingNotification(QStringLiteral("room2"), QStringLiteral("user3"), false);
-    QCOMPARE(manager.typingNotification(QStringLiteral("room2")), QStringLiteral("user2 and user1 are typing..."));
+    QCOMPARE(manager.typingNotification(QStringLiteral("room2")), QStringLiteral("<strong>user2 and user1</strong> are typing..."));
     QCOMPARE(spy.count(), 4);
 
     manager.insertTypingNotification(QStringLiteral("room2"), QStringLiteral("user1"), false);
-    QCOMPARE(manager.typingNotification(QStringLiteral("room2")), QStringLiteral("user2 is typing..."));
+    QCOMPARE(manager.typingNotification(QStringLiteral("room2")), QStringLiteral("<strong>user2</strong> is typing..."));
     QCOMPARE(spy.count(), 5);
 
     manager.insertTypingNotification(QStringLiteral("room2"), QStringLiteral("user3"), false);
-    QCOMPARE(manager.typingNotification(QStringLiteral("room2")), QStringLiteral("user2 is typing..."));
+    QCOMPARE(manager.typingNotification(QStringLiteral("room2")), QStringLiteral("<strong>user2</strong> is typing..."));
     QCOMPARE(spy.count(), 5);
 
     manager.insertTypingNotification(QStringLiteral("room2"), QStringLiteral("user2"), false);
