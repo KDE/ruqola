a1c312534752fb633dde651d3ece06678a6cdf06
Laurent Montel <montel@kde.org>
2020-01-29 22:18:16 +0100

    Implement showdiscussions dialog

---
+++ src/widgets/dialogs/showdiscussionswidget.cpp
@@ -0,0 +1,51 @@
+/*
+   Copyright (c) 2020 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+#include "showdiscussionswidget.h"
+#include <QVBoxLayout>
+#include <KLocalizedString>
+#include <KLineEdit>
+
+ShowDiscussionsWidget::ShowDiscussionsWidget(QWidget *parent)
+    : QWidget(parent)
+{
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(QStringLiteral("mainLayout"));
+    mainLayout->setContentsMargins(0, 0, 0, 0);
+
+    mSearchDiscussionLineEdit = new KLineEdit(this);
+    mSearchDiscussionLineEdit->setObjectName(QStringLiteral("mSearchDiscussionLineEdit"));
+    mSearchDiscussionLineEdit->setClearButtonEnabled(true);
+    mSearchDiscussionLineEdit->setTrapReturnKey(true);
+    mSearchDiscussionLineEdit->setPlaceholderText(i18n("Search Discussion..."));
+    connect(mSearchDiscussionLineEdit, &KLineEdit::textChanged, this, &ShowDiscussionsWidget::slotSearchMessageTextChanged);
+    mainLayout->addWidget(mSearchDiscussionLineEdit);
+}
+
+ShowDiscussionsWidget::~ShowDiscussionsWidget()
+{
+
+}
+
+void ShowDiscussionsWidget::slotSearchMessageTextChanged(const QString &str)
+{
+    //mModel->setFilterString(str);
+}
