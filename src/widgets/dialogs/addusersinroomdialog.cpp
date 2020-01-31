02d1ecf1e7d0d6944aaf6e2453f348b3bb9ca1db
Laurent Montel <montel@kde.org>
2020-01-31 13:24:33 +0100

    Start to implement add user in room

---
+++ src/widgets/dialogs/addusersinroomdialog.cpp
@@ -0,0 +1,67 @@
+/*
+   Copyright (c) 2020 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "addusersinroomdialog.h"
+#include "addusersinroomwidget.h"
+#include <KConfigGroup>
+#include <KLocalizedString>
+#include <KSharedConfig>
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+namespace {
+static const char myConfigGroupName[] = "AddUsersInRoomDialog";
+}
+AddUsersInRoomDialog::AddUsersInRoomDialog(QWidget *parent)
+    : QDialog(parent)
+{
+    setWindowTitle(i18nc("@title:window", "Create Channel"));
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(QStringLiteral("mainLayout"));
+
+    mAddUsersInRoomWidget = new AddUsersInRoomWidget(this);
+    mAddUsersInRoomWidget->setObjectName(QStringLiteral("mAddUsersInRoomWidget"));
+    mainLayout->addWidget(mAddUsersInRoomWidget);
+
+    QDialogButtonBox *button = new QDialogButtonBox(QDialogButtonBox::Close, this);
+    button->setObjectName(QStringLiteral("button"));
+    mainLayout->addWidget(button);
+    connect(button, &QDialogButtonBox::rejected, this, &AddUsersInRoomDialog::reject);
+    readConfig();
+}
+
+AddUsersInRoomDialog::~AddUsersInRoomDialog()
+{
+    writeConfig();
+}
+
+void AddUsersInRoomDialog::readConfig()
+{
+    KConfigGroup group(KSharedConfig::openConfig(), myConfigGroupName);
+    const QSize sizeDialog = group.readEntry("Size", QSize(800, 600));
+    if (sizeDialog.isValid()) {
+        resize(sizeDialog);
+    }
+}
+
+void AddUsersInRoomDialog::writeConfig()
+{
+    KConfigGroup group(KSharedConfig::openConfig(), myConfigGroupName);
+    group.writeEntry("Size", size());
+}
