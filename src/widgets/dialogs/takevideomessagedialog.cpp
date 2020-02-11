9349503e71bcb28574745a7d3645c8e5a82bbc5d
Laurent Montel <montel@kde.org>
2020-02-11 22:41:32 +0100

    Start to implement take video message

---
+++ src/widgets/dialogs/takevideomessagedialog.cpp
@@ -0,0 +1,47 @@
+/*
+   Copyright (c) 2020 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "takevideomessagedialog.h"
+#include "showimagewidget.h"
+
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+#include <KLocalizedString>
+
+TakeVideoMessageDialog::TakeVideoMessageDialog(QWidget *parent)
+    : QDialog(parent)
+{
+    setWindowTitle(i18nc("@title:window", "Video Message"));
+    auto *mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(QStringLiteral("mainLayout"));
+
+//    mShowImageWidget = new ShowImageWidget(this);
+//    mShowImageWidget->setObjectName(QStringLiteral("mShowImageWidget"));
+//    mainLayout->addWidget(mShowImageWidget);
+
+    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Close, this);
+    buttonBox->setObjectName(QStringLiteral("button"));
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &TakeVideoMessageDialog::reject);
+    mainLayout->addWidget(buttonBox);
+}
+
+TakeVideoMessageDialog::~TakeVideoMessageDialog()
+{
+}
