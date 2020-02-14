ed5e86ad622d82fd51fb7279c264dce5127380c3
Laurent Montel <montel@kde.org>
2020-02-14 18:32:25 +0100

    Start to implement "invite users"

---
+++ src/widgets/dialogs/autotests/inviteusersdialogtest.cpp
@@ -0,0 +1,43 @@
+/*
+   Copyright (c) 2020 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "inviteusersdialogtest.h"
+#include "dialogs/inviteusersdialog.h"
+#include <QDialogButtonBox>
+#include <QTest>
+#include <QVBoxLayout>
+QTEST_MAIN(InviteUsersDialogTest)
+
+InviteUsersDialogTest::InviteUsersDialogTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+void InviteUsersDialogTest::shouldHaveDefaultValues()
+{
+    InviteUsersDialog w;
+    QVERIFY(!w.windowTitle().isEmpty());
+    auto *mainLayout = w.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+
+    auto *buttonBox = w.findChild<QDialogButtonBox *>(QStringLiteral("button"));
+    QVERIFY(buttonBox);
+}
---
+++ src/widgets/dialogs/inviteusersdialog.cpp
@@ -0,0 +1,45 @@
+/*
+   Copyright (c) 2020 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+#include "inviteusersdialog.h"
+
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+#include <KLocalizedString>
+
+InviteUsersDialog::InviteUsersDialog(QWidget *parent)
+    : QDialog(parent)
+{
+    setWindowTitle(i18nc("@title:window", "Invite Users"));
+    auto *mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(QStringLiteral("mainLayout"));
+
+    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
+    buttonBox->setObjectName(QStringLiteral("button"));
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &InviteUsersDialog::reject);
+    connect(buttonBox, &QDialogButtonBox::accepted, this, &InviteUsersDialog::accept);
+    mainLayout->addWidget(buttonBox);
+}
+
+InviteUsersDialog::~InviteUsersDialog()
+{
+
+}
