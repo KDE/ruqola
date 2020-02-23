d0f1d62a9c159b4eaca54aa666d417f43b64931c
Laurent Montel <montel@kde.org>
2020-02-23 18:16:09 +0100

    Add missing i18n

--- src/rocketchatrestapi-qt5/restapiabstractjob.cpp
+++ src/rocketchatrestapi-qt5/restapiabstractjob.cpp
@@ -431,6 +431,8 @@ QString RestApiAbstractJob::errorMessage(const QString &str)
         return i18n("You are the last owner. Please set new owner before leaving the room.");
     } else if (str == QLatin1String("error-room-archived")) {
         return i18n("The private group is archived");
+    } else if (str == QLatin1String("error-user-already-owner")) {
+        return i18n("User is already an owner");
     } else {
         qCWarning(ROCKETCHATQTRESTAPI_LOG) << " unknown error type " << str;
         return {};
