978fb882eadb2f8a2b374ddce562226efb0fe2a0
Laurent Montel <montel@kde.org>
2020-03-06 13:28:45 +0100

    Add command support

---
+++ src/rocketchatrestapi-qt5/commands/getcommandsjob.cpp
@@ -0,0 +1,114 @@
+/*
+   Copyright (c) 2020 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "getcommandsjob.h"
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+#include <QUrlQuery>
+using namespace RocketChatRestApi;
+GetCommandsJob::GetCommandsJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+GetCommandsJob::~GetCommandsJob()
+{
+}
+
+bool GetCommandsJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool GetCommandsJob::start()
+{
+    if (!canStart()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "Impossible to start owninfo job";
+        deleteLater();
+        return false;
+    }
+    QNetworkReply *reply = mNetworkAccessManager->get(request());
+    connect(reply, &QNetworkReply::finished, this, &GetCommandsJob::slotGetCommandsFinished);
+    addLoggerInfo(QByteArrayLiteral("GetCommandsJob: Ask info about me"));
+    return true;
+}
+
+void GetCommandsJob::slotGetCommandsFinished()
+{
+    auto *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QByteArray data = reply->readAll();
+        const QJsonDocument replyJson = QJsonDocument::fromJson(data);
+        const QJsonObject replyObject = replyJson.object();
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            addLoggerInfo(QByteArrayLiteral("GetCommandsJob: success: ") + replyJson.toJson(QJsonDocument::Indented));
+            Q_EMIT getCommandsDone(replyObject);
+        } else {
+            emitFailedMessage(replyObject, reply);
+            addLoggerWarning(QByteArrayLiteral("GetCommandsJob: problem: ") + replyJson.toJson(QJsonDocument::Indented));
+        }
+        reply->deleteLater();
+    }
+    deleteLater();
+}
+
+QString GetCommandsJob::commandName() const
+{
+    return mCommandName;
+}
+
+void GetCommandsJob::setCommandName(const QString &commandName)
+{
+    mCommandName = commandName;
+}
+
+QNetworkRequest GetCommandsJob::request() const
+{
+    QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::CommandsGet);
+    QUrlQuery queryUrl;
+    queryUrl.addQueryItem(QStringLiteral("command"), mCommandName);
+    addQueryParameter(queryUrl);
+    url.setQuery(queryUrl);
+
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    request.setAttribute(QNetworkRequest::HttpPipeliningAllowedAttribute, true);
+#if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
+    request.setAttribute(QNetworkRequest::HTTP2AllowedAttribute, true);
+#else
+    request.setAttribute(QNetworkRequest::Http2AllowedAttribute, true);
+#endif
+    return request;
+}
+
+bool GetCommandsJob::canStart() const
+{
+    if (mCommandName.isEmpty()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "GetCommandsJob: commandName is empty";
+        return false;
+    }
+    if (!RestApiAbstractJob::canStart()) {
+        return false;
+    }
+    return true;
+}
---
+++ src/rocketchatrestapi-qt5/commands/runcommandjob.cpp
@@ -0,0 +1,128 @@
+/*
+   Copyright (c) 2020 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "runcommandjob.h"
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+using namespace RocketChatRestApi;
+RunCommandJob::RunCommandJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+RunCommandJob::~RunCommandJob()
+{
+}
+
+bool RunCommandJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        return false;
+    }
+    const QByteArray baPostData = json().toJson(QJsonDocument::Compact);
+    addLoggerInfo("RunCommandJob::start: " + baPostData);
+    QNetworkReply *reply = mNetworkAccessManager->post(request(), baPostData);
+    connect(reply, &QNetworkReply::finished, this, &RunCommandJob::slotRunCommand);
+    return true;
+}
+
+void RunCommandJob::slotRunCommand()
+{
+    auto *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QByteArray data = reply->readAll();
+        const QJsonDocument replyJson = QJsonDocument::fromJson(data);
+        const QJsonObject replyObject = replyJson.object();
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            addLoggerInfo(QByteArrayLiteral("RunCommandJob: success: ") + replyJson.toJson(QJsonDocument::Indented));
+            Q_EMIT runCommandDone();
+        } else {
+            emitFailedMessage(replyObject, reply);
+            addLoggerWarning(QByteArrayLiteral("RunCommandJob: Problem: ") + replyJson.toJson(QJsonDocument::Indented));
+        }
+        reply->deleteLater();
+    }
+    deleteLater();
+}
+
+RunCommandJob::RunCommandInfo RunCommandJob::runCommandInfo() const
+{
+    return mRunCommandInfo;
+}
+
+void RunCommandJob::setRunCommandInfo(const RunCommandInfo &runCommandInfo)
+{
+    mRunCommandInfo = runCommandInfo;
+}
+
+bool RunCommandJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool RunCommandJob::canStart() const
+{
+    if (!RestApiAbstractJob::canStart()) {
+        return false;
+    }
+    if (!mRunCommandInfo.isValid()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "RunCommandJob: RoomId and CommandName are empty";
+        return false;
+    }
+    return true;
+}
+
+QNetworkRequest RunCommandJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::CommandsRun);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    addRequestAttribute(request);
+    return request;
+}
+
+QJsonDocument RunCommandJob::json() const
+{
+    QJsonObject jsonObj;
+    jsonObj[QLatin1String("command")] = mRunCommandInfo.commandName;
+    jsonObj[QLatin1String("roomId")] = mRunCommandInfo.roomId;
+    if (!mRunCommandInfo.threadMessageId.isEmpty()) {
+        jsonObj[QLatin1String("tmid")] = mRunCommandInfo.threadMessageId;
+    }
+    if (!mRunCommandInfo.triggerId.isEmpty()) {
+        jsonObj[QLatin1String("triggerId")] = mRunCommandInfo.triggerId;
+    }
+
+    if (!mRunCommandInfo.params.isEmpty()) {
+        //TODO jsonObj[QLatin1String("triggerId")] = mRunCommandInfo.triggerId;
+    }
+    const QJsonDocument postData = QJsonDocument(jsonObj);
+    return postData;
+}
+
+bool RunCommandJob::RunCommandInfo::isValid() const
+{
+    return !commandName.isEmpty() && !roomId.isEmpty();
+}
