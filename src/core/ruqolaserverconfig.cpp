afd518dfa3705e2bceef4a72929d65e97831c30a
Laurent Montel <montel@kde.org>
2020-04-09 22:53:11 +0200

    Remove comment

--- src/core/ruqolaserverconfig.cpp
+++ src/core/ruqolaserverconfig.cpp
@@ -185,7 +185,6 @@ void RuqolaServerConfig::addOauthService(const QString &service)
         //Hide warning as it's not a service
         qCDebug(RUQOLA_LOG) << "_OAuth_Proxy_host found ";
     } else if (serviceLower.endsWith(QLatin1String("_oauth_meteor"))) {
-        //Hide warning as it's not a service
         qCDebug(RUQOLA_LOG) << "Accounts_OAuth_Meteor found ";
     } else {
         qCDebug(RUQOLA_LOG) << "Unknown service type: " << service;
