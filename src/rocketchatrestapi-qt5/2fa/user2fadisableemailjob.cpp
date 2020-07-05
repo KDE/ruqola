43afec485cae08222290dcf9f8477c6a2e68de6e
Laurent Montel <montel@kde.org>
2020-07-05 17:23:26 +0200

    Implement enable/disable 2FA email

---
+++ src/rocketchatrestapi-qt5/2fa/user2fadisableemailjob.cpp
@@ -0,0 +1,96 @@
+/*
+   Copyright (c) 2020 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "user2fadisableemailjob.h"
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+using namespace RocketChatRestApi;
+User2FADisableEmailJob::User2FADisableEmailJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+User2FADisableEmailJob::~User2FADisableEmailJob()
+{
+}
+
+bool User2FADisableEmailJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        return false;
+    }
+    addStartRestApiInfo("User2FADisableEmailJob::start");
+    QNetworkReply *reply = submitPostRequest(json());
+    connect(reply, &QNetworkReply::finished, this, &User2FADisableEmailJob::slotDisableEmail);
+    return true;
+}
+
+void User2FADisableEmailJob::slotDisableEmail()
+{
+    auto *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QByteArray data = reply->readAll();
+        const QJsonDocument replyJson = QJsonDocument::fromJson(data);
+        const QJsonObject replyObject = replyJson.object();
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            addLoggerInfo(QByteArrayLiteral("User2FADisableEmailJob: success: ") + replyJson.toJson(QJsonDocument::Indented));
+            Q_EMIT disableEmailDone();
+        } else {
+            emitFailedMessage(replyObject, reply);
+            addLoggerWarning(QByteArrayLiteral("User2FADisableEmailJob: Problem: ") + replyJson.toJson(QJsonDocument::Indented));
+        }
+        reply->deleteLater();
+    }
+    deleteLater();
+}
+
+bool User2FADisableEmailJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool User2FADisableEmailJob::canStart() const
+{
+    if (!RestApiAbstractJob::canStart()) {
+        return false;
+    }
+    return true;
+}
+
+QNetworkRequest User2FADisableEmailJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::Users2FADisableEmail);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    addRequestAttribute(request);
+    return request;
+}
+
+QJsonDocument User2FADisableEmailJob::json() const
+{
+    QJsonObject jsonObj;
+    const QJsonDocument postData = QJsonDocument(jsonObj);
+    return postData;
+}
---
+++ src/rocketchatrestapi-qt5/autotests/user2faenableemailjobtest.cpp
@@ -0,0 +1,73 @@
+/*
+   Copyright (c) 2020 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "user2faenableemailjobtest.h"
+#include "2fa/user2faenableemailjob.h".h"
+#include "ruqola_restapi_helper.h"
+#include <QTest>
+#include <QJsonDocument>
+QTEST_GUILESS_MAIN(User2FAEnableEmailJobTest)
+using namespace RocketChatRestApi;
+User2FAEnableEmailJobTest::User2FAEnableEmailJobTest(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void User2FAEnableEmailJobTest::shouldHaveDefaultValue()
+{
+    User2FAEnableEmailJob job;
+    verifyDefaultValue(&job);
+    QVERIFY(job.requireHttpAuthentication());
+    QVERIFY(!job.hasQueryParameterSupport());
+}
+
+void User2FAEnableEmailJobTest::shouldGenerateRequest()
+{
+    User2FAEnableEmailJob job;
+    QNetworkRequest request = QNetworkRequest(QUrl());
+    verifyAuthentication(&job, request);
+    QCOMPARE(request.url(), QUrl(QStringLiteral("http://www.kde.org/api/v1/users.2fa.enable-email")));
+    QCOMPARE(request.header(QNetworkRequest::ContentTypeHeader).toString(), QStringLiteral("application/json"));
+}
+
+void User2FAEnableEmailJobTest::shouldGenerateJson()
+{
+    User2FAEnableEmailJob job;
+    QCOMPARE(job.json().toJson(QJsonDocument::Compact), QByteArray("{}"));
+}
+
+void User2FAEnableEmailJobTest::shouldNotStarting()
+{
+    User2FAEnableEmailJob job;
+
+    RestApiMethod method;
+    method.setServerUrl(QStringLiteral("http://www.kde.org"));
+    job.setRestApiMethod(&method);
+
+    QNetworkAccessManager mNetworkAccessManager;
+    job.setNetworkAccessManager(&mNetworkAccessManager);
+    QVERIFY(!job.canStart());
+    const QString auth = QStringLiteral("foo");
+    const QString userId = QStringLiteral("foo");
+    job.setAuthToken(auth);
+    QVERIFY(!job.canStart());
+    job.setUserId(userId);
+    QVERIFY(job.canStart());
+}
