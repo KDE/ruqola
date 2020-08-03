41b33b3ff0a9d2d392d3866030fde666802c04b6
Laurent Montel <montel@kde.org>
2020-08-03 09:00:53 +0200

    Start to implement usersupdatejob

---
+++ src/rocketchatrestapi-qt5/users/usersupdatejob.cpp
@@ -0,0 +1,105 @@
+/*
+   Copyright (c) 2020 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "usersupdatejob.h"
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+using namespace RocketChatRestApi;
+UsersUpdateJob::UsersUpdateJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+UsersUpdateJob::~UsersUpdateJob()
+{
+}
+
+bool UsersUpdateJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        return false;
+    }
+    addStartRestApiInfo("UsersUpdateJob::start");
+    QNetworkReply *reply = submitPostRequest(json());
+    connect(reply, &QNetworkReply::finished, this, &UsersUpdateJob::slotSetStatus);
+    return true;
+}
+
+void UsersUpdateJob::slotSetStatus()
+{
+    auto *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QByteArray data = reply->readAll();
+        const QJsonDocument replyJson = QJsonDocument::fromJson(data);
+        const QJsonObject replyObject = replyJson.object();
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            addLoggerInfo(QByteArrayLiteral("UsersUpdateJob: success: ") + replyJson.toJson(QJsonDocument::Indented));
+            Q_EMIT setStatusDone();
+        } else {
+            emitFailedMessage(replyObject, reply);
+            addLoggerWarning(QByteArrayLiteral("UsersUpdateJob: Problem: ") + replyJson.toJson(QJsonDocument::Indented));
+        }
+        reply->deleteLater();
+    }
+    deleteLater();
+}
+
+bool UsersUpdateJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool UsersUpdateJob::canStart() const
+{
+    if (!RestApiAbstractJob::canStart()) {
+        return false;
+    }
+//    if (mStatusUserId.isEmpty()) {
+//        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "UsersUpdateJob: mUserId is empty";
+//        return false;
+//    }
+//    if (mStatus == UsersUpdateJob::Unknown) {
+//        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "UsersUpdateJob: mStatus is not defined";
+//        return false;
+//    }
+    return true;
+}
+
+QNetworkRequest UsersUpdateJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::UsersSetStatus);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    addRequestAttribute(request);
+    return request;
+}
+
+QJsonDocument UsersUpdateJob::json() const
+{
+    QJsonObject jsonObj;
+
+    const QJsonDocument postData = QJsonDocument(jsonObj);
+    return postData;
+}
