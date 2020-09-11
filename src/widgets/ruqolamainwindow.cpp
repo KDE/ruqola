429e3eb5d36e616d41f77bfe866e5de95ab2d65b
Laurent Montel <montel@kde.org>
2020-09-11 13:26:42 +0200

    Disable saveAs for the moment

--- src/widgets/ruqolamainwindow.cpp
+++ src/widgets/ruqolamainwindow.cpp
@@ -143,7 +143,7 @@ void RuqolaMainWindow::slotRaiseWindow()
 
 void RuqolaMainWindow::changeActionStatus(bool enabled)
 {
-    mSaveAs->setEnabled(enabled);
+    //Laurent disable for the moment mSaveAs->setEnabled(enabled);
 }
 
 void RuqolaMainWindow::updateActions()
