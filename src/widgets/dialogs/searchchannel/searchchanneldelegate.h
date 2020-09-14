478ed16009f7fc47bd08e427dc1eee958bc7bea4
Laurent Montel <montel@kde.org>
2020-09-14 14:01:50 +0200

    Fix sizehint
    (cherry picked from commit c45c9b85da1fb2590abf6d831244d2bc6d358000)

--- src/widgets/dialogs/searchchannel/searchchanneldelegate.h
+++ src/widgets/dialogs/searchchannel/searchchanneldelegate.h
@@ -31,6 +31,7 @@ public:
     ~SearchChannelDelegate() override;
     void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
     bool editorEvent(QEvent *event, QAbstractItemModel *model, const QStyleOptionViewItem &option, const QModelIndex &index) override;
+    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
 
 Q_SIGNALS:
     void channelSelected(const QModelIndex &index);
