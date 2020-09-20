6921a8f5a9cac99a6c4ed915618f18f075c51f99
Laurent Montel <montel@kde.org>
2020-09-20 15:54:14 +0200

    Add credit

--- src/apps/widget/main.cpp
+++ src/apps/widget/main.cpp
@@ -72,6 +72,7 @@ int main(int argc, char *argv[])
     aboutData.addCredit(i18n("Till Adam"), i18n("Windows compile bug fixing"), QStringLiteral("adam@kde.org"));
     aboutData.addCredit(i18n("Shantanu Tushar"), i18n("Bug fixing"), QStringLiteral("shantanu@kde.org"));
     aboutData.addCredit(i18n("Alessandro Ambrosano"), i18n("Bug fixing"), QStringLiteral("alessandro.ambrosano@gmail.com"));
+    aboutData.addCredit(i18n("Hannah von Reth"), i18n("Bug fixing for Windows/Mac"), QStringLiteral("vonreth@kde.org"));
 
     KAboutData::setApplicationData(aboutData);
 
