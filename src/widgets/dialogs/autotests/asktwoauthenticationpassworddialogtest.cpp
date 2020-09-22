abe16ce5e2d27362840d6794c1e1a93214b3479a
Laurent Montel <montel@kde.org>
2020-09-22 13:17:51 +0200

    Start to implement asking two auth password

---
+++ src/widgets/dialogs/autotests/asktwoauthenticationpassworddialogtest.cpp
@@ -0,0 +1,46 @@
+/*
+   Copyright (c) 2020 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "asktwoauthenticationpassworddialogtest.h"
+#include "dialogs/asktwoauthenticationpassworddialog.h"
+#include "dialogs/asktwoauthenticationpasswordwidget.h"
+#include <QDialogButtonBox>
+#include <QTest>
+#include <QVBoxLayout>
+QTEST_MAIN(AskTwoAuthenticationPasswordDialogTest)
+AskTwoAuthenticationPasswordDialogTest::AskTwoAuthenticationPasswordDialogTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+void AskTwoAuthenticationPasswordDialogTest::shouldHaveDefaultValues()
+{
+    AskTwoAuthenticationPasswordDialog w;
+    QVERIFY(!w.windowTitle().isEmpty());
+    auto *mainLayout = w.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+
+    AskTwoAuthenticationPasswordWidget *mAskTwoAuthicationPasswordWidget = w.findChild<AskTwoAuthenticationPasswordWidget *>(QStringLiteral("mAskTwoAuthicationPasswordWidget"));
+    QVERIFY(mAskTwoAuthicationPasswordWidget);
+
+    auto *buttonBox = w.findChild<QDialogButtonBox *>(QStringLiteral("button"));
+    QVERIFY(buttonBox);
+}
