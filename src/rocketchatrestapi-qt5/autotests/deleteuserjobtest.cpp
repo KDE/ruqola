c149bfcf150a6e8822c79566e36aa85c45768176
Laurent Montel <montel@kde.org>
2020-10-07 13:38:43 +0200

    Add delete user job

---
+++ src/rocketchatrestapi-qt5/autotests/deleteuserjobtest.cpp
@@ -0,0 +1,83 @@
+/*
+   Copyright (c) 2020 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "deleteuserjobtest.h"
+#include "users/deleteuserjob.h"
+#include "ruqola_restapi_helper.h"
+#include <QTest>
+#include <QJsonDocument>
+QTEST_GUILESS_MAIN(DeleteUserJobTest)
+using namespace RocketChatRestApi;
+DeleteUserJobTest::DeleteUserJobTest(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void DeleteUserJobTest::shouldHaveDefaultValue()
+{
+    DeleteUserJob job;
+    verifyDefaultValue(&job);
+    QVERIFY(job.requireHttpAuthentication());
+    QVERIFY(!job.hasUserIdentifier());
+    QVERIFY(!job.hasQueryParameterSupport());
+}
+
+void DeleteUserJobTest::shouldGenerateRequest()
+{
+    DeleteUserJob job;
+    QNetworkRequest request = QNetworkRequest(QUrl());
+    verifyAuthentication(&job, request);
+    QCOMPARE(request.url(), QUrl(QStringLiteral("http://www.kde.org/api/v1/users.delete")));
+    QCOMPARE(request.header(QNetworkRequest::ContentTypeHeader).toString(), QStringLiteral("application/json"));
+}
+
+void DeleteUserJobTest::shouldGenerateJson()
+{
+    DeleteUserJob job;
+    UserBaseJob::UserInfo info;
+    info.userIdentifier = QStringLiteral("foo1");
+    info.userInfoType = UserBaseJob::UserInfoType::UserId;
+    job.setUserInfo(info);
+    QCOMPARE(job.json().toJson(QJsonDocument::Compact), QStringLiteral("{\"userId\":\"%1\"}").arg(info.userIdentifier).toLatin1());
+}
+
+void DeleteUserJobTest::shouldNotStarting()
+{
+    DeleteUserJob job;
+
+    RestApiMethod method;
+    method.setServerUrl(QStringLiteral("http://www.kde.org"));
+    job.setRestApiMethod(&method);
+
+    QNetworkAccessManager mNetworkAccessManager;
+    job.setNetworkAccessManager(&mNetworkAccessManager);
+    QVERIFY(!job.canStart());
+    const QString auth = QStringLiteral("foo");
+    const QString userId = QStringLiteral("foo");
+    job.setAuthToken(auth);
+    QVERIFY(!job.canStart());
+    job.setUserId(userId);
+    QVERIFY(!job.canStart());
+    UserBaseJob::UserInfo info;
+    info.userIdentifier = QStringLiteral("foo1");
+    info.userInfoType = UserBaseJob::UserInfoType::UserName;
+    job.setUserInfo(info);
+    QVERIFY(job.canStart());
+}
---
+++ src/rocketchatrestapi-qt5/users/deleteuserjob.cpp
@@ -0,0 +1,101 @@
+/*
+   Copyright (c) 2020 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "deleteuserjob.h"
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+using namespace RocketChatRestApi;
+DeleteUserJob::DeleteUserJob(QObject *parent)
+    : UserBaseJob(parent)
+{
+}
+
+DeleteUserJob::~DeleteUserJob()
+{
+}
+
+bool DeleteUserJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        return false;
+    }
+    addStartRestApiInfo("DeleteUserJob::start");
+    QNetworkReply *reply = submitPostRequest(json());
+    connect(reply, &QNetworkReply::finished, this, &DeleteUserJob::slotDeleteUser);
+    return true;
+}
+
+void DeleteUserJob::slotDeleteUser()
+{
+    auto *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QJsonDocument replyJson = convertToJsonDocument(reply);
+        const QJsonObject replyObject = replyJson.object();
+
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            addLoggerInfo(QByteArrayLiteral("DeleteUserJob: success: ") + replyJson.toJson(QJsonDocument::Indented));
+            Q_EMIT deleteUserDone();
+        } else {
+            emitFailedMessage(replyObject, reply);
+            addLoggerWarning(QByteArrayLiteral("DeleteUserJob: Problem: ") + replyJson.toJson(QJsonDocument::Indented));
+        }
+        reply->deleteLater();
+    }
+    deleteLater();
+}
+
+bool DeleteUserJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool DeleteUserJob::canStart() const
+{
+    if (!RestApiAbstractJob::canStart()) {
+        return false;
+    }
+    if (!hasUserIdentifier()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "DeleteUserJob: identifier is empty";
+        return false;
+    }
+    return true;
+}
+
+QNetworkRequest DeleteUserJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::UsersDelete);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    addRequestAttribute(request);
+    return request;
+}
+
+QJsonDocument DeleteUserJob::json() const
+{
+    QJsonObject jsonObj;
+    generateJson(jsonObj);
+    const QJsonDocument postData = QJsonDocument(jsonObj);
+    return postData;
+}
