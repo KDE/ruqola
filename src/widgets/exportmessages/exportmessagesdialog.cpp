d94332c4b0f3bcca7a4f683f920ad1bd8b7e2eb1
Laurent Montel <montel@kde.org>
2020-11-18 06:58:38 +0100

    Make enable by default

--- src/widgets/exportmessages/exportmessagesdialog.cpp
+++ src/widgets/exportmessages/exportmessagesdialog.cpp
@@ -46,9 +46,9 @@ ExportMessagesDialog::ExportMessagesDialog(QWidget *parent)
     connect(button, &QDialogButtonBox::rejected, this, &ExportMessagesDialog::reject);
     connect(button, &QDialogButtonBox::accepted, this, &ExportMessagesDialog::accept);
 
-    QPushButton *okButton = button->button(QDialogButtonBox::Ok);
-    okButton->setEnabled(false);
-    connect(mExportMessagesWidget, &ExportMessagesWidget::updateOkButton, okButton, &QPushButton::setEnabled);
+//    QPushButton *okButton = button->button(QDialogButtonBox::Ok);
+//    okButton->setEnabled(false);
+//    connect(mExportMessagesWidget, &ExportMessagesWidget::updateOkButton, okButton, &QPushButton::setEnabled);
     readConfig();
 }
 
