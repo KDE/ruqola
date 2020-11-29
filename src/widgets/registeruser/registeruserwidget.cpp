8c7af8eaeaf961d1a342b7f14eb117bcb3065e4b
Laurent Montel <montel@kde.org>
2020-11-29 14:53:26 +0100

    Disable button when we clicked on register

--- src/widgets/registeruser/registeruserwidget.cpp
+++ src/widgets/registeruser/registeruserwidget.cpp
@@ -77,6 +77,7 @@ void RegisterUserWidget::slotUpdateRegisterButton()
 
 void RegisterUserWidget::slotRegisterNewUser()
 {
+    mRegisterButton->setEnabled(false);
     Ruqola::self()->rocketChatAccount()->registerNewUser(registerUserInfo());
 }
 
