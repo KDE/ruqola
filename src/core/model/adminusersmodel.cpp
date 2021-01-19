be2c838605db3b2e31ba21ff3ae790395707c56d
Laurent Montel <montel@kde.org>
2021-01-19 13:46:06 +0100

    Start to implement adminusersmodel

---
+++ src/core/model/adminusersmodel.cpp
@@ -0,0 +1,116 @@
+/*
+   Copyright (c) 2021 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "adminusersmodel.h"
+
+AdminUsersModel::AdminUsersModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+
+}
+
+AdminUsersModel::~AdminUsersModel()
+{
+
+}
+
+int AdminUsersModel::rowCount(const QModelIndex &parent) const
+{
+    if (parent.isValid()) {
+        return 0; // flat model
+    }
+    //return mAdminRooms.count();
+    return 0;
+}
+
+QVariant AdminUsersModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+#if 0
+    if (role == Qt::DisplayRole && orientation == Qt::Horizontal) {
+        switch (static_cast<AdminRoomsRoles>(section)) {
+        case AdminRoomsRoles::Name:
+            return i18n("Name");
+        case AdminRoomsRoles::MessagesCount:
+            return i18n("Number Of Messages");
+        case AdminRoomsRoles::UsersCount:
+            return i18n("Number Of Users");
+        case AdminRoomsRoles::Topic:
+            return i18n("Topic");
+        case AdminRoomsRoles::Identifier:
+            return i18n("Identifier");
+        case AdminRoomsRoles::ReadOnly:
+            return i18n("Read Only");
+        case AdminRoomsRoles::DefaultRoom:
+            return i18n("Default Room");
+        case AdminRoomsRoles::ChannelType:
+            return i18n("Type");
+        case AdminRoomsRoles::ChannelTypeStr:
+            return i18n("Type");
+        }
+    }
+#endif
+    return QVariant();
+}
+
+int AdminUsersModel::columnCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent)
+#if 0
+    return static_cast<int>(AdminRoomsRoles::LastColumn) + 1;
+#else
+    return -1;
+#endif
+}
+
+QVariant AdminUsersModel::data(const QModelIndex &index, int role) const
+{
+#if 0
+    if (index.row() < 0 || index.row() >= mAdminRooms.count()) {
+        return {};
+    }
+    if (role != Qt::DisplayRole) {
+        return {};
+    }
+
+    const AdminRoom &adminroom = mAdminRooms.at(index.row());
+    const int col = index.column();
+    switch (static_cast<AdminRoomsRoles>(col)) {
+    case AdminRoomsRoles::Name:
+        return adminroom.roomName();
+    case AdminRoomsRoles::MessagesCount:
+        return adminroom.messageCount();
+    case AdminRoomsRoles::UsersCount:
+        return adminroom.usersCount();
+    case AdminRoomsRoles::Topic:
+        return adminroom.topic();
+    case AdminRoomsRoles::Identifier:
+        return adminroom.identifier();
+    case AdminRoomsRoles::ReadOnly:
+        return adminroom.readOnly();
+    case AdminRoomsRoles::DefaultRoom:
+        return adminroom.defaultRoom();
+    case AdminRoomsRoles::ChannelType:
+        return adminroom.channelType();
+    case AdminRoomsRoles::ChannelTypeStr:
+        return adminroom.channelTypeStr();
+    }
+#endif
+    return {};
+}
---
+++ src/core/model/adminusersmodel.h
@@ -0,0 +1,42 @@
+/*
+   Copyright (c) 2021 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef ADMINUSERSMODEL_H
+#define ADMINUSERSMODEL_H
+
+#include <QAbstractListModel>
+
+#include "libruqolacore_export.h"
+#include "administrator/adminrooms.h"
+
+class LIBRUQOLACORE_EXPORT AdminUsersModel : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    explicit AdminUsersModel(QObject *parent = nullptr);
+    ~AdminUsersModel() override;
+
+    Q_REQUIRED_RESULT int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    Q_REQUIRED_RESULT QVariant data(const QModelIndex &index, int role) const override;
+    Q_REQUIRED_RESULT QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+    Q_REQUIRED_RESULT int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+};
+
+#endif // ADMINUSERSMODEL_H
