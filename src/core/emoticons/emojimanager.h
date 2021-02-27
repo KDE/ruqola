7e228f34e1c4bae63c6f3243b2f34b3f2e3440d0
Laurent Montel <montel@kde.org>
2021-02-27 16:06:55 +0100

    Add missing Q_REQUIRED_RESULT

--- src/core/emoticons/emojimanager.h
+++ src/core/emoticons/emojimanager.h
@@ -51,8 +51,8 @@ public:
 
     Q_REQUIRED_RESULT UnicodeEmoticon unicodeEmoticonForEmoji(const QString &emojiIdentifier) const;
 
-    QString customEmojiFileName(const QString &emojiIdentifier) const;
-    QString normalizedReactionEmoji(const QString &emojiIdentifier) const;
+    Q_REQUIRED_RESULT QString customEmojiFileName(const QString &emojiIdentifier) const;
+    Q_REQUIRED_RESULT QString normalizedReactionEmoji(const QString &emojiIdentifier) const;
 
 private:
     Q_DISABLE_COPY(EmojiManager)
