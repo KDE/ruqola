ebcb2e90153b2f5851e8ae91a8c306570edf75e2
Laurent Montel <montel@kde.org>
2021-03-09 07:34:13 +0100

    Add channelcompleterfilterproxymodel

---
+++ src/core/model/channelcompleterfilterproxymodel.h
@@ -0,0 +1,37 @@
+/*
+   Copyright (c) 2021 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#pragma once
+
+#include "libruqolacore_export.h"
+#include <QSortFilterProxyModel>
+
+class LIBRUQOLACORE_EXPORT ChannelCompleterFilterProxyModel : public QSortFilterProxyModel
+{
+    Q_OBJECT
+public:
+    explicit ChannelCompleterFilterProxyModel(QObject *parent = nullptr);
+    ~ChannelCompleterFilterProxyModel() override;
+
+    void clear();
+
+protected:
+    bool lessThan(const QModelIndex &left, const QModelIndex &right) const override;
+};
