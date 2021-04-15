9213ae82eccd6c75a7e9a99b667d0fe58f5c465d
Laurent Montel <montel@kde.org>
2021-04-15 13:31:08 +0200

    Add teamremoveroomjob

---
+++ src/rocketchatrestapi-qt5/teams/teamremoveroomjob.cpp
@@ -0,0 +1,126 @@
+/*
+   Copyright (c) 2021 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "teamremoveroomjob.h"
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+using namespace RocketChatRestApi;
+TeamRemoveRoomJob::TeamRemoveRoomJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+TeamRemoveRoomJob::~TeamRemoveRoomJob()
+{
+}
+
+bool TeamRemoveRoomJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        return false;
+    }
+    addStartRestApiInfo("TeamRemoveRoomJob::start");
+    QNetworkReply *reply = submitPostRequest(json());
+    connect(reply, &QNetworkReply::finished, this, &TeamRemoveRoomJob::slotRemoveTeamRoomFinished);
+    return true;
+}
+
+void TeamRemoveRoomJob::slotRemoveTeamRoomFinished()
+{
+    auto reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QJsonDocument replyJson = convertToJsonDocument(reply);
+        const QJsonObject replyObject = replyJson.object();
+
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            addLoggerInfo(QByteArrayLiteral("TeamRemoveRoomJob success: ") + replyJson.toJson(QJsonDocument::Indented));
+            Q_EMIT removeTeamRoomDone();
+        } else {
+            emitFailedMessage(replyObject, reply);
+            addLoggerWarning(QByteArrayLiteral("TeamRemoveRoomJob: Problem: ") + replyJson.toJson(QJsonDocument::Indented));
+        }
+        reply->deleteLater();
+    }
+    deleteLater();
+}
+
+QString TeamRemoveRoomJob::teamId() const
+{
+    return mTeamId;
+}
+
+void TeamRemoveRoomJob::setTeamId(const QString &teamId)
+{
+    mTeamId = teamId;
+}
+
+QString TeamRemoveRoomJob::roomId() const
+{
+    return mRoomId;
+}
+
+void TeamRemoveRoomJob::setRoomId(const QString &roomId)
+{
+    mRoomId = roomId;
+}
+
+bool TeamRemoveRoomJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool TeamRemoveRoomJob::canStart() const
+{
+    if (!RestApiAbstractJob::canStart()) {
+        return false;
+    }
+    if (mRoomId.isEmpty()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "TeamRemoveRoomJob: mRoomId is empty";
+        return false;
+    }
+    if (mTeamId.isEmpty()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "TeamRemoveRoomJob: mTeamId is empty";
+        return false;
+    }
+    return true;
+}
+
+QNetworkRequest TeamRemoveRoomJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::TeamsRemoveRoom);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    addRequestAttribute(request);
+    return request;
+}
+
+QJsonDocument TeamRemoveRoomJob::json() const
+{
+    QJsonObject jsonObj;
+    jsonObj[QLatin1String("roomId")] = mRoomId;
+    jsonObj[QLatin1String("teamId")] = mTeamId;
+    const QJsonDocument postData = QJsonDocument(jsonObj);
+    return postData;
+}
