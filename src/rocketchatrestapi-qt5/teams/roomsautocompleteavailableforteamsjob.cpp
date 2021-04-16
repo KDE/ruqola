265995ac3fc2e762589216ab28dbcb8f6900e849
Laurent Montel <montel@kde.org>
2021-04-16 08:43:43 +0200

    Start to implement RoomsAutocompleteAvailableForTeamsJob

---
+++ src/rocketchatrestapi-qt5/teams/roomsautocompleteavailableforteamsjob.cpp
@@ -0,0 +1,105 @@
+/*
+   Copyright (c) 2021 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "roomsautocompleteavailableforteamsjob.h"
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+#include <QUrlQuery>
+using namespace RocketChatRestApi;
+RoomsAutocompleteAvailableForTeamsJob::RoomsAutocompleteAvailableForTeamsJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+RoomsAutocompleteAvailableForTeamsJob::~RoomsAutocompleteAvailableForTeamsJob()
+{
+}
+
+bool RoomsAutocompleteAvailableForTeamsJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool RoomsAutocompleteAvailableForTeamsJob::start()
+{
+    if (!canStart()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "Impossible to start RoomsAutocompleteAvailableForTeamsJob job";
+        deleteLater();
+        return false;
+    }
+    QNetworkReply *reply = submitGetRequest();
+    connect(reply, &QNetworkReply::finished, this, &RoomsAutocompleteAvailableForTeamsJob::slotRoomsAutoCompleteChannelAndPrivateFinished);
+    addStartRestApiInfo(QByteArrayLiteral("RoomsAutocompleteAvailableForTeamsJob: Ask all rooms"));
+    return true;
+}
+
+void RoomsAutocompleteAvailableForTeamsJob::slotRoomsAutoCompleteChannelAndPrivateFinished()
+{
+    auto reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QJsonDocument replyJson = convertToJsonDocument(reply);
+        const QJsonObject replyObject = replyJson.object();
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            addLoggerInfo(QByteArrayLiteral("RoomsAutocompleteAvailableForTeamsJob: success: ") + replyJson.toJson(QJsonDocument::Indented));
+            Q_EMIT roomsAutoCompleteChannelAndPrivateDone(replyObject);
+        } else {
+            emitFailedMessage(replyObject, reply);
+            addLoggerWarning(QByteArrayLiteral("RoomsAutocompleteAvailableForTeamsJob: Problem: ") + replyJson.toJson(QJsonDocument::Indented));
+        }
+        reply->deleteLater();
+    }
+    deleteLater();
+}
+
+QNetworkRequest RoomsAutocompleteAvailableForTeamsJob::request() const
+{
+    QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::RoomsAutocompleteChannelAndPrivate);
+    QUrlQuery queryUrl;
+    // TODO add support for exception.
+    // const QString val = QStringLiteral("{\"term\": \"%1\"}").arg(mUsersCompleterInfo.pattern);
+    const QString val = QStringLiteral("{\"name\": \"%1\"}").arg(mRoomsAutocompleteInfo.name);
+    queryUrl.addQueryItem(QStringLiteral("selector"), val);
+    url.setQuery(queryUrl);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    addRequestAttribute(request);
+    qDebug() << " url " << url;
+    return request;
+}
+
+RoomsAutocompleteAvailableForTeamsJob::RoomsAutocompleteChannelAndPrivateInfo RoomsAutocompleteAvailableForTeamsJob::roomsCompleterInfo() const
+{
+    return mRoomsAutocompleteInfo;
+}
+
+void RoomsAutocompleteAvailableForTeamsJob::setRoomsCompleterInfo(
+    const RoomsAutocompleteAvailableForTeamsJob::RoomsAutocompleteChannelAndPrivateInfo &roomCompleterInfo)
+{
+    mRoomsAutocompleteInfo = roomCompleterInfo;
+}
+
+bool RoomsAutocompleteAvailableForTeamsJob::RoomsAutocompleteChannelAndPrivateInfo::isValid() const
+{
+    // FIXME
+    return !name.isEmpty();
+}
