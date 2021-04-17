9515ff2405cfb0024fdb98ee240dc8b8bced136a
Laurent Montel <montel@kde.org>
2021-04-17 10:56:54 +0200

    Start to add teamsearchroomforteamwidget

---
+++ src/widgets/teams/teamsearchroomforteamwidget.cpp
@@ -0,0 +1,91 @@
+/*
+   Copyright (c) 2021 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "teamsearchroomforteamwidget.h"
+#include "common/flowlayout.h"
+//#include "misc/clickableuserwidget.h"
+#include <QVBoxLayout>
+
+TeamSearchRoomForTeamWidget::TeamSearchRoomForTeamWidget(QWidget *parent)
+    : QWidget(parent)
+    //    , mSearchUserLineEdit(new AddUsersCompletionLineEdit(this))
+    , mFlowLayout(new FlowLayout)
+{
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(QStringLiteral("mainLayout"));
+    mainLayout->setContentsMargins({});
+
+    //    mSearchUserLineEdit->setObjectName(QStringLiteral("mSearchUserLineEdit"));
+    //    connect(mSearchUserLineEdit, &AddUsersCompletionLineEdit::newUserName, this, &TeamSearchRoomForTeamWidget::slotAddNewName);
+    //    mainLayout->addWidget(mSearchUserLineEdit);
+
+    mFlowLayout->setObjectName(QStringLiteral("mFlowLayout"));
+    mainLayout->addLayout(mFlowLayout);
+}
+
+TeamSearchRoomForTeamWidget::~TeamSearchRoomForTeamWidget()
+{
+    delete mFlowLayout;
+}
+/*
+void TeamSearchRoomForTeamWidget::slotAddNewName(const AddUsersCompletionLineEdit::UserCompletionInfo &info)
+{
+    const QString &userName = info.username;
+    if (mMap.contains(userName)) {
+        return;
+    }
+    auto clickableUserWidget = new ClickableUserWidget(userName, this);
+    clickableUserWidget->setUserId(info.userId);
+    connect(clickableUserWidget, &ClickableUserWidget::removeUser, this, &TeamSearchRoomForTeamWidget::slotRemoveUser);
+    mFlowLayout->addWidget(clickableUserWidget);
+    mMap.insert(userName, clickableUserWidget);
+    Q_EMIT userListChanged(!mMap.isEmpty());
+}
+
+void TeamSearchRoomForTeamWidget::slotRemoveUser(const QString &username)
+{
+    ClickableUserWidget *userWidget = mMap.value(username);
+    if (userWidget) {
+        const int index = mFlowLayout->indexOf(userWidget);
+        if (index != -1) {
+            delete mFlowLayout->takeAt(index);
+            mMap.remove(username);
+            delete userWidget;
+        }
+    }
+    Q_EMIT userListChanged(!mMap.isEmpty());
+}
+*/
+
+void TeamSearchRoomForTeamWidget::setPlaceholderText(const QString &str)
+{
+    //    mSearchUserLineEdit->setPlaceholderText(str);
+}
+
+QString TeamSearchRoomForTeamWidget::placeHolderText() const
+{
+    //  return mSearchUserLineEdit->placeholderText();
+    return {};
+}
+
+QStringList TeamSearchRoomForTeamWidget::roomIds() const
+{
+    return {};
+}
---
+++ src/widgets/teams/teamsearchroomforteamwidget.h
@@ -0,0 +1,51 @@
+/*
+   Copyright (c) 2021 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#pragma once
+
+// #include "dialogs/adduserscompletionlineedit.h"
+#include "libruqolawidgets_private_export.h"
+#include <QMap>
+#include <QWidget>
+class FlowLayout;
+// class ClickableUserWidget;
+class LIBRUQOLAWIDGETS_TESTS_EXPORT TeamSearchRoomForTeamWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit TeamSearchRoomForTeamWidget(QWidget *parent = nullptr);
+    ~TeamSearchRoomForTeamWidget() override;
+
+    void setPlaceholderText(const QString &str);
+
+    Q_REQUIRED_RESULT QString placeHolderText() const;
+
+    Q_REQUIRED_RESULT QStringList roomIds() const;
+Q_SIGNALS:
+    void textChanged(const QString &str);
+    void userListChanged(bool isNotEmpty);
+
+private:
+    //    void slotRemoveUser(const QString &username);
+    //    void slotAddNewName(const AddUsersCompletionLineEdit::UserCompletionInfo &info);
+    //    AddUsersCompletionLineEdit *const mSearchUserLineEdit;
+    FlowLayout *const mFlowLayout;
+    //    QMap<QString, ClickableUserWidget *> mMap;
+};
