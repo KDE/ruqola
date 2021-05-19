9743bc5fa533a6b6120688bf7f232771726da500
Laurent Montel <montel@kde.org>
2021-05-19 22:11:30 +0200

    Start to implement "directory" support

---
+++ src/widgets/directory/directorydialog.h
@@ -0,0 +1,34 @@
+/*
+   Copyright (c) 2021 Laurent Montel <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+#pragma once
+
+#include <QDialog>
+
+#include "libruqolawidgets_private_export.h"
+class DirectoryWidget;
+class LIBRUQOLAWIDGETS_TESTS_EXPORT DirectoryDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit DirectoryDialog(QWidget *parent = nullptr);
+    ~DirectoryDialog() override;
+
+private:
+    DirectoryWidget *const mDirectoryWidget;
+};
