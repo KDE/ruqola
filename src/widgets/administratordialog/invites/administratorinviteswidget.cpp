4e00de19f08c48d1538f1d5fbe91ad9f105557c8
Laurent Montel <montel@kde.org>
2021-05-31 08:16:47 +0200

    Reserve element

--- src/widgets/administratordialog/invites/administratorinviteswidget.cpp
+++ src/widgets/administratordialog/invites/administratorinviteswidget.cpp
@@ -84,7 +84,9 @@ void AdministratorInvitesWidget::slotListInviteDone(const QJsonDocument &obj)
 {
     QVector<InviteInfo> lstInvite;
     const QJsonArray array = obj.array();
-    for (int i = 0; i < array.count(); ++i) {
+    const int arrayCount{array.count()};
+    lstInvite.reserve(arrayCount);
+    for (int i = 0; i < arrayCount; ++i) {
         const QJsonObject o = array.at(i).toObject();
         InviteInfo invite;
         invite.parseInviteInfo(o);
