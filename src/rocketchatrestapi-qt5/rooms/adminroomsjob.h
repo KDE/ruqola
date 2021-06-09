b49462bc47f5292041e7fe2dc44fd028d460e484
Laurent Montel <montel@kde.org>
2021-06-09 09:02:28 +0200

    Add missing ','

--- src/rocketchatrestapi-qt5/rooms/adminroomsjob.h
+++ src/rocketchatrestapi-qt5/rooms/adminroomsjob.h
@@ -30,7 +30,15 @@ class LIBROCKETCHATRESTAPI_QT5_EXPORT AdminRoomsJob : public RestApiAbstractJob
 {
     Q_OBJECT
 public:
-    enum class AdminRoomSearchType { None = 0, Direct = 1, Public = 2, Private = 4, Channel = 8, Team = 16, All = Direct | Private | Channel | Team };
+    enum class AdminRoomSearchType {
+        None = 0,
+        Direct = 1,
+        Public = 2,
+        Private = 4,
+        Channel = 8,
+        Team = 16,
+        All = Direct | Private | Channel | Team,
+    };
     Q_DECLARE_FLAGS(AdminRoomSearchTypes, AdminRoomSearchType)
     Q_FLAG(AdminRoomSearchType)
     struct LIBROCKETCHATRESTAPI_QT5_EXPORT AdminRoomsJobInfo {
