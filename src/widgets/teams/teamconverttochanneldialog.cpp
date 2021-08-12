57849854b9a1cce4179d54bec13537d94b16d52a
Laurent Montel <montel@kde.org>
2021-08-12 13:43:19 +0200

    Start to create "convert team to channel" support

---
+++ src/widgets/teams/teamconverttochanneldialog.cpp
@@ -0,0 +1,47 @@
+/*
+   Copyright (c) 2021 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "teamconverttochanneldialog.h"
+#include "teamconverttochannelwidget.h"
+#include <KLocalizedString>
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+
+TeamConvertToChannelDialog::TeamConvertToChannelDialog(QWidget *parent)
+    : QDialog(parent)
+    , mTeamConvertToChannelWidget(new TeamConvertToChannelWidget(this))
+{
+    setWindowTitle(i18nc("@title:window", "Convert Team to Channel"));
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(QStringLiteral("mainLayout"));
+
+    mTeamConvertToChannelWidget->setObjectName(QStringLiteral("mTeamConvertToChannelWidget"));
+    mainLayout->addWidget(mTeamConvertToChannelWidget);
+
+    auto button = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
+    button->setObjectName(QStringLiteral("button"));
+    mainLayout->addWidget(button);
+    connect(button, &QDialogButtonBox::rejected, this, &TeamConvertToChannelDialog::reject);
+    connect(button, &QDialogButtonBox::accepted, this, &TeamConvertToChannelDialog::accept);
+}
+
+TeamConvertToChannelDialog::~TeamConvertToChannelDialog()
+{
+}
