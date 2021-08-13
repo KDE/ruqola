bf4b85212fa9cf3c6f4d64deaf7a80cce44ebaed
Laurent Montel <montel@kde.org>
2021-08-13 13:50:25 +0200

    Add autotest

--- src/widgets/teams/autotests/teamconverttochanneldialogtest.cpp
+++ src/widgets/teams/autotests/teamconverttochanneldialogtest.cpp
@@ -21,15 +21,28 @@
 
 #include "teamconverttochanneldialogtest.h"
 #include "teams/teamconverttochanneldialog.h"
+#include "teams/teamconverttochannelwidget.h"
+#include <QDialogButtonBox>
+#include <QStandardPaths>
 #include <QTest>
+#include <QVBoxLayout>
 QTEST_MAIN(TeamConvertToChannelDialogTest)
 TeamConvertToChannelDialogTest::TeamConvertToChannelDialogTest(QObject *parent)
     : QObject(parent)
 {
+    QStandardPaths::setTestModeEnabled(true);
 }
 
 void TeamConvertToChannelDialogTest::shouldHaveDefaultValues()
 {
     TeamConvertToChannelDialog d;
-    // TODO
+    QVERIFY(!d.windowTitle().isEmpty());
+
+    auto mainLayout = d.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+
+    auto button = d.findChild<QDialogButtonBox *>(QStringLiteral("button"));
+    QVERIFY(button);
+    auto mTeamConvertToChannelWidget = d.findChild<TeamConvertToChannelWidget *>(QStringLiteral("mTeamConvertToChannelWidget"));
+    QVERIFY(mTeamConvertToChannelWidget);
 }
