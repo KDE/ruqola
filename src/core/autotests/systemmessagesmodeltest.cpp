5a140556da80a906958c1961f3d89342c9600580
Laurent Montel <montel@kde.org>
2021-09-03 13:54:28 +0200

    improve autotest

--- src/core/autotests/systemmessagesmodeltest.cpp
+++ src/core/autotests/systemmessagesmodeltest.cpp
@@ -32,5 +32,36 @@ void SystemMessagesModelTest::shouldHaveDefaultValues()
     SystemMessagesModel w;
     QCOMPARE(w.rowCount(), 18);
     QVERIFY(w.systemMessagesSelected().isEmpty());
-    // TODO
+    QStringList lst;
+    const int rowCountNb = w.rowCount();
+    // First one is not a message type
+    for (int i = 1; i < rowCountNb; i++) {
+        QStandardItem *itemModel = w.item(i);
+        if (itemModel) {
+            lst.append(itemModel->data(SystemMessagesModel::SystemMessages).toString());
+        }
+    }
+    QStringList lstRoles{QStringLiteral("au"),
+                         QStringLiteral("wm"),
+                         QStringLiteral("ut"),
+                         QStringLiteral("ul"),
+                         QStringLiteral("uj"),
+                         QStringLiteral("subscription-role-removed"),
+                         QStringLiteral("subscription-role-added"),
+                         QStringLiteral("ru"),
+                         QStringLiteral("room_unarchived"),
+                         QStringLiteral("room_disabled_encryption"),
+                         QStringLiteral("enabled_encryption"),
+                         QStringLiteral("room_changed_privacy"),
+                         QStringLiteral("room_changed_avatar"),
+                         QStringLiteral("mute_unmute"),
+                         QStringLiteral("r"),
+                         QStringLiteral("rm"),
+                         QStringLiteral("room_archived")};
+
+    for (const QString &r : std::as_const(lst)) {
+        QVERIFY(lstRoles.contains(r));
+        lstRoles.removeAll(r);
+    }
+    QVERIFY(lstRoles.isEmpty());
 }
