e364e4e70fec49594235e8a2a287bc15ba41b828
Laurent Montel <montel@kde.org>
2021-09-24 13:32:17 +0200

    Add support for search string

--- src/rocketchatrestapi-qt5/autotests/customsoundslistjobtest.cpp
+++ src/rocketchatrestapi-qt5/autotests/customsoundslistjobtest.cpp
@@ -44,16 +44,43 @@ void CustomSoundsListJobTest::shouldHaveDefaultValue()
 
 void CustomSoundsListJobTest::shouldGenerateRequest()
 {
-    CustomSoundsListJob job;
-    const QString authToken = QStringLiteral("foo");
-    const QString userId = QStringLiteral("user");
-    job.setUserId(userId);
-    job.setAuthToken(authToken);
-    RestApiMethod method;
-    method.setServerUrl(QStringLiteral("http://www.kde.org"));
-    job.setRestApiMethod(&method);
-    const QNetworkRequest request = job.request();
-    QCOMPARE(request.url(), QUrl(QStringLiteral("http://www.kde.org/api/v1/custom-sounds.list")));
-    QCOMPARE(request.rawHeader(QByteArrayLiteral("X-Auth-Token")), authToken.toLocal8Bit());
-    QCOMPARE(request.rawHeader(QByteArrayLiteral("X-User-Id")), userId.toLocal8Bit());
+    {
+        CustomSoundsListJob job;
+        const QString authToken = QStringLiteral("foo");
+        const QString userId = QStringLiteral("user");
+        job.setUserId(userId);
+        job.setAuthToken(authToken);
+        RestApiMethod method;
+        method.setServerUrl(QStringLiteral("http://www.kde.org"));
+        job.setRestApiMethod(&method);
+        const QNetworkRequest request = job.request();
+        QCOMPARE(request.url(), QUrl(QStringLiteral("http://www.kde.org/api/v1/custom-sounds.list")));
+        QCOMPARE(request.rawHeader(QByteArrayLiteral("X-Auth-Token")), authToken.toLocal8Bit());
+        QCOMPARE(request.rawHeader(QByteArrayLiteral("X-User-Id")), userId.toLocal8Bit());
+    }
+    {
+        CustomSoundsListJob job;
+        const QString authToken = QStringLiteral("foo");
+        const QString userId = QStringLiteral("user");
+        job.setUserId(userId);
+        job.setAuthToken(authToken);
+        RestApiMethod method;
+        method.setServerUrl(QStringLiteral("http://www.kde.org"));
+        job.setRestApiMethod(&method);
+
+        QMap<QString, RocketChatRestApi::QueryParameters::SortOrder> map;
+        map.insert(QStringLiteral("name"), RocketChatRestApi::QueryParameters::SortOrder::Ascendant);
+        RocketChatRestApi::QueryParameters parameters;
+        parameters.setSorting(map);
+        parameters.setOffset(1);
+        parameters.setCount(3);
+        parameters.setSearchString(QStringLiteral("bla"));
+
+        job.setQueryParameters(parameters);
+        const QNetworkRequest request = job.request();
+        QCOMPARE(request.url().query(),
+                 QStringLiteral("count=3&offset=1&query=%7B%22name%22:%7B%22$regex%22:%22bla%22,%22$options%22:%22i%22%7D%7D&sort=%7B%22name%22:1%7D"));
+        QCOMPARE(request.rawHeader(QByteArrayLiteral("X-Auth-Token")), authToken.toLocal8Bit());
+        QCOMPARE(request.rawHeader(QByteArrayLiteral("X-User-Id")), userId.toLocal8Bit());
+    }
 }
