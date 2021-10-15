c57199e90bb6b7de41de9138dfcf68656fb0e15e
Laurent Montel <montel@kde.org>
2021-10-15 13:21:43 +0200

    Add licenseslist job

---
+++ src/rocketchatrestapi-qt5/license/licenseslistjob.cpp
@@ -0,0 +1,82 @@
+/*
+   Copyright (c) 2021 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License or
+   ( at your option ) version 3 or, at the discretion of KDE e.V.
+   ( which shall act as a proxy as in section 14 of the GPLv3 ), any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "licenseslistjob.h"
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+using namespace RocketChatRestApi;
+LicensesListJob::LicensesListJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+LicensesListJob::~LicensesListJob()
+{
+}
+
+bool LicensesListJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool LicensesListJob::start()
+{
+    if (!canStart()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "Impossible to start LicensesListJob";
+        deleteLater();
+        return false;
+    }
+    QNetworkReply *reply = submitGetRequest();
+    connect(reply, &QNetworkReply::finished, this, &LicensesListJob::slotLicensesListFinished);
+    addStartRestApiInfo(QByteArrayLiteral("LicensesListJob: Ask for server statistics"));
+    return true;
+}
+
+void LicensesListJob::slotLicensesListFinished()
+{
+    auto reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply) {
+        const QJsonDocument replyJson = convertToJsonDocument(reply);
+        const QJsonObject replyObject = replyJson.object();
+
+        if (replyObject[QStringLiteral("success")].toBool()) {
+            addLoggerInfo(QByteArrayLiteral("LicensesListJob: success: ") + replyJson.toJson(QJsonDocument::Indented));
+            Q_EMIT licensesListDone(replyObject);
+        } else {
+            emitFailedMessage(replyObject, reply);
+            addLoggerWarning(QByteArrayLiteral("LicensesListJob: Problem: ") + replyJson.toJson(QJsonDocument::Indented));
+        }
+        reply->deleteLater();
+    }
+    deleteLater();
+}
+
+QNetworkRequest LicensesListJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::LicensesGet);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    addRequestAttribute(request, false);
+
+    return request;
+}
