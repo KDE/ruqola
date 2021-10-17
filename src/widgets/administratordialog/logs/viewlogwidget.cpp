63aa235148e9161142a95842c76fa5976d0e8c65
Laurent Montel <montel@kde.org>
2021-10-17 13:21:42 +0200

    GIT_SILENT: remove unused comment

--- src/widgets/administratordialog/logs/viewlogwidget.cpp
+++ src/widgets/administratordialog/logs/viewlogwidget.cpp
@@ -95,41 +95,4 @@ void ViewLogWidget::slotStdoutQueueDone(const QJsonObject &obj)
         mStdoutIdentifier = rcAccount->ddp()->subscribe(QStringLiteral("stream-stdout"), params);
     }
     connect(rcAccount, &RocketChatAccount::insertStdOutInfo, this, &ViewLogWidget::slotInsertStdOutInfo);
-    // Need to update it.
 }
-
-/* RC use this code:
-
-  const foregroundColors = {
-        30: 'gray',
-        31: 'red',
-        32: 'lime',
-        33: 'yellow',
-        34: '#6B98FF',
-        35: '#FF00FF',
-        36: 'cyan',
-        37: 'white',
-};
-
-const ansispan = (str) => {
-        str = str
-                .replace(/\s/g, '&nbsp;')
-                .replace(/(\\n|\n)/g, '<br>')
-                .replace(/>/g, '&gt;')
-                .replace(/</g, '&lt;')
-                .replace(/(.\d{8}-\d\d:\d\d:\d\d\.\d\d\d\(?.{0,2}\)?)/, '<span>$1</span>')
-                .replace(/\033\[1m/g, '<strong>')
-                .replace(/\033\[22m/g, '</strong>')
-                .replace(/\033\[3m/g, '<em>')
-                .replace(/\033\[23m/g, '</em>')
-                .replace(/\033\[m/g, '</span>')
-                .replace(/\033\[0m/g, '</span>')
-                .replace(/\033\[39m/g, '</span>');
-        return Object.entries(foregroundColors).reduce((str, [ansiCode, color]) => {
-                const span = `<span style="color: ${color}">`;
-                return str
-                        .replace(new RegExp(`\\033\\[${ansiCode}m`, 'g'), span)
-                        .replace(new RegExp(`\\033\\[0;${ansiCode}m`, 'g'), span);
-        }, str);
-};
-*/
