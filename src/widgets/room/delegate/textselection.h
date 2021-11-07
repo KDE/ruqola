83fb511ac3ddb0215ef5f21dd60cc3ce49b8d00e
Laurent Montel <montel@kde.org>
2021-11-07 22:17:42 +0100

    GIT_SILENT: minor coding style

--- src/widgets/room/delegate/textselection.h
+++ src/widgets/room/delegate/textselection.h
@@ -46,7 +46,10 @@ class LIBRUQOLAWIDGETS_TESTS_EXPORT TextSelection : public QObject
 public:
     explicit TextSelection(DocumentFactoryInterface *factory);
     Q_REQUIRED_RESULT bool hasSelection() const;
-    enum Format { Text, Html };
+    enum Format {
+        Text,
+        Html,
+    };
     Q_REQUIRED_RESULT QString selectedText(Format format) const;
     Q_REQUIRED_RESULT bool contains(const QModelIndex &index, int charPos) const;
     Q_REQUIRED_RESULT QTextCursor selectionForIndex(const QModelIndex &index, QTextDocument *doc) const;
