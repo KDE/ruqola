d2025740d7b5a0790060aae4c24025fe8ea54f96
Laurent Montel <montel@kde.org>
2021-11-30 07:22:48 +0100

    GIt_SILENT: FIX some reuse lint warning

--- scripts/ruqola-log.sh
+++ scripts/ruqola-log.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: 2018 Montel Laurent <montel@kde.org> 
 export RUQOLA_LOGFILE=log_ruqola_file
 ruqola &
--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,6 @@
 #! /usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: 2018 Montel Laurent <montel@kde.org> 
+
 $EXTRACTRC `find . -name "*.ui" -o -name "*.rc"` >> rc.cpp
 $XGETTEXT `find . -name "*.cpp"` -o $podir/ruqola.pot
