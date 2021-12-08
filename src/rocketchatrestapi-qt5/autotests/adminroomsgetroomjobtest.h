b91277fbc9b14ece6e12192bbf6280b079d1dae0
Laurent Montel <montel@kde.org>
2021-12-08 08:16:59 +0100

    Add adminroomsgetroomjob

---
+++ src/rocketchatrestapi-qt5/autotests/adminroomsgetroomjobtest.h
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+
+class AdminRoomsGetRoomJobTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit AdminRoomsGetRoomJobTest(QObject *parent = nullptr);
+    ~AdminRoomsGetRoomJobTest() override = default;
+private Q_SLOTS:
+    void shouldHaveDefaultValue();
+    void shouldGenerateRequest();
+};
