d2a389891c8b874b3eb211b55309dc625dbbfd73
Laurent Montel <montel@kde.org>
2021-12-10 17:01:43 +0100

    Revert "GIT_SILENT: Time to increase version" (fix bad commit in 1.6)
    This reverts commit 26c8df1f00ceb459faea753cf6357cb71a9558c4.

--- src/widgets/myaccount/myaccountprofileconfigurewidget.cpp
+++ src/widgets/myaccount/myaccountprofileconfigurewidget.cpp
@@ -19,6 +19,7 @@
 #include <QLineEdit>
 #include <QPointer>
 #include <QPushButton>
+#include <kwidgetsaddons_version.h>
 
 MyAccountProfileConfigureWidget::MyAccountProfileConfigureWidget(RocketChatAccount *account, QWidget *parent)
     : QWidget(parent)
@@ -96,7 +97,9 @@ void MyAccountProfileConfigureWidget::slotDeleteMyAccount()
                                       KStandardGuiItem::del(),
                                       KStandardGuiItem::cancel())) {
         QPointer<KPasswordDialog> dlg = new KPasswordDialog(this);
+#if KWIDGETSADDONS_VERSION >= QT_VERSION_CHECK(5, 84, 0)
         dlg->setRevealPasswordAvailable(KAuthorized::authorize(QStringLiteral("lineedit_reveal_password")));
+#endif
         dlg->setPrompt(i18n("Current Password"));
         if (dlg->exec()) {
             mRocketChatAccount->deleteOwnAccount(dlg->password());
