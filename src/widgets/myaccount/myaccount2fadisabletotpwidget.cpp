230260be958fc7b0cdcaded245ad12989aaf97b5
Laurent Montel <montel@kde.org>
2021-12-17 08:50:06 +0100

    Add layout

--- src/widgets/myaccount/myaccount2fadisabletotpwidget.cpp
+++ src/widgets/myaccount/myaccount2fadisabletotpwidget.cpp
@@ -5,10 +5,15 @@
 */
 
 #include "myaccount2fadisabletotpwidget.h"
+#include <KLocalizedString>
+#include <QVBoxLayout>
 
 MyAccount2FaDisableTotpWidget::MyAccount2FaDisableTotpWidget(QWidget *parent)
     : QWidget{parent}
 {
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(QStringLiteral("mainLayout"));
+    mainLayout->setContentsMargins({});
 }
 
 MyAccount2FaDisableTotpWidget::~MyAccount2FaDisableTotpWidget()
