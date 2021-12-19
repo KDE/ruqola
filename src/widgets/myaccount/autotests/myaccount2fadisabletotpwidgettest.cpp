f8e3573c1714b7a0211d411af525e2ea1aac8dfc
Laurent Montel <montel@kde.org>
2021-12-19 22:03:17 +0100

    Add autotest

--- src/widgets/myaccount/autotests/myaccount2fadisabletotpwidgettest.cpp
+++ src/widgets/myaccount/autotests/myaccount2fadisabletotpwidgettest.cpp
@@ -4,7 +4,12 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #include "myaccount2fadisabletotpwidgettest.h"
+#include "myaccount/myaccount2fadisabletotpwidget.h"
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
 #include <QTest>
+#include <QVBoxLayout>
 QTEST_MAIN(MyAccount2FaDisableTotpWidgetTest)
 MyAccount2FaDisableTotpWidgetTest::MyAccount2FaDisableTotpWidgetTest(QObject *parent)
     : QObject{parent}
@@ -13,5 +18,30 @@ MyAccount2FaDisableTotpWidgetTest::MyAccount2FaDisableTotpWidgetTest(QObject *pa
 
 void MyAccount2FaDisableTotpWidgetTest::shouldHaveDefaultValues()
 {
-    // TODO
+    MyAccount2FaDisableTotpWidget w(nullptr);
+
+    auto mainLayout = w.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->contentsMargins(), {});
+
+    auto label = w.findChild<QLabel *>(QStringLiteral("label"));
+    QVERIFY(label);
+    QVERIFY(!label->text().isEmpty());
+
+    auto hboxLayout = w.findChild<QHBoxLayout *>(QStringLiteral("hboxLayout"));
+    QVERIFY(hboxLayout);
+    QCOMPARE(hboxLayout->contentsMargins(), {});
+
+    auto mDisableCodeLineEdit = w.findChild<QLineEdit *>(QStringLiteral("mDisableCodeLineEdit"));
+    QVERIFY(mDisableCodeLineEdit);
+    QVERIFY(!mDisableCodeLineEdit->placeholderText().isEmpty());
+
+    auto verifyButton = w.findChild<QPushButton *>(QStringLiteral("verifyButton"));
+    QVERIFY(verifyButton);
+    QVERIFY(!verifyButton->text().isEmpty());
+    QVERIFY(!verifyButton->isEnabled());
+
+    auto regenerateCode = w.findChild<QPushButton *>(QStringLiteral("regenerateCode"));
+    QVERIFY(regenerateCode);
+    QVERIFY(!regenerateCode->text().isEmpty());
 }
