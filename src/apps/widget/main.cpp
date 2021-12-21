2687971e274022ee43bb2f8c9036a7fbe9c67f64
Laurent Montel <montel@kde.org>
2021-12-21 19:57:33 +0100

    It's by default in qt6

--- src/apps/widget/main.cpp
+++ src/apps/widget/main.cpp
@@ -33,7 +33,9 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+#endif
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QApplication app(argc, argv);
     app.setWindowIcon(QIcon::fromTheme(QStringLiteral("ruqola")));
