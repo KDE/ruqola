300466a4ef84b34ab1556ccdb83a0f85ea39bddd
Laurent Montel <montel@kde.org>
2022-01-11 13:31:35 +0100

    Start to prepare to open message specific from url

--- src/apps/widget/main.cpp
+++ src/apps/widget/main.cpp
@@ -77,6 +77,7 @@ int main(int argc, char *argv[])
     QCommandLineParser parser;
     parser.addOption(QCommandLineOption(QStringList() << QStringLiteral("list-accounts"), i18n("Return lists of accounts")));
     parser.addOption(QCommandLineOption(QStringList() << QStringLiteral("account"), i18n("Start with specific account"), QStringLiteral("accountname")));
+    parser.addOption(QCommandLineOption(QStringList() << QStringLiteral("messageurl"), i18n("Show Message"), QStringLiteral("url")));
 
 #if HAVE_KUSERFEEDBACK
     parser.addOption(QCommandLineOption(QStringLiteral("feedback"), i18n("Lists the available options for user feedback")));
