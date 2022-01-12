c5a915a62c5a133f637179795d4c316e90008a2f
Laurent Montel <montel@kde.org>
2022-01-12 13:46:42 +0100

    Add autotests

---
+++ src/core/autotests/parsemessageurlutilstest.cpp
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "parsemessageurlutilstest.h"
+#include "parsemessageurlutils.h"
+#include <QTest>
+QTEST_MAIN(ParseMessageUrlUtilsTest)
+ParseMessageUrlUtilsTest::ParseMessageUrlUtilsTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void ParseMessageUrlUtilsTest::shouldHaveDefaultValues()
+{
+    ParseMessageUrlUtils w;
+    // TODO
+}
