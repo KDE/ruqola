23aedcca1ecaeb2961957d74dc3472d594d9c816
Laurent Montel <montel@kde.org>
2022-01-13 13:49:05 +0100

    Add commandline option in ruqolacommandlineoptions

---
+++ src/widgets/ruqolacommandlineoptions.cpp
@@ -0,0 +1,7 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "ruqolacommandlineoptions.h"
---
+++ src/widgets/ruqolacommandlineoptions.h
@@ -0,0 +1,24 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+#include "config-ruqola.h"
+#include "libruqolawidgets_export.h"
+#include <KLocalizedString>
+#include <QCommandLineParser>
+
+class QCommandLineParser;
+
+static void ruqolaOptions(QCommandLineParser *parser)
+{
+    parser->addOption(QCommandLineOption(QStringList() << QStringLiteral("list-accounts"), i18n("Return lists of accounts")));
+    parser->addOption(QCommandLineOption(QStringList() << QStringLiteral("account"), i18n("Start with specific account"), QStringLiteral("accountname")));
+    parser->addOption(QCommandLineOption(QStringList() << QStringLiteral("messageurl"), i18n("Show Message"), QStringLiteral("url")));
+
+#if HAVE_KUSERFEEDBACK
+    parser->addOption(QCommandLineOption(QStringLiteral("feedback"), i18n("Lists the available options for user feedback")));
+#endif
+}
