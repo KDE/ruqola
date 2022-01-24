6ac2aa0d7c3eb09fffd5fd70cfea3317979d1df8
Laurent Montel <montel@kde.org>
2022-01-24 08:41:36 +0100

    Start to implement oauth admin support

---
+++ src/widgets/administratordialog/oauth/administratoroauthwidget.cpp
@@ -0,0 +1,6 @@
+#include "administratoroauthwidget.h"
+
+AdministratorOauthWidget::AdministratorOauthWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
---
+++ src/widgets/administratordialog/oauth/administratoroauthwidget.h
@@ -0,0 +1,18 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QWidget>
+
+#include "libruqolawidgets_private_export.h"
+#include <QWidget>
+class LIBRUQOLAWIDGETS_TESTS_EXPORT AdministratorOauthWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit AdministratorOauthWidget(QWidget *parent = nullptr);
+};
