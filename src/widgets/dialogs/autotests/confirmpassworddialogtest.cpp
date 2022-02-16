a5a3ee51b29b3fb8e3b8af6aef6f0320c33a14cb
Laurent Montel <montel@kde.org>
2022-02-16 07:57:24 +0100

    Add autotest

---
+++ src/widgets/dialogs/autotests/confirmpassworddialogtest.cpp
@@ -0,0 +1,30 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "confirmpassworddialogtest.h"
+#include "dialogs/confirmpassworddialog.h"
+#include "dialogs/confirmpasswordwidget.h"
+#include <QDialogButtonBox>
+#include <QTest>
+#include <QVBoxLayout>
+QTEST_MAIN(ConfirmPasswordDialogTest)
+ConfirmPasswordDialogTest::ConfirmPasswordDialogTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void ConfirmPasswordDialogTest::shouldHaveDefaultValues()
+{
+    ConfirmPasswordDialog w;
+    QVERIFY(!w.windowTitle().isEmpty());
+    auto mainLayout = w.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+
+    auto mConfirmPasswordWidget = w.findChild<ConfirmPasswordWidget *>(QStringLiteral("mConfirmPasswordWidget"));
+    QVERIFY(mConfirmPasswordWidget);
+
+    auto buttonBox = w.findChild<QDialogButtonBox *>(QStringLiteral("buttonBox"));
+    QVERIFY(buttonBox);
+}
