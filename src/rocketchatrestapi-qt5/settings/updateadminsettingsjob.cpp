c3c1edfb5d726f4df3baaecd8ee5f084d37d607c
Laurent Montel <montel@kde.org>
2022-02-27 14:07:20 +0100

    Prepare updateadminsettingsjob

---
+++ src/rocketchatrestapi-qt5/settings/updateadminsettingsjob.cpp
@@ -0,0 +1,73 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "updateadminsettingsjob.h"
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+
+#include <KLocalizedString>
+
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+using namespace RocketChatRestApi;
+UpdateAdminSettingsJob::UpdateAdminSettingsJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+UpdateAdminSettingsJob::~UpdateAdminSettingsJob() = default;
+
+bool UpdateAdminSettingsJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        return false;
+    }
+    addStartRestApiInfo("UpdateAdminSettingsJob::start");
+    submitPostRequest(json());
+    return true;
+}
+
+void UpdateAdminSettingsJob::onPostRequestResponse(const QJsonDocument &replyJson)
+{
+    const QJsonObject replyObject = replyJson.object();
+    if (replyObject[QStringLiteral("success")].toBool()) {
+        addLoggerInfo(QByteArrayLiteral("UpdateAdminSettingsJob: success: ") + replyJson.toJson(QJsonDocument::Indented));
+        Q_EMIT usersCreateDone(replyObject);
+    } else {
+        emitFailedMessage(replyObject);
+        addLoggerWarning(QByteArrayLiteral("UpdateAdminSettingsJob: Problem: ") + replyJson.toJson(QJsonDocument::Indented));
+    }
+}
+
+bool UpdateAdminSettingsJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool UpdateAdminSettingsJob::canStart() const
+{
+    if (!RestApiAbstractJob::canStart()) {
+        return false;
+    }
+    return true;
+}
+
+QNetworkRequest UpdateAdminSettingsJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::UsersCreate);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    addRequestAttribute(request);
+    return request;
+}
+
+QJsonDocument UpdateAdminSettingsJob::json() const
+{
+    // TODO
+    return {};
+}
---
+++ src/rocketchatrestapi-qt5/settings/updateadminsettingsjob.h
@@ -0,0 +1,36 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "createupdateuserinfo.h"
+#include "librocketchatrestapi-qt5_export.h"
+#include "restapiabstractjob.h"
+
+namespace RocketChatRestApi
+{
+class LIBROCKETCHATRESTAPI_QT5_EXPORT UpdateAdminSettingsJob : public RestApiAbstractJob
+{
+    Q_OBJECT
+public:
+    explicit UpdateAdminSettingsJob(QObject *parent = nullptr);
+    ~UpdateAdminSettingsJob() override;
+
+    Q_REQUIRED_RESULT bool start() override;
+    Q_REQUIRED_RESULT bool requireHttpAuthentication() const override;
+    Q_REQUIRED_RESULT bool canStart() const override;
+    Q_REQUIRED_RESULT QNetworkRequest request() const override;
+
+    Q_REQUIRED_RESULT QJsonDocument json() const;
+Q_SIGNALS:
+    void usersCreateDone(const QJsonObject &obj);
+
+private:
+    Q_DISABLE_COPY(UpdateAdminSettingsJob)
+    void onPostRequestResponse(const QJsonDocument &replyJson) override;
+    CreateUpdateUserInfo mCreateInfo;
+};
+}
