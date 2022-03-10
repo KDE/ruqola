03dac29f51cff0389526215071d5af6b276ca159
Laurent Montel <montel@kde.org>
2022-03-10 20:56:56 +0100

    Add main.cpp

---
+++ tests/uploadfilewidgetlist/main.cpp
@@ -0,0 +1,14 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include <QCoreApplication>
+int main(int argc, char **argv)
+{
+    QCoreApplication app(argc, argv);
+    // TODO
+    app.exec();
+    return 0;
+}
