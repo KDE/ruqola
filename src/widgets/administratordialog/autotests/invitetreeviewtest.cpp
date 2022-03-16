90d839fab4831faf6dc5449fdc23878151337b2c
Laurent Montel <montel@kde.org>
2022-03-16 08:11:48 +0100

    Add autotest

---
+++ src/widgets/administratordialog/autotests/invitetreeviewtest.cpp
@@ -0,0 +1,26 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "invitetreeviewtest.h"
+#include "administratordialog/invites/invitetreeview.h"
+#include <QTest>
+QTEST_MAIN(InviteTreeViewTest)
+
+InviteTreeViewTest::InviteTreeViewTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void InviteTreeViewTest::shouldHaveDefaultValues()
+{
+    InviteTreeView w;
+    QVERIFY(w.alternatingRowColors());
+    QVERIFY(!w.rootIsDecorated());
+    QVERIFY(w.isSortingEnabled());
+    QCOMPARE(w.selectionMode(), QTreeView::SingleSelection);
+    QCOMPARE(w.contextMenuPolicy(), Qt::CustomContextMenu);
+    QCOMPARE(w.selectionBehavior(), QAbstractItemView::SelectRows);
+}
---
+++ src/widgets/administratordialog/autotests/invitetreeviewtest.h
@@ -0,0 +1,18 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#pragma once
+
+#include <QObject>
+
+class InviteTreeViewTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit InviteTreeViewTest(QObject *parent = nullptr);
+    ~InviteTreeViewTest() override = default;
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+};
