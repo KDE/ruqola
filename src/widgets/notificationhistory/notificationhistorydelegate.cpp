df79308c5141ef3bd71a7ba5cafc6dc7762b3fe9
Laurent Montel <montel@kde.org>
2022-04-04 13:59:24 +0200

    Prepare to use a delegate

---
+++ src/widgets/notificationhistory/notificationhistorydelegate.cpp
@@ -0,0 +1,16 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "notificationhistorydelegate.h"
+
+NotificationHistoryDelegate::NotificationHistoryDelegate(QObject *parent)
+    : QItemDelegate{parent}
+{
+}
+
+NotificationHistoryDelegate::~NotificationHistoryDelegate()
+{
+}
---
+++ src/widgets/notificationhistory/notificationhistorydelegate.h
@@ -0,0 +1,18 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QItemDelegate>
+
+#include "libruqolawidgets_private_export.h"
+
+class LIBRUQOLAWIDGETS_TESTS_EXPORT NotificationHistoryDelegate : public QItemDelegate
+{
+public:
+    explicit NotificationHistoryDelegate(QObject *parent = nullptr);
+    ~NotificationHistoryDelegate() override;
+};
