4aefda7eb00c5ff7c460f930ed234c74506a6512
Laurent Montel <montel@kde.org>
2022-04-27 08:08:52 +0200

    Move delegate here too

--- src/widgets/discussions/showdiscussionswidget.cpp
+++ src/widgets/discussions/showdiscussionswidget.cpp
@@ -5,7 +5,7 @@
 */
 
 #include "showdiscussionswidget.h"
-#include "dialogs/discussion/listdiscussiondelegate.h"
+#include "discussions/discussion/listdiscussiondelegate.h"
 #include "misc/lineeditcatchreturnkey.h"
 #include "misc/messagelistviewbase.h"
 #include "model/discussionsfilterproxymodel.h"
