4f0bc433a7f0f51a95e034888294b9a7e3fab535
Laurent Montel <montel@kde.org>
2022-05-03 20:07:51 +0200

    Add autotest

--- src/widgets/administratorsettingsdialog/autotests/administratorsettingsdialogtest.cpp
+++ src/widgets/administratorsettingsdialog/autotests/administratorsettingsdialogtest.cpp
@@ -6,7 +6,9 @@
 
 #include "administratorsettingsdialogtest.h"
 #include "administratorsettingsdialog/administratorsettingsdialog.h"
+#include <QDialogButtonBox>
 #include <QTest>
+#include <QVBoxLayout>
 QTEST_MAIN(AdministratorSettingsDialogTest)
 
 AdministratorSettingsDialogTest::AdministratorSettingsDialogTest(QObject *parent)
@@ -17,5 +19,14 @@ AdministratorSettingsDialogTest::AdministratorSettingsDialogTest(QObject *parent
 void AdministratorSettingsDialogTest::shouldHaveDefaultValues()
 {
     AdministratorSettingsDialog d(nullptr);
-    // TODO
+    QVERIFY(!d.windowTitle().isEmpty());
+
+    auto mainLayout = d.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+
+    //    auto mAdministratorWidget = d.findChild<AdministratorWidget *>(QStringLiteral("mAdministratorWidget"));
+    //    QVERIFY(mAdministratorWidget);
+
+    auto button = d.findChild<QDialogButtonBox *>(QStringLiteral("button"));
+    QVERIFY(button);
 }
