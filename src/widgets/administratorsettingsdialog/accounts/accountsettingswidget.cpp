fbc3614b29427bd5a8ba93f39a9d1dc890491dcc
Laurent Montel <montel@kde.org>
2022-05-04 08:38:26 +0200

    Start to implement accountsettingswidget

---
+++ src/widgets/administratorsettingsdialog/accounts/accountsettingswidget.cpp
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "accountsettingswidget.h"
+#include <QVBoxLayout>
+
+AccountSettingsWidget::AccountSettingsWidget(QWidget *parent)
+    : QWidget{parent}
+{
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(QStringLiteral("mainLayout"));
+    mainLayout->setContentsMargins({});
+}
+
+AccountSettingsWidget::~AccountSettingsWidget()
+{
+}
---
+++ src/widgets/administratorsettingsdialog/accounts/accountsettingswidget.h
@@ -0,0 +1,18 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "libruqolawidgets_private_export.h"
+#include <QWidget>
+
+class LIBRUQOLAWIDGETS_TESTS_EXPORT AccountSettingsWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit AccountSettingsWidget(QWidget *parent = nullptr);
+    ~AccountSettingsWidget() override;
+};
