f9299e6ba944026fbd7cd3325fdad3cb8f3b5ae3
Laurent Montel <montel@kde.org>
2022-05-09 08:56:17 +0200

    Add autotest

---
+++ src/widgets/notificationhistory/autotests/notificationhistorylistviewtest.cpp
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "notificationhistorylistviewtest.h"
+#include "notificationhistory/notificationhistorylistview.h"
+#include <QTest>
+QTEST_MAIN(NotificationHistoryListViewTest)
+
+NotificationHistoryListViewTest::NotificationHistoryListViewTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void NotificationHistoryListViewTest::shouldHaveDefaultValues()
+{
+    NotificationHistoryListView w;
+    QVERIFY(w.searchText().isEmpty());
+}
