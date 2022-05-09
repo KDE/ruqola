9c4ea48339d450b35949bb772a202a576959c73d
Waqar Ahmed <waqar.17a@gmail.com>
2022-05-09 15:58:07 +0500

    Fix compile

--- src/widgets/administratordialog/customemoji/administratorcustomemojicreatewidget.cpp
+++ src/widgets/administratordialog/customemoji/administratorcustomemojicreatewidget.cpp
@@ -10,8 +10,12 @@
 #include <QLabel>
 #include <QLineEdit>
 
+#include <KColorScheme>
 #include <KLocalizedString>
-#include <KStatefulBrush>
+#include <kconfigwidgets_version.h>
+#if KCONFIGWIDGETS_VERSION >= QT_VERSION_CHECK(5, 93, 0)
+#include <KStatefulBrush> // was moved to own header in 5.93.0
+#endif
 #include <KUrlRequester>
 
 AdministratorCustomEmojiCreateWidget::AdministratorCustomEmojiCreateWidget(QWidget *parent)
