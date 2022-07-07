c7c96cff99174fb38d411dd62927a6530dd9edde
Laurent Montel <montel@kde.org>
2022-07-07 13:40:57 +0200

    Add proxy model

---
+++ src/core/model/bannerinfosfilterproxymodel.cpp
@@ -0,0 +1,37 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "bannerinfosfilterproxymodel.h"
+#include "bannerinfosmodel.h"
+
+BannerInfosFilterProxyModel::BannerInfosFilterProxyModel(QObject *parent)
+    : QSortFilterProxyModel{parent}
+{
+}
+
+BannerInfosFilterProxyModel::~BannerInfosFilterProxyModel() = default;
+
+bool BannerInfosFilterProxyModel::filterAcceptsRow(int source_row, const QModelIndex &source_parent) const
+{
+    if (!mShowUnread) {
+        return QSortFilterProxyModel::filterAcceptsRow(source_row, source_parent);
+    }
+    const QModelIndex sourceIndex = sourceModel()->index(source_row, 0, source_parent);
+    const bool isRead = sourceIndex.data(BannerInfosModel::Read).toBool();
+    if (!isRead) {
+        return QSortFilterProxyModel::filterAcceptsRow(source_row, source_parent);
+    }
+    return false;
+}
+
+bool BannerInfosFilterProxyModel::showUnread() const
+{
+    return mShowUnread;
+}
+
+void BannerInfosFilterProxyModel::setShowUnread(bool newShowUnread)
+{
+    mShowUnread = newShowUnread;
+}
---
+++ src/core/model/bannerinfosfilterproxymodel.h
@@ -0,0 +1,28 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#pragma once
+
+#include <QSortFilterProxyModel>
+
+#include "libruqolacore_export.h"
+#include <QSortFilterProxyModel>
+
+class LIBRUQOLACORE_EXPORT BannerInfosFilterProxyModel : public QSortFilterProxyModel
+{
+    Q_OBJECT
+public:
+    explicit BannerInfosFilterProxyModel(QObject *parent = nullptr);
+    ~BannerInfosFilterProxyModel() override;
+
+    Q_REQUIRED_RESULT bool showUnread() const;
+    void setShowUnread(bool newShowUnread);
+
+protected:
+    bool filterAcceptsRow(int source_row, const QModelIndex &source_parent) const override;
+
+private:
+    bool mShowUnread = false;
+};
