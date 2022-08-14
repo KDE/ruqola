856596b4dafaa48bfa11fa63f7e2ab0699b87ff4
Laurent Montel <montel@kde.org>
2022-08-14 14:34:22 +0200

    implement deviceinfos

---
+++ src/core/managedevices/deviceinfos.cpp
@@ -0,0 +1,121 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "deviceinfos.h"
+#include "ruqola_debug.h"
+
+DeviceInfos::DeviceInfos() = default;
+
+DeviceInfos::~DeviceInfos() = default;
+
+QDebug operator<<(QDebug d, const DeviceInfos &t)
+{
+    d << "total " << t.total();
+    d << "offset " << t.offset();
+    d << "deviceInfosCount " << t.deviceInfosCount() << "\n";
+    for (int i = 0, total = t.deviceInfosList().count(); i < total; ++i) {
+        d << t.deviceInfosList().at(i) << "\n";
+    }
+    return d;
+}
+
+int DeviceInfos::offset() const
+{
+    return mOffset;
+}
+
+void DeviceInfos::setOffset(int newOffset)
+{
+    mOffset = newOffset;
+}
+
+int DeviceInfos::total() const
+{
+    return mTotal;
+}
+
+void DeviceInfos::setTotal(int newTotal)
+{
+    mTotal = newTotal;
+}
+
+int DeviceInfos::deviceInfosCount() const
+{
+    return mDeviceInfosCount;
+}
+
+void DeviceInfos::setDeviceInfosCount(int newDeviceInfosCount)
+{
+    mDeviceInfosCount = newDeviceInfosCount;
+}
+
+const QVector<DeviceInfo> &DeviceInfos::deviceInfosList() const
+{
+    return mDeviceInfosList;
+}
+
+void DeviceInfos::setDeviceInfosList(const QVector<DeviceInfo> &newDeviceInfosList)
+{
+    mDeviceInfosList = newDeviceInfosList;
+}
+
+bool DeviceInfos::isEmpty() const
+{
+    return mDeviceInfosList.isEmpty();
+}
+
+void DeviceInfos::clear()
+{
+    mDeviceInfosList.clear();
+}
+
+int DeviceInfos::count() const
+{
+    return mDeviceInfosList.count();
+}
+
+DeviceInfo DeviceInfos::at(int index) const
+{
+    if (index < 0 || index > mDeviceInfosList.count()) {
+        qCWarning(RUQOLA_LOG) << "Invalid index " << index;
+        return {};
+    }
+    return mDeviceInfosList.at(index);
+}
+
+void DeviceInfos::parseDiscussions(const QJsonObject &deviceInfosObj)
+{
+    mDeviceInfosList.clear();
+    mDeviceInfosCount = deviceInfosObj[QStringLiteral("count")].toInt();
+    mOffset = deviceInfosObj[QStringLiteral("offset")].toInt();
+    mTotal = deviceInfosObj[QStringLiteral("total")].toInt();
+    mDeviceInfosList.reserve(mDeviceInfosCount);
+    parseDiscussionsObj(deviceInfosObj);
+}
+
+void DeviceInfos::parseDiscussionsObj(const QJsonObject &deviceInfosObj)
+{
+    const QJsonArray discussionsArray = deviceInfosObj[QStringLiteral("sessions")].toArray();
+    for (const QJsonValue &current : discussionsArray) {
+        if (current.type() == QJsonValue::Object) {
+            const QJsonObject discussionObject = current.toObject();
+            DeviceInfo m;
+            m.parseDeviceInfo(discussionObject);
+            mDeviceInfosList.append(std::move(m));
+        } else {
+            qCWarning(RUQOLA_LOG) << "Problem when parsing discussions" << current;
+        }
+    }
+}
+
+void DeviceInfos::parseMoreDiscussions(const QJsonObject &deviceInfosObj)
+{
+    const int discussionsCount = deviceInfosObj[QStringLiteral("count")].toInt();
+    mOffset = deviceInfosObj[QStringLiteral("offset")].toInt();
+    mTotal = deviceInfosObj[QStringLiteral("total")].toInt();
+    parseDiscussionsObj(deviceInfosObj);
+    mDeviceInfosCount += discussionsCount;
+}
---
+++ src/core/managedevices/deviceinfos.h
@@ -0,0 +1,50 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "deviceinfo.h"
+#include "libruqolacore_export.h"
+#include <QDebug>
+#include <QJsonObject>
+#include <QString>
+#include <QVector>
+
+class LIBRUQOLACORE_EXPORT DeviceInfos
+{
+public:
+    DeviceInfos();
+    ~DeviceInfos();
+
+    Q_REQUIRED_RESULT int offset() const;
+    void setOffset(int newOffset);
+
+    Q_REQUIRED_RESULT int total() const;
+    void setTotal(int newTotal);
+
+    Q_REQUIRED_RESULT int deviceInfosCount() const;
+    void setDeviceInfosCount(int newDeviceInfosCount);
+
+    Q_REQUIRED_RESULT const QVector<DeviceInfo> &deviceInfosList() const;
+    void setDeviceInfosList(const QVector<DeviceInfo> &newDeviceInfosList);
+
+    Q_REQUIRED_RESULT bool isEmpty() const;
+    void clear();
+    Q_REQUIRED_RESULT int count() const;
+    Q_REQUIRED_RESULT DeviceInfo at(int index) const;
+
+    void parseDiscussions(const QJsonObject &discussionsObj);
+    void parseMoreDiscussions(const QJsonObject &deviceInfosObj);
+
+private:
+    void parseDiscussionsObj(const QJsonObject &discussionsObj);
+    QVector<DeviceInfo> mDeviceInfosList;
+    int mDeviceInfosCount = 0;
+    int mOffset = 0;
+    int mTotal = 0;
+};
+Q_DECLARE_METATYPE(DeviceInfos)
+LIBRUQOLACORE_EXPORT QDebug operator<<(QDebug d, const DeviceInfos &t);
