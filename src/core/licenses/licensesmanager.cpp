b28749aa41814ecdf1694ec179518e64281dab83
Laurent Montel <montel@kde.org>
2022-08-14 19:57:56 +0200

    Start to implement license manager

---
+++ src/core/licenses/licensesmanager.cpp
@@ -0,0 +1,11 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "licensesmanager.h"
+
+LicensesManager::LicensesManager() = default;
+
+LicensesManager::~LicensesManager() = default;
---
+++ src/core/licenses/licensesmanager.h
@@ -0,0 +1,17 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+#include "libruqola_private_export.h"
+#include <QJsonObject>
+#include <QString>
+
+class LIBRUQOLACORE_TESTS_EXPORT LicensesManager
+{
+public:
+    LicensesManager();
+    ~LicensesManager();
+};
