161868e77441f99ae43cb1ef0e5fa75867f4e2b6
Laurent Montel <montel@kde.org>
2022-08-15 09:22:02 +0200

    Prepare autotest

---
+++ src/core/autotests/licensesmanagertest.cpp
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2019-2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "licensesmanagertest.h"
+#include "licenses/licensesmanager.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(LicensesManagerTest)
+LicensesManagerTest::LicensesManagerTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void LicensesManagerTest::shouldHaveDefaultValues()
+{
+    LicensesManager w;
+    // TODO
+}
---
+++ src/core/autotests/licensesmanagertest.h
@@ -0,0 +1,19 @@
+/*
+   SPDX-FileCopyrightText: 2019-2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+
+class LicensesManagerTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit LicensesManagerTest(QObject *parent = nullptr);
+    ~LicensesManagerTest() override = default;
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+};
