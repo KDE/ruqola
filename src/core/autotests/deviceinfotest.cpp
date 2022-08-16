755b4332fe541bf89b251596220090e0d456bc5e
Laurent Montel <montel@kde.org>
2022-08-16 08:28:03 +0200

    Prepare deviceinfo autotest

--- src/core/autotests/deviceinfotest.cpp
+++ src/core/autotests/deviceinfotest.cpp
@@ -6,6 +6,7 @@
 
 #include "deviceinfotest.h"
 #include "managedevices/deviceinfo.h"
+#include "ruqola_autotest_helper.h"
 #include <QTest>
 QTEST_GUILESS_MAIN(DeviceInfoTest)
 DeviceInfoTest::DeviceInfoTest(QObject *parent)
@@ -24,4 +25,30 @@ void DeviceInfoTest::shouldHaveDefaultValues()
     QCOMPARE(w.loginAt(), -1);
     QVERIFY(w.os().isEmpty());
     QVERIFY(w.client().isEmpty());
+}
+
+void DeviceInfoTest::shouldDeviceInfo()
+{
+    QFETCH(QString, name);
+    QFETCH(DeviceInfo, deviceInfo);
+    const QString originalJsonFile = QLatin1String(RUQOLA_DATA_DIR) + QLatin1String("/deviceinfo/") + name + QLatin1String(".json");
+    const QJsonObject obj = AutoTestHelper::loadJsonObject(originalJsonFile);
+
+    DeviceInfo result;
+    result.parseDeviceInfo(obj);
+    const bool equal = result == deviceInfo;
+    if (!equal) {
+        qDebug() << " result " << result;
+        qDebug() << " deviceInfo " << deviceInfo;
+    }
+    QVERIFY(equal);
+}
+
+void DeviceInfoTest::shouldDeviceInfo_data()
+{
+    QTest::addColumn<QString>("name");
+    QTest::addColumn<DeviceInfo>("deviceInfo");
+    QTest::addRow("deviceinfoempty") << QStringLiteral("deviceinfoempty") << DeviceInfo();
+    {
+    }
 }
--- src/core/autotests/deviceinfotest.h
+++ src/core/autotests/deviceinfotest.h
@@ -17,4 +17,6 @@ public:
 
 private Q_SLOTS:
     void shouldHaveDefaultValues();
+    void shouldDeviceInfo();
+    void shouldDeviceInfo_data();
 };
