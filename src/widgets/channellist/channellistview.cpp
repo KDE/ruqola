6e2a8d4c9a61d7e816ff03b178be171d4df8f5f6
Laurent Montel <montel@kde.org>
2022-08-18 21:32:02 +0200

    Fix update channel list

--- src/widgets/channellist/channellistview.cpp
+++ src/widgets/channellist/channellistview.cpp
@@ -55,7 +55,7 @@ void ChannelListView::setCurrentRocketChatAccount(RocketChatAccount *currentRock
     mCurrentRocketChatAccount = currentRocketChatAccount;
     connect(mCurrentRocketChatAccount, &RocketChatAccount::roomRemoved, this, &ChannelListView::slotRoomRemoved);
     mUpdateChannelViewConnect = connect(mCurrentRocketChatAccount, &RocketChatAccount::needUpdateChannelView, this, [this]() {
-        update();
+        filterModel()->invalidate();
     });
     mChannelListDelegate->setCurrentRocketChatAccount(currentRocketChatAccount);
 }
