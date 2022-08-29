6aa083b7f370755e9686bbbe2f4c0b4bce332a68
Laurent Montel <montel@kde.org>
2022-08-29 22:04:00 +0200

    Prepare to implement translator settings

---
+++ src/widgets/translation/translationconfigurewidget.cpp
@@ -0,0 +1,16 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "translationconfigurewidget.h"
+#include <KLocalizedString>
+#include <QVBoxLayout>
+
+TranslationConfigureWidget::TranslationConfigureWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
+
+TranslationConfigureWidget::~TranslationConfigureWidget() = default;
