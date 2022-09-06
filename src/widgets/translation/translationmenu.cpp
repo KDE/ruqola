513226500763f217385792a64d460734c8929a65
Laurent Montel <montel@kde.org>
2022-09-06 19:22:55 +0200

    We need a specific menu

---
+++ src/widgets/translation/translationmenu.cpp
@@ -0,0 +1,13 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "translationmenu.h"
+
+TranslationMenu::TranslationMenu()
+{
+}
+
+TranslationMenu::~TranslationMenu() = default;
---
+++ src/widgets/translation/translationmenu.h
@@ -0,0 +1,16 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "libruqolawidgets_private_export.h"
+
+class LIBRUQOLAWIDGETS_TESTS_EXPORT TranslationMenu
+{
+public:
+    TranslationMenu();
+    ~TranslationMenu();
+};
