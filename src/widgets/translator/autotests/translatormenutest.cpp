d1f3f810e653f238d112ca7e8b651330f4814c4e
Laurent Montel <montel@kde.org>
2022-09-07 13:27:56 +0200

    Add autotest

---
+++ src/widgets/translator/autotests/translatormenutest.cpp
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "translatormenutest.h"
+#include "translator/translatormenu.h"
+#include <QTest>
+QTEST_MAIN(TranslatorMenuTest)
+TranslatorMenuTest::TranslatorMenuTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void TranslatorMenuTest::shouldHaveDefaultValues()
+{
+    TranslatorMenu w;
+    QVERIFY(w.menu());
+}
