c56dbce1090e550df8481f96e67b1e95943cc3d8
Laurent Montel <montel@kde.org>
2022-11-24 19:44:41 +0100

    Add oauthsettings

---
+++ src/widgets/administratorsettingsdialog/autotests/oauthsettingswidgettest.cpp
@@ -0,0 +1,13 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "oauthsettingswidgettest.h"
+#include <QTest>
+QTEST_MAIN(OauthSettingsWidgetTest)
+OauthSettingsWidgetTest::OauthSettingsWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
---
+++ src/widgets/administratorsettingsdialog/oauth/oauthsettingswidget.cpp
@@ -0,0 +1,19 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "oauthsettingswidget.h"
+
+OauthSettingsWidget::OauthSettingsWidget(RocketChatAccount *account, QWidget *parent)
+    : SettingsWidgetBase(account, parent)
+{
+}
+
+OauthSettingsWidget::~OauthSettingsWidget() = default;
+
+void OauthSettingsWidget::initialize(const QMap<QString, QVariant> &mapSettings)
+{
+    // TODO
+}
