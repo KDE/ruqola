da26f00562b7bb98821266b9263f2ab13adfa6e8
Laurent Montel <montel@kde.org>
2022-12-02 07:44:16 +0100

    Prepare autotest

---
+++ src/widgets/administratorsettingsdialog/autotests/analiticswidgettest.cpp
@@ -0,0 +1,13 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "analiticswidgettest.h"
+#include <QTest>
+QTEST_MAIN(AnaliticsWidgetTest)
+AnaliticsWidgetTest::AnaliticsWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
