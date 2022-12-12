d84745799cd87a96c7a2aefc6aa7bbb15ad69052
Laurent Montel <montel@kde.org>
2022-12-12 19:36:15 +0100

    continue to implement block support

--- src/core/messages/block.cpp
+++ src/core/messages/block.cpp
@@ -8,8 +8,31 @@
 
 Block::Block() = default;
 
+void Block::parseBlock(const QJsonObject &block)
+{
+    // TODO
+}
+
 bool Block::operator==(const Block &other) const
 {
     // TODO
     return true;
+}
+
+QJsonObject Block::serialize(const Block &block)
+{
+    // TODO
+    return {};
+}
+
+Block Block::fromJSon(const QJsonObject &o)
+{
+    // TODO
+    return {};
+}
+
+QDebug operator<<(QDebug d, const Block &t)
+{
+    // TODO
+    return d;
 }
--- src/core/messages/block.h
+++ src/core/messages/block.h
@@ -8,9 +8,16 @@
 
 #include "libruqola_private_export.h"
 #include <QDebug>
+#include <QJsonObject>
 class LIBRUQOLACORE_TESTS_EXPORT Block
 {
 public:
     Block();
     Q_REQUIRED_RESULT bool operator==(const Block &other) const;
+
+    static QJsonObject serialize(const Block &block);
+    static Block fromJSon(const QJsonObject &o);
+    void parseBlock(const QJsonObject &block);
 };
+LIBRUQOLACORE_EXPORT QDebug operator<<(QDebug d, const Block &t);
+Q_DECLARE_TYPEINFO(Block, Q_MOVABLE_TYPE);
