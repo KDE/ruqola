6b9558a2d2dd6fe281850c66dd686a5d094aa6d6
Laurent Montel <montel@kde.org>
2022-12-12 19:36:02 +0100

    Add autotests

---
+++ src/core/autotests/blocktest.cpp
@@ -0,0 +1,21 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "blocktest.h"
+#include "messages/block.h"
+#include <QJsonObject>
+#include <QTest>
+QTEST_GUILESS_MAIN(BlockTest)
+BlockTest::BlockTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void BlockTest::shouldHaveDefaultValues()
+{
+    Block b;
+    // TODO
+}
