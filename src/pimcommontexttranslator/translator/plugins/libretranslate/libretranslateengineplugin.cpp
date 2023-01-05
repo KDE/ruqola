eb340e53ef5bd47e9835b9cd8175c1f93d561c23
Laurent Montel <montel@kde.org>
2023-01-05 13:48:17 +0100

    Sync lib

--- src/pimcommontexttranslator/translator/plugins/libretranslate/libretranslateengineplugin.cpp
+++ src/pimcommontexttranslator/translator/plugins/libretranslate/libretranslateengineplugin.cpp
@@ -2,6 +2,9 @@
   SPDX-FileCopyrightText: 2022-2023 Laurent Montel <montel@kde.org>
 
   SPDX-License-Identifier: GPL-2.0-or-later
+
+  based on digikam onlinetranslator code
+
 */
 
 #include "libretranslateengineplugin.h"
