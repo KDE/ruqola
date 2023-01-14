7aff2e418d7bbaa6cf6b8cdfec66362f2c171d21
Laurent Montel <montel@kde.org>
2023-01-14 13:55:43 +0100

    Prepare to add listview

---
+++ src/widgets/servererrorinfohistory/servererrorinfomessagehistorylistview.cpp
@@ -0,0 +1,14 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "servererrorinfomessagehistorylistview.h"
+
+ServerErrorInfoMessageHistoryListView::ServerErrorInfoMessageHistoryListView(QWidget *parent)
+    : MessageListViewBase(parent)
+{
+}
+
+ServerErrorInfoMessageHistoryListView::~ServerErrorInfoMessageHistoryListView() = default;
---
+++ src/widgets/servererrorinfohistory/servererrorinfomessagehistorylistview.h
@@ -0,0 +1,18 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "libruqolawidgets_private_export.h"
+#include "misc/messagelistviewbase.h"
+
+class LIBRUQOLAWIDGETS_TESTS_EXPORT ServerErrorInfoMessageHistoryListView : public MessageListViewBase
+{
+    Q_OBJECT
+public:
+    explicit ServerErrorInfoMessageHistoryListView(QWidget *parent = nullptr);
+    ~ServerErrorInfoMessageHistoryListView() override;
+};
