660e4f3775f0f7643e05ad107da459bc6a48de80
Laurent Montel <montel@kde.org>
2023-01-14 10:05:22 +0100

    Add autotest

---
+++ src/widgets/servererrorinfohistory/autotests/servererrorinfomessagehistorydialogtest.cpp
@@ -0,0 +1,18 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "servererrorinfomessagehistorydialogtest.h"
+#include <QTest>
+QTEST_MAIN(ServerErrorInfoMessageHistoryDialogTest)
+ServerErrorInfoMessageHistoryDialogTest::ServerErrorInfoMessageHistoryDialogTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void ServerErrorInfoMessageHistoryDialogTest::shouldHaveDefaultValues()
+{
+    // TODO
+}
