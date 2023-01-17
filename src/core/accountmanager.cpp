c13242e8b28ca71f7a57c8f73e96489fb36e6e8a
Laurent Montel <montel@kde.org>
2023-01-17 19:41:40 +0100

    Remove unused variable

--- src/core/accountmanager.cpp
+++ src/core/accountmanager.cpp
@@ -36,14 +36,14 @@ bool AccountManager::showMessage(const ParseMessageUrlUtils &parseUrl)
 {
     auto account = mRocketChatAccountModel->accountFromServerUrl(parseUrl.serverHost());
     if (account) {
-        const QString path{parseUrl.path()};
+        // const QString path{parseUrl.path()};
         const QString messageId = parseUrl.messageId();
         // qDebug() << " parseUrl " << parseUrl;
         // https://<server url>/channel/python?msg=sn3gEQom7NcLxTg5h
         setCurrentAccount(account->accountName());
         // qDebug() << " account->accountName() : " << account->accountName();
         Q_EMIT mCurrentAccount->raiseWindow();
-        Q_EMIT mCurrentAccount->selectChannelAndMessage(parseUrl.messageId(), parseUrl.roomId(), parseUrl.roomIdType(), parseUrl.channelType());
+        Q_EMIT mCurrentAccount->selectChannelAndMessage(messageId, parseUrl.roomId(), parseUrl.roomIdType(), parseUrl.channelType());
         return true;
     }
     return false;
