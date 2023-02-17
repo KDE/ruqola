0cb4ee0d0b0d8fc3d0fcbded32592883316abbd5
Laurent Montel <montel@kde.org>
2023-02-17 18:40:28 +0100

    Implement to load/save timestamp in localglobaldatabase

--- src/core/localdatabase/localglobaldatabase.cpp
+++ src/core/localdatabase/localglobaldatabase.cpp
@@ -9,6 +9,11 @@
 #include "localdatabaseutils.h"
 #include "ruqola_database_debug.h"
 
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QSqlTableModel>
+
 static const char s_schemaAccountDataBase[] = "CREATE TABLE GLOBAL (identifier TEXT PRIMARY KEY NOT NULL, timestamp INTEGER)";
 enum class Fields {
     Identifier,
@@ -44,21 +49,41 @@ QString LocalGlobalDatabase::generateIdentifier(const QString &accountName, cons
 // TODO improve for identifier => account name/room name.
 void LocalGlobalDatabase::updateTimeStamp(const QString &accountName, const QString &roomName, qint64 timestamp, TimeStampType type)
 {
-    const QString identifier = generateIdentifier(accountName, roomName, type);
-
-    // TODO
+#if HAVE_DATABASE_SUPPORT
+    QSqlDatabase db;
+    if (initializeDataBase(accountName, roomName, db)) {
+        const QString identifier = generateIdentifier(accountName, roomName, type);
+        QSqlQuery query(QStringLiteral("INSERT OR REPLACE INTO GLOBAL VALUES (?, ?)"), db);
+        query.addBindValue(identifier);
+        query.addBindValue(timestamp);
+        if (!query.exec()) {
+            qCWarning(RUQOLA_DATABASE_LOG) << "Couldn't insert-or-replace in GLOBAL table" << db.databaseName() << query.lastError();
+        }
+    }
+#endif
 }
 
 void LocalGlobalDatabase::removeTimeStamp(const QString &accountName, const QString &roomName, TimeStampType type)
 {
+#if HAVE_DATABASE_SUPPORT
+    QSqlDatabase db;
+    if (!checkDataBase(accountName, db)) {
+        return;
+    }
     const QString identifier = generateIdentifier(accountName, roomName, type);
-
-    // TODO
+    QSqlQuery query(QStringLiteral("DELETE FROM GLOBAL WHERE roomId = ?"), db);
+    query.addBindValue(identifier);
+    if (!query.exec()) {
+        qCWarning(RUQOLA_DATABASE_LOG) << "Couldn't insert-or-replace in GLOBAL table" << db.databaseName() << query.lastError();
+    }
+#endif
 }
 
 qint64 LocalGlobalDatabase::timeStamp(const QString &accountName, const QString &roomName, TimeStampType type)
 {
+#if HAVE_DATABASE_SUPPORT
     // TODO
     const QString identifier = generateIdentifier(accountName, roomName, type);
+#endif
     return {};
 }
