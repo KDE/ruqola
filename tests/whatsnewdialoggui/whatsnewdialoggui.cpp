3a17feb8060b1b31df426d8d2ea9f01487ea8206
Laurent Montel <montel@kde.org>
2023-02-27 19:25:47 +0100

    Add new test apps

---
+++ tests/whatsnewdialoggui/whatsnewdialoggui.cpp
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2022-2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "whatsnew/whatsnewdialog.h"
+#include <QApplication>
+#include <QStandardPaths>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QStandardPaths::setTestModeEnabled(true);
+
+    WhatsNewDialog w;
+    w.updateInformations();
+    w.show();
+    return app.exec();
+}
