14056235e087b8dccb4ef502dc6abf74bb5f4100
Laurent Montel <montel@kde.org>
2023-03-04 17:38:36 +0100

    Create directorystackedwidget

---
+++ src/widgets/directory/directorystackedwidget.cpp
@@ -0,0 +1,16 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "directorystackedwidget.h"
+
+DirectoryStackedWidget::DirectoryStackedWidget(QWidget *parent)
+    : QStackedWidget(parent)
+{
+}
+
+DirectoryStackedWidget::~DirectoryStackedWidget()
+{
+}
