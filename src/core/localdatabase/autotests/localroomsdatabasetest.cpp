f78547c59af23a3076422a6007ac2e15e3a2fa33
Laurent Montel <montel@kde.org>
2023-04-06 13:43:20 +0200

    Add autotests

---
+++ src/core/localdatabase/autotests/localroomsdatabasetest.cpp
@@ -0,0 +1,22 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "localroomsdatabasetest.h"
+#include "localdatabase/localroomsdatabase.h"
+#include <QStandardPaths>
+#include <QTest>
+
+QTEST_GUILESS_MAIN(LocalRoomsDatabaseTest)
+
+LocalRoomsDatabaseTest::LocalRoomsDatabaseTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void LocalRoomsDatabaseTest::shouldDefaultValues()
+{
+    LocalRoomsDatabase logger;
+    QCOMPARE(logger.schemaDatabaseStr(), QStringLiteral("CREATE TABLE ROOMS (roomId TEXT PRIMARY KEY NOT NULL, timestamp INTEGER, json TEXT)"));
+}
