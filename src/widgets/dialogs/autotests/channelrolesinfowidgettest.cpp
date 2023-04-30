77cfb64b2e0a7fe9dea315caa68888b53ce9a623
Laurent Montel <montel@kde.org>
2023-04-30 13:42:06 +0200

    continue to implement autotest

--- src/widgets/dialogs/autotests/channelrolesinfowidgettest.cpp
+++ src/widgets/dialogs/autotests/channelrolesinfowidgettest.cpp
@@ -7,6 +7,7 @@
 #include "channelrolesinfowidgettest.h"
 #include "dialogs/channelrolesinfowidget.h"
 #include <QFormLayout>
+#include <QGroupBox>
 #include <QTest>
 QTEST_MAIN(ChannelRolesInfoWidgetTest)
 
@@ -21,4 +22,11 @@ void ChannelRolesInfoWidgetTest::shouldHaveDefaultValues()
     auto mainLayout = w.findChild<QHBoxLayout *>(QStringLiteral("mainLayout"));
     QVERIFY(mainLayout);
     QCOMPARE(mainLayout->contentsMargins(), QMargins{});
+
+    auto box = w.findChild<QGroupBox *>(QStringLiteral("box"));
+    QVERIFY(box);
+    QVERIFY(!box->title().isEmpty());
+
+    auto mFormLayout = w.findChild<QFormLayout *>(QStringLiteral("mFormLayout"));
+    QVERIFY(mFormLayout);
 }
