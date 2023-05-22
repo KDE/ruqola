f71a3617a169ecffc4e3665ebf400d68044129e9
Laurent Montel <montel@kde.org>
2023-05-22 20:10:22 +0200

    Add test apps

---
+++ src/widgets/tests/testcreatesoundmessage_gui.cpp
@@ -0,0 +1,25 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "dialogs/createsoundmessagedialog.h"
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QStandardPaths>
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    QStandardPaths::setTestModeEnabled(true);
+    QCommandLineParser parser;
+    parser.addVersionOption();
+    parser.addHelpOption();
+
+    parser.process(app);
+    auto d = new CreateSoundMessageDialog;
+    d->resize(800, 600);
+    d->show();
+
+    return app.exec();
+}
