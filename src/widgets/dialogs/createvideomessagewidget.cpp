11a471f408e9ab38400777f4a9907b20030074d7
Laurent Montel <montel@kde.org>
2023-05-28 09:12:48 +0200

    Build it only in qt6

--- src/widgets/dialogs/createvideomessagewidget.cpp
+++ src/widgets/dialogs/createvideomessagewidget.cpp
@@ -5,30 +5,23 @@
 */
 
 #include "createvideomessagewidget.h"
-#include <QCamera>
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-#include <QCameraImageCapture>
-#include <QCameraViewfinder>
-#else
-#include <QCameraDevice>
-#include <QMediaDevices>
-#include <QMediaRecorder>
-#include <QVideoWidget>
-#endif
 #include <KMessageWidget>
+#include <QCamera>
+#include <QCameraDevice>
 #include <QComboBox>
 #include <QHBoxLayout>
 #include <QLabel>
+#include <QMediaDevices>
+#include <QMediaRecorder>
 #include <QToolButton>
 #include <QVBoxLayout>
+#include <QVideoWidget>
 
 #include <KLocalizedString>
 
 CreateVideoMessageWidget::CreateVideoMessageWidget(QWidget *parent)
     : QWidget(parent)
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     , mVideoWidget(new QVideoWidget(this))
-#endif
     , mErrorLabel(new QLabel(this))
     , mCamera(new QCamera(this))
     , mListCamera(new QComboBox(this))
@@ -51,10 +44,8 @@ CreateVideoMessageWidget::CreateVideoMessageWidget(QWidget *parent)
     mListCamera->setObjectName(QStringLiteral("mListCamera"));
     mainLayout->addWidget(mListCamera);
 
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     mVideoWidget->setObjectName(QStringLiteral("mVideoWidget"));
     mainLayout->addWidget(mVideoWidget, 1);
-#endif
 
     auto hboxLayout = new QHBoxLayout;
     hboxLayout->setObjectName(QStringLiteral("hboxLayout"));
@@ -70,42 +61,24 @@ CreateVideoMessageWidget::CreateVideoMessageWidget(QWidget *parent)
     hboxLayout->addWidget(mDurationLabel);
     mainLayout->addLayout(hboxLayout);
 
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    mCamera->setObjectName(QStringLiteral("mCamera"));
-
-    auto viewfinder = new QCameraViewfinder(this);
-    viewfinder->show();
-
-    mCamera->setViewfinder(viewfinder);
-    auto imageCapture = new QCameraImageCapture(mCamera, this);
-
-    mCamera->setCaptureMode(QCamera::CaptureStillImage);
-    mCamera->start();
-#endif
-
     mErrorLabel->setObjectName(QStringLiteral("mErrorLabel"));
     mErrorLabel->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Maximum);
     mainLayout->addWidget(mErrorLabel);
     mErrorLabel->setVisible(false);
     updateCameras();
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     setCamera(QMediaDevices::defaultVideoInput());
-#endif
 }
 
 CreateVideoMessageWidget::~CreateVideoMessageWidget() = default;
 
 void CreateVideoMessageWidget::updateCameras()
 {
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     const QList<QCameraDevice> availableCameras = QMediaDevices::videoInputs();
     for (const QCameraDevice &cameraDevice : availableCameras) {
         mListCamera->addItem(cameraDevice.description(), QVariant::fromValue(cameraDevice));
     }
-#endif
 }
 
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
 void CreateVideoMessageWidget::setCamera(const QCameraDevice &cameraDevice)
 {
     mCamera.reset(new QCamera(cameraDevice));
@@ -130,19 +103,15 @@ void CreateVideoMessageWidget::setCamera(const QCameraDevice &cameraDevice)
 
     mCamera->start();
 }
-#endif
 
 void CreateVideoMessageWidget::displayCameraError()
 {
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     if (mCamera->error() != QCamera::NoError) {
         mMessageWidget->setText(mCamera->errorString());
         mMessageWidget->animatedShow();
     }
-#endif
 }
 
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
 void CreateVideoMessageWidget::updateRecorderState(QMediaRecorder::RecorderState state)
 {
 #if 0
@@ -168,7 +137,6 @@ void CreateVideoMessageWidget::updateRecorderState(QMediaRecorder::RecorderState
     }
 #endif
 }
-#endif
 
 void CreateVideoMessageWidget::updateCameraActive(bool active)
 {
@@ -191,30 +159,22 @@ void CreateVideoMessageWidget::stopCamera()
 
 void CreateVideoMessageWidget::record()
 {
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     mMediaRecorder->record();
     updateRecordTime();
-#endif
 }
 
 void CreateVideoMessageWidget::pause()
 {
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     mMediaRecorder->pause();
-#endif
 }
 
 void CreateVideoMessageWidget::stop()
 {
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     mMediaRecorder->stop();
-#endif
 }
 
 void CreateVideoMessageWidget::updateRecordTime()
 {
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     const QString str = i18n("Recorded %1 sec", mMediaRecorder->duration() / 1000);
     mDurationLabel->setText(str);
-#endif
 }
--- src/widgets/dialogs/createvideomessagewidget.h
+++ src/widgets/dialogs/createvideomessagewidget.h
@@ -7,11 +7,9 @@
 #pragma once
 
 #include "libruqolawidgets_private_export.h"
-#include <QWidget>
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
 #include <QMediaCaptureSession>
 #include <QMediaRecorder>
-#endif
+#include <QWidget>
 class QLabel;
 class QCamera;
 class QComboBox;
@@ -31,10 +29,8 @@ private:
     void stopCamera();
     void startCamera();
     void updateCameras();
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     void setCamera(const QCameraDevice &cameraDevice);
     void updateRecorderState(QMediaRecorder::RecorderState state);
-#endif
     void record();
     void pause();
     void stop();
@@ -43,11 +39,9 @@ private:
     void updateCameraActive(bool active);
     void displayCameraError();
 
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     QVideoWidget *const mVideoWidget;
     QScopedPointer<QMediaRecorder> mMediaRecorder;
     QMediaCaptureSession mCaptureSession;
-#endif
     QLabel *const mErrorLabel;
     QScopedPointer<QCamera> mCamera;
     QComboBox *const mListCamera;
