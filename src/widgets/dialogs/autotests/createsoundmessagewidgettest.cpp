2573ba7e40c98045e16556f4dbb05a884df5af3a
Laurent Montel <montel@kde.org>
2023-06-12 08:29:03 +0200

    improve autotests

--- src/widgets/dialogs/autotests/createsoundmessagewidgettest.cpp
+++ src/widgets/dialogs/autotests/createsoundmessagewidgettest.cpp
@@ -6,8 +6,12 @@
 
 #include "createsoundmessagewidgettest.h"
 #include "dialogs/createsoundmessagewidget.h"
+#include <QComboBox>
+#include <QLabel>
 #include <QTest>
+#include <QToolButton>
 #include <QVBoxLayout>
+
 QTEST_MAIN(CreateSoundMessageWidgetTest)
 CreateSoundMessageWidgetTest::CreateSoundMessageWidgetTest(QObject *parent)
     : QObject{parent}
@@ -16,6 +20,25 @@ CreateSoundMessageWidgetTest::CreateSoundMessageWidgetTest(QObject *parent)
 
 void CreateSoundMessageWidgetTest::shouldHaveDefaultValues()
 {
-    // TODO
     CreateSoundMessageWidget w;
+
+    auto mainLayout = w.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->contentsMargins(), QMargins{});
+
+    auto mDeviceComboBox = w.findChild<QComboBox *>(QStringLiteral("mDeviceComboBox"));
+    QVERIFY(mDeviceComboBox);
+
+    auto mRecordButton = w.findChild<QToolButton *>(QStringLiteral("mRecordButton"));
+    QVERIFY(mRecordButton);
+
+    auto mPauseButton = w.findChild<QToolButton *>(QStringLiteral("mPauseButton"));
+    QVERIFY(mPauseButton);
+
+    auto mStopButton = w.findChild<QToolButton *>(QStringLiteral("mStopButton"));
+    QVERIFY(mStopButton);
+
+    auto mLabelDuration = w.findChild<QLabel *>(QStringLiteral("mLabelDuration"));
+    QVERIFY(mLabelDuration);
+    QCOMPARE(mLabelDuration->textFormat(), Qt::PlainText);
 }
