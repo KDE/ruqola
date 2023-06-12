30e8484951b7047d682dfe8b72572875fc1ead64
Laurent Montel <montel@kde.org>
2023-06-12 08:47:23 +0200

    Add autotests

---
+++ src/widgets/dialogs/autotests/createsoundmessagewizardtest.cpp
@@ -0,0 +1,13 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "createsoundmessagewizardtest.h"
+#include <QTest>
+QTEST_MAIN(CreateSoundMessageWizardTest)
+CreateSoundMessageWizardTest::CreateSoundMessageWizardTest(QObject *parent)
+    : QObject{parent}
+{
+}
