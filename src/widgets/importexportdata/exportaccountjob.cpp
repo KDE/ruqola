f3cd55ebb54ace7dac99226772f6cc762460362e
Laurent Montel <montel@kde.org>
2023-06-23 08:12:24 +0200

    Add export account job

---
+++ src/widgets/importexportdata/exportaccountjob.cpp
@@ -0,0 +1,21 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "exportaccountjob.h"
+
+ExportAccountJob::ExportAccountJob(QObject *parent)
+    : QObject{parent}
+{
+}
+
+ExportAccountJob::~ExportAccountJob()
+{
+}
+
+void ExportAccountJob::start()
+{
+    // TODO
+}
---
+++ src/widgets/importexportdata/exportaccountjob.h
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+#include "libruqolawidgets_private_export.h"
+
+#include <QObject>
+
+class LIBRUQOLAWIDGETS_TESTS_EXPORT ExportAccountJob : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ExportAccountJob(QObject *parent = nullptr);
+    ~ExportAccountJob() override;
+
+    void start();
+};
