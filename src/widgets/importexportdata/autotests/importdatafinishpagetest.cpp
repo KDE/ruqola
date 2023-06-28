f2487051e05b87b96807cca36e4cfe757c8cb387
Laurent Montel <montel@kde.org>
2023-06-28 08:37:19 +0200

    Add more autotests

--- src/widgets/importexportdata/autotests/importdatafinishpagetest.cpp
+++ src/widgets/importexportdata/autotests/importdatafinishpagetest.cpp
@@ -7,6 +7,10 @@
 #include "importdatafinishpagetest.h"
 #include "importexportdata/importdata/importdatafinishpage.h"
 #include <QTest>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <KMessageWidget>
+
 QTEST_MAIN(ImportDataFinishPageTest)
 
 ImportDataFinishPageTest::ImportDataFinishPageTest(QObject *parent)
@@ -17,7 +21,18 @@ ImportDataFinishPageTest::ImportDataFinishPageTest(QObject *parent)
 void ImportDataFinishPageTest::shouldHaveDefaultValues()
 {
     ImportDataFinishPage w;
-    // TODO
+
+    auto mainLayout = w.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+
+    auto mInfos = w.findChild<QLabel *>(QStringLiteral("mInfos"));
+    QVERIFY(mInfos);
+    QVERIFY(mInfos->text().isEmpty());
+
+
+    auto mMessageWidget = w.findChild<KMessageWidget *>(QStringLiteral("mMessageWidget"));
+    QVERIFY(mMessageWidget);
+    QVERIFY(!mMessageWidget->isCloseButtonVisible());
 }
 
 #include "moc_importdatafinishpagetest.cpp"
