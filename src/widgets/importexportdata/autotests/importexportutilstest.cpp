7adc51f2d275644bba8fb4881edbd02304e91f1b
Laurent Montel <montel@kde.org>
2023-07-03 07:19:43 +0200

    Rename class

---
+++ src/widgets/importexportdata/autotests/importexportutilstest.cpp
@@ -0,0 +1,23 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "importexportutilstest.h"
+#include "importexportdata/importexportutils.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(ImportExportUtilsTest)
+ImportExportUtilsTest::ImportExportUtilsTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void ImportExportUtilsTest::shouldReturnValues()
+{
+    QCOMPARE(ImportExportUtils::configPath(), QStringLiteral("config"));
+    QCOMPARE(ImportExportUtils::localPath(), QStringLiteral("local"));
+    QCOMPARE(ImportExportUtils::cachePath(), QStringLiteral("cache"));
+    QCOMPARE(ImportExportUtils::logsPath(), QStringLiteral("logs"));
+    QCOMPARE(ImportExportUtils::databasePath(), QStringLiteral("database"));
+}
