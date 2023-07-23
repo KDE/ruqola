775a1094e8c84c305630e03d3711d375342daf89
Laurent Montel <montel@kde.org>
2023-07-23 08:14:26 +0200

    Prepare to add autotest

---
+++ src/core/autotests/manageloadhistorytest.cpp
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "manageloadhistorytest.h"
+#include "manageloadhistory.h"
+#include <QTest>
+QTEST_MAIN(ManageLoadHistoryTest)
+ManageLoadHistoryTest::ManageLoadHistoryTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void ManageLoadHistoryTest::shouldHaveDefaultValues()
+{
+    ManageLoadHistory d(nullptr);
+    // TODO
+}
