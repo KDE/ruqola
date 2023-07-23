6ca1add197ebbc16b33b25a0dce7350cac5d280a
Laurent Montel <montel@kde.org>
2023-07-23 08:36:40 +0200

    Add offlinewidgettest

---
+++ src/widgets/offlinewidget/autotests/offlinewidgettest.cpp
@@ -0,0 +1,21 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "offlinewidgettest.h"
+#include "offlinewidget/offlinewidget.h"
+#include <QTest>
+
+QTEST_MAIN(OffLineWidgetTest)
+
+OffLineWidgetTest::OffLineWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void OffLineWidgetTest::shouldHaveDefaultValues()
+{
+    OffLineWidget w;
+    // TODO
+}
