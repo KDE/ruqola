b244ed9c125b9bad62153184ca242db1586a50d1
Laurent Montel <montel@kde.org>
2023-09-06 13:53:35 +0200

    Fix copyright

--- scripts/ruqola-log.sh
+++ scripts/ruqola-log.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # SPDX-License-Identifier: BSD-3-Clause
-# SPDX-FileCopyrightText: 2018 Montel Laurent <montel@kde.org> 
+# SPDX-FileCopyrightText: 2018-2023 Montel Laurent <montel@kde.org> 
 export RUQOLA_LOGFILE=log_ruqola_file
 ruqola &
