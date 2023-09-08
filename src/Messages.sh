137df73ce57587ad785e22d849091d33d7603f29
Laurent Montel <montel@kde.org>
2023-09-08 08:35:21 +0200

    Extract message from .h too

--- src/Messages.sh
+++ src/Messages.sh
@@ -3,4 +3,4 @@
 # SPDX-FileCopyrightText: 2018-2023 Montel Laurent <montel@kde.org> 
 
 $EXTRACTRC `find . -name "*.ui" -o -name "*.rc"` >> rc.cpp
-$XGETTEXT `find . -name "*.cpp"` -o $podir/ruqola.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/ruqola.pot
