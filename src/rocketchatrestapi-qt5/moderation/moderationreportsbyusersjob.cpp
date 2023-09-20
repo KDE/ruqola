f44697fe1b8778eca83a78a1bcadd3127728b4a1
Laurent Montel <montel@kde.org>
2023-09-20 13:55:08 +0200

    Prepare moderationreportsbyusersjob

---
+++ src/rocketchatrestapi-qt5/moderation/moderationreportsbyusersjob.cpp
@@ -0,0 +1,61 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "moderationreportsbyusersjob.h"
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+using namespace RocketChatRestApi;
+ModerationReportsByUsersJob::ModerationReportsByUsersJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+ModerationReportsByUsersJob::~ModerationReportsByUsersJob() = default;
+
+bool ModerationReportsByUsersJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool ModerationReportsByUsersJob::start()
+{
+    if (!canStart()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "Impossible to start ModerationReportsByUsersJob";
+        deleteLater();
+        return false;
+    }
+    submitGetRequest();
+    addStartRestApiInfo(QByteArrayLiteral("ModerationReportsByUsersJob: Ask for licenses list"));
+    return true;
+}
+
+void ModerationReportsByUsersJob::onGetRequestResponse(const QString &replyErrorString, const QJsonDocument &replyJson)
+{
+    const QJsonObject replyObject = replyJson.object();
+
+    if (replyObject[QLatin1String("success")].toBool()) {
+        addLoggerInfo(QByteArrayLiteral("ModerationReportsByUsersJob: success: ") + replyJson.toJson(QJsonDocument::Indented));
+        Q_EMIT licensesListDone(replyObject);
+    } else {
+        emitFailedMessage(replyErrorString, replyObject);
+        addLoggerWarning(QByteArrayLiteral("ModerationReportsByUsersJob: Problem: ") + replyJson.toJson(QJsonDocument::Indented));
+    }
+}
+
+QNetworkRequest ModerationReportsByUsersJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::LicensesGet);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    addRequestAttribute(request, false);
+
+    return request;
+}
+
+#include "moc_moderationreportsbyusersjob.cpp"
---
+++ src/rocketchatrestapi-qt5/moderation/moderationreportsbyusersjob.h
@@ -0,0 +1,35 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "librocketchatrestapi-qt5_export.h"
+#include "restapiabstractjob.h"
+
+#include <QNetworkRequest>
+namespace RocketChatRestApi
+{
+class LIBROCKETCHATRESTAPI_QT5_EXPORT ModerationReportsByUsersJob : public RestApiAbstractJob
+{
+    Q_OBJECT
+public:
+    explicit ModerationReportsByUsersJob(QObject *parent = nullptr);
+    ~ModerationReportsByUsersJob() override;
+
+    Q_REQUIRED_RESULT bool requireHttpAuthentication() const override;
+
+    Q_REQUIRED_RESULT bool start() override;
+
+    Q_REQUIRED_RESULT QNetworkRequest request() const override;
+
+Q_SIGNALS:
+    void licensesListDone(const QJsonObject &obj);
+
+private:
+    Q_DISABLE_COPY(ModerationReportsByUsersJob)
+    LIBROCKETCHATRESTAPI_QT5_NO_EXPORT void onGetRequestResponse(const QString &replyErrorString, const QJsonDocument &replyJson) override;
+};
+}
