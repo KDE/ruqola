ee39e5d31381355e0d52ce20328422a7be791c03
Laurent Montel <montel@kde.org>
2023-09-28 14:02:44 +0200

    Prepare moderationlistmessages

---
+++ src/core/moderationlistmessages.cpp
@@ -0,0 +1,10 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "moderationlistmessages.h"
+
+ModerationListMessages::ModerationListMessages() = default;
+
+ModerationListMessages::~ModerationListMessages() = default;
---
+++ src/core/moderationlistmessages.h
@@ -0,0 +1,16 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "listmessages.h"
+
+class ModerationListMessages : public ListMessages
+{
+public:
+    ModerationListMessages();
+    ~ModerationListMessages() override;
+};
