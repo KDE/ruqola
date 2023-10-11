0a8c8f2822206086e0b76a3124c3a6d77467c36d
Laurent Montel <montel@kde.org>
2023-10-11 08:34:10 +0200

    Add parsing

--- src/core/moderation/moderationreportinfo.cpp
+++ src/core/moderation/moderationreportinfo.cpp
@@ -17,6 +17,12 @@ bool ModerationReportInfo::operator==(const ModerationReportInfo &other) const
 
 void ModerationReportInfo::parseModerationReportInfo(const QJsonObject &o)
 {
+    mReportIdentifier = o[QLatin1String("o")].toString();
+    mDescription = o[QLatin1String("description")].toString();
+    // TODO add more
+    //{"reports":[{"_id":"6523e2465d66533fd6f85856","description":"test message report\n","reportedBy":{"_id":"H7Q9djXQ4zD9T2","username":"bla","name":"foo",
+    // "createdAt":"2018-03-13T16:11:51.761Z"},"room":{"_id":"bBvCRnStXcG68zjna","name":"roomname","t":"p","fname":"roomname"},
+    // "ts":"2023-10-09T11:21:42.135Z"}],"count":1,"offset":0,"total":1,"success":true}
     // TODO
 }
 
