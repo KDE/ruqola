c4a9652394e666e66489b122c629c15eb52210ad
Laurent Montel <montel@kde.org>
2023-10-26 22:48:20 +0200

    Prepare to add moderationreportinfowidget

---
+++ src/widgets/administratordialog/moderationconsole/moderationreportinfowidget.cpp
@@ -0,0 +1,116 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "moderationreportinfowidget.h"
+#include "misc/lineeditcatchreturnkey.h"
+#include "misc/serverscombobox.h"
+#include "model/notificationhistorymodel.h"
+#include "model/notificationhistorymodelfilterproxymodel.h"
+#include "notificationhistorylistview.h"
+#include "notificationhistorymanager.h"
+#include "ruqolawidgets_debug.h"
+#include <KLocalizedString>
+#include <QLineEdit>
+#include <QListView>
+#include <QVBoxLayout>
+
+#include <config-ruqola.h>
+
+#if HAVE_TEXT_TO_SPEECH
+#include <TextEditTextToSpeech/TextToSpeechContainerWidget>
+#endif
+
+ModerationReportInfoWidget::ModerationReportInfoWidget(QWidget *parent)
+    : QWidget{parent}
+    , mListNotificationsListView(new NotificationHistoryListView(this))
+    , mSearchLineEdit(new QLineEdit(this))
+    , mNotificationFilterProxyModel(new NotificationHistoryModelFilterProxyModel(this))
+#if HAVE_TEXT_TO_SPEECH
+    , mTextToSpeechWidget(new TextEditTextToSpeech::TextToSpeechContainerWidget(this))
+#endif
+    , mServersComboBox(new ServersComboBox(this))
+{
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(QStringLiteral("mainLayout"));
+    mainLayout->setContentsMargins({});
+
+    auto searchLayout = new QHBoxLayout;
+    searchLayout->setObjectName(QStringLiteral("searchLayout"));
+    searchLayout->setContentsMargins({});
+
+    mSearchLineEdit->setObjectName(QStringLiteral("mSearchLineEdit"));
+    mSearchLineEdit->setPlaceholderText(i18n("Search..."));
+    searchLayout->addWidget(mSearchLineEdit);
+    mSearchLineEdit->setClearButtonEnabled(true);
+    new LineEditCatchReturnKey(mSearchLineEdit, this);
+    mServersComboBox->setObjectName(QStringLiteral("mServersComboBox"));
+    searchLayout->addWidget(mServersComboBox);
+
+    mainLayout->addLayout(searchLayout);
+
+#if HAVE_TEXT_TO_SPEECH
+    mTextToSpeechWidget->setObjectName(QStringLiteral("mTextToSpeechWidget"));
+    mainLayout->addWidget(mTextToSpeechWidget);
+    connect(mListNotificationsListView,
+            &NotificationHistoryListView::textToSpeech,
+            mTextToSpeechWidget,
+            &TextEditTextToSpeech::TextToSpeechContainerWidget::say);
+#endif
+
+    mListNotificationsListView->setObjectName(QStringLiteral("mListNotifications"));
+    mainLayout->addWidget(mListNotificationsListView);
+
+    auto model = NotificationHistoryManager::self()->notificationHistoryModel();
+
+    mNotificationFilterProxyModel->setObjectName(QStringLiteral("mNotificationFilterProxyModel"));
+    mNotificationFilterProxyModel->setSourceModel(model);
+    mListNotificationsListView->setModel(mNotificationFilterProxyModel);
+
+    connect(mListNotificationsListView, &QListView::doubleClicked, this, &ModerationReportInfoWidget::slotShowMessage);
+    connect(mListNotificationsListView, &NotificationHistoryListView::showMessage, this, &ModerationReportInfoWidget::slotShowMessage);
+
+    connect(model, &QAbstractItemModel::rowsAboutToBeInserted, mListNotificationsListView, &MessageListViewBase::checkIfAtBottom);
+    connect(model, &QAbstractItemModel::rowsAboutToBeRemoved, mListNotificationsListView, &MessageListViewBase::checkIfAtBottom);
+    connect(model, &QAbstractItemModel::modelAboutToBeReset, mListNotificationsListView, &MessageListViewBase::checkIfAtBottom);
+
+    connect(mSearchLineEdit, &QLineEdit::textChanged, this, &ModerationReportInfoWidget::slotTextChanged);
+
+    connect(mServersComboBox, &ServersComboBox::accountSelected, this, &ModerationReportInfoWidget::slotFilterAccount);
+}
+
+ModerationReportInfoWidget::~ModerationReportInfoWidget() = default;
+
+void ModerationReportInfoWidget::slotFilterAccount(const QString &accountName)
+{
+    mNotificationFilterProxyModel->setAccountNameFilter(accountName);
+}
+
+void ModerationReportInfoWidget::slotTextChanged(const QString &str)
+{
+    mNotificationFilterProxyModel->setFilterString(str);
+    mListNotificationsListView->setSearchText(str);
+}
+
+void ModerationReportInfoWidget::slotShowMessage(const QModelIndex &index)
+{
+    if (index.isValid()) {
+        const QString roomId = index.data(NotificationHistoryModel::RoomId).toString();
+        const QString messageId = index.data(NotificationHistoryModel::MessageId).toString();
+        const QString accountName = index.data(NotificationHistoryModel::AccountName).toString();
+        if (!accountName.isEmpty() && !roomId.isEmpty() && !messageId.isEmpty()) {
+            Q_EMIT showNotifyMessage(accountName, messageId, roomId);
+        } else {
+            qCWarning(RUQOLAWIDGETS_LOG) << " Problem with index. AccountName " << accountName << " roomId : " << roomId << "messageId " << messageId;
+        }
+    }
+}
+
+void ModerationReportInfoWidget::addServerList(const QStringList &serverNames)
+{
+    mServersComboBox->addServerList(serverNames);
+}
+
+#include "moc_notificationhistorywidget.cpp"
---
+++ src/widgets/administratordialog/moderationconsole/moderationreportinfowidget.h
@@ -0,0 +1,45 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QWidget>
+
+#include "libruqolawidgets_private_export.h"
+class NotificationHistoryListView;
+class QLineEdit;
+class ServersComboBox;
+class NotificationHistoryModelFilterProxyModel;
+
+namespace TextEditTextToSpeech
+{
+class TextToSpeechContainerWidget;
+}
+
+class LIBRUQOLAWIDGETS_TESTS_EXPORT ModerationReportInfoWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ModerationReportInfoWidget(QWidget *parent = nullptr);
+    ~ModerationReportInfoWidget() override;
+
+    void addServerList(const QStringList &serverNames);
+
+Q_SIGNALS:
+    void showNotifyMessage(const QString &accountName, const QString &messageId, const QString &roomId);
+
+private:
+    void slotTextToSpeech(const QString &messageText);
+    void slotCustomContextMenuRequested(const QPoint &pos);
+    void slotShowMessage(const QModelIndex &index);
+    void slotTextChanged(const QString &str);
+    void slotFilterAccount(const QString &accountName);
+    NotificationHistoryListView *const mListNotificationsListView;
+    QLineEdit *const mSearchLineEdit;
+    NotificationHistoryModelFilterProxyModel *const mNotificationFilterProxyModel;
+    TextEditTextToSpeech::TextToSpeechContainerWidget *const mTextToSpeechWidget = nullptr;
+    ServersComboBox *const mServersComboBox;
+};
