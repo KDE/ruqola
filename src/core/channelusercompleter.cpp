b088893ebc6d662af7bd9da8298abb7a56375913
Laurent Montel <montel@kde.org>
2023-11-07 13:29:42 +0100

    Implement channelusercompleter

--- src/core/channelusercompleter.cpp
+++ src/core/channelusercompleter.cpp
@@ -5,6 +5,8 @@
 */
 
 #include "channelusercompleter.h"
+#include "utils.h"
+#include <QJsonObject>
 
 ChannelUserCompleter::ChannelUserCompleter() = default;
 
@@ -12,11 +14,82 @@ ChannelUserCompleter::~ChannelUserCompleter() = default;
 
 QDebug operator<<(QDebug d, const ChannelUserCompleter &t)
 {
-    // TODO
+    d << "Type " << t.type();
+    d << "Name " << t.name();
+    d << "description " << t.description();
+    d << "userName " << t.userName();
     return d;
 }
 
-void ChannelUserCompleter::parseChannel(const QJsonObject &object, ChannelUserCompleterType)
+void ChannelUserCompleter::parseChannel(const QJsonObject &object, ChannelUserCompleterType type)
 {
-    // TODO
+    mType = type;
+    mName = object.value(QLatin1String("name")).toString();
+    if (mType == ChannelUserCompleterType::DirectChannel) {
+        mUserName = object.value(QLatin1String("username")).toString();
+        mStatusIcon = QIcon::fromTheme(Utils::iconFromStatus(object.value(QLatin1String("status")).toString()));
+    } else {
+        const QString roomType = object.value(QLatin1String("t")).toString();
+        if (roomType == QLatin1Char('c')) {
+            mStatusIcon = QIcon::fromTheme(QStringLiteral("irc-channel-inactive"));
+        } else if (roomType == QLatin1Char('p')) {
+            mStatusIcon = QIcon::fromTheme(QStringLiteral("lock"));
+        }
+    }
+    // TODO mChannelUserIcon
+}
+
+bool ChannelUserCompleter::operator==(const ChannelUserCompleter &other) const
+{
+    return (mType == other.type()) && (mName == other.name()) && (mDescription == other.description() && (mUserName == other.userName()));
+}
+
+QString ChannelUserCompleter::description() const
+{
+    return mDescription;
+}
+
+void ChannelUserCompleter::setDescription(const QString &newDescription)
+{
+    mDescription = newDescription;
+}
+
+QString ChannelUserCompleter::name() const
+{
+    return mName;
+}
+
+void ChannelUserCompleter::setName(const QString &newName)
+{
+    mName = newName;
+}
+
+ChannelUserCompleter::ChannelUserCompleterType ChannelUserCompleter::type() const
+{
+    return mType;
+}
+
+void ChannelUserCompleter::setType(ChannelUserCompleterType newType)
+{
+    mType = newType;
+}
+
+QIcon ChannelUserCompleter::statusIcon() const
+{
+    return mStatusIcon;
+}
+
+QIcon ChannelUserCompleter::channelUserIcon() const
+{
+    return mChannelUserIcon;
+}
+
+QString ChannelUserCompleter::userName() const
+{
+    return mUserName;
+}
+
+void ChannelUserCompleter::setUserName(const QString &newUserName)
+{
+    mUserName = newUserName;
 }
--- src/core/channelusercompleter.h
+++ src/core/channelusercompleter.h
@@ -8,6 +8,7 @@
 
 #include "libruqolacore_export.h"
 #include <QDebug>
+#include <QIcon>
 
 class LIBRUQOLACORE_EXPORT ChannelUserCompleter
 {
@@ -17,13 +18,40 @@ public:
     ~ChannelUserCompleter();
 
     enum ChannelUserCompleterType {
+        Unknown,
         Room,
         DirectChannel,
-        Unknown,
     };
     Q_ENUM(ChannelUserCompleterType)
 
     void parseChannel(const QJsonObject &object, ChannelUserCompleter::ChannelUserCompleterType);
+
+    [[nodiscard]] bool operator==(const ChannelUserCompleter &other) const;
+
+    [[nodiscard]] QString description() const;
+    void setDescription(const QString &newDescription);
+
+    [[nodiscard]] QString name() const;
+    void setName(const QString &newName);
+
+    [[nodiscard]] ChannelUserCompleterType type() const;
+    void setType(ChannelUserCompleterType newType);
+
+    [[nodiscard]] QIcon statusIcon() const;
+
+    [[nodiscard]] QIcon channelUserIcon() const;
+
+    [[nodiscard]] QString userName() const;
+    void setUserName(const QString &newUserName);
+
+private:
+    QString mDescription;
+    QString mName;
+    QString mUserName;
+    QIcon mStatusIcon;
+    QIcon mChannelUserIcon;
+
+    ChannelUserCompleterType mType = ChannelUserCompleterType::Unknown;
 };
 Q_DECLARE_TYPEINFO(ChannelUserCompleter, Q_MOVABLE_TYPE);
 LIBRUQOLACORE_EXPORT QDebug operator<<(QDebug d, const ChannelUserCompleter &t);
