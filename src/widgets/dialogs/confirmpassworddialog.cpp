80f5834403b9cf14032fb822efb029a6c9dffd2a
Carl Schwan <carl@carlschwan.eu>
2023-11-15 12:29:43 +0100

    Fix compilation with Qt 6.7 and QT_NO_CONTEXTLESS_CONNECT

--- src/widgets/dialogs/confirmpassworddialog.cpp
+++ src/widgets/dialogs/confirmpassworddialog.cpp
@@ -30,7 +30,7 @@ ConfirmPasswordDialog::ConfirmPasswordDialog(QWidget *parent)
     mainLayout->addWidget(buttonBox);
     auto okButton = buttonBox->button(QDialogButtonBox::Ok);
     okButton->setEnabled(false);
-    connect(mConfirmPasswordWidget, &ConfirmPasswordWidget::enabledOkButton, [okButton](bool enabled) {
+    connect(mConfirmPasswordWidget, &ConfirmPasswordWidget::enabledOkButton, this, [okButton](bool enabled) {
         okButton->setEnabled(enabled);
     });
 }
