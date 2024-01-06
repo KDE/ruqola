25080b547f302fa44aa7f3c54405f511a1b60cbf
Laurent Montel <montel@kde.org>
2024-01-06 20:01:22 +0100

    Prepare to implement messagedelegatehelperurlpreview

---
+++ src/widgets/room/delegate/messagedelegatehelperurlpreview.cpp
@@ -0,0 +1,11 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "messagedelegatehelperurlpreview.h"
+
+MessageDelegateHelperUrlPreview::MessageDelegateHelperUrlPreview()
+{
+}
---
+++ src/widgets/room/delegate/messagedelegatehelperurlpreview.h
@@ -0,0 +1,13 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+class MessageDelegateHelperUrlPreview
+{
+public:
+    MessageDelegateHelperUrlPreview();
+};
