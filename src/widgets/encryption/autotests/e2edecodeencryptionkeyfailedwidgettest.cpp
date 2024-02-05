12943b617e7573899428c6b319b13cd352681d75
Laurent Montel <montel@kde.org>
2024-02-05 17:23:13 +0100

    improve autotest

--- src/widgets/encryption/autotests/e2edecodeencryptionkeyfailedwidgettest.cpp
+++ src/widgets/encryption/autotests/e2edecodeencryptionkeyfailedwidgettest.cpp
@@ -6,6 +6,7 @@
 
 #include "e2edecodeencryptionkeyfailedwidgettest.h"
 #include "encryption/e2edecodeencryptionkeyfailedwidget.h"
+#include <QAction>
 #include <QTest>
 QTEST_MAIN(E2eDecodeEncryptionKeyFailedWidgetTest)
 E2eDecodeEncryptionKeyFailedWidgetTest::E2eDecodeEncryptionKeyFailedWidgetTest(QObject *parent)
@@ -21,6 +22,9 @@ void E2eDecodeEncryptionKeyFailedWidgetTest::shouldHaveDefaultValues()
     QCOMPARE(w.position(), KMessageWidget::Header);
     QVERIFY(!w.text().isEmpty());
     QVERIFY(w.isCloseButtonVisible());
+
+    auto decodeEncryptionKeyAction = w.findChild<QAction *>(QStringLiteral("decodeEncryptionKeyAction"));
+    QVERIFY(decodeEncryptionKeyAction);
 }
 
 #include "moc_e2edecodeencryptionkeyfailedwidgettest.cpp"
