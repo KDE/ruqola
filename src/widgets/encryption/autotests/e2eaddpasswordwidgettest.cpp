acc8c96bf2aae0700d1593338d127903dd3904dd
Laurent Montel <montel@kde.org>
2024-02-05 15:36:35 +0100

    Test Signal

--- src/widgets/encryption/autotests/e2eaddpasswordwidgettest.cpp
+++ src/widgets/encryption/autotests/e2eaddpasswordwidgettest.cpp
@@ -6,6 +6,8 @@
 
 #include "e2eaddpasswordwidgettest.h"
 #include "encryption/e2eaddpasswordwidget.h"
+#include <QAction>
+#include <QSignalSpy>
 #include <QTest>
 QTEST_MAIN(E2eAddPasswordWidgetTest)
 E2eAddPasswordWidgetTest::E2eAddPasswordWidgetTest(QObject *parent)
@@ -21,6 +23,18 @@ void E2eAddPasswordWidgetTest::shouldHaveDefaultValues()
     QCOMPARE(w.position(), KMessageWidget::Header);
     QVERIFY(!w.text().isEmpty());
     QVERIFY(!w.isCloseButtonVisible());
+
+    auto savePasswordAction = w.findChild<QAction *>(QStringLiteral("refreshKeysAction"));
+    QVERIFY(savePasswordAction);
+}
+
+void E2eAddPasswordWidgetTest::shouldEmitSaveSignal()
+{
+    E2eAddPasswordWidget w;
+    QSignalSpy saveSignal(&w, &E2eAddPasswordWidget::savePassword);
+    auto savePasswordAction = w.findChild<QAction *>(QStringLiteral("refreshKeysAction"));
+    savePasswordAction->trigger();
+    QCOMPARE(saveSignal.count(), 1);
 }
 
 #include "moc_e2eaddpasswordwidgettest.cpp"
--- src/widgets/encryption/autotests/e2eaddpasswordwidgettest.h
+++ src/widgets/encryption/autotests/e2eaddpasswordwidgettest.h
@@ -16,4 +16,5 @@ public:
     ~E2eAddPasswordWidgetTest() override = default;
 private Q_SLOTS:
     void shouldHaveDefaultValues();
+    void shouldEmitSaveSignal();
 };
