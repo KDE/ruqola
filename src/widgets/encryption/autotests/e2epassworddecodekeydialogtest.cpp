8cb2628cc91ee22258122472c32791f4e214756a
Laurent Montel <montel@kde.org>
2024-02-08 07:49:42 +0100

    Add autotest

--- src/widgets/encryption/autotests/e2epassworddecodekeydialogtest.cpp
+++ src/widgets/encryption/autotests/e2epassworddecodekeydialogtest.cpp
@@ -6,7 +6,10 @@
 
 #include "e2epassworddecodekeydialogtest.h"
 #include "encryption/e2epassworddecodekeydialog.h"
+#include "encryption/e2epassworddecodekeywidget.h"
+#include <QDialogButtonBox>
 #include <QTest>
+#include <QVBoxLayout>
 QTEST_MAIN(E2ePasswordDecodeKeyDialogTest)
 E2ePasswordDecodeKeyDialogTest::E2ePasswordDecodeKeyDialogTest(QObject *parent)
     : QObject{parent}
@@ -15,8 +18,17 @@ E2ePasswordDecodeKeyDialogTest::E2ePasswordDecodeKeyDialogTest(QObject *parent)
 
 void E2ePasswordDecodeKeyDialogTest::shouldHaveDefaultValues()
 {
-    E2ePasswordDecodeKeyDialog w;
-    // TODO
+    E2ePasswordDecodeKeyDialog d;
+    QVERIFY(!d.windowTitle().isEmpty());
+
+    auto mainLayout = d.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+
+    auto mE2ePasswordDecodeKeyWidget = d.findChild<E2ePasswordDecodeKeyWidget *>(QStringLiteral("mE2ePasswordDecodeKeyWidget"));
+    QVERIFY(mE2ePasswordDecodeKeyWidget);
+
+    auto button = d.findChild<QDialogButtonBox *>(QStringLiteral("button"));
+    QVERIFY(button);
 }
 
 #include "moc_e2epassworddecodekeydialogtest.cpp"
