186bc44afd6fc40c1f1fe03f6654200c93651b6f
Laurent Montel <montel@kde.org>
2024-02-08 08:03:40 +0100

    Add more autotest

--- src/widgets/encryption/autotests/e2epassworddecodekeywidgettest.cpp
+++ src/widgets/encryption/autotests/e2epassworddecodekeywidgettest.cpp
@@ -6,7 +6,10 @@
 
 #include "e2epassworddecodekeywidgettest.h"
 #include "encryption/e2epassworddecodekeywidget.h"
+#include <KPasswordLineEdit>
+#include <QLabel>
 #include <QTest>
+#include <QVBoxLayout>
 QTEST_MAIN(E2ePasswordDecodeKeyWidgetTest)
 
 E2ePasswordDecodeKeyWidgetTest::E2ePasswordDecodeKeyWidgetTest(QObject *parent)
@@ -18,6 +21,19 @@ void E2ePasswordDecodeKeyWidgetTest::shouldHaveDefaultValues()
 {
     E2ePasswordDecodeKeyWidget w;
     // TODO
+
+    auto mainLayout = w.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->contentsMargins(), QMargins{});
+
+    auto label = w.findChild<QLabel *>(QStringLiteral("label"));
+    QVERIFY(label);
+    QVERIFY(!label->text().isEmpty());
+    QVERIFY(label->wordWrap());
+
+    auto mPassword = w.findChild<KPasswordLineEdit *>(QStringLiteral("mPassword"));
+    QVERIFY(mPassword);
+    QVERIFY(mPassword->password().isEmpty());
 }
 
 #include "moc_e2epassworddecodekeywidgettest.cpp"
