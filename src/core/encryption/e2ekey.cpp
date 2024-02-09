f092b53fdefbded3baf76b2dac3cc8891178a9d8
Laurent Montel <montel@kde.org>
2024-02-09 13:52:01 +0100

    Add e2ekey

---
+++ src/core/encryption/e2ekey.cpp
@@ -0,0 +1,13 @@
+/*
+  SPDX-FileCopyrightText: 2024 Laurent Montel <montel.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "e2ekey.h"
+
+E2eKey::E2eKey()
+{
+}
+
+E2eKey::~E2eKey() = default;
---
+++ src/core/encryption/e2ekey.h
@@ -0,0 +1,15 @@
+/*
+  SPDX-FileCopyrightText: 2024 Laurent Montel <montel.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+#include "libruqolacore_export.h"
+
+class LIBRUQOLACORE_EXPORT E2eKey
+{
+public:
+    E2eKey();
+    ~E2eKey();
+};
