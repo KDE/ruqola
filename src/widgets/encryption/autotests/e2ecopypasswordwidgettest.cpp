457d4036cacbbb53891aed5b74486f4c43e608f9
Laurent Montel <montel@kde.org>
2024-02-13 14:49:35 +0100

    Add autotests

--- src/widgets/encryption/autotests/e2ecopypasswordwidgettest.cpp
+++ src/widgets/encryption/autotests/e2ecopypasswordwidgettest.cpp
@@ -6,7 +6,9 @@
 
 #include "e2ecopypasswordwidgettest.h"
 #include "encryption/e2ecopypasswordwidget.h"
+#include <QLabel>
 #include <QTest>
+#include <QVBoxLayout>
 QTEST_MAIN(E2eCopyPasswordWidgetTest)
 E2eCopyPasswordWidgetTest::E2eCopyPasswordWidgetTest(QObject *parent)
     : QObject{parent}
@@ -16,7 +18,15 @@ E2eCopyPasswordWidgetTest::E2eCopyPasswordWidgetTest(QObject *parent)
 void E2eCopyPasswordWidgetTest::shouldHaveDefaultValues()
 {
     E2eCopyPasswordWidget w;
-    // TODO
+
+    auto mainLayout = w.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->contentsMargins(), QMargins{});
+
+    auto label = w.findChild<QLabel *>(QStringLiteral("label"));
+    QVERIFY(label);
+    QVERIFY(!label->text().isEmpty());
+    QCOMPARE(label->textFormat(), Qt::RichText);
 }
 
 #include "moc_e2ecopypasswordwidgettest.cpp"
