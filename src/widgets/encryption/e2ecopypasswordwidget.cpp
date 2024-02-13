67e52e376d72f76a18153d9dfa27957bf7cd4fd9
Laurent Montel <montel@kde.org>
2024-02-13 14:43:22 +0100

    Add label

--- src/widgets/encryption/e2ecopypasswordwidget.cpp
+++ src/widgets/encryption/e2ecopypasswordwidget.cpp
@@ -15,6 +15,16 @@ E2eCopyPasswordWidget::E2eCopyPasswordWidget(QWidget *parent)
     auto mainLayout = new QVBoxLayout(this);
     mainLayout->setObjectName(QStringLiteral("mainLayout"));
     mainLayout->setContentsMargins({});
+    auto label = new QLabel(this);
+    label->setObjectName(QStringLiteral("label"));
+    label->setTextFormat(Qt::RichText);
+    label->setText(
+        i18n("Create secure private rooms and direct messages with end-to-end encryption.<br/><br/>Save your password securely, as the key to encode/decode "
+             "your messages won't be saved on the server. You'll need to enter it on other devices to use e2e encryption.<br/><br/>Change your password "
+             "anytime from "
+             "any browser you've entered it on. Remember to store your password before dismissing this message.<br/><br/>Your password is: <span "
+             "style=\"font-weight: bold;\">{{randomPassword}}</span>"));
+    mainLayout->addWidget(label);
 }
 
 E2eCopyPasswordWidget::~E2eCopyPasswordWidget() = default;
