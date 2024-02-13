e285f18a6abd95f82039459cbc612a82e62c89a0
Laurent Montel <montel@kde.org>
2024-02-13 14:51:38 +0100

    Fix i18n

--- src/widgets/encryption/e2ecopypasswordwidget.cpp
+++ src/widgets/encryption/e2ecopypasswordwidget.cpp
@@ -18,12 +18,14 @@ E2eCopyPasswordWidget::E2eCopyPasswordWidget(QWidget *parent)
     auto label = new QLabel(this);
     label->setObjectName(QStringLiteral("label"));
     label->setTextFormat(Qt::RichText);
+    QString randomPassword; // TODO
     label->setText(
         i18n("Create secure private rooms and direct messages with end-to-end encryption.<br/><br/>Save your password securely, as the key to encode/decode "
              "your messages won't be saved on the server. You'll need to enter it on other devices to use e2e encryption.<br/><br/>Change your password "
              "anytime from "
              "any browser you've entered it on. Remember to store your password before dismissing this message.<br/><br/>Your password is: <span "
-             "style=\"font-weight: bold;\">{{randomPassword}}</span>"));
+             "style=\"font-weight: bold;\">%1</span>",
+             randomPassword));
     mainLayout->addWidget(label);
 }
 
