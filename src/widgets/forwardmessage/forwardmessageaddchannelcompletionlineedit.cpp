7769ca6d77a1482015c3591a3d767ce2d51f12f8
Laurent Montel <montel@kde.org>
2024-02-15 11:36:54 +0100

    Continue to implement completion lineeedit

--- src/widgets/forwardmessage/forwardmessageaddchannelcompletionlineedit.cpp
+++ src/widgets/forwardmessage/forwardmessageaddchannelcompletionlineedit.cpp
@@ -7,23 +7,51 @@
 #include "forwardmessageaddchannelcompletionlineedit.h"
 #include "forwardmessagechannelmodel.h"
 #include <KLocalizedString>
+#include <QTimer>
+#include <chrono>
+
+using namespace std::chrono_literals;
 
 ForwardMessageAddChannelCompletionLineEdit::ForwardMessageAddChannelCompletionLineEdit(QWidget *parent)
     : CompletionLineEdit(parent)
     , mForwardMessageChannelModel(new ForwardMessageChannelModel(this))
+    , mSearchTimer(new QTimer(this))
 {
     setPlaceholderText(i18n("Search rooms..."));
     setCompletionModel(mForwardMessageChannelModel);
     connect(this, &ForwardMessageAddChannelCompletionLineEdit::complete, this, &ForwardMessageAddChannelCompletionLineEdit::slotComplete);
+    connect(mSearchTimer, &QTimer::timeout, this, &ForwardMessageAddChannelCompletionLineEdit::slotSearchTimerFired);
+    connect(this, &QLineEdit::textChanged, this, &ForwardMessageAddChannelCompletionLineEdit::slotSearchTextEdited);
 }
 
 ForwardMessageAddChannelCompletionLineEdit::~ForwardMessageAddChannelCompletionLineEdit() = default;
 
+void ForwardMessageAddChannelCompletionLineEdit::slotSearchTimerFired()
+{
+    mSearchTimer->stop();
+    slotTextChanged(text());
+}
+
+void ForwardMessageAddChannelCompletionLineEdit::slotSearchTextEdited()
+{
+    if (mSearchTimer->isActive()) {
+        mSearchTimer->stop(); // eventually
+    }
+
+    mSearchTimer->setSingleShot(true);
+    mSearchTimer->start(500ms);
+}
+
+void ForwardMessageAddChannelCompletionLineEdit::slotTextChanged(const QString &text)
+{
+    // TODO
+}
+
 void ForwardMessageAddChannelCompletionLineEdit::slotComplete(const QModelIndex &index)
 {
 #if 0
-    const QString completerName = index.data(TeamRoomCompleterModel::TeamName).toString();
-    const QString roomId = index.data(TeamRoomCompleterModel::TeamId).toString();
+    const QString completerName = index.data(ForwardMessageChannelModel::TeamName).toString();
+    const QString roomId = index.data(ForwardMessageChannelModel::TeamId).toString();
     RoomCompletionInfo info;
     info.roomName = completerName;
     info.roomId = roomId;
--- src/widgets/forwardmessage/forwardmessageaddchannelcompletionlineedit.h
+++ src/widgets/forwardmessage/forwardmessageaddchannelcompletionlineedit.h
@@ -9,6 +9,7 @@
 #include "common/completionlineedit.h"
 #include "libruqolawidgets_private_export.h"
 class ForwardMessageChannelModel;
+class QTimer;
 class LIBRUQOLAWIDGETS_TESTS_EXPORT ForwardMessageAddChannelCompletionLineEdit : public CompletionLineEdit
 {
     Q_OBJECT
@@ -18,5 +19,9 @@ public:
 
 private:
     LIBRUQOLAWIDGETS_NO_EXPORT void slotComplete(const QModelIndex &index);
+    LIBRUQOLAWIDGETS_NO_EXPORT void slotSearchTimerFired();
+    LIBRUQOLAWIDGETS_NO_EXPORT void slotSearchTextEdited();
+    LIBRUQOLAWIDGETS_NO_EXPORT void slotTextChanged(const QString &text);
     ForwardMessageChannelModel *const mForwardMessageChannelModel;
+    QTimer *const mSearchTimer;
 };
