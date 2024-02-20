5a3c49ab994b90e80a1675b12380f2247daae4ab
Laurent Montel <montel@kde.org>
2024-02-20 13:18:11 +0100

    Use Q_RELOCATABLE_TYPE

--- src/rocketchatrestapi-qt/misc/oauthappscreatejob.h
+++ src/rocketchatrestapi-qt/misc/oauthappscreatejob.h
@@ -45,4 +45,4 @@ private:
     OauthAppsCreateInfo mOauthAppsCreateInfo;
 };
 }
-Q_DECLARE_TYPEINFO(RocketChatRestApi::OauthAppsCreateJob::OauthAppsCreateInfo, Q_MOVABLE_TYPE);
+Q_DECLARE_TYPEINFO(RocketChatRestApi::OauthAppsCreateJob::OauthAppsCreateInfo, Q_RELOCATABLE_TYPE);
