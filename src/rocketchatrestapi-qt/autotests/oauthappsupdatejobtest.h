d4d01b76d78a4447f70a6987f390bb0f42bda826
Laurent Montel <montel@kde.org>
2024-02-20 13:27:10 +0100

    Add oauthappsupdatejob
    (cherry picked from commit 9cffa0ef1718546abac43136700043172c9a5bbe)

---
+++ src/rocketchatrestapi-qt/autotests/oauthappsupdatejobtest.h
@@ -0,0 +1,22 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+
+class OauthAppsUpdateJobTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit OauthAppsUpdateJobTest(QObject *parent = nullptr);
+    ~OauthAppsUpdateJobTest() override = default;
+private Q_SLOTS:
+    void shouldHaveDefaultValue();
+    void shouldGenerateRequest();
+    void shouldGenerateJson();
+    void shouldNotStarting();
+};
