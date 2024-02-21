0a62ed230215e7c0d951353dec41fb1dc5053c7e
Laurent Montel <montel@kde.org>
2024-02-21 13:44:21 +0100

    Add licensesinfojob

---
+++ src/rocketchatrestapi-qt/license/licensesinfojob.cpp
@@ -0,0 +1,61 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "licensesinfojob.h"
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkReply>
+using namespace RocketChatRestApi;
+LicensesInfoJob::LicensesInfoJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+LicensesInfoJob::~LicensesInfoJob() = default;
+
+bool LicensesInfoJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool LicensesInfoJob::start()
+{
+    if (!canStart()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "Impossible to start LicensesInfoJob";
+        deleteLater();
+        return false;
+    }
+    submitGetRequest();
+    addStartRestApiInfo(QByteArrayLiteral("LicensesInfoJob: Ask for licenses info."));
+    return true;
+}
+
+void LicensesInfoJob::onGetRequestResponse(const QString &replyErrorString, const QJsonDocument &replyJson)
+{
+    const QJsonObject replyObject = replyJson.object();
+
+    if (replyObject[QLatin1String("success")].toBool()) {
+        addLoggerInfo(QByteArrayLiteral("LicensesInfoJob: success: ") + replyJson.toJson(QJsonDocument::Indented));
+        Q_EMIT licensesInfoDone(replyObject);
+    } else {
+        emitFailedMessage(replyErrorString, replyObject);
+        addLoggerWarning(QByteArrayLiteral("LicensesInfoJob: Problem: ") + replyJson.toJson(QJsonDocument::Indented));
+    }
+}
+
+QNetworkRequest LicensesInfoJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::LicensesInfo);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    addRequestAttribute(request, false);
+
+    return request;
+}
+
+#include "moc_licensesinfojob.cpp"
---
+++ src/rocketchatrestapi-qt/license/licensesinfojob.h
@@ -0,0 +1,34 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "librocketchatrestapi-qt_export.h"
+#include "restapiabstractjob.h"
+
+namespace RocketChatRestApi
+{
+class LIBROCKETCHATRESTAPI_QT_EXPORT LicensesInfoJob : public RestApiAbstractJob
+{
+    Q_OBJECT
+public:
+    explicit LicensesInfoJob(QObject *parent = nullptr);
+    ~LicensesInfoJob() override;
+
+    [[nodiscard]] bool requireHttpAuthentication() const override;
+
+    [[nodiscard]] bool start() override;
+
+    [[nodiscard]] QNetworkRequest request() const override;
+
+Q_SIGNALS:
+    void licensesInfoDone(const QJsonObject &obj);
+
+private:
+    Q_DISABLE_COPY(LicensesInfoJob)
+    LIBROCKETCHATRESTAPI_QT_NO_EXPORT void onGetRequestResponse(const QString &replyErrorString, const QJsonDocument &replyJson) override;
+};
+}
