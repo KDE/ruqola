ab5eb12458c75ac79ddba1f79b1bad3085018bc5
Laurent Montel <montel@kde.org>
2024-02-21 21:36:29 +0100

    Add autotest

---
+++ src/plugins/authentication/personalaccesstoken/autotests/personalaccesstokenpluginutiltest.cpp
@@ -0,0 +1,21 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "personalaccesstokenpluginutiltest.h"
+#include "../personalaccesstokenpluginutil.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(PersonalAccessTokenPluginUtilTest)
+PersonalAccessTokenPluginUtilTest::PersonalAccessTokenPluginUtilTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void PersonalAccessTokenPluginUtilTest::shouldHaveDefaultValues()
+{
+    PersonalAccessTokenPluginUtil::PersonalAccessTokenPluginInfo info;
+    QVERIFY(info.token.isEmpty());
+    QVERIFY(info.userId.isEmpty());
+}
