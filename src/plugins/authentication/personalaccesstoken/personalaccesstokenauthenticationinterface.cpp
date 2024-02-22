8f30e01f866bc5c74854eb91055f4a43e27656e4
Laurent Montel <montel@kde.org>
2024-02-22 08:16:04 +0100

    Move PersonalAccessTokenAuthenticationConfigDialog in showConfigureDialog method

--- src/plugins/authentication/personalaccesstoken/personalaccesstokenauthenticationinterface.cpp
+++ src/plugins/authentication/personalaccesstoken/personalaccesstokenauthenticationinterface.cpp
@@ -19,20 +19,21 @@ PersonalAccessTokenAuthenticationInterface::~PersonalAccessTokenAuthenticationIn
 
 void PersonalAccessTokenAuthenticationInterface::login()
 {
-    // TODO add widget ?
-    QPointer<PersonalAccessTokenAuthenticationConfigDialog> dlg = new PersonalAccessTokenAuthenticationConfigDialog(nullptr);
-    if (dlg->exec()) {
-        const PersonalAccessTokenPluginUtil::PersonalAccessTokenPluginInfo info = dlg->info();
-
-        // TODO
-    }
-    delete dlg;
+    // TODO
 }
 
 bool PersonalAccessTokenAuthenticationInterface::showConfigureDialog(QWidget *parent)
 {
-    // TODO
-    return false;
+    bool result = false;
+    QPointer<PersonalAccessTokenAuthenticationConfigDialog> dlg = new PersonalAccessTokenAuthenticationConfigDialog(nullptr);
+    if (dlg->exec()) {
+        const PersonalAccessTokenPluginUtil::PersonalAccessTokenPluginInfo info = dlg->info();
+
+        result = true;
+        // TODO
+    }
+    delete dlg;
+    return result;
 }
 
 #include "moc_personalaccesstokenauthenticationinterface.cpp"
