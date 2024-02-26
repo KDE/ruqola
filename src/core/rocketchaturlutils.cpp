7df7df532a723ee45687d5d5b8ed1db358d2ae96
Laurent Montel <montel@kde.org>
2024-02-26 13:29:05 +0100

    Use token/userid

--- src/core/rocketchaturlutils.cpp
+++ src/core/rocketchaturlutils.cpp
@@ -24,17 +24,23 @@ bool RocketChatUrlUtils::parseUrl(const QString &link)
         break;
     }
     case ParseRocketChatUrlUtils::UrlType::Server: {
+        const ParseRocketChatUrlUtils::ParsingInfo parseInfo = parseUrl.parsingInfo();
         AccountManager::AccountManagerInfo info;
-        info.serverUrl = parseUrl.parsingInfo().serverHost;
+        info.serverUrl = parseInfo.serverHost;
         info.enabled = true;
         info.accountName = QStringLiteral("importaccount");
+        // Add authentication support
+        if (!parseInfo.token.isEmpty() && !parseInfo.userId.isEmpty()) {
+            info.token = parseInfo.token;
+            info.userId = parseInfo.userId;
+            info.authMethodType = AuthenticationManager::AuthMethodType::PersonalAccessToken;
+        }
         Ruqola::self()->accountManager()->addAccount(std::move(info));
 
         auto notification = new KNotification(QStringLiteral("Account-Added"), KNotification::CloseOnTimeout);
         notification->setTitle(i18n("Account Added"));
         notification->setText(i18n("A new account was added."));
         notification->sendEvent();
-        // TODO add auth/token!
         return true;
     }
     case ParseRocketChatUrlUtils::UrlType::Invite: {
@@ -44,7 +50,7 @@ bool RocketChatUrlUtils::parseUrl(const QString &link)
         break;
     }
     case ParseRocketChatUrlUtils::UrlType::ConferenceCall:
-        qDebug() << " ParseRocketChatUrlUtils::UrlType Not implement yet ";
+        qDebug() << " ParseRocketChatUrlUtils::UrlType ConferenceCall Not implement yet ";
         return true;
     case ParseRocketChatUrlUtils::UrlType::Unknown:
         break;
