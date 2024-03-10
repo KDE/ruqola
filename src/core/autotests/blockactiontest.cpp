1e7edd6eb9783f57e5ca1227a8692d9c717cec41
Laurent Montel <montel@kde.org>
2024-03-10 11:35:08 +0100

    Add autotest

---
+++ src/core/autotests/blockactiontest.cpp
@@ -0,0 +1,14 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "blockactiontest.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(BlockActionTest)
+
+BlockActionTest::BlockActionTest(QObject *parent)
+    : QObject{parent}
+{
+}
