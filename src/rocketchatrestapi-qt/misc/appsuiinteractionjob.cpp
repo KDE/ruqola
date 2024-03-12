b01235c3f1c76b6c4f753c1a5d4dc6be349799ac
Laurent Montel <montel@kde.org>
2024-03-12 13:17:57 +0100

    Add missing file
    (cherry picked from commit ccedb92a5a3cedcee482620cc228a510b60fbb8f)

---
+++ src/rocketchatrestapi-qt/misc/appsuiinteractionjob.cpp
@@ -0,0 +1,99 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "appsuiinteractionjob.h"
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+
+#include <QJsonDocument>
+#include <QJsonObject>
+using namespace RocketChatRestApi;
+AppsUiInteractionJob::AppsUiInteractionJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+AppsUiInteractionJob::~AppsUiInteractionJob() = default;
+
+bool AppsUiInteractionJob::start()
+{
+    if (!canStart()) {
+        deleteLater();
+        return false;
+    }
+    addStartRestApiInfo("AppsUiInteractionJob::start");
+    submitPostRequest(json());
+    return true;
+}
+
+void AppsUiInteractionJob::onPostRequestResponse(const QString &replyErrorString, const QJsonDocument &replyJson)
+{
+    // qDebug() << " response " << replyErrorString << "replyJson  " << replyJson;
+    const QJsonObject replyObject = replyJson.object();
+    if (replyObject[QLatin1String("success")].toBool()) {
+        addLoggerInfo(QByteArrayLiteral("AppsUiInteractionJob success: ") + replyJson.toJson(QJsonDocument::Indented));
+        const QJsonObject obj = QJsonDocument::fromJson(replyObject[QLatin1String("message")].toString().toUtf8()).object();
+        Q_EMIT methodCallDone(obj);
+    } else {
+        emitFailedMessage(replyErrorString, replyObject);
+        addLoggerWarning(QByteArrayLiteral("AppsUiInteractionJob: Problem: ") + replyJson.toJson(QJsonDocument::Indented));
+    }
+}
+
+AppsUiInteractionJob::AppsUiInteractionJobInfo AppsUiInteractionJob::methodCallJobInfo() const
+{
+    return mAppsUiInteractionJobInfo;
+}
+
+void AppsUiInteractionJob::setAppsUiInteractionJobInfo(const AppsUiInteractionJobInfo &newAppsUiInteractionJobInfo)
+{
+    mAppsUiInteractionJobInfo = newAppsUiInteractionJobInfo;
+}
+
+bool AppsUiInteractionJob::requireHttpAuthentication() const
+{
+    return !mAppsUiInteractionJobInfo.anonymous;
+}
+
+bool AppsUiInteractionJob::canStart() const
+{
+    if (!RestApiAbstractJob::canStart()) {
+        return false;
+    }
+    if (!mAppsUiInteractionJobInfo.isValid()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "AppsUiInteractionJob: mAppsUiInteractionJobInfo is invalid";
+        return false;
+    }
+    return true;
+}
+
+QNetworkRequest AppsUiInteractionJob::request() const
+{
+    const QUrl url =
+        mRestApiMethod->generateUrl(mAppsUiInteractionJobInfo.anonymous ? RestApiUtil::RestApiUrlType::MethodCallAnon : RestApiUtil::RestApiUrlType::MethodCall,
+                                    mAppsUiInteractionJobInfo.methodName);
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    addRequestAttribute(request);
+    return request;
+}
+
+QJsonDocument AppsUiInteractionJob::json() const
+{
+    QJsonObject jsonObj;
+    // We need to convert to string
+    jsonObj[QLatin1String("message")] = QString::fromUtf8(QJsonDocument(mAppsUiInteractionJobInfo.messageObj).toJson(QJsonDocument::Compact));
+    const QJsonDocument postData = QJsonDocument(jsonObj);
+    return postData;
+}
+
+bool AppsUiInteractionJob::AppsUiInteractionJobInfo::isValid() const
+{
+    // TODO verify if messageObj is empty
+    return !methodName.isEmpty() && !messageObj.isEmpty();
+}
+
+#include "moc_appsuiinteractionjob.cpp"
---
+++ src/rocketchatrestapi-qt/misc/appsuiinteractionjob.h
@@ -0,0 +1,47 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "librocketchatrestapi-qt_export.h"
+#include "restapiabstractjob.h"
+#include <QJsonObject>
+
+namespace RocketChatRestApi
+{
+class LIBROCKETCHATRESTAPI_QT_EXPORT AppsUiInteractionJob : public RestApiAbstractJob
+{
+    Q_OBJECT
+public:
+    struct LIBROCKETCHATRESTAPI_QT_EXPORT AppsUiInteractionJobInfo {
+        QJsonObject messageObj;
+        QString methodName;
+        bool anonymous = false;
+        [[nodiscard]] bool isValid() const;
+    };
+    explicit AppsUiInteractionJob(QObject *parent = nullptr);
+    ~AppsUiInteractionJob() override;
+
+    [[nodiscard]] bool start() override;
+    [[nodiscard]] bool requireHttpAuthentication() const override;
+    [[nodiscard]] bool canStart() const override;
+    [[nodiscard]] QNetworkRequest request() const override;
+
+    [[nodiscard]] QJsonDocument json() const;
+
+    [[nodiscard]] AppsUiInteractionJobInfo methodCallJobInfo() const;
+    void setAppsUiInteractionJobInfo(const AppsUiInteractionJobInfo &newAppsUiInteractionJobInfo);
+
+Q_SIGNALS:
+    void methodCallDone(const QJsonObject &replyObject);
+
+private:
+    Q_DISABLE_COPY(AppsUiInteractionJob)
+    LIBROCKETCHATRESTAPI_QT_NO_EXPORT void onPostRequestResponse(const QString &replyErrorString, const QJsonDocument &replyJson) override;
+    AppsUiInteractionJobInfo mAppsUiInteractionJobInfo;
+};
+}
+Q_DECLARE_TYPEINFO(RocketChatRestApi::AppsUiInteractionJob::AppsUiInteractionJobInfo, Q_RELOCATABLE_TYPE);
