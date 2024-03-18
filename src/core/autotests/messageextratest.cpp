85d7d2606d46a2c32aa81ec1202575c537e2111e
Laurent Montel <montel@kde.org>
2024-03-18 13:10:20 +0100

    Add messageextra autotest

---
+++ src/core/autotests/messageextratest.cpp
@@ -0,0 +1,24 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "messageextratest.h"
+#include "messages/messageextra.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(MessageExtraTest)
+MessageExtraTest::MessageExtraTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void MessageExtraTest::shouldHaveDefaultValues()
+{
+    MessageExtra extra;
+    QVERIFY(extra.localTranslation().isEmpty());
+    QVERIFY(extra.discussionRoomId().isEmpty());
+    QVERIFY(extra.threadMessageId().isEmpty());
+    QCOMPARE(extra.threadCount(), 0);
+    QCOMPARE(extra.discussionCount(), 0);
+}
---
+++ src/core/autotests/messageextratest.h
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+
+class MessageExtraTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MessageExtraTest(QObject *parent = nullptr);
+    ~MessageExtraTest() override = default;
+
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+};
