f96e8df04c1ec971091219c885198ad00b90944f
Laurent Montel <montel@kde.org>
2024-03-20 13:36:13 +0100

    Add ModerationReportedUserInfos

---
+++ src/core/moderation/moderationreporteduserinfos.cpp
@@ -0,0 +1,128 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "moderationreporteduserinfos.h"
+#include "ruqola_debug.h"
+
+#include <QJsonArray>
+#include <QJsonObject>
+
+ModerationReportedUserInfos::ModerationReportedUserInfos() = default;
+
+QDebug operator<<(QDebug d, const ModerationReportedUserInfos &t)
+{
+    d.space() << "total" << t.total();
+    d.space() << "offset" << t.offset();
+    d.space() << "ModerationReportedUserInfosCount" << t.ModerationReportedUserInfosCount() << "\n";
+    for (int i = 0, total = t.ModerationReportedUserInfosList().count(); i < total; ++i) {
+        d << t.ModerationReportedUserInfosList().at(i) << "\n";
+    }
+    return d;
+}
+
+int ModerationReportedUserInfos::offset() const
+{
+    return mOffset;
+}
+
+void ModerationReportedUserInfos::setOffset(int newOffset)
+{
+    mOffset = newOffset;
+}
+
+int ModerationReportedUserInfos::total() const
+{
+    return mTotal;
+}
+
+void ModerationReportedUserInfos::setTotal(int newTotal)
+{
+    mTotal = newTotal;
+}
+
+int ModerationReportedUserInfos::ModerationReportedUserInfosCount() const
+{
+    return mModerationReportedUserInfosCount;
+}
+
+void ModerationReportedUserInfos::setModerationReportedUserInfosCount(int newModerationReportedUserInfosCount)
+{
+    mModerationReportedUserInfosCount = newModerationReportedUserInfosCount;
+}
+
+const QList<ModerationReportedUserInfo> &ModerationReportedUserInfos::ModerationReportedUserInfosList() const
+{
+    return mModerationReportedUserInfosList;
+}
+
+void ModerationReportedUserInfos::setModerationReportedUserInfosList(const QList<ModerationReportedUserInfo> &newModerationReportedUserInfosList)
+{
+    mModerationReportedUserInfosList = newModerationReportedUserInfosList;
+}
+
+bool ModerationReportedUserInfos::isEmpty() const
+{
+    return mModerationReportedUserInfosList.isEmpty();
+}
+
+void ModerationReportedUserInfos::clear()
+{
+    mModerationReportedUserInfosList.clear();
+}
+
+int ModerationReportedUserInfos::count() const
+{
+    return mModerationReportedUserInfosList.count();
+}
+
+ModerationReportedUserInfo ModerationReportedUserInfos::at(int index) const
+{
+    if (index < 0 || index > mModerationReportedUserInfosList.count()) {
+        qCWarning(RUQOLA_LOG) << "Invalid index " << index;
+        return {};
+    }
+    return mModerationReportedUserInfosList.at(index);
+}
+
+void ModerationReportedUserInfos::parseModerationReportedUserInfos(const QJsonObject &ModerationReportedUserInfosObj)
+{
+    mModerationReportedUserInfosList.clear();
+    // qDebug() << " ModerationReportedUserInfosObj " << ModerationReportedUserInfosObj;
+    mModerationReportedUserInfosCount = ModerationReportedUserInfosObj[QLatin1StringView("count")].toInt();
+    mOffset = ModerationReportedUserInfosObj[QLatin1StringView("offset")].toInt();
+    mTotal = ModerationReportedUserInfosObj[QLatin1StringView("total")].toInt();
+    mModerationReportedUserInfosList.reserve(mModerationReportedUserInfosCount);
+    parseModerationReportedUserInfosObj(ModerationReportedUserInfosObj);
+}
+
+void ModerationReportedUserInfos::parseModerationReportedUserInfosObj(const QJsonObject &ModerationReportedUserInfosObj)
+{
+    const QJsonArray moderationsArray = ModerationReportedUserInfosObj[QLatin1StringView("reports")].toArray();
+    for (const QJsonValue &current : moderationsArray) {
+        if (current.type() == QJsonValue::Object) {
+            const QJsonObject moderationObject = current.toObject();
+            ModerationReportedUserInfo m;
+            // TODO m.parseModerationReportedUserInfo(moderationObject);
+            mModerationReportedUserInfosList.append(std::move(m));
+        } else {
+            qCWarning(RUQOLA_LOG) << "Problem when parsing moderations" << current;
+        }
+    }
+}
+
+void ModerationReportedUserInfos::parseMoreModerationReportedUserInfos(const QJsonObject &ModerationReportedUserInfosObj)
+{
+    const int ModerationReportedUserInfosCount = ModerationReportedUserInfosObj[QLatin1StringView("count")].toInt();
+    mOffset = ModerationReportedUserInfosObj[QLatin1StringView("offset")].toInt();
+    mTotal = ModerationReportedUserInfosObj[QLatin1StringView("total")].toInt();
+    parseModerationReportedUserInfosObj(ModerationReportedUserInfosObj);
+    mModerationReportedUserInfosCount += ModerationReportedUserInfosCount;
+}
+
+ModerationReportedUserInfo ModerationReportedUserInfos::takeAt(int index)
+{
+    return mModerationReportedUserInfosList.takeAt(index);
+}
---
+++ src/core/moderation/moderationreporteduserinfos.h
@@ -0,0 +1,50 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "libruqolacore_export.h"
+#include "moderationreporteduserinfo.h"
+#include <QDebug>
+#include <QList>
+
+class LIBRUQOLACORE_EXPORT ModerationReportedUserInfos
+{
+public:
+    ModerationReportedUserInfos();
+
+    void clear();
+
+    [[nodiscard]] int offset() const;
+    void setOffset(int newOffset);
+
+    [[nodiscard]] int total() const;
+    void setTotal(int newTotal);
+
+    [[nodiscard]] int ModerationReportedUserInfosCount() const;
+    void setModerationReportedUserInfosCount(int newModerationReportedUserInfosCount);
+
+    [[nodiscard]] const QList<ModerationReportedUserInfo> &ModerationReportedUserInfosList() const;
+    void setModerationReportedUserInfosList(const QList<ModerationReportedUserInfo> &newModerationReportedUserInfosList);
+
+    [[nodiscard]] bool isEmpty() const;
+    [[nodiscard]] int count() const;
+    [[nodiscard]] ModerationReportedUserInfo at(int index) const;
+
+    void parseModerationReportedUserInfos(const QJsonObject &ModerationReportedUserInfosObj);
+    void parseMoreModerationReportedUserInfos(const QJsonObject &ModerationReportedUserInfosObj);
+
+    ModerationReportedUserInfo takeAt(int index);
+
+private:
+    LIBRUQOLACORE_NO_EXPORT void parseModerationReportedUserInfosObj(const QJsonObject &ModerationReportedUserInfosObj);
+    QList<ModerationReportedUserInfo> mModerationReportedUserInfosList;
+    int mModerationReportedUserInfosCount = 0;
+    int mOffset = 0;
+    int mTotal = 0;
+};
+Q_DECLARE_METATYPE(ModerationReportedUserInfos)
+LIBRUQOLACORE_EXPORT QDebug operator<<(QDebug d, const ModerationReportedUserInfos &t);
