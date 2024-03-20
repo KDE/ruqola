f22d675f6b6df2023bcb1a088357445f3dab9731
Laurent Montel <montel@kde.org>
2024-03-20 13:30:10 +0100

    Add ModerationReportedUserInfo

---
+++ src/core/moderation/moderationreporteduserinfo.cpp
@@ -0,0 +1,15 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "moderationreporteduserinfo.h"
+
+ModerationReportedUserInfo::ModerationReportedUserInfo() = default;
+
+QDebug operator<<(QDebug d, const ModerationReportedUserInfo &t)
+{
+    // TODO
+    return d;
+}
---
+++ src/core/moderation/moderationreporteduserinfo.h
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+#include "libruqolacore_export.h"
+#include <QDebug>
+
+class LIBRUQOLACORE_EXPORT ModerationReportedUserInfo
+{
+public:
+    ModerationReportedUserInfo();
+    ~ModerationReportedUserInfo() = default;
+};
+
+Q_DECLARE_METATYPE(ModerationReportedUserInfo)
+Q_DECLARE_TYPEINFO(ModerationReportedUserInfo, Q_RELOCATABLE_TYPE);
+LIBRUQOLACORE_EXPORT QDebug operator<<(QDebug d, const ModerationReportedUserInfo &t);
