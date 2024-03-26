25f16839c3f66373c8a336dcdbb1f4341bb2e2b6
Laurent Montel <montel@kde.org>
2024-03-26 19:44:41 +0100

    continue to implement roomextra

--- src/core/roomextra.h
+++ src/core/roomextra.h
@@ -6,12 +6,26 @@
 
 #pragma once
 #include "libruqolacore_export.h"
+#include "teams/teaminfo.h"
 
 #include <QSharedData>
-
+#include <QStringList>
 class LIBRUQOLACORE_EXPORT RoomExtra : public QSharedData
 {
 public:
     RoomExtra();
     ~RoomExtra();
+
+private:
+    // muted - collection of muted users by its usernames
+    QStringList mMutedUsers;
+
+    QStringList mIgnoredUsers;
+
+    QStringList mHighlightsWord;
+
+    // Autotranslate
+    QString mAutotranslateLanguage;
+
+    TeamInfo mTeamInfo;
 };
