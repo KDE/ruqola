e3d2242e680c50221ca3bf0d2718b0878d5309e8
Laurent Montel <montel@kde.org>
2024-04-02 08:52:06 +0200

    Prepare autotest

---
+++ src/core/activities/autotests/activitiesmanagertest.cpp
@@ -0,0 +1,18 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "activitiesmanagertest.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(ActivitiesManagerTest)
+ActivitiesManagerTest::ActivitiesManagerTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void ActivitiesManagerTest::shouldHaveDefaultValues()
+{
+    // TODO
+}
