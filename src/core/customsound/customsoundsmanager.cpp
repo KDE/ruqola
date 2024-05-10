d536edc9951553748e3e8d313377b2b6cf03ab37
Laurent Montel <montel@kde.org>
2024-05-10 13:49:54 +0200

    Add info

--- src/core/customsound/customsoundsmanager.cpp
+++ src/core/customsound/customsoundsmanager.cpp
@@ -158,6 +158,8 @@ void CustomSoundsManager::initializeDefaultSounds()
 
     setCustomSoundsInfo(listSounds);
     // TODO download it in customsound repo I think.
+
+    // https://<server_url>/sounds/ding.mp3?cacheKey=6.7.2
 }
 
 const QList<CustomSoundInfo> &CustomSoundsManager::customSoundsInfo() const
