d01f0e50e94088adbe51f659445a4a90bcbd43da
Laurent Montel <montel@kde.org>
2024-06-03 13:54:17 +0200

    Add appscategoryinfo autotest

---
+++ src/core/autotests/appscategoryinfotest.cpp
@@ -0,0 +1,21 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "appscategoryinfotest.h"
+#include "apps/appscategoryinfo.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(AppsCategoryInfoTest)
+AppsCategoryInfoTest::AppsCategoryInfoTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void AppsCategoryInfoTest::shouldHaveDefaultValues()
+{
+    AppsCategoryInfo d;
+
+    // TODO
+}
