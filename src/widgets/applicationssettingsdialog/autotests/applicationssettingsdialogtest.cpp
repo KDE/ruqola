789a699a327a04c2121b9b5208203209339af39d
Laurent Montel <montel@kde.org>
2024-06-07 08:46:54 +0200

    Add autotest

---
+++ src/widgets/applicationssettingsdialog/autotests/applicationssettingsdialogtest.cpp
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "applicationssettingsdialogtest.h"
+#include "applicationssettingsdialog/applicationssettingsdialog.h"
+#include <QTest>
+
+QTEST_MAIN(ApplicationsSettingsDialogTest)
+ApplicationsSettingsDialogTest::ApplicationsSettingsDialogTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void ApplicationsSettingsDialogTest::shouldHaveDefaultValues()
+{
+    ApplicationsSettingsDialog d;
+}
---
+++ src/widgets/applicationssettingsdialog/autotests/applicationssettingsdialogtest.h
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+
+class ApplicationsSettingsDialogTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ApplicationsSettingsDialogTest(QObject *parent = nullptr);
+    ~ApplicationsSettingsDialogTest() override = default;
+
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+};
