630dea71828b110a35cdebc8f9f246071f4330dd
Laurent Montel <montel@kde.org>
2024-06-13 13:31:12 +0200

    Prepare to add cache

--- src/widgets/applicationssettingsdialog/applicationssettingsdelegate.h
+++ src/widgets/applicationssettingsdialog/applicationssettingsdelegate.h
@@ -6,8 +6,8 @@
 
 #pragma once
 
+#include "lrucache.h"
 #include <QItemDelegate>
-
 class ApplicationsSettingsDelegate : public QItemDelegate
 {
     Q_OBJECT
@@ -18,4 +18,9 @@ public:
     void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
 
     [[nodiscard]] QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+
+private:
+    // Cache SizeHint value
+    // We need to clear it when we resize widget.
+    mutable LRUCache<QByteArray, QSize> mSizeHintCache;
 };
