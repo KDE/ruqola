d57b96bac0c1c41838ccfb3d44dc037e0463ea2d
Laurent Montel <montel@kde.org>
2024-06-16 11:46:52 +0200

    Implement autotest

--- src/widgets/applicationssettingsdialog/autotests/applicationssettingsaskapplicationwidgettest.cpp
+++ src/widgets/applicationssettingsdialog/autotests/applicationssettingsaskapplicationwidgettest.cpp
@@ -6,7 +6,10 @@
 
 #include "applicationssettingsaskapplicationwidgettest.h"
 #include "applicationssettingsdialog/applicationssettingsaskapplicationwidget.h"
+#include <KTextEdit>
+#include <QLabel>
 #include <QTest>
+#include <QVBoxLayout>
 QTEST_MAIN(ApplicationsSettingsAskApplicationWidgetTest)
 ApplicationsSettingsAskApplicationWidgetTest::ApplicationsSettingsAskApplicationWidgetTest(QObject *parent)
     : QObject{parent}
@@ -16,7 +19,18 @@ ApplicationsSettingsAskApplicationWidgetTest::ApplicationsSettingsAskApplication
 void ApplicationsSettingsAskApplicationWidgetTest::shouldHaveDefaultValues()
 {
     ApplicationsSettingsAskApplicationWidget d;
-    // TODO
+    auto mainLayout = d.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->contentsMargins(), QMargins{});
+
+    auto label = d.findChild<QLabel *>(QStringLiteral("label"));
+    QVERIFY(label);
+    QVERIFY(!label->text().isEmpty());
+
+    auto mMessage = d.findChild<KTextEdit *>(QStringLiteral("mMessage"));
+    QVERIFY(mMessage);
+    QVERIFY(mMessage->toPlainText().isEmpty());
+    QVERIFY(!mMessage->placeholderText().isEmpty());
 }
 
 #include "moc_applicationssettingsaskapplicationwidgettest.cpp"
