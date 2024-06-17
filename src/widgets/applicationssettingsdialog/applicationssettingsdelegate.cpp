5d7cfaa6efe0bcf8b16dca3fe7d7ca284c34ddd3
Laurent Montel <montel@kde.org>
2024-06-17 08:08:51 +0200

    Continue to implement delegate

--- src/widgets/applicationssettingsdialog/applicationssettingsdelegate.cpp
+++ src/widgets/applicationssettingsdialog/applicationssettingsdelegate.cpp
@@ -7,11 +7,15 @@
 #include "applicationssettingsdelegate.h"
 #include "common/delegatepaintutil.h"
 #include "config-ruqola.h"
+#include "delegateutils/messagedelegateutils.h"
 #include "model/appsmarketplacemodel.h"
+#include "rocketchataccount.h"
+#include "textconverter.h"
 #if USE_SIZEHINT_CACHE_SUPPORT
 #include "ruqola_sizehint_cache_debug.h"
 #endif
 #include <QPainter>
+#include <QTextDocument>
 
 ApplicationsSettingsDelegate::ApplicationsSettingsDelegate(QObject *parent)
     : QItemDelegate{parent}
@@ -82,4 +86,53 @@ QByteArray ApplicationsSettingsDelegate::cacheIdentifier(const QModelIndex &inde
     Q_ASSERT(!identifier.isEmpty());
     return identifier;
 }
+
+QTextDocument *ApplicationsSettingsDelegate::documentForModelIndex(const QModelIndex &index, int width) const
+{
+    Q_ASSERT(index.isValid());
+    const QByteArray appsId = cacheIdentifier(index);
+    const QString description = index.data(AppsMarketPlaceModel::Description).toString();
+    // return documentForDelegate(mRocketChatAccount, appsId, description, width);
+    return {};
+}
+
+QTextDocument *
+ApplicationsSettingsDelegate::documentForDelegate(RocketChatAccount *rcAccount, const QByteArray &messageId, const QString &messageStr, int width) const
+{
+    auto it = mDocumentCache.find(messageId);
+    if (it != mDocumentCache.end()) {
+        auto ret = it->value.get();
+        if (width != -1 && !qFuzzyCompare(ret->textWidth(), width)) {
+            ret->setTextWidth(width);
+        }
+        return ret;
+    }
+    if (messageStr.isEmpty()) {
+        return nullptr;
+    }
+    // Use TextConverter in case it starts with a [](URL) reply marker
+    QByteArray needUpdateMessageId; // TODO use it ?
+    int maximumRecursiveQuotedText = -1;
+    if (rcAccount) {
+        maximumRecursiveQuotedText = rcAccount->ruqolaServerConfig()->messageQuoteChainLimit();
+    }
+    const TextConverter::ConvertMessageTextSettings settings(messageStr,
+                                                             rcAccount ? rcAccount->userName() : QString(),
+                                                             {},
+                                                             rcAccount ? rcAccount->highlightWords() : QStringList(),
+                                                             rcAccount ? rcAccount->emojiManager() : nullptr,
+                                                             rcAccount ? rcAccount->messageCache() : nullptr,
+                                                             {},
+                                                             {},
+                                                             {},
+                                                             maximumRecursiveQuotedText);
+
+    int recursiveIndex = 0;
+    const QString contextString = TextConverter::convertMessageText(settings, needUpdateMessageId, recursiveIndex);
+    auto doc = MessageDelegateUtils::createTextDocument(false, contextString, width);
+    auto ret = doc.get();
+    mDocumentCache.insert(messageId, std::move(doc));
+    return ret;
+}
+
 #include "moc_applicationssettingsdelegate.cpp"
--- src/widgets/applicationssettingsdialog/applicationssettingsdelegate.h
+++ src/widgets/applicationssettingsdialog/applicationssettingsdelegate.h
@@ -8,6 +8,8 @@
 
 #include "lrucache.h"
 #include <QItemDelegate>
+class QTextDocument;
+class RocketChatAccount;
 class ApplicationsSettingsDelegate : public QItemDelegate
 {
     Q_OBJECT
@@ -34,7 +36,10 @@ private:
     [[nodiscard]] ApplicationsSettingsDelegate::Layout doLayout(const QStyleOptionViewItem &option, const QModelIndex &index) const;
 
     [[nodiscard]] QByteArray cacheIdentifier(const QModelIndex &index) const;
+    [[nodiscard]] QTextDocument *documentForModelIndex(const QModelIndex &index, int width) const;
+    [[nodiscard]] QTextDocument *documentForDelegate(RocketChatAccount *rcAccount, const QByteArray &messageId, const QString &messageStr, int width) const;
     // Cache SizeHint value
     // We need to clear it when we resize widget.
     mutable LRUCache<QByteArray, QSize> mSizeHintCache;
+    mutable LRUCache<QByteArray, std::unique_ptr<QTextDocument>> mDocumentCache;
 };
