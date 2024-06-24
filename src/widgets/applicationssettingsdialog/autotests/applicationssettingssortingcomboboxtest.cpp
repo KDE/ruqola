00e35b426b8db0aae4fd68b66a5cdcb350f1ed43
Laurent Montel <montel@kde.org>
2024-06-24 09:01:34 +0200

    Add autotest

---
+++ src/widgets/applicationssettingsdialog/autotests/applicationssettingssortingcomboboxtest.cpp
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "applicationssettingssortingcomboboxtest.h"
+#include "applicationssettingsdialog/applicationssettingssortingcombobox.h"
+#include <QTest>
+
+QTEST_MAIN(ApplicationsSettingsSortingComboBoxTest)
+ApplicationsSettingsSortingComboBoxTest::ApplicationsSettingsSortingComboBoxTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void ApplicationsSettingsSortingComboBoxTest::shouldHaveDefaultValues()
+{
+    ApplicationsSettingsSortingComboBox d;
+    QCOMPARE(d.currentSorting(), AppsMarketPlaceFilterProxyModel::Sorting::UnknownSorting);
+}
---
+++ src/widgets/applicationssettingsdialog/autotests/applicationssettingssortingcomboboxtest.h
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+
+class ApplicationsSettingsSortingComboBoxTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ApplicationsSettingsSortingComboBoxTest(QObject *parent = nullptr);
+    ~ApplicationsSettingsSortingComboBoxTest() override = default;
+
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+};
