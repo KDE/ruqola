b8f35dd9c9207bbe48fc42cf3746295ac8bf7a21
Laurent Montel <montel@kde.org>
2024-06-25 07:46:36 +0200

    Duplicate code for starting to implement cmark support

---
+++ src/core/textconvertercmark.h
@@ -0,0 +1,57 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "libruqolacore_export.h"
+#include <QList>
+#include <QMap>
+#include <QString>
+#include <QStringList>
+
+class EmojiManager;
+class Message;
+class MessageCache;
+
+namespace TextConverterCMark
+{
+struct LIBRUQOLACORE_EXPORT ConvertMessageTextSettings {
+    ConvertMessageTextSettings(const QString &_str,
+                               const QString &_userName,
+                               const QList<Message> &_allMessages,
+                               const QStringList &_highlightWords,
+                               EmojiManager *_emojiManager,
+                               MessageCache *_messageCache,
+                               const QMap<QString, QByteArray> &_mentions,
+                               const QMap<QString, QByteArray> &_channels,
+                               const QString &_searchedText = {},
+                               int _maximumRecursiveQuotedText = -1)
+        : str(_str)
+        , userName(_userName)
+        , allMessages(_allMessages)
+        , highlightWords(_highlightWords)
+        , emojiManager(_emojiManager)
+        , messageCache(_messageCache)
+        , mentions(_mentions)
+        , channels(_channels)
+        , searchedText(_searchedText)
+        , maximumRecursiveQuotedText(_maximumRecursiveQuotedText)
+    {
+    }
+    const QString str;
+    const QString userName;
+    const QList<Message> allMessages;
+    const QStringList highlightWords;
+    EmojiManager *const emojiManager;
+    MessageCache *const messageCache;
+    const QMap<QString, QByteArray> mentions;
+    const QMap<QString, QByteArray> channels;
+    const QString searchedText;
+    int maximumRecursiveQuotedText = -1;
+};
+
+[[nodiscard]] LIBRUQOLACORE_EXPORT QString convertMessageText(const ConvertMessageTextSettings &settings, QByteArray &needUpdateMessageId, int &recusiveIndex);
+}
