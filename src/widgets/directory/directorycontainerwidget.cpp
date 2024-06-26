ee9e0482af834900d8bfe6fd76adc9390110caed
Laurent Montel <montel@kde.org>
2024-06-26 13:54:01 +0200

    Add layout

--- src/widgets/directory/directorycontainerwidget.cpp
+++ src/widgets/directory/directorycontainerwidget.cpp
@@ -5,10 +5,13 @@
 */
 
 #include "directorycontainerwidget.h"
-
+#include <QVBoxLayout>
 DirectoryContainerWidget::DirectoryContainerWidget(QWidget *parent)
     : QWidget{parent}
 {
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setContentsMargins({});
+    mainLayout->setObjectName(QStringLiteral("mainLayout"));
 }
 
 DirectoryContainerWidget::~DirectoryContainerWidget() = default;
