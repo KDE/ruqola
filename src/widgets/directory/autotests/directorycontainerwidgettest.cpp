f45b94e057bdd4810106a93a58cca9b01af27c41
Laurent Montel <montel@kde.org>
2024-06-26 20:25:23 +0200

    Add new autotest

---
+++ src/widgets/directory/autotests/directorycontainerwidgettest.cpp
@@ -0,0 +1,21 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "directorycontainerwidgettest.h"
+#include "directory/directorycontainerwidget.h"
+#include <QTest>
+#include <QVBoxLayout>
+
+QTEST_MAIN(DirectoryContainerWidgetTest)
+DirectoryContainerWidgetTest::DirectoryContainerWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void DirectoryContainerWidgetTest::shouldHaveDefaultValues()
+{
+    DirectoryContainerWidget w(nullptr, DirectoryWidget::Room);
+    // TODO
+}
