9c9f077d78171d5a8e6768b417831d3657d4841d
Laurent Montel <montel@kde.org>
2024-06-27 07:53:12 +0200

    Implement whatsnewtranslation

---
+++ src/widgets/whatsnew/whatsnewtranslation.cpp
@@ -0,0 +1,86 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "whatsnewtranslation.h"
+#include <KLocalizedString>
+
+QList<WhatsNewInfo> WhatsNewTranslations::createWhatsNewInfo()
+{
+    QList<WhatsNewInfo> listInfo;
+    {
+        WhatsNewInfo info;
+        info.setChanges({i18n("Store Message in Local Database (experimental)")});
+        info.setNewFeatures({
+            i18n("Download Server Icon from Server"),
+            i18n("Show Server Error Info in Specific DialogBox"),
+            i18n("Allow to Copy Image in Clipboard"),
+            i18n("Improve debug support (Allow to show current account permissions)"),
+            i18n("Add support for quoted text (text which starts by \'>\')"),
+            i18n("Show leader/moderator/owner in channel info."),
+            i18n("Video/Sound Message support (kf6 only)."),
+            i18n("Import/Export Accounts."),
+        });
+        info.setBugFixings({i18n("Fix avatar support"),
+                            i18n("Fix emoji support (use new ktextaddons/textemoticons)"),
+                            i18n("Fix animated gif in reactions"),
+                            i18n("Fix kf6 support")});
+        info.setVersion(QStringLiteral("2.0"));
+        listInfo.append(std::move(info));
+    }
+    {
+        WhatsNewInfo info;
+        info.setNewFeatures({i18n("Add Moderation Console (Administration, RC server 6.2)"), i18n("Implement message style"), i18n("Implement Url Preview")});
+        info.setBugFixings({
+            i18n("Fix markdown support (header + check mark)"),
+            i18n("Fix mem leak"),
+            i18n("Cache message size (optimization)"),
+            i18n("Improve select room or user in message line edit (show icon, inform if user is in channel)"),
+            i18n("Fix show commands in message line edit (Hide action when room doesn't support it)."),
+            i18n("Fix download sound/video file when it's not in cache."),
+            i18n("Fix list of authentication method."),
+            i18n("Fix show avatar url."),
+        });
+        info.setVersion(QStringLiteral("2.1"));
+        listInfo.append(std::move(info));
+    }
+    {
+        WhatsNewInfo info;
+        info.setNewFeatures({
+            i18n("Allow to increase/decrease font (CTRL++/CTRL+-)"),
+            i18n("Add channel list style (Condensed/Medium/Extended)"),
+            i18n("Add forward message"),
+            i18n("Improve mentions support."),
+            i18n("Add support for deep linking <a href=\"https://developer.rocket.chat/getting-started/deep-linking\">Deep Linking</a>."),
+            i18n("Implement block actions."),
+            i18n("Implement personal token authentication. <a href=\"https://bugs.kde.org/show_bug.cgi?id=481400\">Bug 481400</a>"),
+            i18n("Add Plasma Activities Support"),
+            i18n("Add Report User Support"),
+            i18n("Implement Channel Sound Notification."),
+            i18n("Implement New Room Sound Notification."),
+            i18n("Implement Sorted/Unsorted markdown list."),
+        });
+        info.setBugFixings({
+            i18n("Fix dark mode support."),
+            i18n("Fix jitsi support."),
+            i18n("Fix translate message in direct channel."),
+            i18n("Don't show @here/@all as user."),
+            i18n("Reduce memory footprint."),
+            i18n("Use RESTAPI for logging."),
+            i18n("Allow to send multi files."),
+            i18n("Fix preview url."),
+        });
+        info.setVersion(QStringLiteral("2.2"));
+        listInfo.append(std::move(info));
+    }
+    {
+        WhatsNewInfo info;
+        info.setNewFeatures({
+            i18n("Implement Application Market Settings."),
+        });
+        info.setVersion(QStringLiteral("2.3"));
+        listInfo.append(std::move(info));
+    }
+    return listInfo;
+}
---
+++ src/widgets/whatsnew/whatsnewtranslation.h
@@ -0,0 +1,12 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+#include "whatsnewinfo.h"
+namespace WhatsNewTranslations
+{
+[[nodiscard]] QList<WhatsNewInfo> createWhatsNewInfo();
+}
