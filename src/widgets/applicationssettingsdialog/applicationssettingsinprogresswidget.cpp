32d7448741e951a9ee946b560c274121b350493d
Laurent Montel <montel@kde.org>
2024-06-28 09:13:36 +0200

    Implement applicationssettingsinprogresswidget

--- src/widgets/applicationssettingsdialog/applicationssettingsinprogresswidget.cpp
+++ src/widgets/applicationssettingsdialog/applicationssettingsinprogresswidget.cpp
@@ -5,16 +5,52 @@
 */
 
 #include "applicationssettingsinprogresswidget.h"
+#include <KPixmapSequenceLoader>
+#include <QLabel>
+#include <QTimer>
 #include <QVBoxLayout>
+#include <kiconloader.h>
 using namespace Qt::Literals::StringLiterals;
 ApplicationsSettingsInProgressWidget::ApplicationsSettingsInProgressWidget(QWidget *parent)
     : QWidget{parent}
+    , mLabel(new QLabel(this))
+    , mProgressTimer(new QTimer(this))
 {
     auto mainLayout = new QVBoxLayout(this);
     mainLayout->setObjectName("mainLayout"_L1);
     mainLayout->setContentsMargins({});
+
+    mLabel->setObjectName("label"_L1);
+    mainLayout->addWidget(mLabel);
+
+    mProgressPix = KPixmapSequenceLoader::load(QStringLiteral("process-working"), KIconLoader::SizeSmallMedium);
+    connect(mProgressTimer, &QTimer::timeout, this, &ApplicationsSettingsInProgressWidget::slotTimerDone);
 }
 
 ApplicationsSettingsInProgressWidget::~ApplicationsSettingsInProgressWidget() = default;
+
+void ApplicationsSettingsInProgressWidget::slotTimerDone()
+{
+    mLabel->setPixmap(mProgressPix.frameAt(mProgressCount));
+    ++mProgressCount;
+    if (mProgressCount == 8) {
+        mProgressCount = 0;
+    }
+
+    mProgressTimer->start(300);
+}
+
+void ApplicationsSettingsInProgressWidget::startAnimation()
+{
+    mProgressCount = 0;
+    mProgressTimer->start(300);
+}
+
+void ApplicationsSettingsInProgressWidget::stopAnimation()
+{
+    if (mProgressTimer->isActive()) {
+        mProgressTimer->stop();
+    }
+}
 
 #include "moc_applicationssettingsinprogresswidget.cpp"
--- src/widgets/applicationssettingsdialog/applicationssettingsinprogresswidget.h
+++ src/widgets/applicationssettingsdialog/applicationssettingsinprogresswidget.h
@@ -6,12 +6,23 @@
 
 #pragma once
 #include "libruqolawidgets_private_export.h"
+#include <KPixmapSequence>
 #include <QWidget>
-
+class QLabel;
+class QTimer;
 class LIBRUQOLAWIDGETS_TESTS_EXPORT ApplicationsSettingsInProgressWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit ApplicationsSettingsInProgressWidget(QWidget *parent = nullptr);
     ~ApplicationsSettingsInProgressWidget() override;
+
+private:
+    LIBRUQOLAWIDGETS_NO_EXPORT void slotTimerDone();
+    LIBRUQOLAWIDGETS_NO_EXPORT void startAnimation();
+    LIBRUQOLAWIDGETS_NO_EXPORT void stopAnimation();
+    int mProgressCount = 0;
+    KPixmapSequence mProgressPix;
+    QLabel *const mLabel;
+    QTimer *const mProgressTimer;
 };
