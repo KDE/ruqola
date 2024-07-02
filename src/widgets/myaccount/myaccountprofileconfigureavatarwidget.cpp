051ac43ae761e93ced931373ba5324499fecf1fa
Laurent Montel <montel@kde.org>
2024-07-02 13:53:01 +0200

    Add @title:window

--- src/widgets/myaccount/myaccountprofileconfigureavatarwidget.cpp
+++ src/widgets/myaccount/myaccountprofileconfigureavatarwidget.cpp
@@ -83,7 +83,7 @@ void AvatarImage::changeImage()
 void AvatarImage::changeUrl()
 {
     bool ok = false;
-    const QString url = QInputDialog::getText(this, i18n("Change Url"), i18n("Define Avatar Url:"), QLineEdit::Normal, {}, &ok);
+    const QString url = QInputDialog::getText(this, i18nc("@title:window", "Change Url"), i18n("Define Avatar Url:"), QLineEdit::Normal, {}, &ok);
     if (ok && !url.isEmpty()) {
         mRocketChatAccount->setAvatarUrl(url);
     }
