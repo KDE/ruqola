1bc58c596517260a089251120de9b5b31a0ad270
Laurent Montel <montel@kde.org>
2024-07-03 08:08:53 +0200

    Add autotest

---
+++ src/widgets/applicationssettingsdialog/autotests/applicationssettingsinstalledcomboboxtest.cpp
@@ -0,0 +1,23 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "applicationssettingsinstalledcomboboxtest.h"
+#include "applicationssettingsdialog/applicationssettingsinstalledcombobox.h"
+
+#include <QTest>
+QTEST_MAIN(ApplicationsSettingsInstalledComboBoxTest)
+ApplicationsSettingsInstalledComboBoxTest::ApplicationsSettingsInstalledComboBoxTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+ApplicationsSettingsInstalledComboBoxTest::~ApplicationsSettingsInstalledComboBoxTest() = default;
+
+void ApplicationsSettingsInstalledComboBoxTest::shouldHaveDefaultValues()
+{
+    ApplicationsSettingsInstalledComboBox d;
+    // TODO
+}
