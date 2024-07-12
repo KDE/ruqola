4b54aad549e49510202f0937c6f3bdb04e10e276
Laurent Montel <montel@kde.org>
2024-07-12 20:54:19 +0200

    Prepare autotest

---
+++ src/core/autotests/encryptionutilstest.cpp
@@ -0,0 +1,18 @@
+/*
+  SPDX-FileCopyrightText: 2024 Laurent Montel <montel.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "encryptionutilstest.h"
+#include "encryption/encryptionutils.h"
+#include <QTest>
+QTEST_MAIN(EncryptionUtilsTest)
+EncryptionUtilsTest::EncryptionUtilsTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void EncryptionUtilsTest::shouldHaveDefaultValues()
+{
+    // TODO
+}
