082208e3d9dfa9ae833486ae26dc816a03e0c1c7
Laurent Montel <montel@kde.org>
2024-07-12 08:24:53 +0200

    Prepare a test apps encryptiontestgui

---
+++ tests/encryptiontestgui/encryptiontestgui.cpp
@@ -0,0 +1,46 @@
+/*
+   SPDX-FileCopyrightText: 2023-2024 Laurent Montel <montel.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "encryptiontestgui.h"
+#include <QApplication>
+#include <QLabel>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QVBoxLayout>
+EncryptionTestGui::EncryptionTestGui(QWidget *parent)
+    : QWidget{parent}
+    , mTextEdit(new QTextEdit(this))
+    , mTextEditResult(new QTextEdit(this))
+    , mTextEditResultCMark(new QTextEdit(this))
+{
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setContentsMargins({});
+    mainLayout->addWidget(mTextEdit);
+
+    auto pushButton = new QPushButton(QStringLiteral("Decode"), this);
+    mainLayout->addWidget(pushButton);
+    connect(pushButton, &QPushButton::clicked, this, [this]() {});
+    mTextEditResult->setReadOnly(true);
+    auto label = new QLabel(QStringLiteral("Decode"), this);
+    mainLayout->addWidget(label);
+    mainLayout->addWidget(mTextEditResult);
+}
+
+EncryptionTestGui::~EncryptionTestGui()
+{
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    EncryptionTestGui w;
+    w.resize(800, 600);
+    w.show();
+    return app.exec();
+}
+
+#include "moc_encryptiontestgui.cpp"
---
+++ tests/encryptiontestgui/encryptiontestgui.h
@@ -0,0 +1,22 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QWidget>
+class QTextEdit;
+class EncryptionTestGui : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit EncryptionTestGui(QWidget *parent = nullptr);
+    ~EncryptionTestGui() override;
+
+private:
+    QTextEdit *const mTextEdit;
+    QTextEdit *const mTextEditResult;
+    QTextEdit *const mTextEditResultCMark;
+};
