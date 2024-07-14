edd33bd4b60978a26911ddf831e8bcab8b0be1e8
Laurent Montel <montel@kde.org>
2024-07-14 18:35:00 +0200

    Increase password size

--- src/core/encryption/e2ekeymanager.cpp
+++ src/core/encryption/e2ekeymanager.cpp
@@ -25,8 +25,9 @@ void E2eKeyManager::decodeEncryptionKey()
 
 QString E2eKeyManager::generateRandomPassword() const
 {
+    const int numberChar = 30;
     QString randomStr;
-    for (int i = 0; i < 15; i++) {
+    for (int i = 0; i < numberChar; i++) {
         const int d = rand() % 200; // Generate a random ASCII value between 0 and 199
         if (d >= 33 && d <= 123) {
             randomStr.append(QLatin1Char(static_cast<char>(d))); // Convert the ASCII value to a character for valid range
