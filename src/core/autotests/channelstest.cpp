fa86884da30cebee56ac969e18a7ee6c1dd5564f
Laurent Montel <montel@kde.org>
2024-08-08 18:25:00 +0200

    Add autotest

---
+++ src/core/autotests/channelstest.cpp
@@ -0,0 +1,24 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "channelstest.h"
+#include "messages/channels.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(ChannelsTest)
+
+ChannelsTest::ChannelsTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void ChannelsTest::shouldHaveDefaultValues()
+{
+    Channels w;
+    QVERIFY(w.isEmpty());
+    QVERIFY(w.channels().isEmpty());
+}
+
+#include "moc_channelstest.cpp"
