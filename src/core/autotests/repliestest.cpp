2207b1aa3f7b3f773bb7eb87a40b04dbb6a98171
Laurent Montel <montel@kde.org>
2024-08-12 13:15:45 +0200

    Add autotests

---
+++ src/core/autotests/repliestest.cpp
@@ -0,0 +1,24 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "repliestest.h"
+#include "messages/replies.h"
+#include <QTest>
+QTEST_MAIN(RepliesTest)
+
+RepliesTest::RepliesTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void RepliesTest::shouldHaveDefaultValues()
+{
+    Replies w;
+    QVERIFY(w.replies().isEmpty());
+    QVERIFY(w.isEmpty());
+}
+
+#include "moc_repliestest.cpp"
