7ff211f09d93c0dd3af1cea96d846c1fb9f3bd3c
Laurent Montel <montel@kde.org>
2024-08-13 07:58:56 +0200

    Prepare to create a button for updating ruqola version

---
+++ src/widgets/misc/verifynewversionwidget.h
@@ -0,0 +1,17 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+#include "libruqolawidgets_private_export.h"
+#include <QObject>
+
+class LIBRUQOLAWIDGETS_TESTS_EXPORT VerifyNewVersionWidget : public QObject
+{
+    Q_OBJECT
+public:
+    explicit VerifyNewVersionWidget(QObject *parent = nullptr);
+    ~VerifyNewVersionWidget() override;
+};
