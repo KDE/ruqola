0121fa3204b5906a8c32c8c9c9d8d6702bfdbc9f
Laurent Montel <montel@kde.org>
2024-09-02 08:39:26 +0200

    Improve autotest

--- src/widgets/welcome/autotests/welcomewidgettest.cpp
+++ src/widgets/welcome/autotests/welcomewidgettest.cpp
@@ -4,7 +4,10 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #include "welcomewidgettest.h"
+#include "welcome/welcomewidget.h"
+#include <QHBoxLayout>
 #include <QTest>
+using namespace Qt::Literals::StringLiterals;
 QTEST_MAIN(WelcomeWidgetTest)
 WelcomeWidgetTest::WelcomeWidgetTest(QObject *parent)
     : QObject{parent}
@@ -13,6 +16,10 @@ WelcomeWidgetTest::WelcomeWidgetTest(QObject *parent)
 
 void WelcomeWidgetTest::shouldHaveDefaultValues()
 {
+    WelcomeWidget w;
+    auto mainLayout = w.findChild<QHBoxLayout *>("mainLayout"_L1);
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->contentsMargins(), QMargins{});
     // TODO
 }
 
