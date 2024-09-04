f137b825fe0a3625b55e7a7fc10d10c5e7e1683b
Laurent Montel <montel@kde.org>
2024-09-04 07:36:21 +0200

    Add moderationreportuserinfos autotest

--- src/core/autotests/moderationreportuserinfostest.cpp
+++ src/core/autotests/moderationreportuserinfostest.cpp
@@ -6,7 +6,9 @@
 
 #include "moderationreportuserinfostest.h"
 #include "moderation/moderationreportuserinfos.h"
+#include "ruqola_autotest_helper.h"
 #include <QTest>
+using namespace Qt::Literals::StringLiterals;
 QTEST_GUILESS_MAIN(ModerationReportUserInfosTest)
 ModerationReportUserInfosTest::ModerationReportUserInfosTest(QObject *parent)
     : QObject{parent}
@@ -15,12 +17,37 @@ ModerationReportUserInfosTest::ModerationReportUserInfosTest(QObject *parent)
 
 void ModerationReportUserInfosTest::shouldHaveDefaultValues()
 {
-    ModerationReportUserInfos w;
-    QCOMPARE(w.moderationReportUserInfosCount(), 0);
-    QCOMPARE(w.offset(), 0);
-    QCOMPARE(w.total(), 0);
-    QVERIFY(w.moderationReportUserInfosList().isEmpty());
-    QVERIFY(!w.user().isValid());
+    ModerationReportUserInfos infos;
+    QCOMPARE(infos.moderationReportUserInfosCount(), 0);
+    QCOMPARE(infos.offset(), 0);
+    QCOMPARE(infos.total(), 0);
+    QVERIFY(infos.moderationReportUserInfosList().isEmpty());
+    QVERIFY(!infos.user().isValid());
+}
+
+void ModerationReportUserInfosTest::shouldLoadReportUserInfos_data()
+{
+    QTest::addColumn<QString>("name");
+    QTest::addColumn<ModerationReportUserInfos>("reportuserinfos");
+
+    {
+        ModerationReportUserInfos moderationReportUserInfos;
+        User user;
+        user.setStatus(User::PresenceStatus::Unknown);
+        moderationReportUserInfos.setUser(user);
+        QTest::addRow("empty") << QStringLiteral("empty") << moderationReportUserInfos;
+    }
+}
+
+void ModerationReportUserInfosTest::shouldLoadReportUserInfos()
+{
+    QFETCH(QString, name);
+    QFETCH(ModerationReportUserInfos, reportuserinfos);
+    const QString originalJsonFile = QLatin1StringView(RUQOLA_DATA_DIR) + "/moderationreportuserinfos/"_L1 + name + ".json"_L1;
+    const QJsonObject obj = AutoTestHelper::loadJsonObject(originalJsonFile);
+    ModerationReportUserInfos m;
+    m.parseModerationReportUserInfos(obj);
+    QCOMPARE(m, reportuserinfos);
 }
 
 #include "moc_moderationreportuserinfostest.cpp"
--- src/core/autotests/moderationreportuserinfostest.h
+++ src/core/autotests/moderationreportuserinfostest.h
@@ -16,4 +16,6 @@ public:
     ~ModerationReportUserInfosTest() override = default;
 private Q_SLOTS:
     void shouldHaveDefaultValues();
+    void shouldLoadReportUserInfos();
+    void shouldLoadReportUserInfos_data();
 };
