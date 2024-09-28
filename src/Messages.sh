eab1926065281c307d5fd82ad9a84a36454476d1
Laurent Montel <laurent@kdab.com>
2024-09-28 08:51:52 +0200

    Exclude cmark-rc

--- src/Messages.sh
+++ src/Messages.sh
@@ -3,4 +3,5 @@
 # SPDX-FileCopyrightText: 2018-2023 Montel Laurent <montel@kde.org> 
 
 $EXTRACTRC `find . -name "*.ui" -o -name "*.rc"` >> rc.cpp
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/ruqola.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/cmark-rc/'` -o $podir/ruqola.pot4
+rm -f rc.cpp
