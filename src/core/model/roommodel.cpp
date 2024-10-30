69f60e3f593a30a7ac5b0a8ff5be50341deff16c
Laurent Montel <montel@kde.org>
2024-10-30 13:20:18 +0100

    Fix mem leak

--- src/core/model/roommodel.cpp
+++ src/core/model/roommodel.cpp
@@ -33,8 +33,8 @@ void RoomModel::clear()
 {
     if (!mRoomsList.isEmpty()) {
         beginResetModel();
-        mRoomsList.clear();
         qDeleteAll(mRoomsList);
+        mRoomsList.clear();
         endResetModel();
     }
 }
