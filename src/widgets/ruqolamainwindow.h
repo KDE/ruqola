47016e2e8584b8e7e0eb2db002bf4a66cbee37c9
Laurent Montel <montel@kde.org>
2024-11-15 08:24:02 +0100

    Add kdsingleapplication. Based on patch from Nicolas Fella
    https://invent.kde.org/network/ruqola/-/merge_requests/125/

--- src/widgets/ruqolamainwindow.h
+++ src/widgets/ruqolamainwindow.h
@@ -40,9 +40,7 @@ public:
     explicit RuqolaMainWindow(QWidget *parent = nullptr);
     ~RuqolaMainWindow() override;
 
-#if !defined(Q_OS_WIN) && !defined(Q_OS_MACOS)
     void slotActivateRequested(const QStringList &arguments, const QString &workingDirectory);
-#endif
 
     void parseCommandLine(QCommandLineParser *parser);
 
