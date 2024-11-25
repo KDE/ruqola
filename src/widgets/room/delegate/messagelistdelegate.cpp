488d50f3a68b7a51c61b042c50764a469ef571b7
Laurent Montel <montel@kde.org>
2024-11-25 08:16:18 +0100

    Message::VideoConference is not a systemmessage => we can add emoji etc.

--- src/widgets/room/delegate/messagelistdelegate.cpp
+++ src/widgets/room/delegate/messagelistdelegate.cpp
@@ -749,7 +749,7 @@ static void positionPopup(QPoint pos, QWidget *parentWindow, QWidget *popup)
 bool MessageListDelegate::isSystemMessage(const Message *message) const
 {
     const Message::MessageType messageType = message->messageType();
-    const bool isSystemMessage = (messageType == Message::System) || (messageType == Message::Information) || (messageType == Message::VideoConference);
+    const bool isSystemMessage = (messageType == Message::System) || (messageType == Message::Information) /* || (messageType == Message::VideoConference)*/;
     return isSystemMessage || message->privateMessage();
 }
 
