b10b1c4d826d1287d8be5846fe457d7496569a29
Laurent Montel <montel@kde.org>
2024-12-02 08:08:41 +0100

    Prepare appsmarketplaceinstalledinfo

---
+++ src/core/apps/appsmarketplaceinstalledinfo.h
@@ -0,0 +1,21 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "libruqolacore_export.h"
+#include <QDebug>
+#include <QJsonObject>
+class LIBRUQOLACORE_EXPORT AppsMarketPlaceInstalledInfo
+{
+public:
+    AppsMarketPlaceInstalledInfo();
+    ~AppsMarketPlaceInstalledInfo();
+};
+
+Q_DECLARE_METATYPE(AppsMarketPlaceInstalledInfo)
+Q_DECLARE_TYPEINFO(AppsMarketPlaceInstalledInfo, Q_RELOCATABLE_TYPE);
+LIBRUQOLACORE_EXPORT QDebug operator<<(QDebug d, const AppsMarketPlaceInstalledInfo &t);
