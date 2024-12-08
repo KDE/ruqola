e1b325511f0ee3e41ec8eb507264106dd19668f4
Laurent Montel <montel@kde.org>
2024-12-08 11:02:28 +0100

    Remove it. Python test support was removed long time ago

--- example/python/ruqola-python/lineedit.py
+++
@@ -1,7 +0,0 @@
-from PySide2 import QtWidgets, QtGui
-
-class LineEdit(QtWidgets.QLineEdit):
-    def __init__(self):
-        super().__init__()
-        self.setPlaceholderText(self.tr("Add text here..."))
-        
