41779667dd87d4cd969ffdfe590b09f2bd808673
Laurent Montel <montel@kde.org>
2024-12-12 13:55:30 +0100

    Prepare to implement applicationssettingsdescriptiontabwidget

---
+++ src/widgets/applicationssettingsdialog/applicationssettingsdescriptiontabwidget.cpp
@@ -0,0 +1,15 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "applicationssettingsdescriptiontabwidget.h"
+
+ApplicationsSettingsDescriptionTabWidget::ApplicationsSettingsDescriptionTabWidget(QWidget *parent)
+    : QWidget(parent)
+{
+}
+
+ApplicationsSettingsDescriptionTabWidget::~ApplicationsSettingsDescriptionTabWidget() = default;
+
+#include "moc_applicationssettingsdescriptiontabwidget.cpp"
---
+++ src/widgets/applicationssettingsdialog/applicationssettingsdescriptiontabwidget.h
@@ -0,0 +1,17 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "libruqolawidgets_private_export.h"
+#include <QWidget>
+class LIBRUQOLAWIDGETS_TESTS_EXPORT ApplicationsSettingsDescriptionTabWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ApplicationsSettingsDescriptionTabWidget(QWidget *parent = nullptr);
+    ~ApplicationsSettingsDescriptionTabWidget() override;
+};
