7e8dc7c720609b26e258f7d175b83a502c0ca8a8
Laurent Montel <montel@kde.org>
2024-12-19 19:54:48 +0100

    coding style

--- src/rocketchatrestapi-qt/license/licenseslistjob.cpp
+++ src/rocketchatrestapi-qt/license/licenseslistjob.cpp
@@ -5,12 +5,12 @@
 */
 
 #include "licenseslistjob.h"
-using namespace Qt::Literals::StringLiterals;
 
 #include "restapimethod.h"
 #include "rocketchatqtrestapi_debug.h"
 #include <QJsonDocument>
 #include <QJsonObject>
+using namespace Qt::Literals::StringLiterals;
 using namespace RocketChatRestApi;
 LicensesListJob::LicensesListJob(QObject *parent)
     : RestApiAbstractJob(parent)
