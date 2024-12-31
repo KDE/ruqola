baf76958004696e7f62dd196611a303eac9bb88f
Laurent Montel <montel@kde.org>
2024-12-31 13:38:33 +0100

    Prepare to add AppUpdateInfoJob

---
+++ src/rocketchatrestapi-qt/apps/appupdateinfojob.cpp
@@ -0,0 +1,122 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "appupdateinfojob.h"
+
+#include "restapimethod.h"
+#include "rocketchatqtrestapi_debug.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QUrlQuery>
+
+using namespace Qt::Literals::StringLiterals;
+using namespace RocketChatRestApi;
+AppUpdateInfoJob::AppUpdateInfoJob(QObject *parent)
+    : RestApiAbstractJob(parent)
+{
+}
+
+AppUpdateInfoJob::~AppUpdateInfoJob() = default;
+
+bool AppUpdateInfoJob::requireHttpAuthentication() const
+{
+    return true;
+}
+
+bool AppUpdateInfoJob::start()
+{
+    if (!canStart()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "Impossible to start get app count job";
+        deleteLater();
+        return false;
+    }
+    submitPostRequest(json());
+    addStartRestApiInfo(QByteArrayLiteral("AppUpdateInfoJob: get app count info starting"));
+    return true;
+}
+
+bool AppUpdateInfoJob::canStart() const
+{
+    if (!RestApiAbstractJob::canStart()) {
+        return false;
+    }
+    if (mAppInfoType == AppInfoType::Unknown) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "mAppInfoType undefined";
+        return false;
+    }
+    if (mAppsId.isEmpty()) {
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "mAppsId is empty";
+        return false;
+    }
+    return true;
+}
+
+QJsonDocument AppUpdateInfoJob::json() const
+{
+    // TODO
+    QJsonObject jsonObj;
+    const QJsonDocument postData = QJsonDocument(jsonObj);
+    return postData;
+}
+
+AppUpdateInfoJob::AppInfoType AppUpdateInfoJob::appInfoType() const
+{
+    return mAppInfoType;
+}
+
+void AppUpdateInfoJob::setAppInfoType(AppInfoType newAppInfoType)
+{
+    mAppInfoType = newAppInfoType;
+}
+
+QByteArray AppUpdateInfoJob::appsId() const
+{
+    return mAppsId;
+}
+
+void AppUpdateInfoJob::setAppsId(const QByteArray &newAppsId)
+{
+    mAppsId = newAppsId;
+}
+
+QString AppUpdateInfoJob::generateUrlExtension() const
+{
+    QString url = QString::fromLatin1(mAppsId) + QLatin1Char('/');
+
+    switch (mAppInfoType) {
+    case AppInfoType::Unknown:
+        qCWarning(ROCKETCHATQTRESTAPI_LOG) << "Unknown type";
+        break;
+    case AppInfoType::Versions:
+        url += QStringLiteral("versions");
+        break;
+    case AppInfoType::Logs:
+        url += QStringLiteral("logs");
+        break;
+    case AppInfoType::Apis:
+        url += QStringLiteral("apis");
+        break;
+    case AppInfoType::ScreenShots:
+        url += QStringLiteral("screenshots");
+        break;
+    case AppInfoType::Settings:
+        url += QStringLiteral("settings");
+        break;
+    }
+    return url;
+}
+
+QNetworkRequest AppUpdateInfoJob::request() const
+{
+    const QUrl url = mRestApiMethod->generateUrl(RestApiUtil::RestApiUrlType::Empty, RestApiUtil::RestApiUrlExtensionType::Apps, generateUrlExtension());
+    QNetworkRequest request(url);
+    addAuthRawHeader(request);
+    addRequestAttribute(request);
+
+    return request;
+}
+
+#include "moc_appupdateinfojob.cpp"
---
+++ src/rocketchatrestapi-qt/apps/appupdateinfojob.h
@@ -0,0 +1,55 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "librocketchatrestapi-qt_export.h"
+#include "restapiabstractjob.h"
+
+namespace RocketChatRestApi
+{
+class LIBROCKETCHATRESTAPI_QT_EXPORT AppUpdateInfoJob : public RestApiAbstractJob
+{
+    Q_OBJECT
+public:
+    enum class AppInfoType {
+        Unknown,
+        Versions,
+        Logs,
+        Apis,
+        ScreenShots,
+        Settings,
+    };
+
+    explicit AppUpdateInfoJob(QObject *parent = nullptr);
+    ~AppUpdateInfoJob() override;
+
+    [[nodiscard]] bool requireHttpAuthentication() const override;
+
+    [[nodiscard]] bool start() override;
+
+    [[nodiscard]] QNetworkRequest request() const override;
+
+    [[nodiscard]] QByteArray appsId() const;
+    void setAppsId(const QByteArray &newAppsId);
+
+    [[nodiscard]] AppInfoType appInfoType() const;
+    void setAppInfoType(AppInfoType newAppInfoType);
+
+    [[nodiscard]] bool canStart() const override;
+
+    [[nodiscard]] QJsonDocument json() const;
+
+Q_SIGNALS:
+    void appInfoDone(const QJsonObject &obj);
+
+private:
+    LIBROCKETCHATRESTAPI_QT_NO_EXPORT void onPostRequestResponse(const QString &replyErrorString, const QJsonDocument &replyJson) override;
+    [[nodiscard]] LIBROCKETCHATRESTAPI_QT_NO_EXPORT QString generateUrlExtension() const;
+    QByteArray mAppsId;
+    AppInfoType mAppInfoType = AppInfoType::Unknown;
+};
+}
