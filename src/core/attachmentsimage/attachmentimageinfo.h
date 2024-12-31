e5c4cdeae4a80c14e82b1e77e1b78cb9809cb698
Laurent Montel <montel@kde.org>
2024-12-31 08:38:58 +0100

    Prepare to implement attachmentimageinfo

---
+++ src/core/attachmentsimage/attachmentimageinfo.h
@@ -0,0 +1,15 @@
+/*
+   SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "libruqolacore_export.h"
+class LIBRUQOLACORE_EXPORT AttachmentImageInfo
+{
+public:
+    AttachmentImageInfo();
+    ~AttachmentImageInfo();
+};
