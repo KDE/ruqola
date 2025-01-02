12628578865b7c1b45f87f697ff6c0e9eb11d601
Laurent Montel <montel@kde.org>
2025-01-02 07:35:02 +0100

    Add enum + virtual method

--- src/widgets/room/plugins/plugintool.h
+++ src/widgets/room/plugins/plugintool.h
@@ -12,10 +12,19 @@ class LIBRUQOLAWIDGETS_EXPORT PluginTool : public QObject
 {
     Q_OBJECT
 public:
+    enum class ToolType : uint8_t {
+        Tools,
+        Channels,
+        MessageViewToolBar,
+    };
     explicit PluginTool(QObject *parent = nullptr);
     ~PluginTool() override;
 
     [[nodiscard]] virtual bool toolFound() const = 0;
+    [[nodiscard]] virtual ToolType toolType() const = 0;
+    [[nodiscard]] virtual QString iconName() const = 0;
+    [[nodiscard]] virtual QString toolTip() const = 0;
+    [[nodiscard]] virtual QString description() const = 0;
 
 Q_SIGNALS:
     void activated();
