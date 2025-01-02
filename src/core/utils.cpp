4131cdd1002166b84f596668fe297cccd147ff5b
Laurent Montel <montel@kde.org>
2025-01-02 13:29:03 +0100

    Allow to search binary, fix on windows too

--- src/core/utils.cpp
+++ src/core/utils.cpp
@@ -392,3 +392,17 @@ QString Utils::createUniqueAccountName(const QStringList &list, const QString &a
     }
     return newAccountName;
 }
+
+QString Utils::findExecutable(const QString &exec)
+{
+#ifdef Q_OS_WIN
+    const QString executableName = exec + QStringLiteral(".exe");
+    QString path = QStandardPaths::findExecutable(executableName, {QCoreApplication::applicationDirPath()});
+    if (path.isEmpty()) {
+        path = QStandardPaths::findExecutable(executableName);
+    }
+#else
+    const QString path = QStandardPaths::findExecutable(exec);
+#endif
+    return path;
+}
