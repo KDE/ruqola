030ce1e21578230916c8e60544262cd84a4250c3
Laurent Montel <montel@kde.org>
2025-01-05 21:12:36 +0100

    Add info about tool plugin support

--- src/widgets/whatsnew/whatsnewtranslations.cpp
+++ src/widgets/whatsnew/whatsnewtranslations.cpp
@@ -13,7 +13,9 @@ WhatsNewTranslations::~WhatsNewTranslations() = default;
 // Use by newFeaturesMD5
 QList<KLazyLocalizedString> WhatsNewTranslations::lastNewFeatures() const
 {
-    const QList<KLazyLocalizedString> info{};
+    const QList<KLazyLocalizedString> info{
+        kli18n("Add support for tools plugins"),
+    };
     return info;
 }
 
