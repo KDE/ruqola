7592d12ac9366a56ffbd258f8707d9d74947da99
Laurent Montel <montel@kde.org>
2025-01-10 13:34:59 +0100

    We can't add duplicate token name
    (cherry picked from commit 9453b1de957b25e89e8a12a94128137bf27cde23)

--- src/widgets/myaccount/myaccountpersonalaccesscreatewidget.h
+++ src/widgets/myaccount/myaccountpersonalaccesscreatewidget.h
@@ -20,10 +20,12 @@ public:
 
     [[nodiscard]] bool bypassTwoFactor() const;
     [[nodiscard]] QString tokenName() const;
+    void setExistingTokenNames(const QStringList &lst);
 Q_SIGNALS:
     void updateOkButton(bool enabled);
 
 private:
+    QStringList mExistingTokenNames;
     QCheckBox *const mBypassTwoFactor;
     QLineEdit *const mTokenName;
 };
