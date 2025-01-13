4d8a2e3db6b34535376d9d124b1681709e2aec13
Laurent Montel <montel@kde.org>
2025-01-13 07:57:38 +0100

    Add autotest

---
+++ src/widgets/needupdateversion/autotests/needupdateparsehtmljobtest.cpp
@@ -0,0 +1,15 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "needupdateparsehtmljobtest.h"
+
+#include <QTest>
+
+QTEST_MAIN(NeedUpdateParseHtmlJobTest)
+
+NeedUpdateParseHtmlJobTest::NeedUpdateParseHtmlJobTest(QObject *parent)
+    : QObject{parent}
+{
+}
