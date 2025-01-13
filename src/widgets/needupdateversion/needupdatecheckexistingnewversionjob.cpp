6d0a3fd3fea73d1615bada86b75e09fe705c3c4d
Laurent Montel <montel@kde.org>
2025-01-13 20:11:11 +0100

    Implement needupdatecheckexistingnewversionjob

--- src/widgets/needupdateversion/needupdatecheckexistingnewversionjob.cpp
+++ src/widgets/needupdateversion/needupdatecheckexistingnewversionjob.cpp
@@ -5,6 +5,11 @@
 */
 
 #include "needupdatecheckexistingnewversionjob.h"
+#include "needupdateparsehtmljob.h"
+#include "needupdateparsehtmlutil.h"
+#include "ruqolawidgets_debug.h"
+
+#include <QDate>
 
 NeedUpdateCheckExistingNewVersionJob::NeedUpdateCheckExistingNewVersionJob(QObject *parent)
     : QObject{parent}
@@ -12,5 +17,66 @@ NeedUpdateCheckExistingNewVersionJob::NeedUpdateCheckExistingNewVersionJob(QObje
 }
 
 NeedUpdateCheckExistingNewVersionJob::~NeedUpdateCheckExistingNewVersionJob() = default;
+
+void NeedUpdateCheckExistingNewVersionJob::start()
+{
+    if (!canStart()) {
+        qCWarning(RUQOLAWIDGETS_LOG) << "Impossible to start NeedUpdateCheckExistingNewVersionJob";
+        deleteLater();
+        return;
+    }
+    auto job = new NeedUpdateParseHtmlJob(this);
+    job->setUrl(mUrl);
+    connect(job, &NeedUpdateParseHtmlJob::downLoadDone, this, &NeedUpdateCheckExistingNewVersionJob::slotDownloadDone);
+    job->start();
+}
+
+void NeedUpdateCheckExistingNewVersionJob::slotDownloadDone(const QString &str)
+{
+    const QString compileDateStr = NeedUpdateParseHtmlUtil::extractDate(str);
+    if (compileDateStr.isEmpty()) {
+        Q_EMIT foundNewVersion(false);
+        deleteLater();
+        return;
+    }
+
+    const QDate currentCompiledDate = QDate::fromString(mCompileDate, QStringLiteral("MMM dd yyyy"));
+    qCDebug(RUQOLAWIDGETS_LOG) << " currentCompiledDate " << currentCompiledDate << " original " << mCompileDate;
+
+    const QDate dateFromUrl = QDate::fromString(compileDateStr, QStringLiteral("yyyy-MM-dd"));
+    qCDebug(RUQOLAWIDGETS_LOG) << " dateFromUrl " << dateFromUrl << " original " << compileDateStr;
+
+    if (dateFromUrl > currentCompiledDate) {
+        Q_EMIT foundNewVersion(true);
+    } else {
+        Q_EMIT foundNewVersion(false);
+    }
+    deleteLater();
+}
+
+QString NeedUpdateCheckExistingNewVersionJob::url() const
+{
+    return mUrl;
+}
+
+void NeedUpdateCheckExistingNewVersionJob::setUrl(const QString &newUrl)
+{
+    mUrl = newUrl;
+}
+
+bool NeedUpdateCheckExistingNewVersionJob::canStart() const
+{
+    return !mUrl.isEmpty() && !mCompileDate.isEmpty();
+}
+
+QString NeedUpdateCheckExistingNewVersionJob::compileDate() const
+{
+    return mCompileDate;
+}
+
+void NeedUpdateCheckExistingNewVersionJob::setCompileDate(const QString &newCompileDate)
+{
+    mCompileDate = newCompileDate;
+}
 
 #include "moc_needupdatecheckexistingnewversionjob.cpp"
--- src/widgets/needupdateversion/needupdatecheckexistingnewversionjob.h
+++ src/widgets/needupdateversion/needupdatecheckexistingnewversionjob.h
@@ -6,12 +6,31 @@
 
 #pragma once
 
+#include "libruqolawidgets_private_export.h"
 #include <QObject>
 
-class NeedUpdateCheckExistingNewVersionJob : public QObject
+class LIBRUQOLAWIDGETS_TESTS_EXPORT NeedUpdateCheckExistingNewVersionJob : public QObject
 {
     Q_OBJECT
 public:
     explicit NeedUpdateCheckExistingNewVersionJob(QObject *parent = nullptr);
     ~NeedUpdateCheckExistingNewVersionJob() override;
+
+    void start();
+
+    [[nodiscard]] QString url() const;
+    void setUrl(const QString &newUrl);
+
+    [[nodiscard]] bool canStart() const;
+
+    [[nodiscard]] QString compileDate() const;
+    void setCompileDate(const QString &newCompileDate);
+
+Q_SIGNALS:
+    void foundNewVersion(bool state);
+
+private:
+    LIBRUQOLAWIDGETS_NO_EXPORT void slotDownloadDone(const QString &str);
+    QString mUrl;
+    QString mCompileDate;
 };
