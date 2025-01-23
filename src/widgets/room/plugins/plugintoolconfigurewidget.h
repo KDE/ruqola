455370bc094a6668150c8ebadd9fa3e053b1a4a5
Laurent Montel <montel@kde.org>
2025-01-23 19:47:56 +0100

    Prepare to implement plugintoolconfigurewidget

---
+++ src/widgets/room/plugins/plugintoolconfigurewidget.h
@@ -0,0 +1,16 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#pragma once
+
+#include <QWidget>
+
+class PluginToolConfigureWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit PluginToolConfigureWidget(QWidget *parent = nullptr);
+    ~PluginToolConfigureWidget() override;
+};
