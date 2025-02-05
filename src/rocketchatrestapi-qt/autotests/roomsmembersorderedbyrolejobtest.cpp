32ca068e5e74bf21950a371aad256bb48a09c23b
Laurent Montel <montel@kde.org>
2025-02-05 20:22:09 +0100

    Prepare autotest

---
+++ src/rocketchatrestapi-qt/autotests/roomsmembersorderedbyrolejobtest.cpp
@@ -0,0 +1,81 @@
+/*
+   SPDX-FileCopyrightText: 2024-2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "roomsmembersorderedbyrolejobtest.h"
+#include "restapimethod.h"
+#include "rooms/roomsimagesjob.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(RoomsMembersOrderedByRoleJobTest)
+using namespace RocketChatRestApi;
+RoomsMembersOrderedByRoleJobTest::RoomsMembersOrderedByRoleJobTest(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void RoomsMembersOrderedByRoleJobTest::shouldHaveDefaultValue()
+{
+    RoomsImagesJob job;
+    QVERIFY(!job.restApiMethod());
+    QVERIFY(!job.networkAccessManager());
+    QVERIFY(!job.start());
+    QVERIFY(!job.roomsImagesJobInfo().isValid());
+    QVERIFY(job.requireHttpAuthentication());
+    QVERIFY(!job.restApiLogger());
+    QVERIFY(job.hasQueryParameterSupport());
+    QVERIFY(!job.requireTwoFactorAuthentication());
+}
+
+void RoomsMembersOrderedByRoleJobTest::shouldGenerateRequest()
+{
+    RoomsImagesJob job;
+    RestApiMethod method;
+    method.setServerUrl(QStringLiteral("http://www.kde.org"));
+    job.setRestApiMethod(&method);
+    const QByteArray roomId("bla");
+    RoomsImagesJob::RoomsImagesJobInfo info;
+    info.roomId = QByteArrayLiteral("room_id");
+    info.startingFromId = QByteArrayLiteral("start_Id");
+    info.count = 5;
+    info.offset = 0;
+    job.setRoomsImagesJobInfo(info);
+    const QNetworkRequest request = job.request();
+    QCOMPARE(request.url(), QUrl(QStringLiteral("http://www.kde.org/api/v1/rooms.images?roomId=room_id&startingFromId=start_Id&offset=0&count=5")));
+}
+
+void RoomsMembersOrderedByRoleJobTest::shouldNotStarting()
+{
+    RoomsImagesJob job;
+
+    RestApiMethod method;
+    method.setServerUrl(QStringLiteral("http://www.kde.org"));
+    job.setRestApiMethod(&method);
+
+    QNetworkAccessManager mNetworkAccessManager;
+    job.setNetworkAccessManager(&mNetworkAccessManager);
+    QVERIFY(!job.canStart());
+    const QString auth = QStringLiteral("foo");
+    const QString userId = QStringLiteral("foo");
+    job.setAuthToken(auth);
+    QVERIFY(!job.canStart());
+    job.setUserId(userId);
+    QVERIFY(!job.canStart());
+    RoomsImagesJob::RoomsImagesJobInfo info;
+    info.roomId = QByteArrayLiteral("room_id");
+    info.startingFromId = QByteArrayLiteral("start_id");
+    info.count = 0;
+    info.offset = 0;
+    job.setRoomsImagesJobInfo(info);
+
+    QVERIFY(!job.canStart());
+    info.startingFromId = QByteArrayLiteral("room33");
+    job.setRoomsImagesJobInfo(info);
+    QVERIFY(!job.canStart());
+    info.count = 4;
+    job.setRoomsImagesJobInfo(info);
+    QVERIFY(job.canStart());
+}
+
+#include "moc_roomsmembersorderedbyrolejobtest.cpp"
---
+++ src/rocketchatrestapi-qt/autotests/roomsmembersorderedbyrolejobtest.h
@@ -0,0 +1,21 @@
+/*
+   SPDX-FileCopyrightText: 2024-2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+
+class RoomsMembersOrderedByRoleJobTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit RoomsMembersOrderedByRoleJobTest(QObject *parent = nullptr);
+    ~RoomsMembersOrderedByRoleJobTest() override = default;
+private Q_SLOTS:
+    void shouldHaveDefaultValue();
+    void shouldGenerateRequest();
+    void shouldNotStarting();
+};
