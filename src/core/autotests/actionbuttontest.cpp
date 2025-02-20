8146348b5cb13fd435fdea8e3de097d06eb36ad0
Laurent Montel <montel@kde.org>
2025-02-20 13:41:36 +0100

    Add autotest

---
+++ src/core/autotests/actionbuttontest.cpp
@@ -0,0 +1,21 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "actionbuttontest.h"
+#include "actionbuttons/actionbutton.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(ActionButtonTest)
+ActionButtonTest::ActionButtonTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void ActionButtonTest::shouldHaveDefaultValues()
+{
+    ActionButton b;
+    QVERIFY(b.appId().isEmpty());
+    QVERIFY(b.actionId().isEmpty());
+}
---
+++ src/core/autotests/actionbuttontest.h
@@ -0,0 +1,19 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+
+class ActionButtonTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ActionButtonTest(QObject *parent = nullptr);
+    ~ActionButtonTest() = default;
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+};
