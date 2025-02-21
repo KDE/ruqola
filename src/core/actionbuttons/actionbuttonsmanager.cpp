645ac7c857284cf2296da7c77dcce22c874d8d84
Laurent Montel <montel@kde.org>
2025-02-21 08:15:36 +0100

    Prepare actionbuttonsmanager

---
+++ src/core/actionbuttons/actionbuttonsmanager.cpp
@@ -0,0 +1,24 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "actionbuttonsmanager.h"
+
+ActionButtonsManager::ActionButtonsManager(QObject *parent)
+    : QObject{parent}
+{
+}
+
+ActionButtonsManager::~ActionButtonsManager() = default;
+
+QList<ActionButton> ActionButtonsManager::actionButtons() const
+{
+    return mActionButtons;
+}
+
+void ActionButtonsManager::setActionButtons(const QList<ActionButton> &newActionButtons)
+{
+    mActionButtons = newActionButtons;
+}
---
+++ src/core/actionbuttons/actionbuttonsmanager.h
@@ -0,0 +1,24 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+#include "actionbutton.h"
+#include "libruqolacore_export.h"
+#include <QObject>
+
+class LIBRUQOLACORE_EXPORT ActionButtonsManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ActionButtonsManager(QObject *parent = nullptr);
+    ~ActionButtonsManager() override;
+
+    [[nodiscard]] QList<ActionButton> actionButtons() const;
+    void setActionButtons(const QList<ActionButton> &newActionButtons);
+
+private:
+    QList<ActionButton> mActionButtons;
+};
