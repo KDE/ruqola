ef29354b6380f6ff819aa7def93be92f069fe22c
Laurent Montel <montel@kde.org>
2025-02-24 20:11:03 +0100

    Add autotests

---
+++ src/widgets/autogenerateui/autotests/autogenerateinteractionuiviewtexttest.cpp
@@ -0,0 +1,64 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "autogenerateinteractionuiviewtexttest.h"
+#include "autogenerateui/autogenerateinteractionuiviewtext.h"
+#include "ruqola_autogenerateui_autotest_helper.h"
+#include <QTest>
+using namespace Qt::Literals::StringLiterals;
+QTEST_MAIN(AutoGenerateInteractionUiViewTextTest)
+
+AutoGenerateInteractionUiViewTextTest::AutoGenerateInteractionUiViewTextTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void AutoGenerateInteractionUiViewTextTest::shouldHaveDefaultValues()
+{
+    AutoGenerateInteractionUiViewText t;
+    QVERIFY(t.text().isEmpty());
+    QVERIFY(!t.emoji());
+    QCOMPARE(t.type(), AutoGenerateInteractionUiViewText::TextType::Unknown);
+}
+
+void AutoGenerateInteractionUiViewTextTest::shouldLoadJson()
+{
+    QFETCH(QString, name);
+    QFETCH(AutoGenerateInteractionUiViewText, textelement);
+    const QString originalJsonFile = QLatin1StringView(RUQOLA_AUTOGENERATEUI_DATA_DIR) + "/autogenerateinteractionuiviewtext/"_L1 + name + ".json"_L1;
+    const QJsonObject obj = AutoTestHelper::loadJsonObject(originalJsonFile);
+
+    AutoGenerateInteractionUiViewText result;
+    result.parse(obj);
+    const bool equal = result == textelement;
+    if (!equal) {
+        qDebug() << " result " << result;
+        qDebug() << " deviceInfo " << textelement;
+    }
+    QVERIFY(equal);
+}
+
+void AutoGenerateInteractionUiViewTextTest::shouldLoadJson_data()
+{
+    QTest::addColumn<QString>("name");
+    QTest::addColumn<AutoGenerateInteractionUiViewText>("textelement");
+
+    {
+        AutoGenerateInteractionUiViewText textElement;
+        textElement.setText("**Past and Incomplete:**"_L1);
+        textElement.setEmoji(false);
+        textElement.setType(AutoGenerateInteractionUiViewText::TextType::Markdown);
+
+        QTest::addRow("mardownelement") << QStringLiteral("mardownelement") << textElement;
+    }
+    {
+        AutoGenerateInteractionUiViewText textElement;
+        textElement.setText("Close"_L1);
+        textElement.setEmoji(false);
+        textElement.setType(AutoGenerateInteractionUiViewText::TextType::PlainText);
+
+        QTest::addRow("plaintextelement") << QStringLiteral("plaintextelement") << textElement;
+    }
+}
---
+++ src/widgets/autogenerateui/autotests/autogenerateinteractionuiviewtexttest.h
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#pragma once
+
+#include <QObject>
+
+class AutoGenerateInteractionUiViewTextTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit AutoGenerateInteractionUiViewTextTest(QObject *parent = nullptr);
+    ~AutoGenerateInteractionUiViewTextTest() override = default;
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+    void shouldLoadJson();
+    void shouldLoadJson_data();
+};
