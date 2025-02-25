8a6391636712c2f172fa4158f238b88386fd0a73
Laurent Montel <montel@kde.org>
2025-02-25 07:55:24 +0100

    Add more autotest

---
+++ src/widgets/autogenerateui/autotests/autogenerateinteractionuiviewplaintextinputelementtest.cpp
@@ -0,0 +1,25 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "autogenerateinteractionuiviewplaintextinputelementtest.h"
+#include "autogenerateui/autogenerateinteractionuiviewplaintextinputelement.h"
+#include <QTest>
+
+QTEST_MAIN(AutoGenerateInteractionUiViewPlainTextInputElementTest)
+
+AutoGenerateInteractionUiViewPlainTextInputElementTest::AutoGenerateInteractionUiViewPlainTextInputElementTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void AutoGenerateInteractionUiViewPlainTextInputElementTest::shouldHaveDefaultValues()
+{
+    AutoGenerateInteractionUiViewPlainTextInputElement e;
+    QVERIFY(e.placeHolder().isEmpty());
+    QVERIFY(e.initialValue().isEmpty());
+    QVERIFY(!e.multiLine());
+    QCOMPARE(e.minLength(), -1);
+    QCOMPARE(e.maxLength(), -1);
+}
