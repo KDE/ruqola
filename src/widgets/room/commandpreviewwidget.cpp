80febde2350f10e5524d586fe64ef03d80a327ca
Laurent Montel <montel@kde.org>
2025-02-28 08:21:21 +0100

    Start to create command preview widget

---
+++ src/widgets/room/commandpreviewwidget.cpp
@@ -0,0 +1,15 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "commandpreviewwidget.h"
+#include <QHBoxLayout>
+
+CommandPreviewWidget::CommandPreviewWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
+
+CommandPreviewWidget::~CommandPreviewWidget() = default;
---
+++ src/widgets/room/commandpreviewwidget.h
@@ -0,0 +1,17 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+#include "libruqolawidgets_private_export.h"
+#include <QWidget>
+
+class LIBRUQOLAWIDGETS_TESTS_EXPORT CommandPreviewWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit CommandPreviewWidget(QWidget *parent = nullptr);
+    ~CommandPreviewWidget() override;
+};
