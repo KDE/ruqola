ef9c8c300719cdb995f43ad9409e4f986c74b8be
Laurent Montel <montel@kde.org>
2025-02-28 13:27:20 +0100

    Improve model

--- src/core/model/previewcommandmodel.cpp
+++ src/core/model/previewcommandmodel.cpp
@@ -12,3 +12,55 @@ PreviewCommandModel::PreviewCommandModel(QObject *parent)
 }
 
 PreviewCommandModel::~PreviewCommandModel() = default;
+
+int PreviewCommandModel::rowCount(const QModelIndex &parent) const
+{
+    if (parent.isValid()) {
+        return 0; // flat model
+    }
+    return mPreviewCommands.count();
+}
+
+QList<PreviewCommand> PreviewCommandModel::previewCommands() const
+{
+    return mPreviewCommands;
+}
+
+void PreviewCommandModel::setPreviewCommands(const QList<PreviewCommand> &newPreviewCommands)
+{
+    clear();
+    if (!newPreviewCommands.isEmpty()) {
+        beginInsertRows(QModelIndex(), 0, newPreviewCommands.count() - 1);
+        mPreviewCommands = newPreviewCommands;
+        endInsertRows();
+    }
+}
+
+void PreviewCommandModel::clear()
+{
+    if (!mPreviewCommands.isEmpty()) {
+        beginResetModel();
+        mPreviewCommands.clear();
+        endResetModel();
+    }
+}
+
+QVariant PreviewCommandModel::data(const QModelIndex &index, int role) const
+{
+    if (index.row() < 0 || index.row() >= mPreviewCommands.count()) {
+        return {};
+    }
+
+    const PreviewCommand &permissionInfo = mPreviewCommands.at(index.row());
+    if (role == Qt::DisplayRole) {
+        return permissionInfo.value();
+    } else if (role == static_cast<int>(PreviewCommandRoles::PreviewCommandInfo)) {
+        return QVariant::fromValue(permissionInfo);
+    } else if (role == static_cast<int>(PreviewCommandRoles::Image)) {
+        // TODO
+        return {};
+    }
+    return permissionInfo.value();
+}
+
+#include "moc_previewcommandmodel.cpp"
--- src/core/model/previewcommandmodel.h
+++ src/core/model/previewcommandmodel.h
@@ -4,12 +4,28 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #pragma once
-
+#include "commands/previewcommand.h"
+#include "libruqolacore_export.h"
 #include <QAbstractListModel>
 
-class PreviewCommandModel : public QAbstractListModel
+class LIBRUQOLACORE_EXPORT PreviewCommandModel : public QAbstractListModel
 {
+    Q_OBJECT
 public:
+    enum class PreviewCommandRoles : uint16_t {
+        PreviewCommandInfo = Qt::UserRole + 1,
+        Image,
+    };
     explicit PreviewCommandModel(QObject *parent = nullptr);
     ~PreviewCommandModel() override;
+
+    [[nodiscard]] int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    [[nodiscard]] QVariant data(const QModelIndex &index, int role) const override;
+
+    [[nodiscard]] QList<PreviewCommand> previewCommands() const;
+    void setPreviewCommands(const QList<PreviewCommand> &newPermissions);
+    void clear();
+
+private:
+    QList<PreviewCommand> mPreviewCommands;
 };
