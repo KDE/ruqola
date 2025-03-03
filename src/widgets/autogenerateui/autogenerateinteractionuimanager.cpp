d0c7ffe7c4b7573cada47c49aad8589c89ea9f45
Laurent Montel <montel@kde.org>
2025-03-03 08:28:41 +0100

    Add manager

---
+++ src/widgets/autogenerateui/autogenerateinteractionuimanager.cpp
@@ -0,0 +1,13 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "autogenerateinteractionuimanager.h"
+
+AutoGenerateInteractionUiManager::AutoGenerateInteractionUiManager(QObject *parent)
+    : QObject{parent}
+{
+}
+
+AutoGenerateInteractionUiManager::~AutoGenerateInteractionUiManager() = default;
