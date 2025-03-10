560d3f44d3b48331b6c58291b1a989c9742b13e0
Laurent Montel <montel@kde.org>
2025-03-10 13:13:23 +0100

    Add SerializeMessageWidget

---
+++ tests/serializemessagesgui/serializemessagewidget.cpp
@@ -0,0 +1,41 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "serializemessagewidget.h"
+#include "rocketchataccount.h"
+#include "room/messagelistview.h"
+#include <QHBoxLayout>
+#include <QPlainTextEdit>
+
+SerializeMessageWidget::SerializeMessageWidget(QWidget *parent)
+    : QWidget{parent}
+    , mPlainTextEdit(new QPlainTextEdit(this))
+    , mMessageListView(new MessageListView(new RocketChatAccount(QStringLiteral("test"), this), MessageListView::Mode::Viewing, this))
+    , mMessageModel(new MessagesModel())
+{
+    auto mainLayout = new QHBoxLayout(this);
+    mainLayout->setContentsMargins({});
+    mainLayout->addWidget(mPlainTextEdit);
+    mainLayout->addWidget(mMessageListView);
+    mMessageListView->setModel(mMessageModel);
+}
+
+SerializeMessageWidget::~SerializeMessageWidget() = default;
+
+void SerializeMessageWidget::addMessage(const Message &msg)
+{
+    mMessageModel->addMessages({msg});
+}
+
+void SerializeMessageWidget::setText(const QString &text)
+{
+    mPlainTextEdit->setPlainText(text);
+}
+
+QString SerializeMessageWidget::text() const
+{
+    return mPlainTextEdit->toPlainText();
+}
---
+++ tests/serializemessagesgui/serializemessagewidget.h
@@ -0,0 +1,29 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "messages/message.h"
+#include <QWidget>
+class QPlainTextEdit;
+class MessageListView;
+class MessagesModel;
+class SerializeMessageWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SerializeMessageWidget(QWidget *parent = nullptr);
+    ~SerializeMessageWidget() override;
+
+    void setText(const QString &text);
+    [[nodiscard]] QString text() const;
+    void addMessage(const Message &msg);
+
+private:
+    QPlainTextEdit *const mPlainTextEdit;
+    MessageListView *const mMessageListView;
+    MessagesModel *const mMessageModel;
+};
