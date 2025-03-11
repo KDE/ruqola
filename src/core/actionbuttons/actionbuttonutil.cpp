befd3e416bbd0f4987d2650a2a20fd744b4e75a2
Laurent Montel <montel@kde.org>
2025-03-11 19:39:43 +0100

    Convert room to ActionButton::RoomTypeFilter

---
+++ src/core/actionbuttons/actionbuttonutil.cpp
@@ -0,0 +1,42 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "actionbuttonutil.h"
+
+ActionButton::RoomTypeFilter ActionButtonUtil::convertRoomTypeToActionButtonRoomTypeFilter(Room *r)
+{
+    if (!r) {
+        return ActionButton::RoomTypeFilter::Unknown;
+    }
+    switch (r->channelType()) {
+    case Room::RoomType::Unknown:
+        return ActionButton::RoomTypeFilter::Unknown;
+    case Room::RoomType::Direct:
+        // Disable for the moment
+        /*
+         if (r->uids().count() > 2) {
+             return ActionButton::RoomTypeFilter::DirectMultiple;
+         }
+         */
+        return ActionButton::RoomTypeFilter::Direct;
+    case Room::RoomType::Channel:
+        if (r->isDiscussionRoom()) {
+            return ActionButton::RoomTypeFilter::PublicDiscussion;
+        }
+        if (r->teamInfo().isValid()) {
+            return ActionButton::RoomTypeFilter::PublicTeam;
+        }
+        return ActionButton::RoomTypeFilter::PublicChannel;
+    case Room::RoomType::Private:
+        if (r->isDiscussionRoom()) {
+            return ActionButton::RoomTypeFilter::PrivateDiscussion;
+        }
+        if (r->teamInfo().isValid()) {
+            return ActionButton::RoomTypeFilter::PrivateTeam;
+        }
+        return ActionButton::RoomTypeFilter::PrivateChannel;
+    }
+    return ActionButton::RoomTypeFilter::Unknown;
+}
---
+++ src/core/actionbuttons/actionbuttonutil.h
@@ -0,0 +1,13 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#pragma once
+#include "actionbutton.h"
+#include "libruqolacore_export.h"
+#include "room.h"
+namespace ActionButtonUtil
+{
+[[nodiscard]] LIBRUQOLACORE_EXPORT ActionButton::RoomTypeFilter convertRoomTypeToActionButtonRoomTypeFilter(Room *r);
+};
