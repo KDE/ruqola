0eb60b68ddb104ba928214e393b1716a2c529c88
Laurent Montel <montel@kde.org>
2025-03-17 07:59:19 +0100

    Add actionelementwidget (allow to show errors)

---
+++ src/widgets/autogenerateui/widgets/actionelementwidget.cpp
@@ -0,0 +1,14 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "actionelementwidget.h"
+#include <QVBoxLayout>
+
+ActionElementWidget::ActionElementWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
+
+ActionElementWidget::~ActionElementWidget() = default;
