b25ed66fd1bdf3319e3355137dbcf482b10f92bb
Laurent Montel <montel@kde.org>
2025-04-15 13:22:02 +0200

    Add icon

--- src/widgets/room/usersinroomcombobox.cpp
+++ src/widgets/room/usersinroomcombobox.cpp
@@ -19,10 +19,10 @@ UsersInRoomComboBox::~UsersInRoomComboBox() = default;
 void UsersInRoomComboBox::fillCombobox(bool hasOwnerSupport)
 {
     addItem(i18n("All"), QVariant::fromValue(UsersForRoomFilterProxyModel::FilterUserType::All));
-    addItem(i18n("Connected"), QVariant::fromValue(UsersForRoomFilterProxyModel::FilterUserType::Online));
-    addItem(i18n("Disconnected"), QVariant::fromValue(UsersForRoomFilterProxyModel::FilterUserType::Offline));
-    addItem(i18n("Away"), QVariant::fromValue(UsersForRoomFilterProxyModel::FilterUserType::Away));
-    addItem(i18n("Busy"), QVariant::fromValue(UsersForRoomFilterProxyModel::FilterUserType::Busy));
+    addItem(QIcon::fromTheme(QStringLiteral("user-online")), i18n("Connected"), QVariant::fromValue(UsersForRoomFilterProxyModel::FilterUserType::Online));
+    addItem(QIcon::fromTheme(QStringLiteral("user-offline")), i18n("Disconnected"), QVariant::fromValue(UsersForRoomFilterProxyModel::FilterUserType::Offline));
+    addItem(QIcon::fromTheme(QStringLiteral("user-away")), i18n("Away"), QVariant::fromValue(UsersForRoomFilterProxyModel::FilterUserType::Away));
+    addItem(QIcon::fromTheme(QStringLiteral("user-busy")), i18n("Busy"), QVariant::fromValue(UsersForRoomFilterProxyModel::FilterUserType::Busy));
     if (hasOwnerSupport) {
         addItem(i18n("Owners"), QVariant::fromValue(UsersForRoomFilterProxyModel::FilterUserType::Owners));
     }
