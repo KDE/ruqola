498ee4f4585dd513917ee16cee5d92c09810cbff
Laurent Montel <montel@kde.org>
2025-04-17 07:59:04 +0200

    Add info

--- src/widgets/whatsnew/whatsnewtranslations.cpp
+++ src/widgets/whatsnew/whatsnewtranslations.cpp
@@ -13,7 +13,9 @@ WhatsNewTranslations::~WhatsNewTranslations() = default;
 // Use by newFeaturesMD5
 QList<KLazyLocalizedString> WhatsNewTranslations::lastNewFeatures() const
 {
-    const QList<KLazyLocalizedString> info{};
+    const QList<KLazyLocalizedString> info{
+        kli18n("Add AI plugin"),
+    };
     return info;
 }
 
