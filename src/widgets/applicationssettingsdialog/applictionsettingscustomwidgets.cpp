dbaa50e5c7d6391d936ba3b24146f897010d5ea8
Laurent Montel <montel@kde.org>
2025-04-23 07:30:10 +0200

    Add checkbox support

--- src/widgets/applicationssettingsdialog/applictionsettingscustomwidgets.cpp
+++ src/widgets/applicationssettingsdialog/applictionsettingscustomwidgets.cpp
@@ -5,10 +5,65 @@
 */
 
 #include "applictionsettingscustomwidgets.h"
+#include "rocketchataccount.h"
+#include "ruqolawidgets_debug.h"
 
-ApplictionSettingsCustomWidgetsBase::ApplictionSettingsCustomWidgetsBase(QWidget *parent)
+#include <QCheckBox>
+#include <QHBoxLayout>
+
+ApplictionSettingsCustomWidgetsBase::ApplictionSettingsCustomWidgetsBase(const QByteArray &appId, RocketChatAccount *account, QWidget *parent)
     : QWidget{parent}
+    , mRocketChatAccount(account)
+    , mAppId(appId)
 {
 }
 
 ApplictionSettingsCustomWidgetsBase::~ApplictionSettingsCustomWidgetsBase() = default;
+
+QString ApplictionSettingsCustomWidgetsBase::getTranslatedIdentifier(const QString &lang, const QString &id) const
+{
+    if (id.isEmpty()) {
+        return {};
+    }
+    const QString translatedString =
+        mRocketChatAccount->getTranslatedIdentifier(lang, QStringLiteral("app-") + QString::fromLatin1(mAppId) + QLatin1Char('.') + id);
+    if (translatedString.isEmpty()) {
+        qCWarning(RUQOLAWIDGETS_LOG) << " Translated string not found: " << QString::fromLatin1(mAppId) + id;
+        return id;
+    }
+    return translatedString;
+}
+
+ApplictionSettingsCustomWidgetsCheckBox::ApplictionSettingsCustomWidgetsCheckBox(const QByteArray &appId,
+                                                                                 RocketChatAccount *account,
+                                                                                 const ApplicationsSettingsSettingsInfo &info,
+                                                                                 QWidget *parent)
+    : ApplictionSettingsCustomWidgetsBase(appId, account, parent)
+    , mCheckBox(new QCheckBox(this))
+{
+    const QString lang = QLocale().name();
+    QHBoxLayout *hbox = new QHBoxLayout;
+    mCheckBox->setText(getTranslatedIdentifier(lang, info.i18nLabel()));
+    mCheckBox->setObjectName(info.id());
+    mCheckBox->setToolTip(getTranslatedIdentifier(lang, info.i18nDescription()));
+    hbox->addWidget(mCheckBox);
+    const QVariant r = info.value().isValid() ? info.value() : info.packageValue();
+    mCheckBox->setCheckState(r.toBool() ? Qt::Checked : Qt::Unchecked);
+
+    connect(mCheckBox, &QCheckBox::clicked, this, [this]() {
+        Q_EMIT dataChanged(true);
+    });
+    connect(this, &ApplictionSettingsCustomWidgetsCheckBox::resetValue, this, [this, r]() {
+        QSignalBlocker b(mCheckBox);
+        mCheckBox->setCheckState(r.toBool() ? Qt::Checked : Qt::Unchecked);
+    });
+}
+
+ApplictionSettingsCustomWidgetsCheckBox::~ApplictionSettingsCustomWidgetsCheckBox() = default;
+
+QString ApplictionSettingsCustomWidgetsCheckBox::value() const
+{
+    return mCheckBox->isChecked() ? QStringLiteral("true") : QStringLiteral("false");
+}
+
+#include "moc_applictionsettingscustomwidgets.cpp"
--- src/widgets/applicationssettingsdialog/applictionsettingscustomwidgets.h
+++ src/widgets/applicationssettingsdialog/applictionsettingscustomwidgets.h
@@ -6,14 +6,41 @@
 
 #pragma once
 
+#include "apps/applicationssettingssettingsinfo.h"
 #include <QWidget>
-
+class RocketChatAccount;
+class QCheckBox;
 class ApplictionSettingsCustomWidgetsBase : public QWidget
 {
     Q_OBJECT
 public:
-    explicit ApplictionSettingsCustomWidgetsBase(QWidget *parent = nullptr);
+    explicit ApplictionSettingsCustomWidgetsBase(const QByteArray &appId, RocketChatAccount *account, QWidget *parent = nullptr);
     ~ApplictionSettingsCustomWidgetsBase() override;
 
     [[nodiscard]] virtual QString value() const = 0;
+
+Q_SIGNALS:
+    void dataChanged(bool status);
+    void resetValue();
+
+protected:
+    [[nodiscard]] QString getTranslatedIdentifier(const QString &lang, const QString &id) const;
+    RocketChatAccount *const mRocketChatAccount;
+    const QByteArray mAppId;
+};
+
+class ApplictionSettingsCustomWidgetsCheckBox : public ApplictionSettingsCustomWidgetsBase
+{
+    Q_OBJECT
+public:
+    explicit ApplictionSettingsCustomWidgetsCheckBox(const QByteArray &appId,
+                                                     RocketChatAccount *account,
+                                                     const ApplicationsSettingsSettingsInfo &info,
+                                                     QWidget *parent = nullptr);
+    ~ApplictionSettingsCustomWidgetsCheckBox() override;
+
+    [[nodiscard]] QString value() const override;
+
+private:
+    QCheckBox *const mCheckBox;
 };
