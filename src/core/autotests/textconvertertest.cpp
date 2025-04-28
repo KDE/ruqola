2d2b3d9271aa917b989eec4bdadecfee214d5ac5
Laurent Montel <montel@kde.org>
2025-04-28 15:23:32 +0200

    Fix typo

--- src/core/autotests/textconvertertest.cpp
+++ src/core/autotests/textconvertertest.cpp
@@ -538,9 +538,9 @@ void TextConverterTest::shouldShowUsers_data()
         mentions.append(QPair(QStringLiteral("nico.bla"), "bb_new"_ba));
         mentions.append(QPair(QStringLiteral("nico"), "bb"_ba));
         QList<Channels::ChannelInfo> lst;
-        QTest::newRow("channel-user-unknown") << QStringLiteral("@nico foo @nico.bla")
-                                              << QStringLiteral("<p><a href='ruqola:/user/bb'>@nico</a> foo <a href='ruqola:/user/bb'>@nico</a>.bla</p>\n")
-                                              << mentions << lst;
+        QTest::newRow("quasi same name") << QStringLiteral("@nico foo @nico.bla")
+                                         << QStringLiteral("<p><a href='ruqola:/user/bb'>@nico</a> foo <a href='ruqola:/user/bb'>@nico</a>.bla</p>\n")
+                                         << mentions << lst;
     }
 }
 
