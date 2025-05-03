96c173c0017ec5d6b54dcb94df79aca09fa2890b
Laurent Montel <montel@kde.org>
2025-05-03 09:36:47 +0200

    Don't clear threadmessageid when dnd done

--- src/widgets/room/messagelinewidget.cpp
+++ src/widgets/room/messagelinewidget.cpp
@@ -589,7 +589,6 @@ bool MessageLineWidget::handleMimeData(const QMimeData *mimeData)
         if (dlg->exec()) {
             const UploadFileDialog::UploadFileInfo uploadFileInfo = dlg->fileInfo();
             sendFile(uploadFileInfo);
-            setThreadMessageId({});
         }
         delete dlg;
     };
