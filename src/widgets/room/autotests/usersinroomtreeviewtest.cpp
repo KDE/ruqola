53db5435a47f150c420c7ddb6cdc76fa75dcdecf
Laurent Montel <montel@kde.org>
2025-06-19 07:33:14 +0200

    Add autotest

---
+++ src/widgets/room/autotests/usersinroomtreeviewtest.cpp
@@ -0,0 +1,29 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "usersinroomtreeviewtest.h"
+#include "room/usersinroomtreeview.h"
+#include <QHeaderView>
+#include <QTest>
+QTEST_MAIN(UsersInRoomTreeViewTest)
+
+UsersInRoomTreeViewTest::UsersInRoomTreeViewTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void UsersInRoomTreeViewTest::shouldHaveDefaultValues()
+{
+    UsersInRoomTreeView w;
+    QVERIFY(!w.dragEnabled());
+    QVERIFY(!w.rootIsDecorated());
+    QVERIFY(w.itemsExpandable());
+    QCOMPARE(w.indentation(), 0);
+    QCOMPARE(w.horizontalScrollBarPolicy(), Qt::ScrollBarAlwaysOff);
+    QVERIFY(w.isHeaderHidden());
+}
+
+#include "moc_usersinroomtreeviewtest.cpp"
