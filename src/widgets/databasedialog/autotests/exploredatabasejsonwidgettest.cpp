668d2467f9e73f84e24e44df77bdf6b8b37494c2
Laurent Montel <montel@kde.org>
2025-07-27 19:07:55 +0200

    Prepare autotest

---
+++ src/widgets/databasedialog/autotests/exploredatabasejsonwidgettest.cpp
@@ -0,0 +1,13 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "exploredatabasejsonwidgettest.h"
+#include <QTest>
+QTEST_MAIN(ExploreDatabaseJsonWidgetTest)
+
+ExploreDatabaseJsonWidgetTest::ExploreDatabaseJsonWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
