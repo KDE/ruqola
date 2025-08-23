d30b5a2e007bf213a07bb9cd76ee07c9b094061a
Laurent Montel <montel@kde.org>
2025-08-23 13:50:42 +0200

    Add automoc

--- src/widgets/room/autotests/quicktextformatmessagetest.cpp
+++ src/widgets/room/autotests/quicktextformatmessagetest.cpp
@@ -7,6 +7,7 @@
 #include "room/quicktextformatmessage.h"
 #include <QHBoxLayout>
 #include <QTest>
+#include <QToolButton>
 QTEST_MAIN(QuickTextFormatMessageTest)
 using namespace Qt::Literals::StringLiterals;
 QuickTextFormatMessageTest::QuickTextFormatMessageTest(QObject *parent)
@@ -21,5 +22,26 @@ void QuickTextFormatMessageTest::shouldHaveDefaultValues()
     QVERIFY(mainLayout);
     QCOMPARE(mainLayout->contentsMargins(), QMargins{});
     QCOMPARE(mainLayout->spacing(), 0);
-    // TODO
+
+    auto boldButton = t.findChild<QToolButton *>(u"boldButton"_s);
+    QCOMPARE(boldButton->iconSize(), QSize(12, 12));
+    QVERIFY(boldButton->autoRaise());
+
+    auto italicButton = t.findChild<QToolButton *>(u"italicButton"_s);
+    QCOMPARE(italicButton->iconSize(), QSize(12, 12));
+    QVERIFY(italicButton->autoRaise());
+
+    auto strikeThroughButton = t.findChild<QToolButton *>(u"strikeThroughButton"_s);
+    QCOMPARE(strikeThroughButton->iconSize(), QSize(12, 12));
+    QVERIFY(strikeThroughButton->autoRaise());
+
+    auto blockQuoteButton = t.findChild<QToolButton *>(u"blockQuoteButton"_s);
+    QCOMPARE(blockQuoteButton->iconSize(), QSize(12, 12));
+    QVERIFY(blockQuoteButton->autoRaise());
+
+    auto insertLinkButton = t.findChild<QToolButton *>(u"insertLinkButton"_s);
+    QCOMPARE(insertLinkButton->iconSize(), QSize(12, 12));
+    QVERIFY(insertLinkButton->autoRaise());
 }
+
+#include "moc_quicktextformatmessagetest.cpp"
--- tests/quicktextformatgui/quicktextformatgui.cpp
+++ tests/quicktextformatgui/quicktextformatgui.cpp
@@ -60,3 +60,5 @@ int main(int argc, char *argv[])
     w.show();
     return app.exec();
 }
+
+#include "moc_quicktextformatgui.cpp"
