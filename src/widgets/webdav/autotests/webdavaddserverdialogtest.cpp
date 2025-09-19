f912bf6bcb32dd35d7d90c7a623c23897f23f0bf
Laurent Montel <montel@kde.org>
2025-09-19 22:21:02 +0200

    Prepare autotest

---
+++ src/widgets/webdav/autotests/webdavaddserverdialogtest.cpp
@@ -0,0 +1,34 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "webdavaddserverdialogtest.h"
+#include "webdav/webdavaddserverdialog.h"
+#include "webdav/webdavaddserverwidget.h"
+#include <QDialogButtonBox>
+#include <QTest>
+#include <QVBoxLayout>
+using namespace Qt::Literals::StringLiterals;
+QTEST_MAIN(WebDavAddServerDialogTest)
+
+using namespace Qt::Literals::StringLiterals;
+WebDavAddServerDialogTest::WebDavAddServerDialogTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void WebDavAddServerDialogTest::shouldHaveDefaultValues()
+{
+    const WebDavAddServerDialog d(nullptr);
+    QVERIFY(!d.windowTitle().isEmpty());
+
+    auto mainLayout = d.findChild<QVBoxLayout *>(u"mainLayout"_s);
+    QVERIFY(mainLayout);
+
+    auto mWebDavAddServerWidget = d.findChild<WebDavAddServerWidget *>(u"mWebDavAddServerWidget"_s);
+    QVERIFY(mWebDavAddServerWidget);
+
+    auto buttonBox = d.findChild<QDialogButtonBox *>(u"buttonBox"_s);
+    QVERIFY(buttonBox);
+}
---
+++ src/widgets/webdav/autotests/webdavaddserverdialogtest.h
@@ -0,0 +1,19 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#pragma once
+
+#include <QObject>
+
+class WebDavAddServerDialogTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit WebDavAddServerDialogTest(QObject *parent = nullptr);
+    ~WebDavAddServerDialogTest() override = default;
+
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+};
