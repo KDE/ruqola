926d25f43d463d9f18fb9d5a2706dc9109564ea2
Laurent Montel <montel@kde.org>
2025-09-20 21:46:25 +0200

    Add autotest

--- src/widgets/webdav/autotests/webdavaddserverwidgettest.cpp
+++ src/widgets/webdav/autotests/webdavaddserverwidgettest.cpp
@@ -5,7 +5,9 @@
 */
 #include "webdavaddserverwidgettest.h"
 #include "webdav/webdavaddserverwidget.h"
+#include <KPasswordLineEdit>
 #include <QFormLayout>
+#include <QLineEdit>
 #include <QTest>
 
 using namespace Qt::Literals::StringLiterals;
@@ -21,6 +23,20 @@ void WebDavAddServerWidgetTest::shouldHaveDefaultValues()
     auto mainLayout = w.findChild<QFormLayout *>(u"mainLayout"_s);
     QVERIFY(mainLayout);
     QCOMPARE(mainLayout->contentsMargins(), QMargins());
+
+    auto mName = w.findChild<QLineEdit *>(u"mName"_s);
+    QVERIFY(mName);
+    QVERIFY(mName->text().isEmpty());
+    auto mUrl = w.findChild<QLineEdit *>(u"mUrl"_s);
+    QVERIFY(mUrl);
+    QVERIFY(mUrl->text().isEmpty());
+    auto mUserName = w.findChild<QLineEdit *>(u"mUserName"_s);
+    QVERIFY(mUserName);
+    QVERIFY(mUserName->text().isEmpty());
+    auto mPasswordLineEdit = w.findChild<KPasswordLineEdit *>(u"mPasswordLineEdit"_s);
+    QVERIFY(mPasswordLineEdit);
+
+    QVERIFY(!w.addServerInfo().isValid());
 }
 
 #include "moc_webdavaddserverwidgettest.cpp"
