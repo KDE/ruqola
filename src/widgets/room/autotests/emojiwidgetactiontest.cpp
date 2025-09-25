3341966efdeecb3df93cc17e82d1aa241cc476e9
Laurent Montel <montel@kde.org>
2025-09-25 13:24:57 +0200

    Add emojiwidgetactiontest

---
+++ src/widgets/room/autotests/emojiwidgetactiontest.cpp
@@ -0,0 +1,16 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "emojiwidgetactiontest.h"
+#include <QTest>
+QTEST_MAIN(EmojiWidgetActionTest)
+
+EmojiWidgetActionTest::EmojiWidgetActionTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+#include "moc_emojiwidgetactiontest.cpp"
---
+++ src/widgets/room/autotests/emojiwidgetactiontest.h
@@ -0,0 +1,16 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include <QObject>
+
+class EmojiWidgetActionTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit EmojiWidgetActionTest(QObject *parent = nullptr);
+    ~EmojiWidgetActionTest() override = default;
+};
