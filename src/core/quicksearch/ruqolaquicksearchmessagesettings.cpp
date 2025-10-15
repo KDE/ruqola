7c747d6ff9c2b1b8ea8739a239a076a8574090fa
Laurent Montel <montel@kde.org>
2025-10-15 13:16:10 +0200

    Prepare to implement ruqolaquicksearchmessagesettings

---
+++ src/core/quicksearch/ruqolaquicksearchmessagesettings.cpp
@@ -0,0 +1,150 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "ruqolaquicksearchmessagesettings.h"
+#include "model/messagesmodel.h"
+#include "ruqola_quick_search_debug.h"
+
+RuqolaQuickSearchMessageSettings::RuqolaQuickSearchMessageSettings(MessagesModel *model, QObject *parent)
+    : QObject(parent)
+    , mMessageModel(model)
+{
+}
+
+RuqolaQuickSearchMessageSettings::~RuqolaQuickSearchMessageSettings() = default;
+
+void RuqolaQuickSearchMessageSettings::clear()
+{
+    mCurrentMessageIdentifier.clear();
+    mCurrentSearchIndex = -1;
+}
+
+bool RuqolaQuickSearchMessageSettings::canSearchMessage() const
+{
+    if (!mMessageModel) {
+        qCWarning(RUQOLA_QUICK_SEARCH_LOG) << " Model is not defined. It's a bug";
+        return false;
+    }
+    if (mCurrentMessageIdentifier.isEmpty()) {
+        if (mMessageModel->isEmpty()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void RuqolaQuickSearchMessageSettings::lastMessageUuid()
+{
+#if 0
+    auto msg = mMessageModel->messages().constLast();
+    if (msg.isValid()) {
+        mCurrentMessageIdentifier = msg.uuid();
+    } else {
+        qCWarning(RUQOLA_QUICK_SEARCH_LOG) << "Invalid message. It's a bug";
+    }
+#endif
+}
+
+void RuqolaQuickSearchMessageSettings::next()
+{
+    if (!canSearchMessage()) {
+        return;
+    }
+    const QByteArray previousMessageIdentifier = mCurrentMessageIdentifier;
+    if (mCurrentMessageIdentifier.isEmpty()) {
+        lastMessageUuid();
+        if (mCurrentMessageIdentifier.isEmpty()) {
+            return;
+        }
+    }
+    const int storeCurrentSearchIndex = mCurrentSearchIndex;
+    if (mCurrentSearchIndex == -1) {
+        mCurrentSearchIndex = 0;
+    } else {
+        mCurrentSearchIndex++;
+    }
+#if 0
+    // qDebug() << " mCurrentSearchIndex " << mCurrentSearchIndex << " mMessageModel->message(mCurrentMessageIdentifier).numberOfTextSearched() "
+    //          << mMessageModel->message(mCurrentMessageIdentifier).numberOfTextSearched();
+    if (mCurrentSearchIndex >= mMessageModel->message(mCurrentMessageIdentifier).numberOfTextSearched()) {
+        auto hasSearchedString = [](const Message &msg) {
+            return msg.numberOfTextSearched() > 0;
+        };
+
+        mCurrentSearchIndex = 0;
+        auto msg = mMessageModel->findNextMessageAfter(mCurrentMessageIdentifier, hasSearchedString);
+        if (msg.isValid()) {
+            mCurrentMessageIdentifier = msg.uuid();
+        } else {
+            mCurrentSearchIndex = storeCurrentSearchIndex;
+            // Invalidate it.
+            // clear();
+            return;
+        }
+    }
+    Q_EMIT refreshMessage(mCurrentMessageIdentifier, previousMessageIdentifier, mCurrentSearchIndex);
+#endif
+}
+
+void RuqolaQuickSearchMessageSettings::previous()
+{
+    if (!canSearchMessage()) {
+        return;
+    }
+    const QByteArray previousMessageIdentifier = mCurrentMessageIdentifier;
+    if (mCurrentMessageIdentifier.isEmpty()) {
+        lastMessageUuid();
+        if (mCurrentMessageIdentifier.isEmpty()) {
+            return;
+        }
+    }
+    if (mCurrentSearchIndex == -1) {
+        mCurrentSearchIndex = 0;
+    } else {
+        mCurrentSearchIndex--;
+        if (mCurrentSearchIndex < 0) {
+#if 0
+            auto hasSearchedString = [](const Message &msg) {
+                return msg.numberOfTextSearched() > 0;
+            };
+
+            auto msg = mMessageModel->findLastMessageBefore(mCurrentMessageIdentifier, hasSearchedString);
+            if (msg.isValid()) {
+                mCurrentMessageIdentifier = msg.uuid();
+                mCurrentSearchIndex = msg.numberOfTextSearched() - 1;
+            } else {
+                // Keep in 0 index
+                mCurrentSearchIndex = 0;
+                // Invalidate it.
+                // clear();
+                return;
+            }
+#endif
+        }
+    }
+    Q_EMIT refreshMessage(mCurrentMessageIdentifier, previousMessageIdentifier, mCurrentSearchIndex);
+}
+
+QByteArray RuqolaQuickSearchMessageSettings::currentMessageIdentifier() const
+{
+    return mCurrentMessageIdentifier;
+}
+
+void RuqolaQuickSearchMessageSettings::setCurrentMessageIdentifier(const QByteArray &newCurrentMessageIdentifier)
+{
+    mCurrentMessageIdentifier = newCurrentMessageIdentifier;
+}
+
+int RuqolaQuickSearchMessageSettings::currentSearchIndex() const
+{
+    return mCurrentSearchIndex;
+}
+
+void RuqolaQuickSearchMessageSettings::setCurrentSearchIndex(int newCurrentSearchIndex)
+{
+    mCurrentSearchIndex = newCurrentSearchIndex;
+}
+
+#include "moc_ruqolaquicksearchmessagesettings.cpp"
---
+++ src/core/quicksearch/ruqolaquicksearchmessagesettings.h
@@ -0,0 +1,38 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+#include "libruqola_private_export.h"
+#include <QByteArray>
+#include <QObject>
+class MessagesModel;
+class LIBRUQOLACORE_TESTS_EXPORT RuqolaQuickSearchMessageSettings : public QObject
+{
+    Q_OBJECT
+public:
+    explicit RuqolaQuickSearchMessageSettings(MessagesModel *model, QObject *parent = nullptr);
+    ~RuqolaQuickSearchMessageSettings() override;
+
+    void next();
+    void previous();
+
+    [[nodiscard]] QByteArray currentMessageIdentifier() const;
+    void setCurrentMessageIdentifier(const QByteArray &newCurrentMessageIdentifier);
+
+    [[nodiscard]] int currentSearchIndex() const;
+    void setCurrentSearchIndex(int newCurrentSearchIndex);
+
+    void clear();
+Q_SIGNALS:
+    void refreshMessage(const QByteArray &identifier, const QByteArray &previousIdentifier, int index);
+
+private:
+    [[nodiscard]] bool canSearchMessage() const;
+    void lastMessageUuid();
+    MessagesModel *const mMessageModel;
+    QByteArray mCurrentMessageIdentifier;
+    int mCurrentSearchIndex = -1;
+};
