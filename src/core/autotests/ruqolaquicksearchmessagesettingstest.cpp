60a5e675fd8cdef7613d3a7272397eaf21adb90a
Laurent Montel <montel@kde.org>
2025-10-20 07:53:54 +0200

    update autotest

--- src/core/autotests/ruqolaquicksearchmessagesettingstest.cpp
+++ src/core/autotests/ruqolaquicksearchmessagesettingstest.cpp
@@ -6,6 +6,7 @@
 
 #include "ruqolaquicksearchmessagesettingstest.h"
 // #include "core/models/textautogeneratemessagesmodel.h"
+#include "model/messagesmodel.h"
 #include "quicksearch/ruqolaquicksearchmessagesettings.h"
 #include <QSignalSpy>
 #include <QTest>
@@ -21,6 +22,7 @@ void RuqolaQuickSearchMessageSettingsTest::shouldHaveDefaultValues()
     RuqolaQuickSearchMessageSettings w(nullptr);
     QCOMPARE(w.currentSearchIndex(), -1);
     QVERIFY(w.currentMessageIdentifier().isEmpty());
+    QCOMPARE(w.foundSearchCount(), -1);
 }
 
 void RuqolaQuickSearchMessageSettingsTest::shouldClear()
@@ -41,39 +43,51 @@ void RuqolaQuickSearchMessageSettingsTest::shouldClear()
 
 void RuqolaQuickSearchMessageSettingsTest::shouldTestNextSearchStringIndex()
 {
-#if 0
-    TextAutoGenerateText::TextAutoGenerateMessagesModel model;
-    QList<TextAutoGenerateText::TextAutoGenerateMessage> messages;
+    MessagesModel model;
+    QList<Message> messages;
     {
-        TextAutoGenerateText::TextAutoGenerateMessage msg;
+        Message msg;
+#if 0
         msg.setUuid("foo1"_ba);
         msg.setContent(u"kde and kde"_s);
-        msg.setSender(TextAutoGenerateText::TextAutoGenerateMessage::Sender::User);
+        msg.setSender(Message::Sender::User);
         msg.generateHtml();
+#endif
         messages.append(msg);
     }
     {
-        TextAutoGenerateText::TextAutoGenerateMessage msg;
+        Message msg;
+#if 0
         msg.setUuid("foo2"_ba);
         msg.setContent(u"bla bla"_s);
-        msg.setSender(TextAutoGenerateText::TextAutoGenerateMessage::Sender::User);
+        msg.setSender(Message::Sender::User);
         msg.generateHtml();
+#endif
         messages.append(msg);
     }
     {
-        TextAutoGenerateText::TextAutoGenerateMessage msg;
+        Message msg;
+#if 0
         msg.setUuid("foo3"_ba);
         msg.setContent(u"kde"_s);
-        msg.setSender(TextAutoGenerateText::TextAutoGenerateMessage::Sender::User);
+        msg.setSender(Message::Sender::User);
         msg.generateHtml();
+#endif
         messages.append(msg);
     }
+#if 0
     model.setMessages(messages);
     const int nbSearchString = model.setSearchText(u"kde"_s);
+#endif
     RuqolaQuickSearchMessageSettings messageSettings(&model);
 
+#if 0
     QCOMPARE(nbSearchString, 3);
+    messageSettings.setFoundSearchCount(nbSearchString);
+#endif
     QSignalSpy spy(&messageSettings, &RuqolaQuickSearchMessageSettings::refreshMessage);
+
+    QSignalSpy spyUpdateButtons(&messageSettings, &RuqolaQuickSearchMessageSettings::updateNextPreviousButtons);
 
     messageSettings.next();
     QCOMPARE(spy.count(), 1);
@@ -81,10 +95,18 @@ void RuqolaQuickSearchMessageSettingsTest::shouldTestNextSearchStringIndex()
     QCOMPARE(messageSettings.currentMessageIdentifier(), "foo3"_ba);
     QCOMPARE(messageSettings.currentSearchIndex(), 0);
 
+    QCOMPARE(spyUpdateButtons.count(), 1);
+    QCOMPARE(spyUpdateButtons.at(0).at(0), false);
+    QCOMPARE(spyUpdateButtons.at(0).at(1), true);
+
     messageSettings.next();
     // No other message
     QCOMPARE(messageSettings.currentMessageIdentifier(), "foo3"_ba);
     QCOMPARE(messageSettings.currentSearchIndex(), 0);
+
+    QCOMPARE(spyUpdateButtons.count(), 2);
+    QCOMPARE(spyUpdateButtons.at(1).at(0), false);
+    QCOMPARE(spyUpdateButtons.at(1).at(1), true);
 
     messageSettings.next();
     // No other message
@@ -95,51 +117,62 @@ void RuqolaQuickSearchMessageSettingsTest::shouldTestNextSearchStringIndex()
     // No other message
     QCOMPARE(messageSettings.currentMessageIdentifier(), "foo3"_ba);
     QCOMPARE(messageSettings.currentSearchIndex(), 0);
-#endif
 }
 
 void RuqolaQuickSearchMessageSettingsTest::shouldTestPreviousSearchStringIndex()
 {
-#if 0
-    TextAutoGenerateText::TextAutoGenerateMessagesModel model;
-    QList<TextAutoGenerateText::TextAutoGenerateMessage> messages;
+    MessagesModel model;
+    QList<Message> messages;
     {
-        TextAutoGenerateText::TextAutoGenerateMessage msg;
+        Message msg;
+#if 0
         msg.setUuid("foo1"_ba);
         msg.setContent(u"kde and kde"_s);
-        msg.setSender(TextAutoGenerateText::TextAutoGenerateMessage::Sender::User);
+        msg.setSender(Message::Sender::User);
         msg.generateHtml();
+#endif
         messages.append(msg);
     }
     {
-        TextAutoGenerateText::TextAutoGenerateMessage msg;
+        Message msg;
+#if 0
         msg.setUuid("foo2"_ba);
         msg.setContent(u"bla bla"_s);
-        msg.setSender(TextAutoGenerateText::TextAutoGenerateMessage::Sender::User);
+        msg.setSender(Message::Sender::User);
         msg.generateHtml();
+#endif
         messages.append(msg);
     }
     {
-        TextAutoGenerateText::TextAutoGenerateMessage msg;
+        Message msg;
+#if 0
         msg.setUuid("foo3"_ba);
         msg.setContent(u"kde"_s);
-        msg.setSender(TextAutoGenerateText::TextAutoGenerateMessage::Sender::User);
+        msg.setSender(Message::Sender::User);
         msg.generateHtml();
+#endif
         messages.append(msg);
     }
+#if 0
     model.setMessages(messages);
     const int nbSearchString = model.setSearchText(u"kde"_s);
+#endif
     RuqolaQuickSearchMessageSettings messageSettings(&model);
-
+#if 0
+    messageSettings.setFoundSearchCount(nbSearchString);
+#endif
     QSignalSpy spy(&messageSettings, &RuqolaQuickSearchMessageSettings::refreshMessage);
-
+    QSignalSpy spyUpdateButtons(&messageSettings, &RuqolaQuickSearchMessageSettings::updateNextPreviousButtons);
+#if 0
     QCOMPARE(nbSearchString, 3);
+#endif
     messageSettings.previous();
     QCOMPARE(spy.count(), 1);
     QCOMPARE(spy.at(0).at(0), "foo3"_ba);
     QCOMPARE(spy.at(0).at(1), ""_ba);
     QCOMPARE(spy.at(0).at(2), 0);
 
+    QCOMPARE(spyUpdateButtons.count(), 0);
     // Last one
     QCOMPARE(messageSettings.currentMessageIdentifier(), "foo3"_ba);
     QCOMPARE(messageSettings.currentSearchIndex(), 0);
@@ -150,6 +183,10 @@ void RuqolaQuickSearchMessageSettingsTest::shouldTestPreviousSearchStringIndex()
     QCOMPARE(spy.at(1).at(0), "foo1"_ba);
     QCOMPARE(spy.at(1).at(1), "foo3"_ba);
     QCOMPARE(spy.at(1).at(2), 1);
+
+    QCOMPARE(spyUpdateButtons.count(), 1);
+    QCOMPARE(spyUpdateButtons.at(0).at(0), true);
+    QCOMPARE(spyUpdateButtons.at(0).at(1), true);
 
     QCOMPARE(messageSettings.currentMessageIdentifier(), "foo1"_ba);
     QCOMPARE(messageSettings.currentSearchIndex(), 1);
@@ -164,6 +201,10 @@ void RuqolaQuickSearchMessageSettingsTest::shouldTestPreviousSearchStringIndex()
     QCOMPARE(messageSettings.currentMessageIdentifier(), "foo1"_ba);
     QCOMPARE(messageSettings.currentSearchIndex(), 0);
 
+    QCOMPARE(spyUpdateButtons.count(), 2);
+    QCOMPARE(spyUpdateButtons.at(1).at(0), true);
+    QCOMPARE(spyUpdateButtons.at(1).at(1), true);
+
     // no other search element
     messageSettings.previous();
 
@@ -173,12 +214,9 @@ void RuqolaQuickSearchMessageSettingsTest::shouldTestPreviousSearchStringIndex()
     QCOMPARE(messageSettings.currentMessageIdentifier(), "foo1"_ba);
     QCOMPARE(messageSettings.currentSearchIndex(), 0);
 
-    messageSettings.previous();
-    // No new signal
-    QCOMPARE(spy.count(), 3);
-
-    QCOMPARE(messageSettings.currentMessageIdentifier(), "foo1"_ba);
-    QCOMPARE(messageSettings.currentSearchIndex(), 0);
+    QCOMPARE(spyUpdateButtons.count(), 3);
+    QCOMPARE(spyUpdateButtons.at(2).at(0), true);
+    QCOMPARE(spyUpdateButtons.at(2).at(1), false);
 
     messageSettings.previous();
     // No new signal
@@ -193,7 +231,13 @@ void RuqolaQuickSearchMessageSettingsTest::shouldTestPreviousSearchStringIndex()
 
     QCOMPARE(messageSettings.currentMessageIdentifier(), "foo1"_ba);
     QCOMPARE(messageSettings.currentSearchIndex(), 0);
-#endif
+
+    messageSettings.previous();
+    // No new signal
+    QCOMPARE(spy.count(), 3);
+
+    QCOMPARE(messageSettings.currentMessageIdentifier(), "foo1"_ba);
+    QCOMPARE(messageSettings.currentSearchIndex(), 0);
 }
 
 #include "moc_ruqolaquicksearchmessagesettingstest.cpp"
