d80fbd4b7c897b362ddc47df9a86c526290a5e42
Laurent Montel <montel@kde.org>
2025-10-29 21:06:54 +0100

    Prepare to implement texttospeechenqueueinfo

---
+++ src/core/texttospeech/texttospeechenqueueinfo.cpp
@@ -0,0 +1,11 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "texttospeechenqueueinfo.h"
+
+TextToSpeechEnqueueInfo::TextToSpeechEnqueueInfo() = default;
+
+TextToSpeechEnqueueInfo::~TextToSpeechEnqueueInfo() = default;
---
+++ src/core/texttospeech/texttospeechenqueueinfo.h
@@ -0,0 +1,14 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+class TextToSpeechEnqueueInfo
+{
+public:
+    TextToSpeechEnqueueInfo();
+    ~TextToSpeechEnqueueInfo();
+};
