5f57ec19746f09058543573ad20a0c60259a17e9
Laurent Montel <montel@kde.org>
2025-10-30 07:47:04 +0100

    Implement texttospeechenqueueinfo

--- src/core/texttospeech/texttospeechenqueueinfo.cpp
+++ src/core/texttospeech/texttospeechenqueueinfo.cpp
@@ -9,3 +9,33 @@
 TextToSpeechEnqueueInfo::TextToSpeechEnqueueInfo() = default;
 
 TextToSpeechEnqueueInfo::~TextToSpeechEnqueueInfo() = default;
+
+QByteArray TextToSpeechEnqueueInfo::roomId() const
+{
+    return mRoomId;
+}
+
+void TextToSpeechEnqueueInfo::setRoomId(const QByteArray &newRoomId)
+{
+    mRoomId = newRoomId;
+}
+
+QByteArray TextToSpeechEnqueueInfo::messageId() const
+{
+    return mMessageId;
+}
+
+void TextToSpeechEnqueueInfo::setMessageId(const QByteArray &newMessageId)
+{
+    mMessageId = newMessageId;
+}
+
+QString TextToSpeechEnqueueInfo::accountName() const
+{
+    return mAccountName;
+}
+
+void TextToSpeechEnqueueInfo::setAccountName(const QString &newAccountName)
+{
+    mAccountName = newAccountName;
+}
--- src/core/texttospeech/texttospeechenqueueinfo.h
+++ src/core/texttospeech/texttospeechenqueueinfo.h
@@ -5,10 +5,24 @@
 */
 
 #pragma once
-
+#include <QString>
 class TextToSpeechEnqueueInfo
 {
 public:
     TextToSpeechEnqueueInfo();
     ~TextToSpeechEnqueueInfo();
+
+    [[nodiscard]] QByteArray roomId() const;
+    void setRoomId(const QByteArray &newRoomId);
+
+    [[nodiscard]] QByteArray messageId() const;
+    void setMessageId(const QByteArray &newMessageId);
+
+    [[nodiscard]] QString accountName() const;
+    void setAccountName(const QString &newAccountName);
+
+private:
+    QByteArray mMessageId;
+    QByteArray mRoomId;
+    QString mAccountName;
 };
