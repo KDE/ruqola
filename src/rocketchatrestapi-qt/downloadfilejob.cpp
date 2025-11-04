d9ea98c603cde7152dbcee90354035a93e02237f
Laurent Montel <montel@kde.org>
2025-11-04 19:45:17 +0100

    coding style

--- src/rocketchatrestapi-qt/downloadfilejob.cpp
+++ src/rocketchatrestapi-qt/downloadfilejob.cpp
@@ -79,8 +79,9 @@ QNetworkRequest DownloadFileJob::request() const
 {
     QNetworkRequest req(mUrl);
     addAuthRawHeader(req);
-    if (!mMimeType.isEmpty())
+    if (!mMimeType.isEmpty()) {
         req.setHeader(QNetworkRequest::ContentTypeHeader, mMimeType);
+    }
     req.setAttribute(QNetworkRequest::HttpPipeliningAllowedAttribute, true);
     req.setAttribute(QNetworkRequest::Http2AllowedAttribute, true);
     return req;
