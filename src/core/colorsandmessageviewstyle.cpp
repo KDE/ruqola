f1889c800a12c51f7ffd4fedb668e661f99e0caa
Laurent Montel <montel@kde.org>
2025-11-12 23:06:01 +0100

    Activate HAVE_TEXTUTILS_COLOR_AND_MESSAGE_VIEW_STYLE_SUPPORT

--- src/core/colorsandmessageviewstyle.cpp
+++ src/core/colorsandmessageviewstyle.cpp
@@ -10,47 +10,12 @@
 
 ColorsAndMessageViewStyle::ColorsAndMessageViewStyle()
 {
-#if !HAVE_TEXTUTILS_COLOR_AND_MESSAGE_VIEW_STYLE_SUPPORT
-    regenerateColorScheme();
-    qGuiApp->installEventFilter(this);
-#endif
 }
-
-#if !HAVE_TEXTUTILS_COLOR_AND_MESSAGE_VIEW_STYLE_SUPPORT
-bool ColorsAndMessageViewStyle::eventFilter([[maybe_unused]] QObject *obj, QEvent *event)
-{
-    if (event->type() == QEvent::ApplicationPaletteChange) {
-        regenerateColorScheme();
-    }
-    return false;
-}
-#endif
 
 ColorsAndMessageViewStyle &ColorsAndMessageViewStyle::self()
 {
     static ColorsAndMessageViewStyle c;
     return c;
 }
-#if !HAVE_TEXTUTILS_COLOR_AND_MESSAGE_VIEW_STYLE_SUPPORT
-KColorScheme ColorsAndMessageViewStyle::schemeView() const
-{
-    return mSchemeView;
-}
-
-KColorScheme ColorsAndMessageViewStyle::schemeWindow() const
-{
-    return mSchemeWindow;
-}
-
-void ColorsAndMessageViewStyle::regenerateColorScheme()
-{
-    const KColorScheme colorScheme(QPalette::Active, KColorScheme::Window);
-    if (!(mSchemeWindow == colorScheme)) {
-        mSchemeView = KColorScheme();
-        mSchemeWindow = colorScheme;
-        Q_EMIT needToUpdateColors();
-    }
-}
-#endif
 
 #include "moc_colorsandmessageviewstyle.cpp"
--- src/core/colorsandmessageviewstyle.h
+++ src/core/colorsandmessageviewstyle.h
@@ -6,43 +6,18 @@
 
 #pragma once
 #include "config-ruqola.h"
-#if HAVE_TEXTUTILS_COLOR_AND_MESSAGE_VIEW_STYLE_SUPPORT
-#include <TextUtils/TextUtilsColorsAndMessageViewStyle>
-#endif
 #include <QObject>
+#include <TextUtils/TextUtilsColorsAndMessageViewStyle>
 
 #include <KColorScheme>
 
 #include "libruqolacore_export.h"
 
-class LIBRUQOLACORE_EXPORT ColorsAndMessageViewStyle :
-#if HAVE_TEXTUTILS_COLOR_AND_MESSAGE_VIEW_STYLE_SUPPORT
-    public TextUtils::TextUtilsColorsAndMessageViewStyle
-#else
-    public QObject
-#endif
+class LIBRUQOLACORE_EXPORT ColorsAndMessageViewStyle : public TextUtils::TextUtilsColorsAndMessageViewStyle
 {
     Q_OBJECT
 public:
     static ColorsAndMessageViewStyle &self();
 
     ColorsAndMessageViewStyle();
-#if !HAVE_TEXTUTILS_COLOR_AND_MESSAGE_VIEW_STYLE_SUPPORT
-    [[nodiscard]] KColorScheme schemeView() const;
-
-    [[nodiscard]] KColorScheme schemeWindow() const;
-
-Q_SIGNALS:
-    void needToUpdateColors();
-    void needUpdateMessageStyle();
-    void needUpdateFontSize();
-
-protected:
-    [[nodiscard]] bool eventFilter(QObject *obj, QEvent *event) override;
-
-private:
-    LIBRUQOLACORE_NO_EXPORT void regenerateColorScheme();
-    KColorScheme mSchemeView;
-    KColorScheme mSchemeWindow;
-#endif
 };
