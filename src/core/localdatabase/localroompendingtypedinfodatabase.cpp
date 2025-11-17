f44f37e3c3e2fc9b13ab4e8ad088f50ce5a59a70
Laurent Montel <montel@kde.org>
2025-11-17 22:30:21 +0100

    Prepare local database roompendingtypedinfo

---
+++ src/core/localdatabase/localroompendingtypedinfodatabase.cpp
@@ -0,0 +1,13 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "localroompendingtypedinfodatabase.h"
+#include "localdatabaseutils.h"
+
+LocalRoomPendingTypedInfoDatabase::LocalRoomPendingTypedInfoDatabase()
+    : LocalDatabaseBase(LocalDatabaseUtils::localMessagesDatabasePath(), LocalDatabaseBase::DatabaseType::PendingTypedInfo)
+{
+}
---
+++ src/core/localdatabase/localroompendingtypedinfodatabase.h
@@ -0,0 +1,14 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#pragma once
+
+#include "localdatabasebase.h"
+
+class LocalRoomPendingTypedInfoDatabase : public LocalDatabaseBase
+{
+public:
+    LocalRoomPendingTypedInfoDatabase();
+};
