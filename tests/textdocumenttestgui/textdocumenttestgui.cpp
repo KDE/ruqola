d6ab65890d79a27cf977fe9f48129762634dfda7
Laurent Montel <montel@kde.org>
2025-11-20 23:03:50 +0100

    Add widget

--- tests/textdocumenttestgui/textdocumenttestgui.cpp
+++ tests/textdocumenttestgui/textdocumenttestgui.cpp
@@ -6,12 +6,27 @@
 
 #include "textdocumenttestgui.h"
 #include <QApplication>
+#include <QPlainTextEdit>
 #include <QStandardPaths>
-
+#include <QTextEdit>
+#include <QVBoxLayout>
 using namespace Qt::Literals::StringLiterals;
 TextDocumentTestGui::TextDocumentTestGui(QWidget *parent)
     : QWidget(parent)
 {
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setContentsMargins({});
+
+    auto input = new QPlainTextEdit(this);
+    mainLayout->addWidget(input);
+
+    auto output = new QTextEdit(this);
+    output->setReadOnly(true);
+    mainLayout->addWidget(output);
+
+    connect(input, &QPlainTextEdit::textChanged, this, [input, output]() {
+        output->document()->setHtml(input->toPlainText());
+    });
 }
 
 TextDocumentTestGui::~TextDocumentTestGui() = default;
