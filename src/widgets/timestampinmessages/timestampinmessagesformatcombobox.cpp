0759c5c2670bfaf7466435ad333f657f14b9f6df
Laurent Montel <montel@kde.org>
2026-01-07 08:33:09 +0100

    Fill combobox

--- src/widgets/timestampinmessages/timestampinmessagesformatcombobox.cpp
+++ src/widgets/timestampinmessages/timestampinmessagesformatcombobox.cpp
@@ -4,7 +4,8 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #include "timestampinmessagesformatcombobox.h"
-
+#include <KLocalizedString>
+using namespace Qt::Literals::StringLiterals;
 TimeStampInMessagesFormatComboBox::TimeStampInMessagesFormatComboBox(QWidget *parent)
     : QComboBox(parent)
 {
@@ -15,7 +16,13 @@ TimeStampInMessagesFormatComboBox::~TimeStampInMessagesFormatComboBox() = defaul
 
 void TimeStampInMessagesFormatComboBox::initialize()
 {
-    // TODO
+    addItem(i18n("Short Time"), u"p"_s);
+    addItem(i18n("Long Time"), u"pp"_s);
+    addItem(i18n("Short Date"), u"P"_s);
+    addItem(i18n("Long Date"), u"Pp"_s);
+    addItem(i18n("Full Date and Time"), u"PPPppp"_s);
+    addItem(i18n("Full Date and Time (long)"), u"PPPPpppp"_s);
+    addItem(i18n("Relative time"), u"relative"_s);
 }
 
 #include "moc_timestampinmessagesformatcombobox.cpp"
