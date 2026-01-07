76981152943246617cf625982d8bc32a4fd7db1b
Laurent Montel <montel@kde.org>
2026-01-07 08:16:57 +0100

    Add autotest

--- src/widgets/timestampinmessages/autotests/timestampinmessageswidgettest.cpp
+++ src/widgets/timestampinmessages/autotests/timestampinmessageswidgettest.cpp
@@ -4,10 +4,14 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #include "timestampinmessageswidgettest.h"
+#include "timestampinmessages/timestampinmessagesformatcombobox.h"
 #include "timestampinmessages/timestampinmessageswidget.h"
+#include <QDateEdit>
+#include <QFormLayout>
 #include <QTest>
+#include <QTimeEdit>
 QTEST_MAIN(TimeStampInMessagesWidgetTest)
-
+using namespace Qt::Literals::StringLiterals;
 TimeStampInMessagesWidgetTest::TimeStampInMessagesWidgetTest(QObject *parent)
     : QObject{parent}
 {
@@ -16,7 +20,18 @@ TimeStampInMessagesWidgetTest::TimeStampInMessagesWidgetTest(QObject *parent)
 void TimeStampInMessagesWidgetTest::shouldHaveDefaultValues()
 {
     const TimeStampInMessagesWidget d(nullptr);
-    // TODO
+    auto mainLayout = d.findChild<QFormLayout *>(u"mainLayout"_s);
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->contentsMargins(), QMargins{});
+
+    auto mDateEdit = d.findChild<QDateEdit *>(u"mDateEdit"_s);
+    QVERIFY(mDateEdit);
+
+    auto mTimeEdit = d.findChild<QTimeEdit *>(u"mTimeEdit"_s);
+    QVERIFY(mTimeEdit);
+
+    auto mTimeStampInMessagesFormatComboBox = d.findChild<TimeStampInMessagesFormatComboBox *>(u"mTimeStampInMessagesFormatComboBox"_s);
+    QVERIFY(mTimeStampInMessagesFormatComboBox);
 }
 
 void TimeStampInMessagesWidgetTest::shouldHaveTimeStampInfoDefaultValues()
