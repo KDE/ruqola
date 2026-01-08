660f264ca0609c55f406b0b8a7462cf25e498382
Laurent Montel <montel@kde.org>
2026-01-08 08:54:47 +0100

    Add autotest

---
+++ src/widgets/timestampinmessages/autotests/timestampinmessagestimezonecomboboxtest.cpp
@@ -0,0 +1,21 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "timestampinmessagestimezonecomboboxtest.h"
+#include "timestampinmessages/timestampinmessagestimezonecombobox.h"
+#include <QTest>
+
+QTEST_MAIN(TimeStampInMessagesTimeZoneComboBoxTest)
+
+TimeStampInMessagesTimeZoneComboBoxTest::TimeStampInMessagesTimeZoneComboBoxTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void TimeStampInMessagesTimeZoneComboBoxTest::shouldHaveDefaultValues()
+{
+    const TimeStampInMessagesTimeZoneComboBox w;
+    QVERIFY(w.count() > 0);
+}
