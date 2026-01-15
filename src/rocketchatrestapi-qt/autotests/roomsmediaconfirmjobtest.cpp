5cb0e8cabdb494755b69639cd0530b69f76bb0e3
Laurent Montel <montel@kde.org>
2026-01-15 13:40:38 +0100

    [RC 8.0.0] we need roomsmediaconfirm endpoint

---
+++ src/rocketchatrestapi-qt/autotests/roomsmediaconfirmjobtest.cpp
@@ -0,0 +1,82 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "roomsmediaconfirmjobtest.h"
+
+#include "restapimethod.h"
+#include "rooms/roomsmediaconfirmjob.h"
+#include "ruqola_restapi_helper.h"
+
+#include <QJsonDocument>
+#include <QTest>
+
+using namespace Qt::Literals::StringLiterals;
+QTEST_GUILESS_MAIN(RoomsMediaConfirmJobTest)
+using namespace RocketChatRestApi;
+RoomsMediaConfirmJobTest::RoomsMediaConfirmJobTest(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void RoomsMediaConfirmJobTest::shouldHaveDefaultValue()
+{
+    RoomsMediaConfirmJob job;
+    QVERIFY(job.fileId().isEmpty());
+    QVERIFY(job.roomId().isEmpty());
+    RuqolaRestApiHelper::verifyDefaultValue(&job);
+    QVERIFY(job.requireHttpAuthentication());
+    QVERIFY(!job.hasQueryParameterSupport());
+}
+
+void RoomsMediaConfirmJobTest::shouldGenerateRequest()
+{
+    RoomsMediaConfirmJob job;
+    QNetworkRequest request = QNetworkRequest(QUrl());
+    RuqolaRestApiHelper::verifyAuthentication(&job, request);
+    QCOMPARE(request.url(), QUrl(u"http://www.kde.org/api/v1/rooms.mediaConfirm"_s));
+    QCOMPARE(request.header(QNetworkRequest::ContentTypeHeader).toString(), u"application/json"_s);
+}
+
+void RoomsMediaConfirmJobTest::shouldGenerateJson()
+{
+    RoomsMediaConfirmJob job;
+
+    const QByteArray roomId("room1");
+    job.setRoomId(roomId);
+    const QByteArray fileId("file25"_ba);
+    job.setFileId(fileId);
+
+    QCOMPARE(job.json().toJson(QJsonDocument::Compact),
+             QStringLiteral(R"({"fileId":"%2","roomId":"%1"})").arg(QLatin1StringView(roomId), QLatin1StringView(fileId)).toLatin1());
+}
+
+void RoomsMediaConfirmJobTest::shouldNotStarting()
+{
+    RoomsMediaConfirmJob job;
+
+    RestApiMethod method;
+    method.setServerUrl(u"http://www.kde.org"_s);
+    job.setRestApiMethod(&method);
+
+    QNetworkAccessManager mNetworkAccessManager;
+    job.setNetworkAccessManager(&mNetworkAccessManager);
+    QVERIFY(!job.canStart());
+    const QString auth = u"foo"_s;
+    const QString userId = u"foo"_s;
+    job.setAuthToken(auth);
+    QVERIFY(!job.canStart());
+    job.setUserId(userId);
+    QVERIFY(!job.canStart());
+
+    const QByteArray roomId("room1");
+    job.setRoomId(roomId);
+    QVERIFY(!job.canStart());
+    const QByteArray fileId("file25"_ba);
+    job.setFileId(fileId);
+    QVERIFY(job.canStart());
+}
+
+#include "moc_roomsmediaconfirmjobtest.cpp"
