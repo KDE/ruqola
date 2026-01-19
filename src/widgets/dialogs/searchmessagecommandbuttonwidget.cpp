71f4281f23727a47541305516662b7c3fd253218
Laurent Montel <montel@kde.org>
2026-01-19 08:09:15 +0100

    Prepare to add searchmessagecommandbuttonwidget

---
+++ src/widgets/dialogs/searchmessagecommandbuttonwidget.cpp
@@ -0,0 +1,14 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "searchmessagecommandbuttonwidget.h"
+
+SearchMessageCommandButtonWidget::SearchMessageCommandButtonWidget(QWidget *parent)
+    : QWidget{parent}
+{
+}
+
+SearchMessageCommandButtonWidget::~SearchMessageCommandButtonWidget() = default;
