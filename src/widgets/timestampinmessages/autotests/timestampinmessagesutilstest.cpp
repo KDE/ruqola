5f226d546920783d72f0af174a135687d9d8a977
Laurent Montel <montel@kde.org>
2026-01-20 13:40:54 +0100

    Add autotest

---
+++ src/widgets/timestampinmessages/autotests/timestampinmessagesutilstest.cpp
@@ -0,0 +1,19 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "timestampinmessagesutilstest.h"
+#include "timestampinmessages/timestampinmessagesutils.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(TimeStampInMessagesUtilsTest)
+
+TimeStampInMessagesUtilsTest::TimeStampInMessagesUtilsTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void TimeStampInMessagesUtilsTest::shouldGenerateTimeStampStr()
+{
+    // TODO
+}
---
+++ src/widgets/timestampinmessages/autotests/timestampinmessagesutilstest.h
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QObject>
+
+class TimeStampInMessagesUtilsTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TimeStampInMessagesUtilsTest(QObject *parent = nullptr);
+    ~TimeStampInMessagesUtilsTest() override = default;
+
+private Q_SLOTS:
+    void shouldGenerateTimeStampStr();
+};
