0b2dfe8ce1792cf9ec19bacb847437e58323f9f2
Laurent Montel <montel@kde.org>
2026-01-21 08:15:12 +0100

    Add autotest

--- src/widgets/timestampinmessages/autotests/timestampinmessagesformatcomboboxtest.cpp
+++ src/widgets/timestampinmessages/autotests/timestampinmessagesformatcomboboxtest.cpp
@@ -7,6 +7,7 @@
 #include "timestampinmessages/timestampinmessagesformatcombobox.h"
 #include <QTest>
 QTEST_MAIN(TimeStampInMessagesFormatComboBoxTest)
+using namespace Qt::Literals::StringLiterals;
 TimeStampInMessagesFormatComboBoxTest::TimeStampInMessagesFormatComboBoxTest(QObject *parent)
     : QObject{parent}
 {
@@ -16,6 +17,15 @@ void TimeStampInMessagesFormatComboBoxTest::shouldHaveDefaultValues()
 {
     const TimeStampInMessagesFormatComboBox w;
     QVERIFY(w.count() > 0);
+    QCOMPARE(w.count(), 7);
+
+    QCOMPARE(w.itemData(0).toString(), u"t"_s);
+    QCOMPARE(w.itemData(1).toString(), u"T"_s);
+    QCOMPARE(w.itemData(2).toString(), u"d"_s);
+    QCOMPARE(w.itemData(3).toString(), u"D"_s);
+    QCOMPARE(w.itemData(4).toString(), u"f"_s);
+    QCOMPARE(w.itemData(5).toString(), u"F"_s);
+    QCOMPARE(w.itemData(6).toString(), u"R"_s);
 }
 
 #include "moc_timestampinmessagesformatcomboboxtest.cpp"
