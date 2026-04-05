dadc019e837f4484c6aafd061817e460bd3f9b25
Laurent Montel <montel@kde.org>
2026-04-05 15:17:03 +0200

    Add autotest

---
+++ src/widgets/autotests/ruqolaloginbuttontest.cpp
@@ -0,0 +1,21 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "ruqolaloginbuttontest.h"
+#include "loginwidget/ruqolaloginbutton.h"
+#include <QTest>
+QTEST_MAIN(RuqolaLoginButtonTest)
+
+RuqolaLoginButtonTest::RuqolaLoginButtonTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void RuqolaLoginButtonTest::shouldHaveDefaultValues()
+{
+    const RuqolaLoginButton b;
+    QVERIFY(!b.loginInProgress());
+}
