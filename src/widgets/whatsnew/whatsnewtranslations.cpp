3b050c18bc3d84d906622576001aee3079dcdba0
Laurent Montel <montel@kde.org>
2026-05-08 13:08:14 +0200

    Add lastnewfeatures

--- src/widgets/whatsnew/whatsnewtranslations.cpp
+++ src/widgets/whatsnew/whatsnewtranslations.cpp
@@ -14,7 +14,7 @@ WhatsNewTranslations::~WhatsNewTranslations() = default;
 // Use by newFeaturesMD5
 QList<KLazyLocalizedString> WhatsNewTranslations::lastNewFeatures() const
 {
-    const QList<KLazyLocalizedString> info{};
+    const QList<KLazyLocalizedString> info{kli18n("Improve offline Mode")};
     return info;
 }
 QList<TextAddonsWidgets::WhatsNewInfo> WhatsNewTranslations::createWhatsNewInfo() const
