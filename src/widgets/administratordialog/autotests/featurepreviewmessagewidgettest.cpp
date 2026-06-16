48626f630557e389cd6b7932c86d398d7a0283a2
Laurent Montel <montel@kde.org>
2026-06-16 08:28:05 +0200

    Add autotest

---
+++ src/widgets/administratordialog/autotests/featurepreviewmessagewidgettest.cpp
@@ -0,0 +1,21 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "featurepreviewmessagewidgettest.h"
+#include "administratordialog/featurepreview/featurepreviewmessagewidget.h"
+#include <QTest>
+QTEST_MAIN(FeaturePreviewMessageWidgetTest)
+
+FeaturePreviewMessageWidgetTest::FeaturePreviewMessageWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void FeaturePreviewMessageWidgetTest::shouldHaveDefaultValues()
+{
+    const FeaturePreviewMessageWidget w;
+    QCOMPARE(w.messageType(), KMessageWidget::Information);
+    QCOMPARE(w.position(), KMessageWidget::Footer);
+}
