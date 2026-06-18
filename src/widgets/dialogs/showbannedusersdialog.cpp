d9285460f899de8f45df8ba36d734018a5076c66
Laurent Montel <montel@kde.org>
2026-06-18 07:57:50 +0200

    Add showbannedusersdialog

---
+++ src/widgets/dialogs/showbannedusersdialog.cpp
@@ -0,0 +1,57 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "showbannedusersdialog.h"
+#include "rocketchataccount.h"
+#include <KConfigGroup>
+#include <KLocalizedString>
+#include <KSharedConfig>
+#include <KWindowConfig>
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+#include <QWindow>
+namespace
+{
+const char myShowBannedUsersDialogGroupName[] = "ShowBannedUsersDialog";
+}
+
+using namespace Qt::Literals::StringLiterals;
+ShowBannedUsersDialog::ShowBannedUsersDialog(RocketChatAccount *account, QWidget *parent)
+    : QDialog(parent)
+    , mRocketChatAccount(account)
+{
+    setWindowTitle(i18nc("@title:window", "Show Banned Users - %1", account ? account->accountName() : u"account"_s));
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(u"mainLayout"_s);
+
+    setAttribute(Qt::WA_DeleteOnClose);
+    auto button = new QDialogButtonBox(QDialogButtonBox::Close, this);
+    button->setObjectName(u"button"_s);
+    mainLayout->addWidget(button);
+    connect(button, &QDialogButtonBox::rejected, this, &ShowBannedUsersDialog::reject);
+
+    readConfig();
+}
+
+ShowBannedUsersDialog::~ShowBannedUsersDialog()
+{
+    writeConfig();
+}
+
+void ShowBannedUsersDialog::readConfig()
+{
+    create(); // ensure a window is created
+    windowHandle()->resize(QSize(800, 600));
+    const KConfigGroup group(KSharedConfig::openStateConfig(), QLatin1StringView(myShowBannedUsersDialogGroupName));
+    KWindowConfig::restoreWindowSize(windowHandle(), group);
+    resize(windowHandle()->size()); // workaround for QTBUG-40584
+}
+
+void ShowBannedUsersDialog::writeConfig()
+{
+    KConfigGroup group(KSharedConfig::openStateConfig(), QLatin1StringView(myShowBannedUsersDialogGroupName));
+    KWindowConfig::saveWindowSize(windowHandle(), group);
+}
---
+++ src/widgets/dialogs/showbannedusersdialog.h
@@ -0,0 +1,22 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+#include "libruqolawidgets_private_export.h"
+#include <QDialog>
+class RocketChatAccount;
+class LIBRUQOLAWIDGETS_TESTS_EXPORT ShowBannedUsersDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit ShowBannedUsersDialog(RocketChatAccount *account, QWidget *parent = nullptr);
+    ~ShowBannedUsersDialog() override;
+
+private:
+    LIBRUQOLAWIDGETS_NO_EXPORT void readConfig();
+    LIBRUQOLAWIDGETS_NO_EXPORT void writeConfig();
+    RocketChatAccount *const mRocketChatAccount;
+};
