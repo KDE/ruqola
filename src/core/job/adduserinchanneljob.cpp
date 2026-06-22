7b15b81dddaf6e995432708d8c5a9c9c8ab513ca
Laurent Montel <montel@kde.org>
2026-06-22 08:12:49 +0200

    Extract code

---
+++ src/core/job/adduserinchanneljob.cpp
@@ -0,0 +1,59 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "adduserinchanneljob.h"
+#include "channels/channelinvitejob.h"
+#include "connection.h"
+#include "rocketchataccount.h"
+#include "ruqola_debug.h"
+using namespace RocketChatRestApi;
+using namespace Qt::Literals::StringLiterals;
+
+AddUserInChannelJob::AddUserInChannelJob(RocketChatAccount *account, QObject *parent)
+    : QObject{parent}
+    , mRocketChatAccount(account)
+{
+}
+
+AddUserInChannelJob::~AddUserInChannelJob() = default;
+
+void AddUserInChannelJob::start()
+{
+    auto job = new ChannelInviteJob(this);
+    mRocketChatAccount->restApi()->initializeRestApiJob(job);
+    ChannelGroupBaseJob::ChannelGroupInfo info;
+    info.channelGroupInfoType = ChannelGroupBaseJob::ChannelGroupInfoType::RoomIdentifier;
+    info.identifier = QString::fromLatin1(mInfo.roomId);
+    job->setChannelGroupInfo(info);
+    const ChannelInviteJob::ChannelInviteInfo inviteInfo{
+        .identifier = QString::fromLatin1(mInfo.userId),
+        .channelGroupInfoType = ChannelInviteJob::ChannelInviteInfoType::UserId,
+    };
+    job->setChannelInviteInfo(inviteInfo);
+    if (!job->start()) {
+        qCWarning(RUQOLA_LOG) << "Impossible to start addUserInChannel job";
+    }
+}
+
+AddUserInChannelJob::AddUserInChannelJobInfo AddUserInChannelJob::info() const
+{
+    return mInfo;
+}
+
+void AddUserInChannelJob::setInfo(const AddUserInChannelJobInfo &newInfo)
+{
+    mInfo = newInfo;
+}
+
+bool AddUserInChannelJob::AddUserInChannelJobInfo::isValid() const
+{
+    return !roomId.isEmpty() && !userId.isEmpty();
+}
+
+bool AddUserInChannelJob::canStart() const
+{
+    return mInfo.isValid();
+}
---
+++ src/core/job/adduserinchanneljob.h
@@ -0,0 +1,33 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+#include "libruqolacore_export.h"
+#include <QObject>
+class RocketChatAccount;
+class LIBRUQOLACORE_EXPORT AddUserInChannelJob : public QObject
+{
+    Q_OBJECT
+public:
+    struct AddUserInChannelJobInfo {
+        QByteArray roomId;
+        QByteArray userId;
+        [[nodiscard]] bool isValid() const;
+    };
+
+    explicit AddUserInChannelJob(RocketChatAccount *account, QObject *parent = nullptr);
+    ~AddUserInChannelJob() override;
+
+    [[nodiscard]] bool canStart() const;
+
+    void start();
+    AddUserInChannelJobInfo info() const;
+    void setInfo(const AddUserInChannelJobInfo &newInfo);
+
+private:
+    AddUserInChannelJobInfo mInfo;
+    RocketChatAccount *mRocketChatAccount;
+};
