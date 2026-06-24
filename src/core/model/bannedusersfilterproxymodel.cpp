f3a95050ef07f53d016f4e5e57ce3729666c2701
Laurent Montel <montel@kde.org>
2026-06-24 13:37:40 +0200

    Fix filtering

--- src/core/model/bannedusersfilterproxymodel.cpp
+++ src/core/model/bannedusersfilterproxymodel.cpp
@@ -4,7 +4,7 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #include "bannedusersfilterproxymodel.h"
-
+#include "bannedusersmodel.h"
 BannedUsersFilterProxyModel::BannedUsersFilterProxyModel(QObject *parent)
     : QSortFilterProxyModel{parent}
 {
@@ -14,7 +14,26 @@ BannedUsersFilterProxyModel::~BannedUsersFilterProxyModel() = default;
 
 void BannedUsersFilterProxyModel::setFilterString(const QString &string)
 {
-    mFilterString = string;
-    invalidate();
+    if (mFilterString != string) {
+        mFilterString = string;
+        invalidate();
+    }
+}
+
+bool BannedUsersFilterProxyModel::filterAcceptsRow(int source_row, const QModelIndex &source_parent) const
+{
+    if (mFilterString.isEmpty()) {
+        return true;
+    }
+
+    const QModelIndex modelIndex = sourceModel()->index(source_row, 0, source_parent);
+    auto match = [&](int role) {
+        return modelIndex.data(role).toString().contains(mFilterString, Qt::CaseInsensitive);
+    };
+    if (!match(BannedUsersModel::UserName) && !match(BannedUsersModel::Name)) {
+        return false;
+    }
+
+    return true;
 }
 #include "moc_bannedusersfilterproxymodel.cpp"
