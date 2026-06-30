1b05d3ce92cd8a06277d0e7bd384a666afe3933d
Laurent Montel <montel@kde.org>
2026-06-30 20:36:52 +0200

    Clean up

--- src/widgets/dialogs/showreadreceiptswidget.cpp
+++ src/widgets/dialogs/showreadreceiptswidget.cpp
@@ -11,14 +11,13 @@
 #include "model/bannedusersmodel.h"
 #include "rocketchataccount.h"
 #include "rooms/roomsbannedusersjob.h"
-#include "rooms/roomsunbanuserjob.h"
 #include "ruqolawidgets_debug.h"
 #include "showbannedusersdelegate.h"
-#include "showbanneduserslistview.h"
 #include <KLineEditEventHandler>
 #include <KLocalizedString>
 #include <QLabel>
 #include <QLineEdit>
+#include <QListView>
 #include <QVBoxLayout>
 constexpr ushort numberOfElment = 20;
 
@@ -26,11 +25,10 @@ using namespace Qt::Literals::StringLiterals;
 ShowReadReceiptsWidget::ShowReadReceiptsWidget(RocketChatAccount *account, QWidget *parent)
     : QWidget(parent)
     , mCurrentRocketChatAccount(account)
-    , mSearchBannedUserLineEdit(new QLineEdit(this))
-    , mInfo(new QLabel(this))
-    , mListBannedUsers(new ShowBannedUsersListView(this))
+    , mShowReadReceiptsLineEdit(new QLineEdit(this))
+    , mListReadReceipts(new QListView(this))
     , mModel(new BannedUsersModel(this))
-    , mBannedUsersFilterProxyModel(new BannedUsersFilterProxyModel(this))
+    , mReadReceiptsFilterProxyModel(new BannedUsersFilterProxyModel(this))
 {
     auto mainLayout = new QVBoxLayout(this);
     mainLayout->setObjectName(u"mainLayout"_s);
@@ -40,131 +38,32 @@ ShowReadReceiptsWidget::ShowReadReceiptsWidget(RocketChatAccount *account, QWidg
     if (mCurrentRocketChatAccount) {
         delegate->setRocketChatAccount(mCurrentRocketChatAccount);
     }
-    mListBannedUsers->setItemDelegate(delegate);
+    mListReadReceipts->setItemDelegate(delegate);
 
-    auto searchBannedUsersLayout = new QHBoxLayout;
-    searchBannedUsersLayout->setObjectName(u"searchBannedUsersLayout"_s);
-    searchBannedUsersLayout->setContentsMargins({});
-    mainLayout->addLayout(searchBannedUsersLayout);
+    mShowReadReceiptsLineEdit->setObjectName(u"mShowReadReceiptsLineEdit"_s);
+    mShowReadReceiptsLineEdit->setClearButtonEnabled(true);
+    KLineEditEventHandler::catchReturnKey(mShowReadReceiptsLineEdit);
+    mShowReadReceiptsLineEdit->setPlaceholderText(i18nc("@info:placeholder", "Search Read Receipts…"));
+    connect(mShowReadReceiptsLineEdit, &QLineEdit::textChanged, this, &ShowReadReceiptsWidget::slotSearchReadReceiptsChanged);
+    mainLayout->addWidget(mShowReadReceiptsLineEdit);
 
-    mSearchBannedUserLineEdit->setObjectName(u"mSearchBannedUserLineEdit"_s);
-    mSearchBannedUserLineEdit->setClearButtonEnabled(true);
-    KLineEditEventHandler::catchReturnKey(mSearchBannedUserLineEdit);
-    mSearchBannedUserLineEdit->setPlaceholderText(i18nc("@info:placeholder", "Search Banned Users…"));
-    connect(mSearchBannedUserLineEdit, &QLineEdit::textChanged, this, &ShowReadReceiptsWidget::slotSearchMessageTextChanged);
-    searchBannedUsersLayout->addWidget(mSearchBannedUserLineEdit);
-
-    mInfo->setObjectName(u"mInfo"_s);
-    mInfo->setTextFormat(Qt::RichText);
-    mInfo->setContextMenuPolicy(Qt::NoContextMenu);
-    mainLayout->addWidget(mInfo);
-    QFont labFont = mInfo->font();
-    labFont.setBold(true);
-    mInfo->setFont(labFont);
-    connect(mInfo, &QLabel::linkActivated, this, &ShowReadReceiptsWidget::slotLoadMoreBannedUsers);
-
-    mListBannedUsers->setObjectName(u"mListBannedUsers"_s);
-    mainLayout->addWidget(mListBannedUsers);
-    mBannedUsersFilterProxyModel->setSourceModel(mModel);
-    mListBannedUsers->setModel(mBannedUsersFilterProxyModel);
-
-    connect(mModel, &BannedUsersModel::hasFullListChanged, this, &ShowReadReceiptsWidget::updateLabel);
-    connect(mModel, &BannedUsersModel::totalChanged, this, &ShowReadReceiptsWidget::updateLabel);
-    connect(mModel, &BannedUsersModel::loadingInProgressChanged, this, &ShowReadReceiptsWidget::updateLabel);
-    connect(mListBannedUsers, &ShowBannedUsersListView::unbanUser, this, &ShowReadReceiptsWidget::slotUnbanUser);
-    updateLabel();
+    mListReadReceipts->setObjectName(u"mListReadReceipts"_s);
+    mainLayout->addWidget(mListReadReceipts);
+    mReadReceiptsFilterProxyModel->setSourceModel(mModel);
+    mListReadReceipts->setModel(mReadReceiptsFilterProxyModel);
 }
 
 ShowReadReceiptsWidget::~ShowReadReceiptsWidget() = default;
 
-void ShowReadReceiptsWidget::slotUnbanUser(const QString &userName)
+void ShowReadReceiptsWidget::slotSearchReadReceiptsChanged(const QString &str)
 {
-    auto job = new RocketChatRestApi::RoomsUnbanUserJob(this);
-    const RocketChatRestApi::RoomsUnbanUserJob::RoomsUnbanUserInfo info{
-        .type = RocketChatRestApi::RoomsUnbanUserJob::IdentifierType::UserName,
-        .identifier = userName,
-        .roomId = mModel->roomId(),
-    };
-    job->setRoomsUnbanUserInfo(info);
-
-    mCurrentRocketChatAccount->restApi()->initializeRestApiJob(job);
-    connect(job, &RocketChatRestApi::RoomsUnbanUserJob::roomsUnbanUserDone, this, [this, userName]() {
-        slotUnBanUsersDone(userName);
-    });
-    if (!job->start()) {
-        qCWarning(RUQOLAWIDGETS_LOG) << "Impossible to start RoomsUnbanUserJob job";
-    }
-}
-
-void ShowReadReceiptsWidget::slotUnBanUsersDone(const QString &userName)
-{
-    qCDebug(RUQOLAWIDGETS_LOG) << "Unban done";
-    mModel->removeBannedUsers(userName);
-    updateLabel();
-}
-
-void ShowReadReceiptsWidget::slotSearchMessageTextChanged(const QString &str)
-{
-    mBannedUsersFilterProxyModel->setFilterString(str);
-}
-
-void ShowReadReceiptsWidget::updateLabel()
-{
-    mInfo->setText(mModel->rowCount() == 0 ? i18n("No Banned Users found") : displayShowMessageInRoom());
-}
-
-QString ShowReadReceiptsWidget::displayShowMessageInRoom() const
-{
-    QString displayMessageStr = i18np("%1 Banned User in room (Total: %2)", "%1 Banned Users in room (Total: %2)", mModel->rowCount(), mModel->total());
-    if (!mModel->hasFullList()) {
-        displayMessageStr += u" <a href=\"loadmoreelement\">%1</a>"_s.arg(i18n("(Click here for Loading more…)"));
-    }
-    return displayMessageStr;
-}
-
-void ShowReadReceiptsWidget::slotLoadMoreBannedUsers()
-{
-    loadBannedUsers();
+    mReadReceiptsFilterProxyModel->setFilterString(str);
 }
 
 void ShowReadReceiptsWidget::setRoomId(const QByteArray &roomId)
 {
-    mOffset = 0;
     mModel->setRoomId(roomId);
-    loadBannedUsers();
-}
-
-void ShowReadReceiptsWidget::loadBannedUsers()
-{
-    auto job = new RocketChatRestApi::RoomsBannedUsersJob(this);
-    const RocketChatRestApi::RoomsBannedUsersJob::RoomsBannedUsersJobInfo info{
-        .roomId = mModel->roomId(),
-        .offset = mOffset,
-        .count = numberOfElment,
-    };
-    job->setRoomsBannedUsersJobInfo(info);
-
-    mCurrentRocketChatAccount->restApi()->initializeRestApiJob(job);
-    connect(job, &RocketChatRestApi::RoomsBannedUsersJob::roomsBannedUsersDone, this, &ShowReadReceiptsWidget::slotBannedUsersDone);
-    connect(job, &RocketChatRestApi::RoomsBannedUsersJob::failed, this, &ShowReadReceiptsWidget::slotBannedUsersFailed);
-    if (!job->start()) {
-        qCWarning(RUQOLAWIDGETS_LOG) << "Impossible to start RoomsBannedUsersJob job";
-    }
-}
-
-void ShowReadReceiptsWidget::slotBannedUsersDone(const QJsonObject &obj, const QByteArray &roomId)
-{
-    if (mOffset == 0) {
-        mModel->parseBannedUsers(obj, roomId);
-    } else {
-        mModel->addMoreBannedUsers(obj);
-    }
-    mOffset += numberOfElment;
-}
-
-void ShowReadReceiptsWidget::slotBannedUsersFailed(const QString &serverErrorStr, const QString &descriptionError)
-{
-    qCWarning(RUQOLAWIDGETS_LOG) << "slotBannedUsersFailed:" << serverErrorStr << " descriptionError : " << descriptionError;
+    // loadBannedUsers();
 }
 
 #include "moc_showreadreceiptswidget.cpp"
