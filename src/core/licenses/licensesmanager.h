97965de8c7171c6b2e4dd5c616ef06be6fdf570a
Laurent Montel <montel@kde.org>
2026-07-02 13:58:02 +0200

    Prepare to use enum here

--- src/core/licenses/licensesmanager.h
+++ src/core/licenses/licensesmanager.h
@@ -12,6 +12,36 @@ class QJsonObject;
 class LIBRUQOLACORE_EXPORT LicensesManager
 {
 public:
+    enum class ActiveModule : uint8_t {
+        Auditing,
+        DeviceManagement,
+        MessageReadReceipt,
+        /*
+
+                "auditing",
+                "canned-responses",
+                "ldap-enterprise",
+                "livechat-enterprise",
+                "voip-enterprise",
+                "omnichannel-mobile-enterprise",
+                "engagement-dashboard",
+                "push-privacy",
+                "scalability",
+                "teams-mention",
+                "saml-enterprise",
+                "oauth-enterprise",
+                "device-management",
+                "federation",
+                "videoconference-enterprise",
+                "message-read-receipt",
+                "outlook-calendar",
+                "hide-watermark",
+                "custom-roles",
+                "accessibility-certification",
+                "abac"
+          */
+    };
+
     LicensesManager();
     ~LicensesManager();
 
