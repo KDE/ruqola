d8f7082b040558f4ea11d284f5ced02e5327faa4
Laurent Montel <montel@kde.org>
2026-07-04 15:59:39 +0200

    Fix typo

--- src/rocketchatrestapi-qt/autotests/restapimethodtest.cpp
+++ src/rocketchatrestapi-qt/autotests/restapimethodtest.cpp
@@ -401,7 +401,7 @@ void RestApiMethodTest::shouldGenerateUrl()
     QCOMPARE(rest.generateUrl(RestApiUtil::RestApiUrlType::RoomsBanUser), QUrl(u"http://www.kde.org/api/v1/rooms.banUser"_s));
     QCOMPARE(rest.generateUrl(RestApiUtil::RestApiUrlType::RoomsUnbanUser), QUrl(u"http://www.kde.org/api/v1/rooms.unbanUser"_s));
     QCOMPARE(rest.generateUrl(RestApiUtil::RestApiUrlType::CustomSoundsDelete), QUrl(u"http://www.kde.org/api/v1/custom-sounds.delete"_s));
-    QCOMPARE(rest.generateUrl(RestApiUtil::RestApiUrlType::ImBlockUser), QUrl(u"http://www.kde.org/api/v1/im.blockuser"_s));
+    QCOMPARE(rest.generateUrl(RestApiUtil::RestApiUrlType::ImBlockUser), QUrl(u"http://www.kde.org/api/v1/im.blockUser"_s));
 }
 
 #include "moc_restapimethodtest.cpp"
