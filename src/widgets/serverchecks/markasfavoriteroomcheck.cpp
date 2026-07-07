aa5a096f04616359d2d584c36561333dda5a6cfd
Laurent Montel <montel@kde.org>
2026-07-07 13:42:17 +0200

    Prepare to add new check

---
+++ src/widgets/serverchecks/markasfavoriteroomcheck.cpp
@@ -0,0 +1,27 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "markasfavoriteroomcheck.h"
+#include <KLocalizedString>
+
+QString MarkAsFavoriteRoomCheck::name() const
+{
+    return i18n("Change Favorite Room State");
+}
+
+void MarkAsFavoriteRoomCheck::doStart()
+{
+    if (!mAccount) {
+        reportFailed(i18n("No account."));
+        return;
+    }
+    // TODO
+}
+
+void MarkAsFavoriteRoomCheck::checkState()
+{
+    // TODO
+}
