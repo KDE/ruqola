0b8c5e7c771a0c63d894a3e42f82fa7a44bf0309
Laurent Montel <montel@kde.org>
2026-07-08 19:31:43 +0200

    Improve PendingAttachmentClickableWidget

--- src/widgets/room/pendingattachmentclickablewidget.cpp
+++ src/widgets/room/pendingattachmentclickablewidget.cpp
@@ -4,18 +4,34 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #include "pendingattachmentclickablewidget.h"
+#include <KIconLoader>
 #include <KLocalizedString>
+#include <QFileInfo>
 #include <QFrame>
 #include <QHBoxLayout>
 #include <QLabel>
+#include <QMimeDatabase>
+#include <QMimeType>
 #include <QToolButton>
 using namespace Qt::Literals::StringLiterals;
-PendingAttachmentClickableWidget::PendingAttachmentClickableWidget(const QString &fileName, QWidget *parent)
+PendingAttachmentClickableWidget::PendingAttachmentClickableWidget(const QUrl &url, QWidget *parent)
     : QWidget{parent}
 {
+    const QMimeDatabase db;
+    const QFileInfo info(url.toLocalFile());
+    const QMimeType mimeType = db.mimeTypeForFile(info);
+    const QString mimeTypeIconName = mimeType.iconName();
+
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setContentsMargins({});
+
+    mPendingAttachmentInfo.fileUrl = url;
+    mPendingAttachmentInfo.fileName = info.fileName();
+
     auto chip = new QFrame(this);
     chip->setFrameShape(QFrame::StyledPanel);
     chip->setFrameShadow(QFrame::Raised);
+    mainLayout->addWidget(chip);
 
     auto chipLayout = new QHBoxLayout(chip);
     chipLayout->setContentsMargins(4, 1, 2, 1);
@@ -23,14 +39,15 @@ PendingAttachmentClickableWidget::PendingAttachmentClickableWidget(const QString
 
     auto iconLabel = new QLabel(chip);
     iconLabel->setObjectName(u"iconLabel"_s);
-    iconLabel->setPixmap(QIcon::fromTheme(u"mail-attachment-symbolic"_s).pixmap(14, 14));
+    const QString mimeTypeIconPath = KIconLoader::global()->iconPath(mimeTypeIconName, KIconLoader::Small);
+    iconLabel->setPixmap(QPixmap(mimeTypeIconPath));
     chipLayout->addWidget(iconLabel);
 
     auto nameLabel = new QLabel(chip);
     nameLabel->setObjectName(u"nameLabel"_s);
-    const QString elided = nameLabel->fontMetrics().elidedText(fileName, Qt::ElideMiddle, 120);
+    const QString elided = nameLabel->fontMetrics().elidedText(mPendingAttachmentInfo.fileName, Qt::ElideMiddle, 120);
     nameLabel->setText(elided);
-    nameLabel->setToolTip(fileName);
+    nameLabel->setToolTip(url.toString());
     chipLayout->addWidget(nameLabel);
 
     auto removeBtn = new QToolButton(chip);
@@ -40,12 +57,17 @@ PendingAttachmentClickableWidget::PendingAttachmentClickableWidget(const QString
     removeBtn->setFixedSize(18, 18);
     removeBtn->setIconSize(QSize(12, 12));
     removeBtn->setToolTip(i18nc("@info:tooltip", "Remove attachment"));
-    connect(removeBtn, &QToolButton::clicked, this, [this, fileName]() {
-        Q_EMIT removeAttachment(fileName);
+    connect(removeBtn, &QToolButton::clicked, this, [this, url]() {
+        Q_EMIT removeAttachment(url);
     });
     chipLayout->addWidget(removeBtn);
 }
 
 PendingAttachmentClickableWidget::~PendingAttachmentClickableWidget() = default;
+
+PendingAttachmentClickableWidget::PendingAttachmentInfo PendingAttachmentClickableWidget::pendingAttachmentInfo() const
+{
+    return mPendingAttachmentInfo;
+}
 
 #include "moc_pendingattachmentclickablewidget.cpp"
--- src/widgets/room/pendingattachmentclickablewidget.h
+++ src/widgets/room/pendingattachmentclickablewidget.h
@@ -6,14 +6,31 @@
 #pragma once
 
 #include "libruqolawidgets_private_export.h"
+#include <QUrl>
 #include <QWidget>
 class LIBRUQOLAWIDGETS_TESTS_EXPORT PendingAttachmentClickableWidget : public QWidget
 {
     Q_OBJECT
 public:
-    explicit PendingAttachmentClickableWidget(const QString &filename, QWidget *parent = nullptr);
+    struct PendingAttachmentInfo {
+        QUrl fileUrl;
+        QString fileName;
+        QString name;
+        QString alternativeText;
+        [[nodiscard]] bool isValid() const
+        {
+            return fileUrl.isValid() && fileUrl.isLocalFile();
+        }
+    };
+    explicit PendingAttachmentClickableWidget(const QUrl &url, QWidget *parent = nullptr);
     ~PendingAttachmentClickableWidget() override;
 
+    [[nodiscard]] PendingAttachmentInfo pendingAttachmentInfo() const;
+
 Q_SIGNALS:
-    void removeAttachment(const QString &fileName);
+    void removeAttachment(const QUrl &url);
+
+private:
+    PendingAttachmentInfo mPendingAttachmentInfo;
 };
+Q_DECLARE_TYPEINFO(PendingAttachmentClickableWidget::PendingAttachmentInfo, Q_RELOCATABLE_TYPE);
