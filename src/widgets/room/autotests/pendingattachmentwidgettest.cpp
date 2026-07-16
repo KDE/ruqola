d755b9a5d90a4b1753c04fa0a72ebf84aa5337d9
Laurent Montel <montel@kde.org>
2026-07-17 00:13:23 +0200

    Prepare autotest pendingattachmentwidget

---
+++ src/widgets/room/autotests/pendingattachmentwidgettest.cpp
@@ -0,0 +1,22 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "pendingattachmentwidgettest.h"
+#include "room/pendingattachmentwidget.h"
+
+#include <QTest>
+QTEST_MAIN(PendingAttachmentWidgetTest)
+
+PendingAttachmentWidgetTest::PendingAttachmentWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void PendingAttachmentWidgetTest::shouldHaveDefaultValues()
+{
+    const PendingAttachmentWidget w;
+    // TODO
+}
