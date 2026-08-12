22c3e6c2964775c1efa4003abb587316c8a5ec9d
Laurent Montel <montel@kde.org>
2026-08-12 07:41:20 +0200

    Logout when we reset key

--- src/core/rocketchataccount.cpp
+++ src/core/rocketchataccount.cpp
@@ -2156,6 +2156,7 @@ void RocketChatAccount::resetE2eKey()
 {
     qCDebug(RUQOLA_LOG) << debugCategoryAccountName() << " RocketChatAccount::resetE2eKey() ";
     std::ignore = localDatabaseManager()->deleteKey(accountName(), QString::fromLatin1(userId()));
+    logOut();
 }
 
 void RocketChatAccount::parseVideoConference(const QJsonArray &contents)
