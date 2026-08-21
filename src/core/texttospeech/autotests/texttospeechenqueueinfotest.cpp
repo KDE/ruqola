1704a2e6486fd1090469a7d3c4e9921e8e4b4a3e
Laurent Montel <montel@kde.org>
2026-08-21 22:51:40 +0200

    Improve texttospeechenqueueinfotest

--- src/core/texttospeech/autotests/texttospeechenqueueinfotest.cpp
+++ src/core/texttospeech/autotests/texttospeechenqueueinfotest.cpp
@@ -6,6 +6,8 @@
 #include "texttospeechenqueueinfotest.h"
 #include "texttospeech/texttospeechenqueueinfo.h"
 #include <QTest>
+
+using namespace Qt::Literals::StringLiterals;
 QTEST_GUILESS_MAIN(TextToSpeechEnqueueInfoTest)
 
 TextToSpeechEnqueueInfoTest::TextToSpeechEnqueueInfoTest(QObject *parent)
@@ -20,6 +22,42 @@ void TextToSpeechEnqueueInfoTest::shouldHaveDefaultValues()
     QVERIFY(info.messageId().isEmpty());
     QVERIFY(info.roomId().isEmpty());
     QVERIFY(!info.isValid());
+}
+
+void TextToSpeechEnqueueInfoTest::shouldBeValidWhenAllValuesAreAssigned()
+{
+    TextToSpeechEnqueueInfo info;
+    info.setAccountName(u"accountname"_s);
+    QVERIFY(!info.isValid());
+    info.setRoomId("roomid");
+    QVERIFY(!info.isValid());
+    info.setMessageId("messageid");
+    QVERIFY(info.isValid());
+    QCOMPARE(info.accountName(), u"accountname"_s);
+    QCOMPARE(info.roomId(), "roomid");
+    QCOMPARE(info.messageId(), "messageid");
+}
+
+void TextToSpeechEnqueueInfoTest::shouldCompareInfos()
+{
+    TextToSpeechEnqueueInfo info;
+    info.setAccountName(u"accountname"_s);
+    info.setRoomId("roomid");
+    info.setMessageId("messageid");
+
+    TextToSpeechEnqueueInfo other = info;
+    QCOMPARE(info, other);
+
+    other.setMessageId("anotherid");
+    QVERIFY(!(info == other));
+
+    other = info;
+    other.setRoomId("anotherroomid");
+    QVERIFY(!(info == other));
+
+    other = info;
+    other.setAccountName(u"anotheraccount"_s);
+    QVERIFY(!(info == other));
 }
 
 #include "moc_texttospeechenqueueinfotest.cpp"
--- src/core/texttospeech/autotests/texttospeechenqueueinfotest.h
+++ src/core/texttospeech/autotests/texttospeechenqueueinfotest.h
@@ -16,4 +16,6 @@ public:
 
 private Q_SLOTS:
     void shouldHaveDefaultValues();
+    void shouldBeValidWhenAllValuesAreAssigned();
+    void shouldCompareInfos();
 };
