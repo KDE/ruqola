97369756e1e949af07eb7908cf24bee1a56c63fd
Laurent Montel <montel@kde.org>
2026-08-31 22:53:47 +0200

    Fix filepath when url is too big. Using hash

--- src/core/autotests/rocketchatcachetest.cpp
+++ src/core/autotests/rocketchatcachetest.cpp
@@ -24,7 +24,13 @@ void RocketChatCacheTest::shouldGenerateFileCachePath()
     QFETCH(QString, result);
     const QString accountName(u"foo"_s);
     const QString accountServerHost(u"www.kde.org"_s);
-    QCOMPARE(RocketChatCacheUtils::fileCachePath(url, static_cast<ManagerDataPaths::PathType>(pathType), accountName, accountServerHost), result);
+    const QString cachePath = RocketChatCacheUtils::fileCachePath(url, static_cast<ManagerDataPaths::PathType>(pathType), accountName, accountServerHost);
+    QCOMPARE(cachePath, result);
+    // NAME_MAX is 255 bytes: a longer path element makes it impossible to create the file.
+    const QStringList pathElements = cachePath.split(u'/');
+    for (const QString &pathElement : pathElements) {
+        QVERIFY(pathElement.toUtf8().size() <= 255);
+    }
 }
 
 void RocketChatCacheTest::shouldGenerateFileCachePath_data()
@@ -41,6 +47,12 @@ void RocketChatCacheTest::shouldGenerateFileCachePath_data()
 
     QTest::addRow("test2") << QUrl(u"https://www.kde.com/apps/files_sharing/preview/ooj3432?file=/2020-11-04T10_05_58.500044.jpg&x=4518&y=2542&a=true"_s) << 1
                            << cacheLocationPath + u"/foo/MainCache/www.kde.com//apps/files_sharing/preview/ooj3432_2020-11-04T10_05_58.500044.jpg"_s;
+
+    // BUG: overlong path elements (here a cloudinary preview url flattened into a single
+    // file name) are replaced by a hash, otherwise the file can't be created at all.
+    QTest::addRow("too-long-preview-url") << QUrl(
+        u"https://res.cloudinary.com/zenn/image/upload/s--bNE3XGWd--/c_fit%2Cg_north_west%2Cl_text:notosansjp-medium.otf_55:Memory%2520Usage%2520Benchmark%2520of%252015%2520Desktop%2520UI%2520Frameworks%2Cw_1010%2Cx_90%2Cy_100/g_south_west%2Cl_text:notosansjp-medium.otf_37:mizugeek%2Cx_203%2Cy_121/g_south_west%2Ch_90%2Cl_fetch:aHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jSV9XbUNPZHcteDN5RjJGNDBiSTJEM2YyX1FLQjFpX1FHdkFTRFkzZ3FBZy1IbS0xOD1zOTYtYw==%2Cr_max%2Cw_90%2Cx_87%2Cy_95/v1627283836/default/og-base-w1200-v2.png?_a=BACMTiAE"_s)
+                                          << 4 << cacheLocationPath + u"/foo/PreviewUrl/a5f8a12cc6ba9cd21d5b0fa0df29c7cd.png"_s;
 }
 
 #include "moc_rocketchatcachetest.cpp"
--- src/core/rocketchatcacheutils.cpp
+++ src/core/rocketchatcacheutils.cpp
@@ -6,13 +6,37 @@
 
 #include "rocketchatcacheutils.h"
 
-// #include <QDebug>
+#include <QCryptographicHash>
 #include <QUrlQuery>
 
 using namespace Qt::Literals::StringLiterals;
+
+namespace
+{
+// NAME_MAX is 255 bytes on ext4/btrfs/APFS. Keep a large margin: download jobs may
+// append a suffix, and Windows caps the whole path at 260 characters.
+constexpr qsizetype maxFileNameSize = 120;
+
+// Some servers (e.g. cloudinary preview urls) generate very long path elements. As
+// PreviewUrl flattens the whole url into a single file name we can easily exceed
+// NAME_MAX, and creating the file then fails. Replace only the offending element by
+// a hash of itself: the directory layout is kept, and two urls which used to share
+// a cache entry still do.
+[[nodiscard]] QString shortenedFileName(const QString &fileName)
+{
+    QString result = QString::fromLatin1(QCryptographicHash::hash(fileName.toUtf8(), QCryptographicHash::Sha256).toHex().left(32));
+    // Keep a plausible extension: image loaders and mimetype detection rely on it.
+    const qsizetype dotIndex = fileName.lastIndexOf(u'.');
+    if (dotIndex > 0 && (fileName.size() - dotIndex) <= 8) {
+        result += QStringView(fileName).mid(dotIndex);
+    }
+    return result;
+}
+}
+
 QString RocketChatCacheUtils::fileCachePath(const QUrl &url, ManagerDataPaths::PathType type, const QString &accountName, const QString &accountServerHost)
 {
-    QString cachePath = ManagerDataPaths::self()->path(type, accountName);
+    const QString cachePath = ManagerDataPaths::self()->path(type, accountName);
     // qDebug() << " cachePath" << cachePath;
     QString relativePathInCache = url.path();
     // qDebug() << " relativePathInCache" << relativePathInCache;
@@ -23,18 +47,23 @@ QString RocketChatCacheUtils::fileCachePath(const QUrl &url, ManagerDataPaths::P
     if (type == ManagerDataPaths::PathType::PreviewUrl) {
         relativePathInCache.replace(u'/', u'_');
     }
-    cachePath += u'/' + relativePathInCache;
     if (url.hasQuery()) {
         const QUrlQuery query(url);
         if (query.hasQueryItem(u"etag"_s)) {
-            cachePath += query.queryItemValue(u"etag"_s);
+            relativePathInCache += query.queryItemValue(u"etag"_s);
         }
         if (query.hasQueryItem(u"file"_s)) {
             QString fileQuery = query.queryItemValue(u"file"_s);
             // qDebug() << " fileQuery "<< fileQuery << " url.query " << url.query();
             fileQuery.replace(u'/', u'_');
-            cachePath += fileQuery;
+            relativePathInCache += fileQuery;
         }
     }
-    return cachePath;
+    QStringList pathElements = relativePathInCache.split(u'/');
+    for (QString &pathElement : pathElements) {
+        if (pathElement.toUtf8().size() > maxFileNameSize) {
+            pathElement = shortenedFileName(pathElement);
+        }
+    }
+    return cachePath + u'/' + pathElements.join(u'/');
 }
