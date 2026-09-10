01686e4b8b434abb4314ab15a0d5e5539bcccfe9
Laurent Montel <montel@kde.org>
2026-09-10 20:16:18 +0200

    Make sure that cornerWidget is hidden

--- src/widgets/ruqolamainwindow.cpp
+++ src/widgets/ruqolamainwindow.cpp
@@ -1300,6 +1300,8 @@ void RuqolaMainWindow::slotFullScreen(bool t)
     } else {
         QWidget *w = mb->cornerWidget(Qt::TopRightCorner);
         if (w) {
+            mb->setCornerWidget(nullptr, Qt::TopRightCorner);
+            w->hide();
             w->deleteLater();
         }
     }
