5b443782adf2cac864d674a552ae6a1b13caca7e
Laurent Montel <montel@kde.org>
2026-09-17 22:18:08 +0200

    Remove unused include

--- src/widgets/autogenerateui/widgets/multistaticselectlineedit.h
+++ src/widgets/autogenerateui/widgets/multistaticselectlineedit.h
@@ -5,7 +5,6 @@
 */
 
 #pragma once
-#include "config-ruqola.h"
 #include "libruqolawidgets_private_export.h"
 #include "multistaticselectlineeditmodel.h"
 #include <TextAddonsWidgets/CompletionLineEdit>
